e23237285b14f053a86327cae971f7c89418b760
alish <alish931@abv.bg>
2019-07-12 13:11:04 +0300

    feat: used new table to select the correct Organization

--- apps/api/src/app/user-organization/user-organization.controller.ts
+++ apps/api/src/app/user-organization/user-organization.controller.ts
@@ -1,13 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { ApiUseTags } from '@nestjs/swagger';
+import { Controller, HttpStatus, Get, Query } from '@nestjs/common';
+import { ApiUseTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CrudController } from '../core/crud/crud.controller';
-import { UserOrganization } from '@gauzy/models';
+import { UserOrganization as IUserOrganization } from '@gauzy/models';
 import { UserOrganizationService } from './user-organization.services';
+import { IPagination } from '../core';
+import { UserOrganization } from './user-organization.entity';
 
 @ApiUseTags('UserOrganization')
 @Controller()
-export class UserOrganizationController extends CrudController<UserOrganization> {
+export class UserOrganizationController extends CrudController<IUserOrganization> {
     constructor(private readonly userOrganizationService: UserOrganizationService) {
         super(userOrganizationService);
+    }
+
+    @ApiOperation({ title: 'Find one from the search input' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Found user organization', type: UserOrganization })
+    @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Record not found' })
+    @Get()
+    async findOne(@Query('findInputStr') findInputStr: string): Promise<IUserOrganization> {
+        const findInput = JSON.parse(findInputStr);
+
+        return this.userOrganizationService.findOne({ where: findInput });
     }
 }
