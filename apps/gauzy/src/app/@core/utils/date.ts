57dae862a019f27e0ec500af5d0855f4510c1b2f
Alish <alish931@abv.bg>
2019-08-02 15:11:15 +0300

    feat: improvements on Employees page

---
+++ apps/gauzy/src/app/@core/utils/date.ts
@@ -0,0 +1,16 @@
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+export { monthNames };
