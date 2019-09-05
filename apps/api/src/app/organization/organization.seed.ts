bf6b83724ca281f31f67795c1464ddfe72356721
elvis <dreemsuncho@gmail.com>
2019-09-05 14:42:46 +0300

    Set real & fake images on seed companies #109

--- apps/api/src/app/organization/organization.seed.ts
+++ apps/api/src/app/organization/organization.seed.ts
@@ -1,49 +1,81 @@
-import { Connection } from "typeorm";
-import { environment as env } from '@env-api/environment'
+import { Connection } from 'typeorm';
+import { environment as env } from '@env-api/environment';
 import { Organization } from './organization.entity';
 import * as faker from 'faker';
 import { getDummyImage } from '../core';
 import { CurrenciesEnum, DefaultValueDateTypeEnum } from '@gauzy/models';
 
 export const createOrganizations = async (
-    connection: Connection
-): Promise<{ defaultOrganization: Organization, randomOrganizations: Organization[] }> => {
-    const defaultOrganization = new Organization()
-    const { name, currency, defaultValueDateType } = env.defaultOrganization;
-    const currencies = Object.values(CurrenciesEnum);
-    const defaultDateTypes = Object.values(DefaultValueDateTypeEnum);
+  connection: Connection
+): Promise<{
+  defaultOrganization: Organization;
+  randomOrganizations: Organization[];
+}> => {
+  const defaultOrganization = new Organization();
+  const {
+    name,
+    currency,
+    defaultValueDateType,
+    imageUrl
+  } = env.defaultOrganization;
+  const currencies = Object.values(CurrenciesEnum);
+  const defaultDateTypes = Object.values(DefaultValueDateTypeEnum);
 
+  defaultOrganization.name = name;
+  defaultOrganization.currency = currency;
+  defaultOrganization.defaultValueDateType = defaultValueDateType;
+  defaultOrganization.imageUrl = imageUrl;
 
-    defaultOrganization.name = name;
-    defaultOrganization.currency = currency;
-    defaultOrganization.defaultValueDateType = defaultValueDateType;
-    defaultOrganization.imageUrl = getDummyImage(330, 300, name.charAt(0).toUpperCase());
-    
-    await insertOrganization(connection, defaultOrganization);
+  await insertOrganization(connection, defaultOrganization);
 
-    const randomOrganizations: Organization[] = [];
+  const randomOrganizations: Organization[] = [];
 
-    for (let index = 1; index <= 5; index++) {
-        const organization = new Organization();
-        const name = faker.company.companyName();
+  for (let index = 1; index <= 5; index++) {
+    const organization = new Organization();
+    const companyName = faker.company.companyName();
 
-        organization.name = name;
-        organization.currency = currencies[(index % currencies.length + 1) - 1];
-        organization.defaultValueDateType = defaultDateTypes[(index % defaultDateTypes.length + 1) - 1];
-        organization.imageUrl = getDummyImage(330, 300, name.charAt(0).toUpperCase());
+    const logoAbbreviation = _extractLogoAbbreviation(companyName);
 
-        await insertOrganization(connection, organization);
-        randomOrganizations.push(organization);
-    }
+    organization.name = companyName;
+    organization.currency = currencies[(index % currencies.length) + 1 - 1];
+    organization.defaultValueDateType = defaultDateTypes[(index % defaultDateTypes.length) + 1 - 1];
+    organization.imageUrl = getDummyImage(330, 300, logoAbbreviation);
 
-    return { defaultOrganization, randomOrganizations };
+    await insertOrganization(connection, organization);
+    randomOrganizations.push(organization);
+  }
+
+  return { defaultOrganization, randomOrganizations };
 };
 
-const insertOrganization = async (connection: Connection, organization: Organization): Promise<void> => {
-    await connection
-        .createQueryBuilder()
-        .insert()
-        .into(Organization)
-        .values(organization)
-        .execute();
-}
+const insertOrganization = async (
+  connection: Connection,
+  organization: Organization
+): Promise<void> => {
+  await connection
+    .createQueryBuilder()
+    .insert()
+    .into(Organization)
+    .values(organization)
+    .execute();
+};
+
+const _extractLogoAbbreviation = (companyName: string) => {
+  const logoFirstWordFirstLetterIndex = 0;
+  const companyNameLastEmptyLetterIndex = companyName.lastIndexOf(' ');
+  const logoFirstLetter = companyName[logoFirstWordFirstLetterIndex];
+
+  let logoAbbreviation = logoFirstLetter;
+
+  if (
+    companyNameLastEmptyLetterIndex !== -1 &&
+    companyNameLastEmptyLetterIndex !== logoFirstWordFirstLetterIndex
+  ) {
+    const logoLastWordFirstLetterIndex = companyNameLastEmptyLetterIndex + 1;
+    const logoSecondLetter = companyName[logoLastWordFirstLetterIndex];
+
+    logoAbbreviation += logoSecondLetter;
+  }
+
+  return logoAbbreviation;
+};
