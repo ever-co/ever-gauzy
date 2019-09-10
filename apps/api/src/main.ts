35e29050947a5a1d2a920d20402f5c141faa9fe3
Alish <alish931@abv.bg>
2019-09-10 15:10:11 +0300

    feat: enable swagger UI

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,9 +6,20 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Gauzy controllers')
+    .setVersion('1.0')
+    .setBasePath('api/')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swg', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3000;
