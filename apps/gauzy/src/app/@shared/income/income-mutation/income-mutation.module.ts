6fc2cc12561c83e7ce2282960b729b210d070af2
AlexTasev <al.taseff@gmail.com>
2019-09-13 11:11:38 +0300

    feat: user form translated

--- apps/gauzy/src/app/@shared/income/income-mutation/income-mutation.module.ts
+++ apps/gauzy/src/app/@shared/income/income-mutation/income-mutation.module.ts
@@ -1,47 +1,51 @@
 import { ThemeModule } from '../../../@theme/theme.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { NbCardModule, NbButtonModule, NbIconModule, NbDatepickerModule, NbInputModule, NbSelectModule } from '@nebular/theme';
+import {
+	NbCardModule,
+	NbButtonModule,
+	NbIconModule,
+	NbDatepickerModule,
+	NbInputModule,
+	NbSelectModule
+} from '@nebular/theme';
 import { IncomeService } from '../../../@core/services/income.service';
 import { IncomeMutationComponent } from './income-mutation.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { OrganizationsService } from '../../../@core/services/organizations.service';
+import { EmployeeSelectorsModule } from '../../../@theme/components/header/selectors/employee/employee.module';
 
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
-import { EmployeeSelectorsModule } from '../../../@theme/components/header/selectors/employee/employee.module';
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-    imports: [
-        ThemeModule,
-        FormsModule,
-        NbCardModule,
-        ReactiveFormsModule,
-        NbButtonModule,
-        NbIconModule,
-        NgSelectModule,
-        NbDatepickerModule,
-        NbInputModule,
-        NbSelectModule,
-        EmployeeSelectorsModule,
-        TranslateModule.forChild({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        }),
-    ],
-    declarations: [IncomeMutationComponent],
-    entryComponents: [IncomeMutationComponent],
-    providers: [
-        IncomeService,
-        OrganizationsService
-    ]
+	imports: [
+		ThemeModule,
+		FormsModule,
+		NbCardModule,
+		ReactiveFormsModule,
+		NbButtonModule,
+		NbIconModule,
+		NgSelectModule,
+		NbDatepickerModule,
+		NbInputModule,
+		NbSelectModule,
+		EmployeeSelectorsModule,
+		TranslateModule.forChild({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		})
+	],
+	declarations: [IncomeMutationComponent],
+	entryComponents: [IncomeMutationComponent],
+	providers: [IncomeService, OrganizationsService]
 })
-export class IncomeMutationModule { }
+export class IncomeMutationModule {}
