f485c2e94f7f45e3aec62e3f595d0bda9d7b0f4f
AlexTasev <al.taseff@gmail.com>
2019-09-17 16:59:50 +0300

    feat: translations added to employee pop ups

--- apps/gauzy/src/app/@shared/employee/employee-end-work-popup/employee-end-work.module.ts
+++ apps/gauzy/src/app/@shared/employee/employee-end-work-popup/employee-end-work.module.ts
@@ -1,25 +1,44 @@
 import { ThemeModule } from '../../../@theme/theme.module';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { NbCardModule, NbButtonModule, NbIconModule, NbDatepickerModule, NbInputModule } from '@nebular/theme';
+import {
+	NbCardModule,
+	NbButtonModule,
+	NbIconModule,
+	NbDatepickerModule,
+	NbInputModule
+} from '@nebular/theme';
 import { UserFormsModule } from '../../user/forms/user-forms.module';
 import { EmployeeEndWorkComponent } from './employee-end-work.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient) {
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-    imports: [
-        ThemeModule,
-        FormsModule,
-        NbCardModule,
-        UserFormsModule,
-        NbButtonModule,
-        NbIconModule,
-        NbDatepickerModule,
-        NbInputModule
-    ],
-    exports: [EmployeeEndWorkComponent],
-    declarations: [EmployeeEndWorkComponent],
-    entryComponents: [EmployeeEndWorkComponent],
-    providers: [
-    ]
+	imports: [
+		ThemeModule,
+		FormsModule,
+		NbCardModule,
+		UserFormsModule,
+		NbButtonModule,
+		NbIconModule,
+		NbDatepickerModule,
+		NbInputModule,
+		TranslateModule.forChild({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		})
+	],
+	exports: [EmployeeEndWorkComponent],
+	declarations: [EmployeeEndWorkComponent],
+	entryComponents: [EmployeeEndWorkComponent],
+	providers: []
 })
-export class EmployeeEndWorkModule { }
+export class EmployeeEndWorkModule {}
