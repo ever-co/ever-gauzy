45d9c110fafd91a5a817809ccc83c652d333bacd
Alish <alish931@abv.bg>
2019-09-30 12:31:16 +0300

    feat: added isRecurring column

--- libs/models/src/lib/employee-recurring-expense.ts
+++ libs/models/src/lib/employee-recurring-expense.ts
@@ -1,17 +1,19 @@
 import { BaseEntityModel as IBaseEntityModel } from './base-entity.model';
 
 export interface EmployeeRecurringExpense extends IBaseEntityModel {
-    employeeId: string;
-    month: number;
-    year: number;
-    categoryName: string;
-    value: number;
-    currency: string;
+	employeeId: string;
+	month: number;
+	year: number;
+	categoryName: string;
+	value: number;
+	currency: string;
+	isRecurring: boolean;
 }
 
 export interface EmployeeRecurringExpenseFindInput extends IBaseEntityModel {
-    employeeId?: string;
-    month?: number;
-    year?: number;
-    currency?: string;
+	employeeId?: string;
+	month?: number;
+	year?: number;
+	currency?: string;
+	isRecurring?: boolean;
 }
