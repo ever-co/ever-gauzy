f4d6f7cabd55df6d7d1da7fb5111a47e8425a223
Emil Momchilov <emil@pernik.org>
2019-10-03 14:34:18 +0300

    fix: fixed bug from issue/145

--- apps/gauzy/src/app/pages/dashboard/employee-chart/employee-chart.component.ts
+++ apps/gauzy/src/app/pages/dashboard/employee-chart/employee-chart.component.ts
@@ -7,164 +7,199 @@ import { takeUntil } from 'rxjs/operators';
 import { monthNames } from '../../../@core/utils/date';
 
 @Component({
-    selector: 'ngx-employee-chart',
-    template: `
-        <chart style="height: 620px" type="horizontalBar" [data]="data" [options]="options"></chart>
-    `,
-    styleUrls: ['./employee-chart.component.scss']
+	selector: 'ngx-employee-chart',
+	template: `
+		<chart
+			style="height: 620px"
+			type="horizontalBar"
+			[data]="data"
+			[options]="options"
+		></chart>
+	`,
+	styleUrls: ['./employee-chart.component.scss']
 })
 export class EmployeeChartComponent implements OnInit, OnDestroy {
-    private _ngDestroy$ = new Subject<void>();
-    data: any;
-    options: any;
-    incomeStatistics: number[];
-    expenseStatistics: number[];
-    profitStatistics: number[];
-    bonusStatistics: number[];
+	private _ngDestroy$ = new Subject<void>();
+	data: any;
+	options: any;
+	incomeStatistics: number[];
+	expenseStatistics: number[];
+	profitStatistics: number[];
+	bonusStatistics: number[];
 
-    constructor(private themeService: NbThemeService,
-        private employeeStatisticsService: EmployeeStatisticsService,
-        private store: Store,
-        private toastrService: NbToastrService) { }
+	constructor(
+		private themeService: NbThemeService,
+		private employeeStatisticsService: EmployeeStatisticsService,
+		private store: Store,
+		private toastrService: NbToastrService
+	) {}
 
-    async ngOnInit() {
-        this.store.selectedEmployee$
-            .pipe(takeUntil(this._ngDestroy$))
-            .subscribe(async emp => {
-                if (emp.id) {
-                    try {
-                        const statistics = await this.employeeStatisticsService
-                            .getStatisticsByEmployeeId(emp.id);
+	async ngOnInit() {
+		this.store.selectedEmployee$
+			.pipe(takeUntil(this._ngDestroy$))
+			.subscribe(async (emp) => {
+				if (emp.id) {
+					try {
+						const statistics = await this.employeeStatisticsService.getStatisticsByEmployeeId(
+							emp.id
+						);
 
-                        this.incomeStatistics = statistics.incomeStatistics;
-                        this.expenseStatistics = statistics.expenseStatistics;
-                        this.profitStatistics = statistics.profitStatistics;
-                        this.bonusStatistics = statistics.bonusStatistics;
-                        
-                        const avarageBonus = this.bonusStatistics.filter(Number).reduce((a, b) => a + b, 0) / this.bonusStatistics.filter(Number).length;
-                        this.employeeStatisticsService.avarageBonus$.next(Math.floor(avarageBonus));
+						this.incomeStatistics = statistics.incomeStatistics;
+						this.expenseStatistics = statistics.expenseStatistics;
+						this.profitStatistics = statistics.profitStatistics;
+						this.bonusStatistics = statistics.bonusStatistics;
 
-                        this.themeService.getJsTheme()
-                            .pipe(takeUntil(this._ngDestroy$))
-                            .subscribe(config => {
-                               // const colors: any = config.variables;
-                                const chartjs: any = config.variables.chartjs;
-                                const bonusColors = this.bonusStatistics.map( val => val < 0 ? 'red' : '#0091ff');
-                                const profitColors = this.profitStatistics.map(val => val < 0 ? '#ff7b00' : '#66de0b')
-                                this.data = {
-                                    labels: this._getMonthsWithStatistics(),
-                                    datasets: [
-                                        {
-                                            label: 'Revenue',
-                                            backgroundColor: '#089c17',
-                                            borderWidth: 1,
-                                            data: this.incomeStatistics
-                                        },
-                                        {
-                                            label: 'Expenses',
-                                            backgroundColor: '#dbc300',
-                                            data: this.expenseStatistics
-                                        },
-                                        {
-                                            label: 'Profit',
-                                            backgroundColor: profitColors,
-                                            data: this.profitStatistics,
-                                        },
-                                        {
-                                            label: 'Bonus',
-                                            backgroundColor: bonusColors,
-                                            data: this.bonusStatistics,
-                                        },
-                                    ],
-                                };
+						const avarageBonus =
+							this.bonusStatistics
+								.filter(Number)
+								.reduce((a, b) => a + b, 0) /
+							this.bonusStatistics.filter(Number).length;
+						this.employeeStatisticsService.avarageBonus$.next(
+							Math.floor(avarageBonus)
+						);
 
-                                this.options = {
-                                    responsive: true,
-                                    maintainAspectRatio: false,
-                                    elements: {
-                                        rectangle: {
-                                            borderWidth: 2,
-                                        },
-                                    },
-                                    scales: {
-                                        xAxes: [
-                                            {
-                                                gridLines: {
-                                                    display: true,
-                                                    color: chartjs.axisLineColor,
-                                                },
-                                                ticks: {
-                                                    fontColor: chartjs.textColor,
-                                                },
-                                            },
-                                        ],
-                                        yAxes: [
-                                            {
-                                                gridLines: {
-                                                    display: false,
-                                                    color: chartjs.axisLineColor,
-                                                },
-                                                ticks: {
-                                                    fontColor: chartjs.textColor,
-                                                },
-                                            },
-                                        ],
-                                    },
-                                    legend: {
-                                        position: 'right',
-                                        labels: {
-                                            fontColor: chartjs.textColor,
-                                        },
-                                    },
-                                };
-                            });
-                    } catch (error) {
-                        this.toastrService.danger(error.error.message || error.message, 'Error');
-                    }
-                }
-            });
-    }
+						this.themeService
+							.getJsTheme()
+							.pipe(takeUntil(this._ngDestroy$))
+							.subscribe((config) => {
+								// const colors: any = config.variables;
+								const chartjs: any = config.variables.chartjs;
+								const bonusColors = this.bonusStatistics.map(
+									(val) => (val < 0 ? 'red' : '#0091ff')
+								);
+								const profitColors = this.profitStatistics.map(
+									(val) => (val < 0 ? '#ff7b00' : '#66de0b')
+								);
 
-    private _filterArrayZeroesToMatchWithMonths() {
-        this.expenseStatistics = this.expenseStatistics.filter(Number);
-        this.incomeStatistics = this.incomeStatistics.filter(Number);
-        this.profitStatistics = this.profitStatistics.filter(Number);
-        this.bonusStatistics = this.bonusStatistics.filter(Number);
-    }
+								this.incomeStatistics =
+									statistics.incomeStatistics;
+								this.expenseStatistics =
+									statistics.expenseStatistics;
+								this.profitStatistics =
+									statistics.profitStatistics;
+								this.bonusStatistics =
+									statistics.bonusStatistics;
 
-    private _getMonthsWithStatistics() {
-        const monthsWithStatistics = [];
+								this.data = {
+									labels: this._getMonthsWithStatistics(),
+									datasets: [
+										{
+											label: 'Revenue',
+											backgroundColor: '#089c17',
+											borderWidth: 1,
+											data: this.incomeStatistics
+										},
+										{
+											label: 'Expenses',
+											backgroundColor: '#dbc300',
+											data: this.expenseStatistics
+										},
+										{
+											label: 'Profit',
+											backgroundColor: profitColors,
+											data: this.profitStatistics
+										},
+										{
+											label: 'Bonus',
+											backgroundColor: bonusColors,
+											data: this.bonusStatistics
+										}
+									]
+								};
 
-        this._getLast12months().forEach((month, index) => {
-            if (this.expenseStatistics[index] !== 0 || this.incomeStatistics[index] !== 0) {
-                monthsWithStatistics.push(month);
-            }
-        });
+								this.options = {
+									responsive: true,
+									maintainAspectRatio: false,
+									elements: {
+										rectangle: {
+											borderWidth: 2
+										}
+									},
+									scales: {
+										xAxes: [
+											{
+												gridLines: {
+													display: true,
+													color: chartjs.axisLineColor
+												},
+												ticks: {
+													fontColor: chartjs.textColor
+												}
+											}
+										],
+										yAxes: [
+											{
+												gridLines: {
+													display: false,
+													color: chartjs.axisLineColor
+												},
+												ticks: {
+													fontColor: chartjs.textColor
+												}
+											}
+										]
+									},
+									legend: {
+										position: 'right',
+										labels: {
+											fontColor: chartjs.textColor
+										}
+									}
+								};
+							});
+					} catch (error) {
+						this.toastrService.danger(
+							error.error.message || error.message,
+							'Error'
+						);
+					}
+				}
+			});
+	}
 
-        this._filterArrayZeroesToMatchWithMonths();
-        return monthsWithStatistics;
-    }
+	private _filterArrayZeroesToMatchWithMonths() {
+		this.expenseStatistics = this.expenseStatistics.filter(Number);
+		this.incomeStatistics = this.incomeStatistics.filter(Number);
+		this.profitStatistics = this.profitStatistics.filter(Number);
+		this.bonusStatistics = this.bonusStatistics.filter(Number);
+	}
 
-    private _getLast12months() {
-        const start = (new Date(Date.now())).getMonth() + 1;
-        const end = start + 11;
-        const currentYear = (new Date(Date.now())).getFullYear() - 2000;
+	private _getMonthsWithStatistics() {
+		const monthsWithStatistics = [];
 
-        const monthsNeeded = [];
+		this._getLast12months().forEach((month, index) => {
+			if (
+				this.expenseStatistics[index] !== 0 ||
+				this.incomeStatistics[index] !== 0
+			) {
+				monthsWithStatistics.push(month);
+			}
+		});
 
-        for (let i = start; i <= end; i++) {
-            if (i > 11) {
-                monthsNeeded.push(monthNames[i - 12] + ` '${currentYear}`);
-            } else {
-                monthsNeeded.push(monthNames[i] + ` '${currentYear - 1}`);
-            }
-        }
+		this._filterArrayZeroesToMatchWithMonths();
+		return monthsWithStatistics;
+	}
 
-        return monthsNeeded.reverse();
-    }
+	private _getLast12months() {
+		const start = new Date(Date.now()).getMonth() + 1;
+		const end = start + 11;
+		const currentYear = new Date(Date.now()).getFullYear() - 2000;
 
-    ngOnDestroy() {
-        this._ngDestroy$.next();
-        this._ngDestroy$.complete();
-    }
+		const monthsNeeded = [];
+
+		for (let i = start; i <= end; i++) {
+			if (i > 11) {
+				monthsNeeded.push(monthNames[i - 12] + ` '${currentYear}`);
+			} else {
+				monthsNeeded.push(monthNames[i] + ` '${currentYear - 1}`);
+			}
+		}
+
+		return monthsNeeded.reverse();
+	}
+
+	ngOnDestroy() {
+		this._ngDestroy$.next();
+		this._ngDestroy$.complete();
+	}
 }
