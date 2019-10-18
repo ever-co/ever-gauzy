1330c91a39deb9e2badd55cfc2b010828286e969
blagovest <blaje@abv.bg>
2019-10-18 10:54:38 +0300

    feat: change sidebar direction on select Hebrew

--- apps/gauzy/src/app/@theme/layouts/one-column/one-column.layout.ts
+++ apps/gauzy/src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -1,56 +1,73 @@
-import { AfterViewInit, Component, Inject, PLATFORM_ID, ViewChild, OnInit } from '@angular/core';
+import {
+	AfterViewInit,
+	Component,
+	Inject,
+	PLATFORM_ID,
+	ViewChild,
+	OnInit
+} from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { NbLayoutComponent } from '@nebular/theme';
+import {
+	NbLayoutComponent,
+	NbLayoutDirectionService,
+	NbLayoutDirection
+} from '@nebular/theme';
 import { UsersService } from '../../../@core/services/users.service';
-
 
 import { WindowModeBlockScrollService } from '../../services/window-mode-block-scroll.service';
 import { Store } from '../../../@core/services/store.service';
 import { UsersOrganizationsService } from '../../../@core/services/users-organizations.service';
 
 @Component({
-    selector: 'ngx-one-column-layout',
-    styleUrls: ['./one-column.layout.scss'],
-    templateUrl: './one-column.layout.html',
+	selector: 'ngx-one-column-layout',
+	styleUrls: ['./one-column.layout.scss'],
+	templateUrl: './one-column.layout.html'
 })
 export class OneColumnLayoutComponent implements OnInit, AfterViewInit {
+	@ViewChild(NbLayoutComponent, { static: false }) layout: NbLayoutComponent;
 
-    @ViewChild(NbLayoutComponent, { static: false }) layout: NbLayoutComponent;
+	user: any;
 
-    user: any;
+	userMenu = [
+		{ title: 'Profile', link: '/pages/auth/profile' },
+		{ title: 'Log out', link: '/auth/logout' }
+	];
 
-    userMenu = [
-        { title: 'Profile', link: '/pages/auth/profile' },
-        { title: 'Log out', link: '/auth/logout' }
-    ];
+	constructor(
+		@Inject(PLATFORM_ID) private platformId,
+		private windowModeBlockScrollService: WindowModeBlockScrollService,
+		private usersService: UsersService,
+		private usersOrganizationsService: UsersOrganizationsService,
+		private store: Store,
+		private directionService: NbLayoutDirectionService
+	) {}
 
-    constructor(
-        @Inject(PLATFORM_ID) private platformId,
-        private windowModeBlockScrollService: WindowModeBlockScrollService,
-        private usersService: UsersService,
-        private usersOrganizationsService: UsersOrganizationsService,
-        private store: Store
-    ) { }
+	ngOnInit() {
+		this.loadUserData();
 
-    ngOnInit() {
-        this.loadUserData();
-    }
+		if (this.layout_direction === NbLayoutDirection.RTL) {
+			this.sidebar_class = 'menu-sidebar-rtl';
+		}
+	}
 
-    ngAfterViewInit() {
-        if (isPlatformBrowser(this.platformId)) {
-            this.windowModeBlockScrollService.register(this.layout);
-        }
-    }
+	ngAfterViewInit() {
+		if (isPlatformBrowser(this.platformId)) {
+			this.windowModeBlockScrollService.register(this.layout);
+		}
+	}
 
-    private async loadUserData() {
-        const id = this.store.userId;
-        this.user = await this.usersService.getUserById(id);
+	layout_direction: NbLayoutDirection = this.directionService.getDirection();
+	sidebar_class: string = 'menu-sidebar';
 
-        // We did not doing nothing with the result?
-        // Also if user still did not have organization?
-        // const { orgId } = await this.usersOrganizationsService.findOne({ userId: id }).pipe(first()).toPromise();
+	private async loadUserData() {
+		const id = this.store.userId;
+		this.user = await this.usersService.getUserById(id);
 
-        // TODO: Fix me!
-        // this.store.selectedOrganizationId = orgId;
-    }
+		// We did not doing nothing with the result?
+		// Also if user still did not have organization?
+		// const { orgId } = await this.usersOrganizationsService.findOne({ userId: id }).pipe(first()).toPromise();
+
+		// TODO: Fix me!
+		// this.store.selectedOrganizationId = orgId;
+	}
 }
