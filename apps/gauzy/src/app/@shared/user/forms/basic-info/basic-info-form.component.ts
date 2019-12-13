7931c173329974a0db8467600623c8538a7cc93a
blagovest <blaje@abv.bg>
2019-12-13 15:08:54 +0200

    feat: added image view of the uploaded image and remove option

--- apps/gauzy/src/app/@shared/user/forms/basic-info/basic-info-form.component.ts
+++ apps/gauzy/src/app/@shared/user/forms/basic-info/basic-info-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
 import { User, RolesEnum } from '@gauzy/models';
 import { AuthService } from 'apps/gauzy/src/app/@core/services/auth.service';
@@ -7,10 +7,14 @@ import { RoleService } from 'apps/gauzy/src/app/@core/services/role.service';
 
 @Component({
 	selector: 'ga-user-basic-info-form',
-	templateUrl: 'basic-info-form.component.html'
+	templateUrl: 'basic-info-form.component.html',
+	styleUrls: ['basic-info-form.component.scss']
 })
 export class BasicInfoFormComponent {
 	uploaderPlaceholder: string = 'Image';
+
+	@ViewChild('imagePreview', { static: false })
+	imagePreviewElement: ElementRef;
 
 	readonly form = this.fb.group({
 		username: [''],
@@ -53,6 +57,10 @@ export class BasicInfoFormComponent {
 		private readonly roleService: RoleService
 	) {}
 
+	get showImageMeta() {
+		return this.imageUrl && this.imageUrl.value !== '';
+	}
+
 	async registerUser(roleName: RolesEnum): Promise<User> {
 		if (this.form.valid) {
 			const role = await this.roleService
@@ -77,5 +85,25 @@ export class BasicInfoFormComponent {
 		}
 
 		return;
+	}
+
+	deleteImg() {
+		this.imageUrl.setValue('');
+	}
+
+	ngAfterViewInit() {
+		this._setupLogoUrlValidation();
+	}
+
+	private _setupLogoUrlValidation() {
+		this.imagePreviewElement.nativeElement.onload = () => {
+			this.imageUrl.setErrors(null);
+		};
+
+		this.imagePreviewElement.nativeElement.onerror = () => {
+			if (this.showImageMeta) {
+				this.imageUrl.setErrors({ invalidUrl: true });
+			}
+		};
 	}
 }
