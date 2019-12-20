9ffb8a82b567a1012f12407d1563d400637cb2f5
AlexTasev <al.taseff@gmail.com>
2019-12-20 16:50:47 +0200

    feat: income data seeded for November 2019

--- apps/api/src/app/income/income.seed.ts
+++ apps/api/src/app/income/income.seed.ts
@@ -3,61 +3,41 @@ import { environment as env } from '@env-api/environment';
 import { Income } from './income.entity';
 import * as faker from 'faker';
 import { CurrenciesEnum, Organization, Employee } from '@gauzy/models';
+import { incomeData } from './income-seed-data/november-19';
 
 export const createIncomes = async (
 	connection: Connection,
 	defaultData: {
 		org: Organization;
 		employees: Employee[];
-	},
-	randomData: {
-		orgs: Organization[];
-		employees: Employee[];
 	}
-): Promise<{ defaultIncome: Income; randomIncomes: Income[] }> => {
-	const defaultIncome = new Income();
+): Promise<{ seededIncomes: Income[] }> => {
 	const currencies = Object.values(CurrenciesEnum);
 
-	defaultIncome.employee = randomData.employees[0];
-	defaultIncome.organization = defaultData.org;
-	defaultIncome.amount = 20;
-	defaultIncome.clientId = '15';
-	defaultIncome.clientName = 'Gauzy';
-	defaultIncome.currency = currencies[0];
-	defaultIncome.valueDate = new Date();
-	defaultIncome.notes = 'notes';
+	const seededIncomes: Income[] = [];
 
-	await insertIncome(connection, defaultIncome);
-
-	const randomIncomes: Income[] = [];
-	const clientsArray = ['Nabo', 'Urvex', 'CUEAudio', 'Ever', 'Gauzy'];
-	const notesArray = [
-		'Great job!',
-		'Well done!',
-		'Nice!',
-		'Done',
-		'Great job!'
-	];
-
-	for (let index = 0; index < 5; index++) {
+	incomeData.map(async (seedIncome) => {
 		const income = new Income();
+		const foundEmployee = defaultData.employees.find(
+			(emp) => emp.user.email === seedIncome.email
+		);
 
-		income.employee = randomData.employees[index];
-		income.clientName = clientsArray[index];
-		income.organization = randomData.orgs[index];
-		income.amount = faker.random.number({ min: 10, max: 9999 });
+		income.employee = foundEmployee;
+		income.clientName = seedIncome.clientName;
+		income.organization = defaultData.org;
+		income.amount = seedIncome.amount;
 		income.clientId = faker.random
 			.number({ min: 10, max: 9999 })
 			.toString();
-		income.currency = currencies[(index % currencies.length) + 1 - 1];
-		income.valueDate = faker.date.recent(15);
-		income.notes = notesArray[index];
+		income.currency = seedIncome.currency;
+		income.valueDate = new Date(seedIncome.valueDate);
+		income.notes = seedIncome.notes;
 
 		await insertIncome(connection, income);
-		randomIncomes.push(income);
-	}
+		seededIncomes.push(income);
+	});
 
-	return { defaultIncome, randomIncomes };
+	return { seededIncomes };
 };
 
 const insertIncome = async (
