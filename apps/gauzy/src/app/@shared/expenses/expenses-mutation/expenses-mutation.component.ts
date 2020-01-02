65bc08b3a08e8b2c8f01c9c21571cb7061af9b70
AlexTasev <al.taseff@gmail.com>
2020-01-02 17:41:44 +0200

    feat: expenses popup improvements

--- apps/gauzy/src/app/@shared/expenses/expenses-mutation/expenses-mutation.component.ts
+++ apps/gauzy/src/app/@shared/expenses/expenses-mutation/expenses-mutation.component.ts
@@ -7,6 +7,7 @@ import { OrganizationsService } from '../../../@core/services/organizations.serv
 import { Store } from '../../../@core/services/store.service';
 import { first } from 'rxjs/operators';
 import { EmployeeSelectorComponent } from '../../../@theme/components/header/selectors/employee/employee.component';
+import { OrganizationVendorsService } from '../../../@core/services/organization-vendors.service';
 
 @Component({
 	selector: 'ga-expenses-mutation',
@@ -18,16 +19,26 @@ export class ExpensesMutationComponent implements OnInit {
 	employeeSelector: EmployeeSelectorComponent;
 	form: FormGroup;
 	expense: ExpenseViewModel;
-	fakeVendors: { vendorName: string; vendorId: string }[] = [];
 	fakeCategories: { categoryName: string; categoryId: string }[] = [];
 	currencies = Object.values(CurrenciesEnum);
+	vendor: { vendorName: string; vendorId: string };
+	vendors: { vendorName: string; vendorId: string }[] = [];
+	showNotes = false;
 
 	constructor(
 		public dialogRef: NbDialogRef<ExpensesMutationComponent>,
 		private fb: FormBuilder,
 		private organizationsService: OrganizationsService,
+		private organizationVendorsService: OrganizationVendorsService,
 		private store: Store
 	) {}
+
+	ngOnInit() {
+		this.getData();
+		this._initializeForm();
+
+		// TODO: here we'll get all the categories and vendors for ng-select menus
+	}
 
 	get currency() {
 		return this.form.get('currency');
@@ -35,23 +46,39 @@ export class ExpensesMutationComponent implements OnInit {
 
 	private getFakeId = () => (Math.floor(Math.random() * 101) + 1).toString();
 
-	private getFakeData() {
-		const fakeVendorNames = [
-			'Microsoft',
-			'Google',
-			'CaffeeMania',
-			'CoShare',
-			'Cleaning Company',
-			'Udemy',
-			'MultiSport'
-		];
-
-		fakeVendorNames.forEach((name) => {
-			this.fakeVendors.push({
-				vendorName: name,
-				vendorId: this.getFakeId()
-			});
+	private async getData() {
+		const res = await this.organizationVendorsService.getAll({
+			organizationId: this.store.selectedOrganization.id
 		});
+
+		if (res) {
+			const resultArr = res.items;
+			resultArr.forEach((vendor) => {
+				this.vendors.push({
+					vendorName: vendor.name,
+					vendorId: vendor.id
+				});
+			});
+		}
+
+		if (!this.vendors.length) {
+			const fakeVendorNames = [
+				'Microsoft',
+				'Google',
+				'CaffeeMania',
+				'CoShare',
+				'Cleaning Company',
+				'Udemy',
+				'MultiSport'
+			];
+
+			fakeVendorNames.forEach((name) => {
+				this.vendors.push({
+					vendorName: name,
+					vendorId: this.getFakeId()
+				});
+			});
+		}
 
 		const fakeCategoryNames = [
 			'Rent',
@@ -81,13 +108,6 @@ export class ExpensesMutationComponent implements OnInit {
 		});
 	}
 
-	ngOnInit() {
-		this.getFakeData();
-		this._initializeForm();
-
-		// TODO: here we'll get all the categories and vendors for ng-select menus
-	}
-
 	addOrEditExpense() {
 		this.dialogRef.close(
 			Object.assign(
@@ -95,6 +115,10 @@ export class ExpensesMutationComponent implements OnInit {
 				this.form.value
 			)
 		);
+	}
+
+	showNotesInput() {
+		return (this.showNotes = !this.showNotes);
 	}
 
 	private _initializeForm() {
