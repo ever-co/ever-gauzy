877ad21e73b43905969453cc4658d63c9b8b39ec
Rachit Magon <rachit.magon@gmail.com>
2020-01-17 19:51:04 +0530

    feat: #231 bonus income indicator

---
+++ apps/gauzy/src/app/@shared/table-components/income-amount/income-amount.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+	selector: 'ga-income-amount',
+	template: `
+		<span
+			>{{ value }}
+			<nb-icon
+				*ngIf="rowData?.isBonus"
+				nbTooltip="{{ 'INCOME_PAGE.BONUS_TOOLTIP' | translate }}"
+				icon="gift-outline"
+			>
+			</nb-icon>
+		</span>
+	`,
+	styles: []
+})
+export class IncomeAmountComponent {
+	@Input() value: Date;
+
+	@Input()
+	rowData: any;
+}
--- apps/gauzy/src/app/@shared/table-components/table-components.module.ts
+++ apps/gauzy/src/app/@shared/table-components/table-components.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
-import { DateViewComponent } from './date-view/date-view.component';
 import { CommonModule } from '@angular/common';
-
+import { HttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { NbIconModule, NbTooltipModule } from '@nebular/theme';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from '../../@theme/components/header/selectors/selectors.module';
+import { DateViewComponent } from './date-view/date-view.component';
+import { IncomeAmountComponent } from './income-amount/income-amount.component';
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    entryComponents: [
-        DateViewComponent
-    ],
-    declarations: [DateViewComponent],
-    providers: []
+	imports: [
+		CommonModule,
+		NbIconModule,
+		NbTooltipModule,
+		TranslateModule.forChild({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		})
+	],
+	entryComponents: [DateViewComponent, IncomeAmountComponent],
+	declarations: [DateViewComponent, IncomeAmountComponent],
+	providers: []
 })
-export class TableComponentsModule { }
+export class TableComponentsModule {}
