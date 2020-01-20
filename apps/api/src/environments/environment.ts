af43499cb3c4118057702f87e12ef78b5b0065bc
Rachit Magon <rachit.magon@gmail.com>
2020-01-20 19:16:04 +0530

    feat: #445 add atanas to contractors

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -133,6 +133,13 @@ export const environment: IEnvironment = {
 			firstName: 'Rachit',
 			lastName: 'Magon',
 			imageUrl: 'assets/images/avatars/rachit.png'
+		},
+		{
+			email: 'atanas@ever.co',
+			password: '123456',
+			firstName: 'Atanas',
+			lastName: 'Yonkov',
+			imageUrl: 'assets/images/avatars/atanas.jpeg'
 		}
 	],
 
@@ -153,7 +160,7 @@ export const environment: IEnvironment = {
 		},
 		{
 			name: 'Contractors',
-			defaultMembers: ['rachit@ever.co']
+			defaultMembers: ['rachit@ever.co', 'atanas@ever.co']
 		}
 	]
 };
