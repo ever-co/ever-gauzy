c0cc7869a67ff5feba0cef95de536020a9f534a2
Rachit Magon <rachit.magon@gmail.com>
2020-01-22 11:51:14 +0530

    feat: #113 employee recurring expenses

--- libs/models/src/lib/employee-recurring-expense.ts
+++ libs/models/src/lib/employee-recurring-expense.ts
@@ -2,18 +2,46 @@ import { BaseEntityModel as IBaseEntityModel } from './base-entity.model';
 
 export interface EmployeeRecurringExpense extends IBaseEntityModel {
 	employeeId: string;
-	month: number;
-	year: number;
+	startDay: number;
+	startMonth: number;
+	startYear: number;
+	endDay?: number;
+	endMonth?: number;
+	endYear?: number;
 	categoryName: string;
 	value: number;
 	currency: string;
-	isRecurring: boolean;
 }
 
-export interface EmployeeRecurringExpenseFindInput extends IBaseEntityModel {
+/**
+ * categoryName, value and currency are the new values.
+ * startDay, startMonth, startYear represent the date when this was edited.
+ */
+export interface EmployeeRecurringExpenseEditInput extends IBaseEntityModel {
+	startDay: number;
+	startMonth: number;
+	startYear: number;
+	categoryName: string;
+	value: number;
+	currency: string;
+}
+
+export interface EmployeeRecurringExpenseByMonthFindInput
+	extends IBaseEntityModel {
 	employeeId?: string;
 	month?: number;
 	year?: number;
-	currency?: string;
-	isRecurring?: boolean;
+}
+
+export interface EmployeeRecurringExpenseDeleteInput {
+	deletionType: RecurringExpenseDeletionEnum;
+	day: number;
+	month: number;
+	year: number;
+}
+
+export enum RecurringExpenseDeletionEnum {
+	CURRENT = 'current',
+	FUTURE = 'future',
+	ALL = 'all'
 }
