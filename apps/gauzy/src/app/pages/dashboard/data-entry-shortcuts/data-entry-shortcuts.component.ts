4e71c080daefef3cd5cf7a7de0a3837b57a38c47
Rachit Magon <rachit.magon@gmail.com>
2020-02-13 18:00:57 +0530

    feat: #397 data entry dashboard with shortcuts

---
+++ apps/gauzy/src/app/pages/dashboard/data-entry-shortcuts/data-entry-shortcuts.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { Store } from '../../../@core/services/store.service';
+import { takeUntil } from 'rxjs/operators';
+import { PermissionsEnum } from '@gauzy/models';
+
+@Component({
+	selector: 'ga-data-entry-shortcuts',
+	templateUrl: './data-entry-shortcuts.component.html',
+	styleUrls: ['./data-entry-shortcuts.component.scss']
+})
+export class DataEntryShortcutsComponent implements OnInit, OnDestroy {
+	private _ngDestroy$ = new Subject<void>();
+
+	constructor(
+		private readonly router: Router,
+		private readonly store: Store
+	) {}
+
+	hasPermissionE = false;
+	hasPermissionI = false;
+	hasPermissionIEdit = false;
+	hasPermissionEEdit = false;
+
+	async ngOnInit() {
+		this.store.userRolePermissions$
+			.pipe(takeUntil(this._ngDestroy$))
+			.subscribe(() => {
+				this.hasPermissionE = this.store.hasPermission(
+					PermissionsEnum.ORG_EXPENSES_VIEW
+				);
+				this.hasPermissionI = this.store.hasPermission(
+					PermissionsEnum.ORG_INCOMES_VIEW
+				);
+				this.hasPermissionEEdit = this.store.hasPermission(
+					PermissionsEnum.ORG_EXPENSES_EDIT
+				);
+				this.hasPermissionIEdit = this.store.hasPermission(
+					PermissionsEnum.ORG_INCOMES_EDIT
+				);
+			});
+	}
+
+	async addIncome() {
+		this.router.navigateByUrl('pages/income?openAddDialog=true');
+	}
+
+	async addExpense() {
+		this.router.navigateByUrl('pages/expenses?openAddDialog=true');
+	}
+
+	ngOnDestroy() {
+		this._ngDestroy$.next();
+		this._ngDestroy$.complete();
+	}
+}
