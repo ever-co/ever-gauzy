385278f526d97ead1166d3c973fd4826b53429f5
sunko <sunk0@abv.bg>
2020-02-21 16:56:07 +0200

    feat: added tags mutation on add

--- apps/gauzy/src/app/@shared/tags/tags-mutation.component.ts
+++ apps/gauzy/src/app/@shared/tags/tags-mutation.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NbDialogRef } from '@nebular/theme';
 @Component({
-    selector: 'ngx-tags-mutation',
-    templateUrl: './tags-mutation.component.html',
-    styleUrls: ['./tags-mutation.component.scss']
+	selector: 'ngx-tags-mutation',
+	templateUrl: './tags-mutation.component.html',
+	styleUrls: ['./tags-mutation.component.scss']
 })
- 
-export class TagsMutationComponent implements OnInit{
+export class TagsMutationComponent implements OnInit {
+	selectedUser: { userName: string }[] = [];
+	selectedColor: { color: string }[] = [];
+	constructor(protected dialogRef: NbDialogRef<TagsMutationComponent>) {}
+	ngOnInit() {
+		if (!this.selectedUser.length) {
+			const fakeUsers = ['Employee', 'User', 'Organization'];
 
-    constructor(
-        protected dialogRef: NbDialogRef<TagsMutationComponent>,
-    ){}
-    ngOnInit(){
+			fakeUsers.forEach((name) => {
+				this.selectedUser.push({
+					userName: name
+				});
+			});
+		}
+		if (!this.selectedColor.length) {
+			const selectedColor = ['Critical', 'Important', 'Archived'];
 
-    }
+			selectedColor.forEach((color) =>
+				this.selectedColor.push({
+					color: color
+				})
+			);
+		}
+	}
+	async closeDialog() {
+		this.dialogRef.close();
+	}
 }
