1d09ede8c18f914573a37522f895ec77c3d6f062
Muiz Nadeem <muiz.nadeem@gmail.com>
2020-03-10 11:46:27 +0530

    fix: #660 added comments

--- apps/gauzy/src/app/pages/dashboard/employee-chart/employee-chart.component.ts
+++ apps/gauzy/src/app/pages/dashboard/employee-chart/employee-chart.component.ts
@@ -38,6 +38,10 @@ export class EmployeeChartComponent implements OnInit, OnDestroy {
 		private errorHandler: ErrorHandlingService
 	) {}
 
+	/**
+	 * Loads or reloads chart statistics and chart when employee or date
+	 * is changed from the header component
+	 */
 	async ngOnInit() {
 		this.store.selectedDate$
 			.pipe(takeUntil(this._ngDestroy$))
@@ -53,7 +57,10 @@ export class EmployeeChartComponent implements OnInit, OnDestroy {
 				await this._initializeChart();
 			});
 	}
-
+	/**
+	 * Loads or reloads chart statistics and chart when employee or date
+	 * is changed from the header component
+	 */
 	private async _initializeChart() {
 		if (this.selectedEmployee.id && this.selectedDate) {
 			try {
@@ -145,10 +152,20 @@ export class EmployeeChartComponent implements OnInit, OnDestroy {
 				};
 			});
 	}
-
-	private async _loadData(emp: SelectedEmployee) {
+	/**
+	 * Fetches selected employee's statistics for chosen date for past X months.
+	 * Populates the local statistics variables with fetched data.
+	 */
+	private async _loadData() {
+		/**
+		 * Fetches selected employee's statistics for chosen date for past X months.
+		 */
 		const employeeStatistics = await this.employeeStatisticsService.getAggregatedStatisticsByEmployeeId(
-			{ employeeId: emp.id, valueDate: this.selectedDate, months: 12 }
+			{
+				employeeId: this.selectedEmployee.id,
+				valueDate: this.selectedDate,
+				months: 12
+			}
 		);
 		this.labels = [];
 		this.incomeStatistics = [];
@@ -156,6 +173,9 @@ export class EmployeeChartComponent implements OnInit, OnDestroy {
 		this.profitStatistics = [];
 		this.bonusStatistics = [];
 
+		/**
+		 * Populates the local statistics variables with fetched data.
+		 */
 		employeeStatistics.map((stat) => {
 			const labelValue = `${monthNames[stat.month]} '${stat.year
 				.toString(10)
