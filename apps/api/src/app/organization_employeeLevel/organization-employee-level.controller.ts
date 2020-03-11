09e5c95ef364c6f2a757b46b133234933805dda9
ntmonov <nmonov84@gmail.com>
2020-03-11 10:06:23 +0200

    feat: #724-added-add-delete-edit-buttons

--- apps/api/src/app/organization_employeeLevel/organization-employee-level.controller.ts
+++ apps/api/src/app/organization_employeeLevel/organization-employee-level.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Body,
+	Param,
+	Post,
+	Delete,
+	Put
+} from '@nestjs/common';
 import { CrudController } from '../core';
 import { EmployeeLevel } from './organization-employee-level.entity';
 import { EmployeeLevelService } from './organization-employee-level.service';
+import { EmployeeLevelInput } from '@gauzy/models';
 
 @Controller()
 export class EmployeeLevelController extends CrudController<EmployeeLevel> {
@@ -9,9 +18,31 @@ export class EmployeeLevelController extends CrudController<EmployeeLevel> {
 		super(employeeLevelService);
 	}
 
-	@Get()
-	findAll() {
-		console.log('here');
-		return this.employeeLevelService.findAll();
+	@Get(':orgId')
+	async findByOrgId(@Param() params): Promise<Object> {
+		const orgId = params.orgId;
+		return await this.employeeLevelService.findAll({
+			where: { organizationId: orgId }
+		});
+	}
+
+	@Post()
+	async createEmployeeLevel(@Body() employeeLevel: EmployeeLevelInput) {
+		return await this.employeeLevelService.create(employeeLevel);
+	}
+
+	@Delete(':id')
+	async deleteEmployeeLevel(@Param() params) {
+		const id = params.id;
+		return await this.employeeLevelService.delete(id);
+	}
+
+	@Put(':id')
+	async updateEmployeeLevel(
+		@Param() params,
+		@Body() employeeLevel: EmployeeLevelInput
+	) {
+		const id = params.id;
+		return await this.employeeLevelService.update(id, employeeLevel);
 	}
 }
