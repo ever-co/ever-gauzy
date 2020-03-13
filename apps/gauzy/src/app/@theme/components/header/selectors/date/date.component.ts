d0439c7c7f8a7d7af1ab8de3be639a46a1caf031
Muiz Nadeem <muiz.nadeem@gmail.com>
2020-03-13 10:51:05 +0530

    fix: #719 date component fixed

--- apps/gauzy/src/app/@theme/components/header/selectors/date/date.component.ts
+++ apps/gauzy/src/app/@theme/components/header/selectors/date/date.component.ts
@@ -1,60 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { NbCalendarMonthCellComponent } from '@nebular/theme';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import {
+	NbCalendarMonthCellComponent,
+	NbCalendarMonthPickerComponent,
+	NbCalendarYearPickerComponent
+} from '@nebular/theme';
 import { Store } from 'apps/gauzy/src/app/@core/services/store.service';
-import { monthNames } from '../../../../../@core/utils/date'
+import { monthNames } from '../../../../../@core/utils/date';
 
 @Component({
-    selector: 'ga-date-selector',
-    templateUrl: './date.component.html',
-    styleUrls: ['./date.component.scss'],
-    host: {
-        '(document:click)': 'clickOutside($event)'
-    }
+	selector: 'ga-date-selector',
+	templateUrl: './date.component.html',
+	styleUrls: ['./date.component.scss'],
+	host: {
+		'(document:click)': 'clickOutside($event)'
+	}
 })
 export class DateSelectorComponent implements OnInit {
-    monthCellComponent = NbCalendarMonthCellComponent;
-    loadCalendar = false;
-    dateInputValue: any;
-    date = new Date();
-    min = new Date(this.date.getFullYear() - 9, 6, 15);
-    max = new Date();
+	monthCellComponent = NbCalendarMonthCellComponent;
+	loadCalendar = false;
+	dateInputValue: string;
+	date = new Date();
+	max = new Date();
+	@ViewChild('month', { static: false })
+	monthRef: NbCalendarMonthPickerComponent<any, any>;
 
-    constructor(private store: Store) { }
+	constructor(private store: Store) {}
 
-    ngOnInit() {
-        this.date.setMonth(this.date.getMonth());
-        this.store.selectedDate = this.date;
-        this.dateInputValue = this.formatDateMMMMyy(this.date);
-    }
+	ngOnInit() {
+		this.store.selectedDate = this.date;
+		this.dateInputValue = this.formatDateMMMMyy(this.date);
+	}
 
-    handleDateChange(event) {
-        this.store.selectedDate = event;
-        this.date = event;
-        this.dateInputValue = this.formatDateMMMMyy(this.date);
-    }
+	handleDateChange(event: Date) {
+		this.store.selectedDate = event;
+		this.date = event;
 
-    formatDateMMMMyy(date) {
-        const monthIndex = date.getMonth();
-        const year = date.getFullYear();
+		// Refresh Months when Year is changed
+		this.monthRef.month = event;
+		this.monthRef.initMonths();
 
-        return monthNames[monthIndex] + ', ' + year;
-    }
+		this.dateInputValue = this.formatDateMMMMyy(this.date);
+	}
 
-    getDefaultDate() {
-        const date = new Date();
-        date.setMonth(date.getMonth() - 1);
-        return date;
-    }
+	formatDateMMMMyy(date): string {
+		const monthIndex = date.getMonth();
+		const year = date.getFullYear();
 
-    clear() {
-        this.dateInputValue = '';
-        this.date = this.getDefaultDate();
-        this.store.selectedDate = null;
-    }
+		return monthNames[monthIndex] + ', ' + year;
+	}
 
-    clickOutside(event) {
-        if (!document.getElementById('dashboard-calendar').contains(event.target)) {
-            this.loadCalendar = false;
-        }
-    }
+	clear() {
+		this.dateInputValue = '';
+		this.date = new Date();
+		this.store.selectedDate = null;
+	}
+
+	clickOutside(event) {
+		if (
+			!document
+				.getElementById('dashboard-calendar')
+				.contains(event.target)
+		) {
+			this.loadCalendar = false;
+		}
+	}
 }
