4ff51858218046d01553551bbe7a780cb7346a96
lubomir <lubomir@ever.co>
2020-03-14 03:09:01 +0200

    fix: repaired-deep-scan-error

--- apps/api/src/app/country/country.service.ts
+++ apps/api/src/app/country/country.service.ts
@@ -3,8 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CrudService } from '../core/crud/crud.service';
 import { Country } from './country.entity';
-import * as fs from 'fs';
-import * as csv from 'csv-writer';
 
 @Injectable()
 export class CountryService extends CrudService<Country> {
