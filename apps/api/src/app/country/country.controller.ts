df52d10dbac45cdf88ff9bfff02d902b5777d420
lubomir <lubomir@ever.co>
2020-03-15 00:12:57 +0200

    fix: repair-deep-scan-error

--- apps/api/src/app/country/country.controller.ts
+++ apps/api/src/app/country/country.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CrudController } from '../core/crud/crud.controller';
 import { Country } from './country.entity';
