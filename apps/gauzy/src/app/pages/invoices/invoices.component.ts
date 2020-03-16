fc59f9e04e7d0fead94177ebce0cd36407b527fd
ntmonov <nmonov84@gmail.com>
2020-03-16 11:22:39 +0200

    feat: fixed-UI

--- apps/gauzy/src/app/pages/invoices/invoices.component.ts
+++ apps/gauzy/src/app/pages/invoices/invoices.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
 import { InvoicesMutationComponent } from './invoices-mutation/invoices-mutation.component';
+import { first } from 'rxjs/operators';
 
 @Component({
 	templateUrl: './invoices.component.html'
@@ -12,16 +13,5 @@ export class InvoicesComponent {
 		this.dialogService.open(InvoicesMutationComponent, {
 			context: {}
 		});
-		// const addData = await dialog.onClose.pipe(first()).toPromise();
-		// this.selectedTag = null;
-		// this.disableButton = true;
-		// console.warn(addData);
-		// if (addData) {
-		// 	this.toastrService.primary(
-		// 		this.getTranslation('TAGS_PAGE.TAGS_ADD_TAG'),
-		// 		this.getTranslation('TOASTR.TITLE.SUCCESS')
-		// 	);
-		// }
-		// this.loadSettings();
 	}
 }
