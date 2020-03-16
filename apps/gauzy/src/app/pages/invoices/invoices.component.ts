bef96d184399d1bf8ee92ed63af4f17e5b764357
ntmonov <nmonov84@gmail.com>
2020-03-16 11:24:22 +0200

    feat: deepscan-fix

--- apps/gauzy/src/app/pages/invoices/invoices.component.ts
+++ apps/gauzy/src/app/pages/invoices/invoices.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NbDialogService } from '@nebular/theme';
 import { InvoicesMutationComponent } from './invoices-mutation/invoices-mutation.component';
-import { first } from 'rxjs/operators';
 
 @Component({
 	templateUrl: './invoices.component.html'
