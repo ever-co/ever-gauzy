cc055ca91cee876c56495b82cad7d042ff2eb91f
prysiazhna <olha.prysiazhna@gmail.com>
2020-03-30 19:28:33 +0300

    fix: fix deepscan error [ref:#868]

--- apps/api/src/app/core/seeds/SeedDataService.ts
+++ apps/api/src/app/core/seeds/SeedDataService.ts
@@ -65,8 +65,8 @@ import { Timesheet } from '../../timesheet/timesheet.entity';
 import { OrganizationRecurringExpense } from '../../organization-recurring-expense/organization-recurring-expense.entity';
 import { OrganizationPositions } from '../../organization-positions/organization-positions.entity';
 import { Email } from '../../email/email.entity';
-import { Candidate } from '../../candidate/candidate.entity';
-import { createCandidates } from '../../candidate/candidate.seed';
+// import { Candidate } from '../../candidate/candidate.entity';
+// import { createCandidates } from '../../candidate/candidate.seed';
 
 const allEntities = [
 	TimeOffPolicy,
