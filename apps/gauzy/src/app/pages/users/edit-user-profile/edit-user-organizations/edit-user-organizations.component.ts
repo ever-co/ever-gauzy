8181c4cd6e2b4cf3d62367bfca93e1bfb1d4dbe9
YavorGrancharov <bearhold@gmail.com>
2020-04-01 19:23:47 +0300

    feat: finished task adding multiple organizations to user

--- apps/gauzy/src/app/pages/users/edit-user-profile/edit-user-organizations/edit-user-organizations.component.ts
+++ apps/gauzy/src/app/pages/users/edit-user-profile/edit-user-organizations/edit-user-organizations.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { Organization } from '@gauzy/models';
+import {
+	Organization,
+	UserOrganizationCreateInput,
+	RolesEnum
+} from '@gauzy/models';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { TranslationBaseComponent } from 'apps/gauzy/src/app/@shared/language-base/translation-base.component';
-import { OrganizationsService } from 'apps/gauzy/src/app/@core/services/organizations.service';
-import { Store } from 'apps/gauzy/src/app/@core/services/store.service';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { takeUntil, first } from 'rxjs/operators';
+import { FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { Params, ActivatedRoute, Router } from '@angular/router';
+import { TranslationBaseComponent } from 'apps/gauzy/src/app/@shared/language-base/translation-base.component';
 import { UsersOrganizationsService } from 'apps/gauzy/src/app/@core/services/users-organizations.service';
+import { NbToastrService, NbDialogService } from '@nebular/theme';
+import { Store } from 'apps/gauzy/src/app/@core/services/store.service';
+import { OrganizationsService } from 'apps/gauzy/src/app/@core/services/organizations.service';
 import { UsersService } from 'apps/gauzy/src/app/@core/services';
-import { Params, ActivatedRoute } from '@angular/router';
+import { DeleteConfirmationComponent } from 'apps/gauzy/src/app/@shared/user/forms/delete-confirmation/delete-confirmation.component';
+import { UserIdService } from 'apps/gauzy/src/app/@core/services/edit-user-data.service';
 
 @Component({
 	selector: 'ngx-edit-user-organization',
@@ -23,17 +30,24 @@ export class EditUserOrganizationsComponent extends TranslationBaseComponent
 	organization: Organization;
 
 	form: FormGroup;
-	organizations: string[];
+	organizations: Organization[];
 	selectedOrganizationsId: string[];
 	routeParams: Params;
+	showAddCard: boolean;
+	selectedOrganizationId: string;
+	selectedUserId: string;
+	selectedUserName: string;
 
 	constructor(
 		private route: ActivatedRoute,
-		private readonly fb: FormBuilder,
-		private organizationsService: OrganizationsService,
-		private store: Store,
-		private usersOrganizationService: UsersOrganizationsService,
 		readonly translateService: TranslateService,
+		private userOrganizationsService: UsersOrganizationsService,
+		private readonly toastrService: NbToastrService,
+		private organizationsService: OrganizationsService,
+		private userIdService: UserIdService,
+		private dialogService: NbDialogService,
+		private store: Store,
+		private router: Router,
 		private readonly usersService: UsersService
 	) {
 		super(translateService);
@@ -43,39 +57,187 @@ export class EditUserOrganizationsComponent extends TranslationBaseComponent
 		this.route.params
 			.pipe(takeUntil(this._ngDestroy$))
 			.subscribe((params) => {
-				console.log(params);
 				this.routeParams = params;
-				this._loadOrganizations();
 			});
 
-		this._initializeForm();
+		this.store.selectedOrganization$
+			.pipe(takeUntil(this._ngDestroy$))
+			.subscribe((organization) => {
+				if (organization) {
+					this.showAddCard = false;
+					this.selectedOrganizationId = organization.id;
+				}
+			});
+
+		this.loadPage();
 	}
 
-	private async _initializeForm() {
-		this.form = this.fb.group({
-			organizations: this.organizations
-		});
+	async addOrg(user: UserOrganizationCreateInput) {
+		if (user.isActive) {
+			await this.userOrganizationsService
+				.create(user)
+				.pipe(first())
+				.toPromise();
+
+			this.toastrService.primary(
+				this.getTranslation(
+					'NOTES.ORGANIZATIONS.ADD_NEW_USER_TO_ORGANIZATION',
+					{
+						username: this.selectedUserName,
+						orgname: this.getTranslation(
+							'ORGANIZATIONS_PAGE.EDIT.ADDED_TO_ORGANIZATION'
+						)
+					}
+				),
+				this.getTranslation('TOASTR.TITLE.SUCCESS')
+			);
+
+			this.showAddCard = false;
+			this.loadPage();
+		}
 	}
 
-	private async _loadOrganizations() {
-		const all_orgs = this.organizationsService.getAll();
+	async remove() {
+		const user = await this.usersService.getUserById(this.selectedUserId);
 
-		const allOrgs = [];
-		all_orgs.then((orgs) => {
-			for (let i = 0; i < orgs.items.length; i++) {
-				allOrgs.push({
-					id: orgs.items[i].id,
-					name: orgs.items[i].name,
-					imageUrl: orgs.items[i].imageUrl
-				});
+		const { items } = await this.userOrganizationsService.getAll([
+			'user',
+			'user.role'
+		]);
+
+		let counter = 0;
+
+		let orgUserId: string;
+		let userToRemove: any;
+		let userName: string;
+
+		for (const orgUser of items) {
+			if (
+				orgUser.isActive &&
+				(!orgUser.user.role ||
+					orgUser.user.role.name !== RolesEnum.EMPLOYEE)
+			) {
+				userToRemove = orgUser;
+				userName = orgUser.user.firstName + ' ' + orgUser.user.lastName;
+				orgUserId = orgUser.id;
+				if (userToRemove.user.id === user.id) {
+					counter++;
+				}
 			}
+		}
 
-			this.organizations = allOrgs;
-		});
+		if (counter - 1 < 1) {
+			this.dialogService
+				.open(DeleteConfirmationComponent, {
+					context: {
+						recordType:
+							userName +
+							' ' +
+							this.getTranslation(
+								'FORM.DELETE_CONFIRMATION.DELETE_USER'
+							)
+					}
+				})
+				.onClose.pipe(takeUntil(this._ngDestroy$))
+				.subscribe(async (result) => {
+					if (result) {
+						try {
+							this.usersService.delete(
+								userToRemove.user.id,
+								userToRemove
+							);
+
+							this.toastrService.primary(
+								this.getTranslation(
+									'ORGANIZATIONS_PAGE.EDIT.USER_WAS_DELETED',
+									{
+										name: userName
+									}
+								),
+								this.getTranslation('TOASTR.TITLE.SUCCESS')
+							);
+
+							this.router.navigate(['/pages/users']);
+						} catch (error) {
+							this.toastrService.danger(
+								error.error.message || error.message,
+								'Error'
+							);
+						}
+					}
+				});
+		} else {
+			this.dialogService
+				.open(DeleteConfirmationComponent, {
+					context: {
+						recordType:
+							userName +
+							' ' +
+							this.getTranslation(
+								'FORM.DELETE_CONFIRMATION.USER_RECORD'
+							)
+					}
+				})
+				.onClose.pipe(takeUntil(this._ngDestroy$))
+				.subscribe(async (result) => {
+					if (result) {
+						try {
+							await this.userOrganizationsService.removeUserFromOrg(
+								orgUserId
+							);
+
+							this.toastrService.primary(
+								this.getTranslation(
+									'ORGANIZATIONS_PAGE.EDIT.USER_WAS_REMOVED',
+									{
+										name: userName
+									}
+								),
+								this.getTranslation('TOASTR.TITLE.SUCCESS')
+							);
+
+							this.loadPage();
+						} catch (error) {
+							this.toastrService.danger(
+								error.error.message || error.message,
+								'Error'
+							);
+						}
+					}
+				});
+		}
 	}
 
-	onOrganizationsSelected(organizations: string[]) {
-		this.selectedOrganizationsId = organizations;
+	private async loadPage() {
+		const users = await this.userOrganizationsService.getAll([
+			'user',
+			'user.role'
+		]);
+
+		const { items } = await this.userOrganizationsService.getAll([], {
+			id: this.userIdService.userId
+		});
+
+		this.selectedUserId = items[0].userId;
+
+		const user = await this.usersService.getUserById(items[0].userId);
+		this.selectedUserName = user.firstName + ' ' + user.lastName;
+
+		const all_orgs = await this.organizationsService.getAll();
+
+		const includedOrgs = users.items.filter(
+			(item) => item.user.id === items[0].userId
+		);
+
+		const filtered = all_orgs.items.filter(
+			(a) => includedOrgs.filter((b) => b.orgId === a.id).length
+		);
+
+		this.organizations = filtered;
+	}
+
+	cancel() {
+		this.showAddCard = !this.showAddCard;
 	}
 
 	ngOnDestroy() {
