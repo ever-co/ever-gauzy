29adac02b220e2fb83a8a8d1c99afbe337bc2207
Ruslan Konviser <evereq@gmail.com>
2020-04-08 20:09:03 +0300

    Delete index.ts

--- apps/api/src/app/candidate_source/index.ts
+++
@@ -1,4 +0,0 @@
-export { CandidateSourceModule } from './candidate_source.module';
-export { CandidateSource } from './candidate_source.entity';
-export { CandidateSourceService } from './candidate_source.service';
-export * from './candidate_source.seed';
