74f8837361db0d5a185ec503f063a4cc9d7bbf8e
prysiazhna <olha.prysiazhna@gmail.com>
2020-04-15 14:10:07 +0300

    fix: added cards for education in candidate [ref:#897]

--- apps/gauzy/src/app/pages/candidates/edit-candidate/edit-candidate-profile/edit-candidate-experience/edit-candidate-education/edit-candidate-education.component.ts
+++ apps/gauzy/src/app/pages/candidates/edit-candidate/edit-candidate-profile/edit-candidate-experience/edit-candidate-education/edit-candidate-education.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslationBaseComponent } from 'apps/gauzy/src/app/@shared/language-base/translation-base.component';
-import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { Candidate, Education } from '@gauzy/models';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { CandidateStore } from 'apps/gauzy/src/app/@core/services/candidate-store.service';
+import { NbToastrService } from '@nebular/theme';
 
 @Component({
 	selector: 'ga-edit-candidate-education',
@@ -15,13 +16,22 @@ import { CandidateStore } from 'apps/gauzy/src/app/@core/services/candidate-stor
 export class EditCandidateEducationComponent extends TranslationBaseComponent
 	implements OnInit {
 	showAddCard: boolean;
-	showEditDiv = [];
-	educations: Education[];
+	isEdit = false;
+	editIndex = null;
+	educations: Education[] = [
+		{
+			schoolName: '111',
+			degree: '111',
+			field: '111',
+			completionDate: null,
+			notes: '111'
+		}
+	];
 	private _ngDestroy$ = new Subject<void>();
 	selectedCandidate: Candidate;
 	form: FormGroup;
 	constructor(
-		// private readonly toastrService: NbToastrService,
+		private readonly toastrService: NbToastrService,
 		readonly translateService: TranslateService,
 		private candidateStore: CandidateStore,
 		private fb: FormBuilder
@@ -35,7 +45,7 @@ export class EditCandidateEducationComponent extends TranslationBaseComponent
 				this.selectedCandidate = candidate;
 				if (this.selectedCandidate) {
 					this._initializeForm(this.selectedCandidate.educations);
-					this.addEducation();
+					this.loadData();
 				}
 			});
 	}
@@ -44,25 +54,57 @@ export class EditCandidateEducationComponent extends TranslationBaseComponent
 			educations: this.fb.array([])
 		});
 	}
-
-	showEditCard(index: number) {
-		this.showEditDiv[index] = true;
-		this.showAddCard = !this.showAddCard;
-	}
-
-	addEducation() {
-		const educations = this.form.controls.educations as FormArray;
-		educations.push(
+	private async loadData() {
+		const educationForm = this.form.controls.educations as FormArray;
+		educationForm.push(
 			this.fb.group({
-				schoolName: '',
-				degree: '',
-				field: [''],
-				completionDate: [''],
+				schoolName: ['', Validators.required],
+				degree: ['', Validators.required],
+				field: ['', Validators.required],
+				completionDate: ['', Validators.required],
 				notes: ['']
 			})
 		);
 	}
+	editEducation(index: number) {
+		this.showAddCard = !this.showAddCard;
+		this.form.controls.educations.patchValue([this.educations[index]]);
+		this.isEdit = true;
+		this.editIndex = index;
+	}
+	showCard() {
+		this.showAddCard = !this.showAddCard;
+		this.form.controls.educations.reset();
+	}
+	cancel() {
+		this.showAddCard = !this.showAddCard;
+		this.form.controls.educations.value.length = 0;
+	}
+
 	submitForm() {
-		console.log(this.form.controls.educations.value);
+		const educationForm = this.form.controls.educations as FormArray;
+		if (educationForm.valid) {
+			if (this.isEdit) {
+				const editValue = { ...this.form.controls.educations.value[0] };
+				this.educations[this.editIndex] = editValue;
+				this.isEdit = false;
+				this.editIndex = null;
+			} else {
+				this.educations.push(...this.form.controls.educations.value);
+			}
+			this.showAddCard = !this.showAddCard;
+			this.form.controls.educations.reset();
+			// to do  toastr for success
+		} else {
+			this.toastrService.danger(
+				this.getTranslation('NOTES.CANDIDATE.EXPERIENCE.INVALID_FORM'),
+				this.getTranslation(
+					'TOASTR.MESSAGE.CANDIDATE_EDUCATION_REQUIRED'
+				)
+			);
+		}
+	}
+	removeEducation(index: number) {
+		this.educations.splice(index, 1);
 	}
 }
