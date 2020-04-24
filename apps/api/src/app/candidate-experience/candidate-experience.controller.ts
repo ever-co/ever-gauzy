035a0f3f9211f93ba2336b1d029d23d6ae3068bb
prysiazhna <olha.prysiazhna@gmail.com>
2020-04-24 11:39:54 +0300

    fix: fix controllers  [ref:#1039]

--- apps/api/src/app/candidate-experience/candidate-experience.controller.ts
+++ apps/api/src/app/candidate-experience/candidate-experience.controller.ts
@@ -1,8 +1,9 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, HttpStatus, Get, Query } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CrudController } from '../core/crud/crud.controller';
 import { CandidateExperienceService } from './candidate-experience.service';
 import { CandidateExperience } from './candidate-experience.entity';
+import { IPagination } from '../core';
 
 @ApiTags('candidate_experience')
 @Controller()
@@ -13,5 +14,25 @@ export class CandidateExperienceController extends CrudController<
 		private readonly candidateExperienceService: CandidateExperienceService
 	) {
 		super(candidateExperienceService);
+	}
+
+	@ApiOperation({
+		summary: 'Find all candidate experience.'
+	})
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Found candidate experience',
+		type: CandidateExperience
+	})
+	@ApiResponse({
+		status: HttpStatus.NOT_FOUND,
+		description: 'Record not found'
+	})
+	@Get()
+	async findExperience(
+		@Query('data') data: string
+	): Promise<IPagination<CandidateExperience>> {
+		const { findInput } = JSON.parse(data);
+		return this.candidateExperienceService.findAll({ where: findInput });
 	}
 }
