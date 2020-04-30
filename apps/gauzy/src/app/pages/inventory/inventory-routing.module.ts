b5b44428e1da82bca9f14aacc0836ea5da90c901
Tsvetelina <tsvetelina@ever.co>
2020-04-30 20:28:21 +0300

    feat: #841/product type crud operations

--- apps/gauzy/src/app/pages/inventory/inventory-routing.module.ts
+++ apps/gauzy/src/app/pages/inventory/inventory-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InventoryComponent } from './inventory-table/inventory.component';
+import { ProductTypesComponent } from './product-type/product-types.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: InventoryComponent
+	},
+	{
+		path: 'product-types',
+		component: ProductTypesComponent
 	}
 ];
 
