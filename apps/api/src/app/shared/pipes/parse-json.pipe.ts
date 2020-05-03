3d25a182ca9a19e88ebbf53cf923bc89b6e5c508
RYU Chua <ryucby@gmail.com>
2020-05-03 17:16:27 +0800

    Feat/#1130 New ParseJsonPipe and Fix the Invoice API (#1137)
    * feat(api): new pipe "ParseJsonPipe" to parse query input
    
    * fix(api): fix the invoice api bugs with the JsonPipe
    
    * fix(api): remove unuse import
    
    * fix(api): refactor import & export code for pipe

---
+++ apps/api/src/app/shared/pipes/parse-json.pipe.ts
@@ -0,0 +1,75 @@
+import {
+	ArgumentMetadata,
+	HttpStatus,
+	Injectable,
+	Optional,
+	PipeTransform
+} from '@nestjs/common';
+import {
+	ErrorHttpStatusCode,
+	HttpErrorByCode
+} from '@nestjs/common/utils/http-error-by-code.util';
+import { Validator } from 'class-validator';
+
+export interface ParseJsonPipeOptions {
+	throwInvalidError?: boolean;
+	errorHttpStatusCode?: ErrorHttpStatusCode;
+	exceptionFactory?: (error: string) => any;
+}
+/**
+ * JSON Parse Pipe
+ * Validates UUID passed in request parameters.
+ */
+@Injectable()
+export class ParseJsonPipe implements PipeTransform<string> {
+	/**
+	 * Throw invalid JSON error or not ? default to "false"
+	 */
+	protected throwInvalidError: boolean;
+
+	protected exceptionFactory: (error: string) => any;
+
+	/**
+	 * Instance of class-validator
+	 * Can not be easily injected, and there's no need to do so as we
+	 * only use it for json validation method.
+	 */
+	private readonly validator: Validator = new Validator();
+
+	constructor(@Optional() options?: ParseJsonPipeOptions) {
+		options = options || {};
+
+		const {
+			exceptionFactory,
+			errorHttpStatusCode = HttpStatus.BAD_REQUEST,
+			throwInvalidError = false
+		} = options;
+
+		this.throwInvalidError = throwInvalidError;
+		this.exceptionFactory =
+			exceptionFactory ||
+			((error) => new HttpErrorByCode[errorHttpStatusCode](error));
+	}
+
+	/**
+	 * @param value currently processed route argument
+	 * @param metadata contains metadata about the currently processed route argument
+	 */
+	async transform(value: string, metadata: ArgumentMetadata): Promise<any> {
+		const isJson = this.validator.isJSON(value);
+
+		if (isJson) {
+			try {
+				return JSON.parse(value);
+			} catch (e) {
+				console.log('Json Parser Error:', e);
+			}
+		} else if (this.throwInvalidError) {
+			throw this.exceptionFactory(
+				'Validation failed (JSON string is expected)'
+			);
+		}
+
+		return {};
+	}
+}
