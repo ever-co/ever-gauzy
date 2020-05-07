79f562b34d7647cca6ebd2ef4c8e2d6a72c97f54
Rachit Magon <rachit.magon@gmail.com>
2020-05-07 13:27:15 +0530

    fix: #1158 email template seed

--- apps/api/src/app/email-template/email-template.seed.ts
+++ apps/api/src/app/email-template/email-template.seed.ts
@@ -1,9 +1,8 @@
 import * as fs from 'fs';
-import * as readdirp from 'readdirp';
 import { Connection } from 'typeorm';
 import { EmailTemplate } from './email-template.entity';
 import * as mjml2html from 'mjml';
-
+import * as path from 'path';
 /**
  * Note: This seed file assumes the following directory structure in seeds/data/email/default-email-templates/ folder
  *
@@ -16,31 +15,46 @@ import * as mjml2html from 'mjml';
 export const createEmailTemplates = async (
 	connection: Connection
 ): Promise<any> => {
-	return new Promise((resolve, reject) => {
-		const files = [];
+	const files = [];
+	const FOLDER_PATH = path.resolve(
+		'.',
+		'apps',
+		'api',
+		'src',
+		'app',
+		'core',
+		'seeds',
+		'data',
+		'default-email-templates'
+	);
 
-		readdirp('./apps/api/src/app/core/seeds/data/default-email-templates', {
-			depth: 2
-		})
-			.on('data', (entry) => {
-				files.push(entry);
-			})
-			.on('error', (error) => {
-				reject(error);
-			})
-			.on('end', async () => {
-				for (const file of files) {
-					const template = await pathToEmailTemplate(
-						file.path,
-						file.fullPath
-					);
-					if (template && template.hbs) {
-						await insertTemplate(connection, template);
-					}
-				}
-				resolve();
-			});
+	findInDir(FOLDER_PATH, files);
+	console.log(files);
+	await fileToTemplate(connection, files);
+};
+
+function findInDir(dir, fileList = []) {
+	const files = fs.readdirSync(dir);
+
+	files.forEach((file) => {
+		const filePath = path.join(dir, file);
+		const fileStat = fs.lstatSync(filePath);
+
+		if (fileStat.isDirectory()) {
+			findInDir(filePath, fileList);
+		} else {
+			fileList.push(filePath);
+		}
 	});
+}
+
+const fileToTemplate = async (connection, files) => {
+	for (const file of files) {
+		const template = await pathToEmailTemplate(file);
+		if (template && template.hbs) {
+			await insertTemplate(connection, template);
+		}
+	}
 };
 
 const insertTemplate = async (
@@ -56,20 +70,18 @@ const insertTemplate = async (
 };
 
 const pathToEmailTemplate = async (
-	path: string,
 	fullPath: string
 ): Promise<EmailTemplate> => {
 	try {
 		const template = new EmailTemplate();
-		const templatePath = path.replace(/\\/g, '/').split('/');
-		if (templatePath.length !== 3) {
-			return;
-		}
-		const fileName = templatePath[2].split('.', 2);
+		const templatePath = fullPath.replace(/\\/g, '/').split('/');
+		const fileName = templatePath[templatePath.length - 1].split('.', 2);
 		const fileExtension = fileName[1];
 		const fileNameWithoutExtension = fileName[0];
-		template.languageCode = templatePath[1];
-		template.name = `${templatePath[0]}/${fileNameWithoutExtension}`;
+		template.languageCode = templatePath[templatePath.length - 2];
+		template.name = `${
+			templatePath[templatePath.length - 3]
+		}/${fileNameWithoutExtension}`;
 		const fileContent = fs.readFileSync(fullPath, 'utf8');
 
 		switch (fileExtension) {
