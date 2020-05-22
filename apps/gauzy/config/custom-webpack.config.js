ccc72929d969251704dc4320045c5574fe03609b
Ruslan Konviser <evereq@gmail.com>
2020-05-23 00:40:31 +0300

    feat: another attempt to make Circle build pass

--- apps/gauzy/config/custom-webpack.config.js
+++ apps/gauzy/config/custom-webpack.config.js
@@ -2,12 +2,24 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 console.log('Using custom Webpack config...');
 
+function isCircleCI(){
+	return process.env.CI === 'true' && process.env.CIRCLECI === 'true';
+}
+
+const isCircleEnv = isCircleCI();
+
+if (isCircleEnv) {
+	console.log('Building in CircleCI environment');
+}
+
 module.exports = {
 	optimization: {
 		minimize: true,
 		minimizer: [
 			new TerserPlugin({
-				parallel: false,
+				parallel: isCircleEnv ? 2 : true,
+				sourceMap: !isCircleEnv,
+				extractComments: !isCircleEnv
 			}),
 		],
 	},
