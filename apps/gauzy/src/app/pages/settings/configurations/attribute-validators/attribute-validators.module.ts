8d4b275d448cdb58801244a75ef5d1dc19566467
Salathiel Genese <salathielgenese@gmail.com>
2020-06-01 03:56:19 +0400

    feat: refactor imported modules

--- apps/gauzy/src/app/pages/settings/configurations/attribute-validators/attribute-validators.module.ts
+++ apps/gauzy/src/app/pages/settings/configurations/attribute-validators/attribute-validators.module.ts
@@ -3,7 +3,7 @@ import { AttributeValidatorsComponent } from './attribute-validators.component';
 import { AttributeValidatorsService } from '../../../../@core/services/attribute-validators.service';
 import { CommonModule } from '@angular/common';
 import { AttributeValidatorsRoutingModule } from './attribute-validators.routing.module';
-import { NbAccordionModule, NbActionsModule, NbCardModule, NbInputModule } from '@nebular/theme';
+import { NbAccordionModule, NbCardModule, NbInputModule, NbToggleModule } from '@nebular/theme';
 import { FormsModule } from '@angular/forms';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
 import { TranslateModule } from '@ngx-translate/core';
@@ -13,14 +13,14 @@ import { TranslateModule } from '@ngx-translate/core';
 @NgModule( {
   imports: [
     AttributeValidatorsRoutingModule,
+    MonacoEditorModule,
     NbAccordionModule,
+    TranslateModule,
+    NbInputModule,
     NbCardModule,
     CommonModule,
     FormsModule,
-    MonacoEditorModule,
-    NbInputModule,
-    TranslateModule,
-    NbActionsModule,
+    NbToggleModule,
   ],
   declarations: [
     AttributeValidatorsComponent,
