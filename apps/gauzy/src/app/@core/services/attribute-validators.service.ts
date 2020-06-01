54c7ef0aa72e511e290b6bd60d1951d5aaab1b8d
Salathiel Genese <salathielgenese@gmail.com>
2020-06-01 04:16:53 +0400

    feat: add in service method to filter by reference

--- apps/gauzy/src/app/@core/services/attribute-validators.service.ts
+++ apps/gauzy/src/app/@core/services/attribute-validators.service.ts
@@ -8,4 +8,8 @@ import { Service } from './service';
 export class AttributeValidatorsService extends Service<AttributeValidator, AttributeValidatorCreateInput, AttributeValidatorUpdateInput>
 {
 
+  public async findByReference( referenceLike: string ): Promise<AttributeValidator[]> {
+    return this.entries.filter( ({ reference }) => reference.includes( referenceLike ) );
+  }
+
 }
