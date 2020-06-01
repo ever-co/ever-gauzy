adb0ec58e6c70e2efa165e85507a2944ed50de49
Kamesh Dashora <dashorakamesh95@gmail.com>
2020-06-01 22:56:24 +0530

    feat(appointment_scheduling): fixes deep scan issue

--- apps/gauzy/src/app/pages/employees/appointment/appointment.module.ts
+++ apps/gauzy/src/app/pages/employees/appointment/appointment.module.ts
@@ -1,28 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import {
-	NbAlertModule,
 	NbButtonModule,
 	NbCardModule,
-	NbDialogModule,
-	NbIconModule,
-	NbInputModule,
 	NbSpinnerModule,
-	NbTooltipModule,
-	NbTreeGridModule,
-	NbSelectModule,
-	NbRouteTabsetModule,
-	NbCheckboxModule
+	NbIconModule
 } from '@nebular/theme';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ThemeModule } from '../../../@theme/theme.module';
 import { AppointmentRoutingModule } from './appointment-routing.module';
 import { AppointmentComponent } from './appointment.component';
-import { TimerPickerModule } from '../../../@shared/timer-picker/timer-picker.module';
-import { ManageAppointmentComponent } from './manage-appointment/manage-appointment.component';
 import { EmployeeAppointmentService } from '../../../@core/services/employee-appointment.service';
 import { EmployeeMultiSelectModule } from '../../../@shared/employee/employee-multi-select/employee-multi-select.module';
 import { SharedModule } from '../../../@shared/shared.module';
