47e8999e2a07f2ee02fb8d1877ec8db9073c554a
Salathiel Genese <salathielgenese@gmail.com>
2020-06-09 20:19:18 +0400

    feat: override controller endpoint to account for relations and filter

--- apps/api/src/app/pipeline/pipeline.controller.ts
+++ apps/api/src/app/pipeline/pipeline.controller.ts
@@ -1,15 +1,37 @@
-import { CrudController } from '../core/crud';
+import { CrudController, IPagination, PaginationParams } from '../core/crud';
 import { Pipeline } from './pipeline.entity';
-import { Controller, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus, Query, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { PipelineService } from './pipeline.service';
+import { ParseJsonPipe } from '../shared/pipes';
+
+
 
 @Controller()
 @ApiTags('Pipeline')
 @UseGuards( AuthGuard( 'jwt' ) )
 export class PipelineController extends CrudController<Pipeline> {
-	public constructor(pipelineService: PipelineService) {
-		super(pipelineService);
+
+	public constructor(
+	  protected pipelineService: PipelineService ) {
+		super( pipelineService );
 	}
+
+  @ApiOperation({ summary: 'find all' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Found records'
+  })
+  @Get()
+  public async findAll( @Query( 'data', ParseJsonPipe ) data: any ): Promise<IPagination<Pipeline>>
+  {
+    const { relations = [], filter: where = null } = data;
+
+    return this.pipelineService.findAll({
+      relations,
+      where,
+    });
+  }
+
 }
