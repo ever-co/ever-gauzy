f12fce84ba40d261fd377fbd62860d3ad7f081ff
Salathiel Genese <salathielgenese@gmail.com>
2020-06-12 02:10:41 +0400

    feat: create stages along pipeline in transaction

--- apps/api/src/app/pipeline/pipeline.service.ts
+++ apps/api/src/app/pipeline/pipeline.service.ts
@@ -1,8 +1,9 @@
 import { CrudService } from '../core/crud';
 import { Pipeline } from './pipeline.entity';
-import { Repository } from 'typeorm';
+import { DeepPartial, EntityManager, Repository, Transaction, TransactionManager } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Stage } from '../stage/stage.entity';
 
 @Injectable()
 export class PipelineService extends CrudService<Pipeline>
@@ -13,6 +14,33 @@ export class PipelineService extends CrudService<Pipeline>
     pipelineRepository: Repository<Pipeline> )
   {
     super( pipelineRepository );
+  }
+
+  @Transaction()
+  public async create( entity: DeepPartial<Pipeline>, @TransactionManager() manager: EntityManager, ...options ): Promise<Pipeline>
+  {
+    // Persist pipeline
+    const { name, description, organizationId } = entity;
+    const pipeline = await manager.save( manager.create( Pipeline, {
+      organizationId,
+      description,
+      name,
+    }) );
+    const { id: pipelineId } = pipeline;
+
+    // Persist pipeline stages
+    if ( entity.stages?.length ) {
+      for ( const { name, index, description } of entity.stages ) {
+        await manager.save( manager.create( Stage, {
+          description,
+          pipelineId,
+          index,
+          name,
+        }) );
+      }
+    }
+
+    return pipeline;
   }
 
 }
