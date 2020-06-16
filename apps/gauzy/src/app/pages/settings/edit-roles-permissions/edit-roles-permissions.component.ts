31cdfe108dd9e9a1efd104ad78902e9f3fe8ffcc
Salathiel Genese <salathielgenese@gmail.com>
2020-06-16 12:57:10 +0400

    fix: enable admin permissions editable state

--- apps/gauzy/src/app/pages/settings/edit-roles-permissions/edit-roles-permissions.component.ts
+++ apps/gauzy/src/app/pages/settings/edit-roles-permissions/edit-roles-permissions.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-	Organization,
-	RolePermissions,
-	RolesEnum,
-	PermissionGroups
-} from '@gauzy/models';
+import { Organization, PermissionGroups, RolePermissions, RolesEnum } from '@gauzy/models';
 import { NbToastrService } from '@nebular/theme';
 import { TranslateService } from '@ngx-translate/core';
 import { RolePermissionsService } from 'apps/gauzy/src/app/@core/services/role-permissions.service';
@@ -25,8 +20,9 @@ export class EditRolesPermissionsComponent extends TranslationBaseComponent
 
 	organization: Organization;
 
-	selectedRole: RolesEnum = RolesEnum.EMPLOYEE;
-	superAdminRole: RolesEnum = RolesEnum.SUPER_ADMIN;
+  adminRole: RolesEnum = RolesEnum.ADMIN;
+  selectedRole: RolesEnum = RolesEnum.EMPLOYEE;
+  superAdminRole: RolesEnum = RolesEnum.SUPER_ADMIN;
 	selectedRoleId: string;
 
 	allRoles: string[] = Object.values(RolesEnum);
