8c95bb2ff718a1e3a23d4cd9056f8b8a2b3e8b65
Salathiel Genese <salathielgenese@gmail.com>
2020-06-19 12:51:34 +0400

    fix: organize imports

--- apps/gauzy/src/app/app.module.ts
+++ apps/gauzy/src/app/app.module.ts
@@ -6,35 +6,30 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, APP_INITIALIZER, ErrorHandler } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {
-	NbChatModule,
-	NbDatepickerModule,
-	NbDialogModule,
-	NbMenuModule,
-	NbSidebarModule,
-	NbToastrModule,
-	NbWindowModule,
-	NbCalendarModule,
-	NbCalendarKitModule
+  NbCalendarKitModule,
+  NbCalendarModule,
+  NbChatModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbMenuModule,
+  NbSidebarModule,
+  NbToastrModule,
+  NbWindowModule,
 } from '@nebular/theme';
 import { TokenInterceptor } from './@core/auth/token.interceptor';
-
-import { HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { Cloudinary as CloudinaryCore } from 'cloudinary-core';
 import { CloudinaryModule } from '@cloudinary/angular-5.x';
-import {
-	cloudinaryConfiguration,
-	environment
-} from '../environments/environment';
+import { cloudinaryConfiguration, environment } from '../environments/environment';
 import { FileUploadModule } from 'ng2-file-upload';
 import { APIInterceptor } from './@core/api.interceptor';
 import { ServerConnectionService } from './@core/services/server-connection.service';
