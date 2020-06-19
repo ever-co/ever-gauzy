e65f820d2ab41fb3a2161e67c98cbae2555e71b0
Mahesh Umachagi <Mahesh-Umachagi@github.com>
2020-06-19 23:02:20 +0530

    fix: expense categories page

--- apps/api/src/app/expense-categories/expense-category.entity.ts
+++ apps/api/src/app/expense-categories/expense-category.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, ManyToMany, JoinTable } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Base } from '../core/entities/base';
 import { IExpenseCategory } from '@gauzy/models';
 import { Tag } from '../tags/tag.entity';
@@ -15,8 +15,14 @@ export class ExpenseCategory extends Base implements IExpenseCategory {
 	tags?: Tag[];
 
 	@ApiProperty({ type: String })
-	@IsNumber()
+	@IsString()
 	@IsNotEmpty()
 	@Column()
 	name: string;
+
+	@ApiProperty({ type: String })
+	@IsString()
+	@IsNotEmpty()
+	@Column()
+	organizationId: string;
 }
