a5c9db3ea1d137d77ab81c9a12fee87756f47120
prysiazhna <olha.prysiazhna@gmail.com>
2020-06-26 18:29:38 +0300

    fix: interview list, fixed star-output [ref:#1156]

--- apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/interview-panel/interview-panel.component.ts
+++ apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/interview-panel/interview-panel.component.ts
@@ -3,7 +3,11 @@ import { Subject } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslationBaseComponent } from 'apps/gauzy/src/app/@shared/language-base/translation-base.component';
 import { CandidateInterviewService } from 'apps/gauzy/src/app/@core/services/candidate-interview.service';
-import { ICandidateInterview } from '@gauzy/models';
+import { ICandidateInterview, Candidate } from '@gauzy/models';
+import { CandidatesService } from 'apps/gauzy/src/app/@core/services/candidates.service';
+import { takeUntil } from 'rxjs/operators';
+import { EmployeesService } from 'apps/gauzy/src/app/@core/services';
+import { Router } from '@angular/router';
 @Component({
 	selector: 'ga-interview-panel',
 	templateUrl: './interview-panel.component.html',
@@ -13,9 +17,14 @@ export class InterviewPanelComponent extends TranslationBaseComponent
 	implements OnInit, OnDestroy {
 	private _ngDestroy$ = new Subject<void>();
 	interviewList: ICandidateInterview[];
+	candidates: Candidate[];
+	averageRating: number;
 	constructor(
 		readonly translateService: TranslateService,
-		private candidateInterviewService: CandidateInterviewService
+		private candidateInterviewService: CandidateInterviewService,
+		private candidatesService: CandidatesService,
+		private employeesService: EmployeesService,
+		private router: Router
 	) {
 		super(translateService);
 	}
@@ -23,18 +32,63 @@ export class InterviewPanelComponent extends TranslationBaseComponent
 		this.loadInterviews();
 	}
 	async loadInterviews() {
-		const res = await this.candidateInterviewService.getAll([
+		const interviews = await this.candidateInterviewService.getAll([
+			'feedbacks',
 			'interviewers',
-			'candidate',
 			'technologies',
 			'personalQualities',
-			'feedbacks'
+			'candidate'
 		]);
-		if (res) {
-			this.interviewList = res.items;
+		if (interviews) {
+			this.interviewList = interviews.items;
+			this.candidatesService
+				.getAll(['user'])
+				.pipe(takeUntil(this._ngDestroy$))
+				.subscribe((candidates) => {
+					this.candidates = candidates.items;
+					this.interviewList.forEach((interview) => {
+						this.candidates.forEach((item) => {
+							if (item.id === interview.candidateId) {
+								interview.candidate = item;
+							}
+						});
+						this.loadEmployee(this.interviewList);
+
+						if (interview.feedbacks.length > 0) {
+							const res: number[] = [];
+							interview.feedbacks.forEach((fb) => {
+								res.push(Number(fb.rating));
+							});
+							const fbSum = res.reduce((sum, current) => {
+								return sum + current;
+							});
+							interview.rating =
+								fbSum / interview.feedbacks.length;
+						}
+					});
+				});
 		}
 	}
-
+	async loadEmployee(list: ICandidateInterview[]) {
+		for (const interview of list) {
+			const employees = [];
+			for (const interviewer of interview.interviewers) {
+				const res = await this.employeesService.getEmployeeById(
+					interviewer.employeeId,
+					['user']
+				);
+				if (res) {
+					employees.push(res);
+				}
+			}
+			interview.employees = employees;
+		}
+	}
+	goToCandidate(id: string) {
+		this.router.navigate([
+			`/pages/employees/candidates/edit/${id}/profile/interview`
+		]);
+	}
 	ngOnDestroy() {
 		this._ngDestroy$.next();
 		this._ngDestroy$.complete();
