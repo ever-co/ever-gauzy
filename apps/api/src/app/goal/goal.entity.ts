eb2f48c588c4e180ad48432e3d5c8c096dbd3d89
Muiz Nadeem <muiz.nadeem@gmail.com>
2020-06-27 18:43:26 +0530

    feat: #398 organization and employee foreign key in goal entity

--- apps/api/src/app/goal/goal.entity.ts
+++ apps/api/src/app/goal/goal.entity.ts
@@ -1,9 +1,11 @@
-import { Entity, Column, OneToMany } from 'typeorm';
+import { Entity, Column, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
 import { Goal as IGoal } from '@gauzy/models';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 import { KeyResult } from '../keyresult/keyresult.entity';
 import { TenantBase } from '../core/entities/tenant-base';
+import { Organization } from '../organization/organization.entity';
+import { Employee } from '../employee/employee.entity';
 
 @Entity('goal')
 export class Goal extends TenantBase implements IGoal {
@@ -16,12 +18,14 @@ export class Goal extends TenantBase implements IGoal {
 	@IsOptional()
 	description?: string;
 
-	@ApiProperty({ type: String })
-	@Column()
+	@ApiProperty({ type: Employee })
+	@ManyToOne((type) => Employee, { nullable: false, onDelete: 'CASCADE' })
+	@JoinColumn()
 	owner: string;
 
-	@ApiProperty({ type: String })
-	@Column()
+	@ApiProperty({ type: Employee })
+	@ManyToOne((type) => Employee, { nullable: false, onDelete: 'CASCADE' })
+	@JoinColumn()
 	lead: string;
 
 	@ApiProperty({ type: String })
@@ -39,6 +43,9 @@ export class Goal extends TenantBase implements IGoal {
 	@ApiProperty({ type: String })
 	@Column()
 	organizationId: string;
+
+	@ManyToOne((type) => Organization, (organization) => organization.id)
+	organization: Organization;
 
 	@ApiProperty({ type: KeyResult })
 	@OneToMany((type) => KeyResult, (keyResult) => keyResult.goal)
