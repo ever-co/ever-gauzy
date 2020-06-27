5a59270e2582eb60672fd5e9d15979401cb53b48
rahul-rathore-576 <rahulrathore576@gmail.com>
2020-06-27 14:58:31 +0530

    feat: sync hubstaff screenshots

--- apps/api/src/app/timesheet/commands/index.ts
+++ apps/api/src/app/timesheet/commands/index.ts
@@ -2,3 +2,4 @@ export * from './time-log-create.command';
 export * from './time-slot-create.command';
 export * from './timesheet-create.command';
 export * from './timesheet-get.command';
+export * from './screenshot-create.command';
