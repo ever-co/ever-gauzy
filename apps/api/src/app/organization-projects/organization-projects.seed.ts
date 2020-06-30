e073efeb98504c482727af29104c6a1f6a163593
Chetan Khandla <ckhandla94@gmail.com>
2020-06-30 10:26:18 +0530

    fix: added deleted files

---
+++ apps/api/src/app/organization-projects/organization-projects.seed.ts
@@ -0,0 +1,34 @@
+import { Connection } from 'typeorm';
+import * as faker from 'faker';
+import * as _ from 'underscore';
+import { Tag } from '../tags/tag.entity';
+import { OrganizationProjects } from './organization-projects.entity';
+import { Organization } from '@gauzy/models';
+
+const defaultProjects = [
+	'Gauzy Platform (Open-Source)',
+	'Gauzy Web Site',
+	'Gauzy Platform SaaS',
+	'Gauzy Platform DevOps'
+];
+
+export const createRandomOrganizationProjects = async (
+	connection: Connection,
+	defaultOrganizations: Organization[]
+) => {
+	const tag = await connection.getRepository(Tag).create({
+		name: 'Web',
+		description: '',
+		color: faker.commerce.color()
+	});
+	const projects: OrganizationProjects[] = [];
+
+	defaultProjects.forEach((name) => {
+		const project = new OrganizationProjects();
+		project.tags = [tag];
+		project.name = name;
+		project.organizationId = defaultOrganizations[0].id;
+		projects.push(project);
+	});
+	await connection.manager.save(projects);
+};
