0e2e41ffabc7f03afc6152283bb7c38005e52a02
abinandh15 <abinandh15@gmail.com>
2020-06-30 22:30:43 +0530

    fix: all entities now extend TenantBase

--- apps/api/src/app/goal-time-frame/goal-time-frame.service.ts
+++ apps/api/src/app/goal-time-frame/goal-time-frame.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CrudService } from '../core';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { GoalTimeFrame } from './goal-time-frame.entity';
+import { TenantAwareCrudService } from '../core/crud/tenant-aware-crud.service';
 
 @Injectable()
-export class GoalTimeFrameService extends CrudService<GoalTimeFrame> {
+export class GoalTimeFrameService extends TenantAwareCrudService<
+	GoalTimeFrame
+> {
 	constructor(
 		@InjectRepository(GoalTimeFrame)
 		private readonly goalTimeFrameRepository: Repository<GoalTimeFrame>
