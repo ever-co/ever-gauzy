f129776c821fd557ff86d5a1b4dddcb74e3c21aa
duongduckien <ddk0705@gmail.com>
2020-07-01 08:48:30 +0700

    feat: #1132  add team approval for request approval

--- apps/api/src/app/request-approval/request-approval.module.ts
+++ apps/api/src/app/request-approval/request-approval.module.ts
@@ -6,11 +6,36 @@ import { RequestApprovalService } from './request-approval.service';
 import { User } from '../user/user.entity';
 import { UserService } from '../user/user.service';
 import { Employee } from '../employee/employee.entity';
+import { OrganizationTeam } from '../organization-team/organization-team.entity';
+import { OrganizationTeamService } from '../organization-team/organization-team.service';
+import { Role } from '../role/role.entity';
+import { Organization } from '../organization/organization.entity';
+import { EmployeeService } from '../employee/employee.service';
+import { RoleService } from '../role/role.service';
+import { OrganizationService } from '../organization/organization.service';
+import { OrganizationTeamEmployeeModule } from '../organization-team-employee/organization-team-employee.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User, RequestApproval, Employee])],
+	imports: [
+		TypeOrmModule.forFeature([
+			User,
+			RequestApproval,
+			Employee,
+			OrganizationTeam,
+			Role,
+			Organization
+		]),
+		OrganizationTeamEmployeeModule
+	],
 	controllers: [RequestApprovalControler],
-	providers: [RequestApprovalService, UserService],
+	providers: [
+		RequestApprovalService,
+		UserService,
+		OrganizationTeamService,
+		EmployeeService,
+		RoleService,
+		OrganizationService
+	],
 	exports: [RequestApprovalService]
 })
 export class RequestApprovalModule {}
