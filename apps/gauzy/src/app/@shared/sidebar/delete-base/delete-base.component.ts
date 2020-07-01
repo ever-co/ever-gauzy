af9c74fcbe8b9cbc5bc73c52551cb719fa5b395d
AndrewDavydenko <drun2098@gmail.com>
2020-07-01 11:46:32 +0300

    feat: calling popup with modal[ref:#1198]

--- apps/gauzy/src/app/@shared/sidebar/delete-base/delete-base.component.ts
+++ apps/gauzy/src/app/@shared/sidebar/delete-base/delete-base.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, Input } from '@angular/core';
 import { NbDialogRef } from '@nebular/theme';
 import { Subject } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslationBaseComponent } from '../../language-base/translation-base.component';
+import { IHelpCenter } from '@gauzy/models';
 
 @Component({
 	selector: 'ga-delete-base',
@@ -11,6 +12,7 @@ import { TranslationBaseComponent } from '../../language-base/translation-base.c
 })
 export class DeleteBaseComponent extends TranslationBaseComponent
 	implements OnDestroy {
+	@Input() base: IHelpCenter;
 	private _ngDestroy$ = new Subject<void>();
 	constructor(
 		protected dialogRef: NbDialogRef<DeleteBaseComponent>,
