ff5f20f17dcd282b98642ba02ffb3fdd3a739114
sutralia <sutralian@gmail.com>
2020-07-05 23:36:06 +0700

    feat: submit local event to gauzy, added time tracker

--- apps/desktop/src/libs/timer.ts
+++ apps/desktop/src/libs/timer.ts
@@ -1,61 +1,160 @@
-import moment, { now } from 'moment';
+import moment from 'moment';
 import { Tray } from 'electron';
 import { TimerData } from '../local-data/timer';
+const Store = require('electron-store');
 export default class Timerhandler {
 	timeRecordMinute = 0;
 	timeRecordHours = 0;
+	timeRecordSecond = 0;
 	timeStart = null;
 	trayTime: Tray;
 	intevalTimer = null;
 	intervalUpdateTime = null;
 	lastTimer: any;
+	store = new Store();
+	configs: any;
 
-	async startTimer(win2, knex) {
+	async startTimer(win2, knex, win3) {
+		const ProjectInfo = this.store.get('project');
+		const auth = this.store.get('auth');
 		this.timeStart = moment();
 		this.lastTimer = await TimerData.createTimer(knex, {
 			day: moment().format('YYYY-MM-DD'),
 			updated_at: moment(),
 			created_at: moment(),
 			durations: 0,
-			projectid: null,
-			userId: null
+			projectid: ProjectInfo.projectId,
+			userId: ProjectInfo.taskId
 		});
+		win2.webContents.send('set_time_slot', {
+			token: auth.token,
+			employeeId: auth.employeeId,
+			duration: 0,
+			keyboard: 0,
+			mouse: 0,
+			overall: 0,
+			startedAt: this.timeStart.utc().format(),
+			timerId: this.lastTimer[0]
+		});
+		win2.webContents.send('set_time_sheet', {
+			token: auth.token,
+			employeeId: auth.employeeId,
+			duration: 0,
+			keyboard: 0,
+			mouse: 0,
+			overall: 0,
+			startedAt: this.timeStart.utc().format(),
+			timerId: this.lastTimer[0],
+			stoppedAt: this.timeStart.utc().format(),
+			projectId: ProjectInfo.projectId,
+			taskId: ProjectInfo.taskId
+		});
+
+		this.configs = this.store.get('configs');
 		this.intevalTimer = setInterval(() => {
 			const now = moment();
 			TimerData.updateDurationOfTimer(knex, {
 				id: this.lastTimer[0],
 				durations: now.diff(moment(this.timeStart), 'milliseconds')
 			});
-			console.log('run time');
-			win2.webContents.send('collect_data', {
-				start: this.timeStart.utc().format(),
-				end: moment().utc().format(),
-				timerId: this.lastTimer[0]
-			});
+
+			if (this.configs && this.configs.aw) {
+				win2.webContents.send('collect_data', {
+					start: this.timeStart.utc().format(),
+					end: moment().utc().format(),
+					tpURL: this.configs.awAPI,
+					tp: 'aw',
+					timerId: this.lastTimer[0]
+				});
+
+				win2.webContents.send('collect_afk', {
+					start: this.timeStart.utc().format(),
+					end: moment().utc().format(),
+					tpURL: this.configs.awAPI,
+					tp: 'aw',
+					timerId: this.lastTimer[0]
+				});
+			}
+
 			this.calculateTimeRecord();
+			win3.webContents.send('timer_push', {
+				second: this.timeRecordSecond,
+				minute: this.timeRecordMinute,
+				hours: this.timeRecordHours
+			});
+
+			this.getSetTimeSlot(win2, knex, auth);
 		}, 1000);
 	}
 
 	calculateTimeRecord() {
 		const now = moment();
+		this.timeRecordSecond = now.diff(moment(this.timeStart), 'seconds');
 		this.timeRecordHours = now.diff(moment(this.timeStart), 'hours');
 		this.timeRecordMinute = now.diff(moment(this.timeStart), 'minutes');
 	}
 
-	updateTime(menuItem, trayApp) {
+	updateTime(win2, knex) {
+		const auth = this.store.get('auth');
+		const projectInfo = this.store.get('project');
 		this.intervalUpdateTime = setInterval(() => {
-			this.trayTime = trayApp;
-			console.log(
-				`Time= ${this.timeRecordHours} : ${this.timeRecordMinute}`
-			);
-			const timeInMinute =
-				this.timeRecordMinute % 60 > 0
-					? this.timeRecordMinute % 60
-					: this.timeRecordMinute;
-			const timeMenu = menuItem.menu.getMenuItemById('0');
-			timeMenu.label = `Now tracking time - ${this.timeRecordHours}h ${timeInMinute}m`;
-			this.trayTime.setContextMenu(menuItem.menu);
-		}, 60 * 1000 + 1000);
+			this.getSetActivity(knex, win2, auth, projectInfo);
+		}, 60 * 1000);
+	}
+
+	getSetTimeSlot(win2, knex, auth) {
+		TimerData.getTimer(knex, this.lastTimer[0]).then((timerD) => {
+			TimerData.getAfk(knex, this.lastTimer[0]).then((afk) => {
+				win2.webContents.send('update_time_slot', {
+					duration: this.timeRecordSecond,
+					keyboard: afk && afk.length > 0 ? afk[0].durations : 0,
+					mouse: afk && afk.length > 0 ? afk[0].durations : 0,
+					overall: afk && afk.length > 0 ? afk[0].durations : 0,
+					timeSlotId: timerD[0].timeSlotId,
+					token: auth.token,
+					timerId: this.lastTimer[0]
+				});
+
+				win2.webContents.send('update_time_sheet', {
+					duration: this.timeRecordSecond,
+					keyboard: afk && afk.length > 0 ? afk[0].durations : 0,
+					mouse: afk && afk.length > 0 ? afk[0].durations : 0,
+					overall: afk && afk.length > 0 ? afk[0].durations : 0,
+					timeSheetId: timerD[0].timeSheetId,
+					token: auth.token,
+					timerId: this.lastTimer[0],
+					timeLogId: timerD[0].timeLogId,
+					stoppedAt: moment().utc().format()
+				});
+			});
+		});
+	}
+
+	getSetActivity(knex, win2, auth, projectInfo) {
+		TimerData.getWindowEvent(knex, this.lastTimer[0]).then((events) => {
+			console.log('result of acti', events);
+			events.map((item) => {
+				if (item.activityId) {
+					win2.webContents.send('update_to_activity', {
+						duration: Math.floor(item.durations),
+						activityId: item.activityId,
+						token: auth.token
+					});
+				} else {
+					win2.webContents.send('set_activity', {
+						employeeId: auth.employeeId,
+						projectId: projectInfo.projectId,
+						taskId: projectInfo.taskId,
+						title: JSON.parse(item.data).app,
+						date: moment().format('YYY-MM-DD'),
+						duration: Math.floor(item.durations),
+						type: 'app',
+						eventId: item.eventId,
+						token: auth.token
+					});
+				}
+			});
+		});
 	}
 
 	stopTime() {
