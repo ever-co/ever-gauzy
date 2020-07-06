0c00138f0a6e94d9c7a03c3d6cfa73e9cdd6c5a1
gitshas <shashi@bughuntres.com>
2020-07-06 21:10:51 +0530

    feat: removed on onboarding locator

--- apps/gauzy-e2e/src/support/Base/pageobjects/RegisterPageObject.ts
+++ apps/gauzy-e2e/src/support/Base/pageobjects/RegisterPageObject.ts
@@ -1,10 +1,9 @@
 export const RegisterPage = {
-	registerLinkCss: '[href="#/auth/register"]',
+	registerLinkCss: 'a.text-link',
 	fullNameFieldCss: '#input-name',
 	emailAddressFieldCss: '#input-email',
 	passwordFieldCss: '#input-password',
 	confirmPassFieldCss: '#input-re-password',
 	termAndConditionCheckboxCss: 'span.custom-checkbox',
-	registerButtonCss: 'nb-register > form > button',
-	organisationNameFieldCss: '[placeholder="Organization Name"]'
+	registerButtonCss: 'nb-register > form > button'
 };
