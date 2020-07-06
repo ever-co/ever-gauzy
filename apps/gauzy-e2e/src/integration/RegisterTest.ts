4ad3a32f805ceed1ccee5786f414342a1ef7334c
gitshas <shashi@bughuntres.com>
2020-07-06 21:14:44 +0530

    feat: added new scenario

--- apps/gauzy-e2e/src/integration/RegisterTest.ts
+++ apps/gauzy-e2e/src/integration/RegisterTest.ts
@@ -1,15 +1,22 @@
 import * as registerPage from '../support/Base/pages/Register.po';
 import * as loginPage from '../support/Base/pages/Login.po';
+import * as onboardingPage from '../support/Base/pages/Onboarding.po';
 import * as faker from 'faker';
+import { OnboardingPageData } from '../support/Base/pagedata/OnboardingPageData';
+import * as dashboradPage from '../support/Base/pages/Dashboard.po';
 
 let fullName = ' ';
 let email = ' ';
 let pass = ' ';
+let organizationName = ' ';
+let taxId = ' ';
 describe('Register Test', () => {
 	before(() => {
 		fullName = faker.name.findName();
 		email = faker.internet.email();
 		pass = faker.internet.password();
+		organizationName = faker.company.companyName();
+		taxId = faker.random.alphaNumeric();
 
 		cy.visit('/');
 		loginPage.verifyTitle();
@@ -33,6 +40,20 @@ describe('Register Test', () => {
 		loginPage.clearPasswordField();
 		loginPage.enterPassword(pass);
 		loginPage.clickLoginButton();
-		registerPage.verifyOrganisationNameField();
+		onboardingPage.verifyOrganisationNameField();
+	});
+
+	it('Should able to create first organiZation', () => {
+		onboardingPage.enterOrganizationName(organizationName);
+		onboardingPage.selectCurrency(OnboardingPageData.currency);
+		onboardingPage.enterOfficialName(organizationName);
+		onboardingPage.enterTaxId(taxId);
+		onboardingPage.clickOnNextButton();
+		onboardingPage.clickOnNextButton();
+		onboardingPage.clickOnNextButton();
+		onboardingPage.clickOnNextButton();
+		onboardingPage.verifyHeadingOnCompletePage();
+		onboardingPage.clickDashboardCard(0);
+		dashboradPage.verifyCreateButton();
 	});
 });
