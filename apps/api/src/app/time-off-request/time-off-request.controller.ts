f1922593518ddd7b6fde7b1da9b39faba8f469fc
AlexTasev <al.taseff@gmail.com>
2020-07-08 18:52:34 +0300

    feat: time off grid page implemented

--- apps/api/src/app/time-off-request/time-off-request.controller.ts
+++ apps/api/src/app/time-off-request/time-off-request.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, UseGuards, HttpStatus, Post, Body } from '@nestjs/common';
+import {
+	Controller,
+	UseGuards,
+	HttpStatus,
+	Post,
+	Body,
+	Get,
+	Query
+} from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CrudController } from '../core/crud/crud.controller';
 import { TimeOffRequest } from './time-off-request.entity';
@@ -6,6 +14,8 @@ import { AuthGuard } from '@nestjs/passport';
 import { TimeOffRequestService } from './time-off-request.service';
 import { PermissionGuard } from '../shared/guards/auth/permission.guard';
 import { TimeOffCreateInput as ITimeOffCreateInput } from '@gauzy/models';
+import { TimeOff as ITimeOff } from '@gauzy/models';
+import { IPagination } from '../core';
 
 @ApiTags('TimeOffRequest')
 @UseGuards(AuthGuard('jwt'))
@@ -13,6 +23,21 @@ import { TimeOffCreateInput as ITimeOffCreateInput } from '@gauzy/models';
 export class TimeOffRequestControler extends CrudController<TimeOffRequest> {
 	constructor(private readonly requestService: TimeOffRequestService) {
 		super(requestService);
+	}
+
+	@ApiOperation({ summary: 'Find all time off requests.' })
+	@UseGuards(PermissionGuard)
+	@Get()
+	async findAllProposals(
+		@Query('data') data: string
+	): Promise<IPagination<ITimeOff>> {
+		const { relations, findInput, filterDate } = JSON.parse(data);
+
+		return this.requestService.getAllTimeOffRequests(
+			relations,
+			findInput,
+			filterDate
+		);
 	}
 
 	@ApiOperation({ summary: 'Create new time off request / holiday record' })
--- apps/api/src/app/time-off-request/time-off-request.service.ts
+++ apps/api/src/app/time-off-request/time-off-request.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, FindManyOptions } from 'typeorm';
 import { TimeOffRequest } from './time-off-request.entity';
 import { CrudService } from '../core/crud/crud.service';
 import { TimeOffCreateInput as ITimeOffCreateInput } from '@gauzy/models';
@@ -19,5 +19,38 @@ export class TimeOffRequestService extends CrudService<TimeOffRequest> {
 		Object.assign(request, entity);
 
 		return await this.timeOfRequestRepository.save(request);
+	}
+
+	async getAllTimeOffRequests(relations, findInput?, filterDate?) {
+		const allRequests = await this.timeOfRequestRepository.find({ where: findInput['organziationId'], relations });
+		let items = [];
+		const total = await this.timeOfRequestRepository.count();
+
+		if (findInput['employeeId']) {
+			allRequests.forEach(request => {
+				request.employees.forEach(e => {
+					if (e.id === findInput['employeeId']) {
+						items.push(request)
+					}
+				})
+			})
+		} else {
+			items = allRequests;
+		}
+
+		if (filterDate) {
+			const dateObject = new Date(filterDate);
+
+			const month = dateObject.getMonth() + 1;
+			const year = dateObject.getFullYear();
+
+			items = [...items].filter((i) => {
+				const currentItemMonth = i.start.getMonth() + 1;
+				const currentItemYear = i.start.getFullYear();
+				return currentItemMonth === month && currentItemYear === year;
+			});
+		}
+
+		return { items, total };
 	}
 }
