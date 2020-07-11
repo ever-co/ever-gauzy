d19b5fb16559fb02963f86ae5ff35a64a736e8b6
Ruslan Konviser <evereq@gmail.com>
2020-07-11 22:06:24 +0300

    Update approvals.component.ts

--- apps/gauzy/src/app/pages/approvals/approvals.component.ts
+++ apps/gauzy/src/app/pages/approvals/approvals.component.ts
@@ -219,44 +219,7 @@ export class ApprovalsComponent extends TranslationBaseComponent
 			this.loadSmartTable();
 		});
 	}
-
-	// private initListApprovals() {
-	//   this.listApprovals = [
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'Business Trip'
-	//     },
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'Borrow Items'
-	//     },
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'General Approval'
-	//     },
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'Contract Approval'
-	//     },
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'Payment Application'
-	//     },
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'Car Rental Application'
-	//     },
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'Job Referral Award'
-	//     },
-	//     {
-	//       icon: 'paper-plane-outline',
-	//       title: 'Procurement'
-	//     }
-	//   ]
-	// }
-
+	
 	manageAppropvalPolicy() {
 		this.router.navigate(['/pages/organization/approval-policy']);
 	}
