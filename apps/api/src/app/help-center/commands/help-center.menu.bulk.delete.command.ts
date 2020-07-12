8e532cbfe6659bc7dac0b79290f87d187b81c9ce
Ruslan Konviser <evereq@gmail.com>
2020-07-12 12:08:18 +0300

    Revert "feat: update"
    This reverts commit eeafe7fec381b30eb206cd0906d66210c9d03f9b.

--- apps/api/src/app/help-center/commands/help-center.menu.bulk.delete.command.ts
+++
@@ -1,8 +0,0 @@
-// import { ICommand } from '@nestjs/cqrs';
-
-// export class CandidateInterviewersInterviewBulkDeleteCommand
-// 	implements ICommand {
-// 	static readonly type = '[CandidateInterviewers] Delete';
-
-// 	constructor(public readonly id: string) {}
-// }
--- libs/models/src/lib/location.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Location {
-	country?: string;
-	city?: string;
-	address?: string;
-	address2?: string;
-	postcode?: string;
-	regionCode?: string;
-}
