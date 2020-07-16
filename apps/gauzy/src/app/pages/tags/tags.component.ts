d603879c9e61efc4bd61f689d3d2770b3522b04f
Ajay Yadav <yadav7868@gmail.com>
2020-07-16 22:04:02 +0530

    feat: #1179 add card view in tags,users,org,goal settings comp

--- apps/gauzy/src/app/pages/tags/tags.component.ts
+++ apps/gauzy/src/app/pages/tags/tags.component.ts
@@ -3,20 +3,17 @@ import { TagsMutationComponent } from '../../@shared/tags/tags-mutation.componen
 import { NbDialogService, NbToastrService } from '@nebular/theme';
 import { LocalDataSource } from 'ng2-smart-table';
 import { TagsService } from '../../@core/services/tags.service';
-import { Tag, Organization } from '@gauzy/models';
+import { Tag, Organization, ComponentLayoutStyleEnum } from '@gauzy/models';
 import { DeleteConfirmationComponent } from '../../@shared/user/forms/delete-confirmation/delete-confirmation.component';
-import { first } from 'rxjs/operators';
+import { first, takeUntil } from 'rxjs/operators';
 import { FormGroup } from '@angular/forms';
 import { TagsColorComponent } from './tags-color/tags-color.component';
 import { TranslationBaseComponent } from '../../@shared/language-base/translation-base.component';
 import { TranslateService } from '@ngx-translate/core';
 import { Store } from '../../@core/services/store.service';
-import { Subscription } from 'rxjs';
-
-export interface SelectedTag {
-	data: Tag;
-	isSelected: false;
-}
+import { Subscription, Subject } from 'rxjs';
+import { ComponentEnum } from '../../@core/constants/layout.constants';
+import { RouterEvent, NavigationEnd, Router } from '@angular/router';
 
 @Component({
 	selector: 'ngx-tags',
@@ -27,19 +24,20 @@ export class TagsComponent extends TranslationBaseComponent
 	implements OnInit, OnDestroy {
 	settingsSmartTable: object;
 	loading = false;
-	selectedTag: SelectedTag;
+	selectedTag: Tag;
 	smartTableSource = new LocalDataSource();
 	tag: Tag;
 	form: FormGroup;
-	data: SelectedTag;
 	disableButton = true;
 	private selectedOrganization: Organization;
 	private subscribeTakingSelectedOrganziation: Subscription;
 	private allTags = [];
-
+	private _ngDestroy$ = new Subject<void>();
 	filterOptions = [{ property: 'all', displayName: 'All' }];
 	private filterOption: any;
-
+	viewComponentName: ComponentEnum;
+	dataLayoutStyle = ComponentLayoutStyleEnum.TABLE;
+	tagsData: Tag[];
 	@ViewChild('tagsTable') tagsTable;
 
 	constructor(
@@ -47,9 +45,11 @@ export class TagsComponent extends TranslationBaseComponent
 		private tagsService: TagsService,
 		readonly translateService: TranslateService,
 		private toastrService: NbToastrService,
+		private router: Router,
 		private store: Store
 	) {
 		super(translateService);
+		this.setView();
 	}
 
 	ngOnInit() {
@@ -60,18 +60,35 @@ export class TagsComponent extends TranslationBaseComponent
 			}
 		);
 
+		this.router.events
+			.pipe(takeUntil(this._ngDestroy$))
+			.subscribe((event: RouterEvent) => {
+				if (event instanceof NavigationEnd) {
+					this.setView();
+				}
+			});
+
 		this.loadSmartTable();
 		this._applyTranslationOnSmartTable();
 	}
 
-	async selectTag(data) {
-		if (data.isSelected) {
-			this.tag = data.data;
-			this.disableButton = false;
+	setView() {
+		this.viewComponentName = ComponentEnum.TAGS;
+		this.store
+			.componentLayout$(this.viewComponentName)
+			.pipe(takeUntil(this._ngDestroy$))
+			.subscribe((componentLayout) => {
+				this.dataLayoutStyle = componentLayout;
+			});
+	}
+
+	async selectTag({ isSelected, data }) {
+		const selectedTag = isSelected ? data : null;
+		if (this.tagsTable) {
 			this.tagsTable.grid.dataSet.willSelect = false;
-		} else {
-			this.disableButton = true;
 		}
+		this.disableButton = !isSelected;
+		this.tag = selectedTag;
 	}
 
 	async add() {
@@ -90,7 +107,13 @@ export class TagsComponent extends TranslationBaseComponent
 		this.loadSettings();
 	}
 
-	async delete() {
+	async delete(selectedItem?: Tag) {
+		if (selectedItem) {
+			this.selectTag({
+				isSelected: true,
+				data: selectedItem
+			});
+		}
 		const result = await this.dialogService
 			.open(DeleteConfirmationComponent)
 			.onClose.pipe(first())
@@ -106,7 +129,13 @@ export class TagsComponent extends TranslationBaseComponent
 		}
 		this.disableButton = true;
 	}
-	async edit() {
+	async edit(selectedItem?: Tag) {
+		if (selectedItem) {
+			this.selectTag({
+				isSelected: true,
+				data: selectedItem
+			});
+		}
 		const dialog = this.dialogService.open(TagsMutationComponent, {
 			context: {
 				tag: this.tag
@@ -171,11 +200,14 @@ export class TagsComponent extends TranslationBaseComponent
 				const result = await this.tagsService.getTagUsageCount(orgId);
 				this.allTags = result.concat(tagsByTenantLevel);
 				this._generateUniqueTags(this.allTags);
+				this.tagsData = this.allTags;
 				this.smartTableSource.load(this.allTags);
 			} else if (tagsByTenantLevel.length) {
 				this._generateUniqueTags(this.allTags);
+				this.tagsData = tagsByTenantLevel;
 				this.smartTableSource.load(tagsByTenantLevel);
 			} else {
+				this.tagsData = [];
 				this.smartTableSource.load([]);
 			}
 		}
