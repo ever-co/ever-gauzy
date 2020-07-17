22ebf8df445d21e4e793b32d4a795349b08a68cf
Ajay Yadav <yadav7868@gmail.com>
2020-07-17 17:01:15 +0530

    feat: #1179 invoice and estimates ui fixed

---
+++ apps/gauzy/src/app/@shared/card-grid-invoice/card-grid-custom-invoice.component.ts
@@ -0,0 +1,53 @@
+import {
+	Component,
+	ComponentFactoryResolver,
+	Input,
+	OnDestroy,
+	OnInit,
+	ViewChild,
+	ViewContainerRef
+} from '@angular/core';
+import { ComponentLayoutStyleEnum } from '@gauzy/models';
+
+@Component({
+	selector: 'ga-custom-component-invoice',
+	template: ` <ng-template #dynamicTarget></ng-template> `
+})
+export class CustomViewInvoiceComponent implements OnInit, OnDestroy {
+	@Input() renderComponent: any;
+	@Input() value: any;
+	@Input() rowData: any;
+	customComponent: any;
+	@ViewChild('dynamicTarget', { read: ViewContainerRef, static: true })
+	dynamicTarget: any;
+
+	constructor(private resolver: ComponentFactoryResolver) {}
+
+	ngOnInit() {
+		this.createCustomComponent();
+		this.patchInstance();
+	}
+
+	ngOnDestroy() {
+		if (this.customComponent) {
+			this.customComponent.destroy();
+		}
+	}
+
+	protected createCustomComponent() {
+		const componentFactory = this.resolver.resolveComponentFactory(
+			this.renderComponent
+		);
+		this.customComponent = this.dynamicTarget.createComponent(
+			componentFactory
+		);
+	}
+
+	protected patchInstance() {
+		Object.assign(this.customComponent.instance, {
+			value: this.value,
+			rowData: this.rowData,
+			layout: ComponentLayoutStyleEnum.CARDS_GRID
+		});
+	}
+}
