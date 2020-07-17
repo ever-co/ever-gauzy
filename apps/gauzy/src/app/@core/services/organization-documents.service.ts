df69ab6638a39960211b83d086a071a4f6402ae1
AlexTasev <al.taseff@gmail.com>
2020-07-17 16:41:27 +0300

    feat: document upload functionality implemented on front-end

---
+++ apps/gauzy/src/app/@core/services/organization-documents.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { OrganizationDocument } from '@gauzy/models';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class OrganizationDocumentsService {
+    constructor(private http: HttpClient) {}
+
+    create(newDocument: OrganizationDocument): Observable<any> {
+        return this.http.post('url', newDocument)
+    }
+
+    delete(id: string): Observable<any> {
+        return this.http.delete(`url/${id}`)
+    }
+}
