bda45d29230ee8dc35502d15ba93b3a3cd66a549
Ruslan Konviser <evereq@gmail.com>
2020-07-27 13:19:35 +0300

    Update organization-contact.seed.ts

--- apps/api/src/app/organization-contact/organization-contact.seed.ts
+++ apps/api/src/app/organization-contact/organization-contact.seed.ts
@@ -29,31 +29,41 @@ export const createDefaultOrganizationContact = async (
       });
     for (const org of organizations) {
       const employees = org.employees;
+
       const tags = org.tags;
+
       for (let i = 0; i < faker.random.number({ min: 1, max: 2 }); i++) {
         const orgContact = new OrganizationContact();
+
         const contact = faker.random.arrayElement(contacts);
+
         orgContact.contact = contact;
+
         orgContact.organizationId = org.id;
-        // orgContact.contactOrganization = org;
+
         orgContact.contactType = faker.random.arrayElement(
           contactTypes
         );
+
         orgContact.emailAddresses = [
           faker.internet.email(contact.firstName, contact.lastName)
         ];
+
         orgContact.inviteStatus = faker.random.arrayElement(
           contactInvitationTypes
         );
+
         orgContact.members = _.chain(employees)
           .shuffle()
           .take(faker.random.number({ min: 1, max: 3 }))
           .values()
           .value();
+
         orgContact.name = contact.name;
         orgContact.phones = [faker.phone.phoneNumber()];
         orgContact.primaryEmail = orgContact.emailAddresses[0];
         orgContact.primaryPhone = orgContact.phones[0];
+
         orgContact.tags = _.chain(tags)
           .shuffle()
           .take(faker.random.number({ min: 1, max: 2 }))
@@ -77,31 +87,41 @@ export const createRandomOrganizationContact = async (
 ): Promise<OrganizationContact[]> => {
 
   let organizationContacts: OrganizationContact[] = [];
+
   const contactTypes = Object.values(ContactType);
+
   const contactInvitationTypes = Object.values(ContactOrganizationInviteStatus);
+
   const contacts = await connection.manager.find(Contact);
 
   for (const tenant of tenants) {
     let organizations = tenantOrganizationsMap.get(tenant);
+
     let employees = tenantEmployeeMap.get(tenant);
+
     for (const org of organizations) {
       const tags = await connection.manager.find(Tag, { where: [{ organization: org }] });
+
       for (let i = 0; i < noOfContactsPerOrganization; i++) {
         let orgContact = new OrganizationContact();
+
         orgContact.contact = faker.random.arrayElement(contacts);
         orgContact.organizationId = org.id;
         orgContact.contactType = faker.random.arrayElement(contactTypes);
         orgContact.emailAddresses = [faker.internet.email(orgContact.contact.firstName, orgContact.contact.lastName)];
         orgContact.inviteStatus = faker.random.arrayElement(contactInvitationTypes);
+
         orgContact.members = _.chain(employees)
           .shuffle()
           .take(faker.random.number({ min: 1, max: 3 }))
           .values()
           .value();
+
         orgContact.name = orgContact.contact.name;
         orgContact.phones = [faker.phone.phoneNumber()];
         orgContact.primaryEmail = orgContact.emailAddresses[0];
         orgContact.primaryPhone = orgContact.phones[0];
+
         orgContact.tags = _.chain(tags)
           .shuffle()
           .take(faker.random.number({ min: 1, max: 2 }))
