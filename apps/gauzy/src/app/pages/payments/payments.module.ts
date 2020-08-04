d189f95e7366a87a26345b26b6faffdde9274913
PavelDenchev <pav.denchev@gmail.com>
2020-08-04 17:30:25 +0300

    feat: deepscan fix

--- apps/gauzy/src/app/pages/payments/payments.module.ts
+++ apps/gauzy/src/app/pages/payments/payments.module.ts
@@ -7,20 +7,7 @@ import {
 	NbCardModule,
 	NbIconModule,
 	NbButtonModule,
-	NbSpinnerModule,
-	NbBadgeModule,
-	NbCheckboxModule,
-	NbDialogModule,
-	NbInputModule,
-	NbRouteTabsetModule,
-	NbSelectModule,
-	NbTooltipModule,
-	NbRadioModule,
-	NbDatepickerModule,
-	NbToggleModule,
-	NbContextMenuModule,
-	NbMenuModule,
-	NbTabsetModule
+	NbDialogModule
 } from '@nebular/theme';
 import { PaymentsRoutingModule } from './payments-routing.module';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
@@ -28,13 +15,6 @@ import { PaymentService } from '../../@core/services/payment.service';
 import { OrganizationContactService } from '../../@core/services/organization-contact.service';
 import { CardGridModule } from '../../@shared/card-grid/card-grid.module';
 import { ThemeModule } from '../../@theme/theme.module';
-import { TableComponentsModule } from '../../@shared/table-components/table-components.module';
-import { TagsColorInputModule } from '../../@shared/tags/tags-color-input/tags-color-input.module';
-import { InvoicesRoutingModule } from '../invoices/invoices-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { EmployeeMultiSelectModule } from '../../@shared/employee/employee-multi-select/employee-multi-select.module';
-import { UserFormsModule } from '../../@shared/user/forms/user-forms.module';
-import { NgSelectModule } from '@ng-select/ng-select';
 import { InvoicesService } from '../../@core/services/invoices.service';
 
 export function HttpLoaderFactory(http: HttpClient) {
