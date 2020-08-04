d33fe4c752b6a29a2362ee9e0514a217370c8529
PavelDenchev <pav.denchev@gmail.com>
2020-08-04 17:20:26 +0300

    feat: added payments without invoices

--- apps/gauzy/src/app/pages/payments/payments.module.ts
+++ apps/gauzy/src/app/pages/payments/payments.module.ts
@@ -3,13 +3,39 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgModule } from '@angular/core';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { PaymentsComponent } from './payments.component';
-import { NbCardModule } from '@nebular/theme';
+import {
+	NbCardModule,
+	NbIconModule,
+	NbButtonModule,
+	NbSpinnerModule,
+	NbBadgeModule,
+	NbCheckboxModule,
+	NbDialogModule,
+	NbInputModule,
+	NbRouteTabsetModule,
+	NbSelectModule,
+	NbTooltipModule,
+	NbRadioModule,
+	NbDatepickerModule,
+	NbToggleModule,
+	NbContextMenuModule,
+	NbMenuModule,
+	NbTabsetModule
+} from '@nebular/theme';
 import { PaymentsRoutingModule } from './payments-routing.module';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { PaymentService } from '../../@core/services/payment.service';
 import { OrganizationContactService } from '../../@core/services/organization-contact.service';
 import { CardGridModule } from '../../@shared/card-grid/card-grid.module';
 import { ThemeModule } from '../../@theme/theme.module';
+import { TableComponentsModule } from '../../@shared/table-components/table-components.module';
+import { TagsColorInputModule } from '../../@shared/tags/tags-color-input/tags-color-input.module';
+import { InvoicesRoutingModule } from '../invoices/invoices-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeMultiSelectModule } from '../../@shared/employee/employee-multi-select/employee-multi-select.module';
+import { UserFormsModule } from '../../@shared/user/forms/user-forms.module';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { InvoicesService } from '../../@core/services/invoices.service';
 
 export function HttpLoaderFactory(http: HttpClient) {
 	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -28,9 +54,12 @@ export function HttpLoaderFactory(http: HttpClient) {
 		PaymentsRoutingModule,
 		Ng2SmartTableModule,
 		CardGridModule,
-		ThemeModule
+		ThemeModule,
+		NbIconModule,
+		NbButtonModule,
+		NbDialogModule.forChild()
 	],
-	providers: [PaymentService, OrganizationContactService],
+	providers: [PaymentService, OrganizationContactService, InvoicesService],
 	entryComponents: [PaymentsComponent],
 	declarations: [PaymentsComponent]
 })
