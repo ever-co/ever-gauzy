7362a0e61697a11bc43dc3d93e02f5f5aecc9ddf
dejan obradovikj <deko898@hotmail.com>
2020-08-07 00:04:58 +0200

    feat: created public page for employee

---
+++ apps/gauzy/src/app/share/employee/employee.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslationBaseComponent } from '../../@shared/language-base/translation-base.component';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PermissionsEnum } from '@gauzy/models';
+import { Store } from '../../@core/services/store.service';
+
+@Component({
+	selector: 'ngx-employee',
+	templateUrl: './employee.component.html',
+	styleUrls: ['./employee.component.scss']
+})
+export class EmployeeComponent extends TranslationBaseComponent
+	implements OnInit {
+	hasEditPermission$: Observable<boolean>;
+	imageUrl: string;
+	imageUpdateButton: boolean;
+	employee$: Observable<any>;
+	hoverState: boolean;
+
+	constructor(
+		private activatedRoute: ActivatedRoute,
+		readonly translateService: TranslateService,
+		private store: Store
+	) {
+		super(translateService);
+	}
+
+	ngOnInit(): void {
+		this.employee$ = this.activatedRoute.data.pipe(
+			map(({ employee }) => employee)
+		);
+
+		this.hasEditPermission$ = this.store.userRolePermissions$.pipe(
+			map(() =>
+				this.store.hasPermission(PermissionsEnum.PUBLIC_PAGE_EDIT)
+			)
+		);
+	}
+
+	updateImageUrl(url: string) {
+		this.imageUrl = url;
+		this.imageUpdateButton = true;
+	}
+
+	async saveImage() {}
+
+	handleImageUploadError(event: any) {}
+}
