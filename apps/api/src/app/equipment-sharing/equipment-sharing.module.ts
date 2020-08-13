5616566ce60721c91b98a3a1b865931949a74789
duongduckien <ddk0705@gmail.com>
2020-08-13 22:47:06 +0700

    fix: #2066 fix deepScan

--- apps/api/src/app/equipment-sharing/equipment-sharing.module.ts
+++ apps/api/src/app/equipment-sharing/equipment-sharing.module.ts
@@ -5,12 +5,6 @@ import { EquipmentSharingController } from './equipment-sharing.controller';
 import { EquipmentSharingService } from './equipment-sharing.service';
 import { RequestApproval } from '../request-approval/request-approval.entity';
 import { CqrsModule } from '@nestjs/cqrs';
-import { Employee } from '../employee/employee.entity';
-import { Organization } from '../organization/organization.entity';
-import { OrganizationTeam } from '../organization-team/organization-team.entity';
-import { Role } from '../role/role.entity';
-import { User } from '../user/user.entity';
-import { TimeOffRequest } from '../time-off-request/time-off-request.entity';
 import { CommandHandlers } from './commands/handlers';
 
 @Module({
