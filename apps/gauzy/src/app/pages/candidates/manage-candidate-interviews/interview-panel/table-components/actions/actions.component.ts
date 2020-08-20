f100380dcd75282e1f4eeff6a4fab29ab5086ae8
prysiazhna <olha.prysiazhna@gmail.com>
2020-08-20 16:48:11 +0300

    fix: fix interview bug, add toastr[ref:#868]

--- apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/interview-panel/table-components/actions/actions.component.ts
+++ apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/interview-panel/table-components/actions/actions.component.ts
@@ -10,6 +10,9 @@ import { ICandidateInterview } from '@gauzy/models';
 import { Subject } from 'rxjs';
 import { CandidatesService } from 'apps/gauzy/src/app/@core/services/candidates.service';
 import { takeUntil } from 'rxjs/operators';
+import { NbToastrService } from '@nebular/theme';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslationBaseComponent } from 'apps/gauzy/src/app/@shared/language-base/translation-base.component';
 
 @Component({
 	selector: 'ga-interview-actions',
@@ -30,7 +33,8 @@ import { takeUntil } from 'rxjs/operators';
 						enabled: !isPastInterview(rowData),
 						disabled: isPastInterview(rowData)
 					}"
-				></nb-icon>
+				>
+				</nb-icon>
 				<nb-icon
 					(click)="removeInterview()"
 					icon="close"
@@ -60,7 +64,6 @@ import { takeUntil } from 'rxjs/operators';
 				cursor: pointer !important;
 			}
 			.enabled {
-				pointer-events: visible;
 				color: #222b45 !important;
 				&:focus {
 					transform: translateY(2px);
@@ -68,17 +71,23 @@ import { takeUntil } from 'rxjs/operators';
 			}
 			.disabled {
 				color: rgba(143, 155, 179, 0.48) !important;
-				pointer-events: none;
 			}
 		`
 	]
 })
-export class InterviewActionsTableComponent implements OnInit, OnDestroy {
+export class InterviewActionsTableComponent extends TranslationBaseComponent
+	implements OnInit, OnDestroy {
 	@Input() rowData: any;
 	@Output() updateResult = new EventEmitter<any>();
 	private _ngDestroy$ = new Subject<void>();
 
-	constructor(private candidatesService: CandidatesService) {}
+	constructor(
+		private candidatesService: CandidatesService,
+		readonly translateService: TranslateService,
+		private toastrService: NbToastrService
+	) {
+		super(translateService);
+	}
 
 	ngOnInit() {
 		this.candidatesService
@@ -105,17 +114,31 @@ export class InterviewActionsTableComponent implements OnInit, OnDestroy {
 		}
 	}
 	removeInterview() {
-		const params = {
-			isEdit: false,
-			data: this.rowData
-		};
-		this.updateResult.emit(params);
+		if (this.isPastInterview(this.rowData)) {
+			this.toastrService.warning(
+				this.getTranslation('TOASTR.TITLE.WARNING'),
+				this.getTranslation('TOASTR.MESSAGE.DELETE_PAST_INTERVIEW')
+			);
+		} else {
+			const params = {
+				isEdit: false,
+				data: this.rowData
+			};
+			this.updateResult.emit(params);
+		}
 	}
 	editInterview() {
-		const params = {
-			isEdit: true,
-			data: this.rowData
-		};
-		this.updateResult.emit(params);
+		if (this.isPastInterview(this.rowData)) {
+			this.toastrService.warning(
+				this.getTranslation('TOASTR.TITLE.WARNING'),
+				this.getTranslation('TOASTR.MESSAGE.EDIT_PAST_INTERVIEW')
+			);
+		} else {
+			const params = {
+				isEdit: true,
+				data: this.rowData
+			};
+			this.updateResult.emit(params);
+		}
 	}
 }
