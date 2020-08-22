10bf76c8e29458a9d8fe5851178c4dbde3442020
abinandh15 <abinandh15@gmail.com>
2020-08-23 00:01:28 +0530

    fix: integration entity setting tied entity seed

--- apps/api/src/app/integration-entity-setting-tied-entity/integration-entity-setting-tied-entitiy.seed.ts
+++ apps/api/src/app/integration-entity-setting-tied-entity/integration-entity-setting-tied-entitiy.seed.ts
@@ -6,39 +6,47 @@ import { IntegrationEntitySetting } from '../integration-entity-setting/integrat
 import { IntegrationEntitySettingTiedEntity } from './integration-entity-setting-tied-entitiy.entity';
 
 export const createRandomIntegrationEntitySettingTiedEntity = async (
-  connection: Connection,
-  tenants: Tenant[]
+	connection: Connection,
+	tenants: Tenant[]
 ): Promise<IntegrationEntitySettingTiedEntity[]> => {
-  if (!tenants) {
-    console.warn(
-      'Warning: tenants not found, Integration Entity Setting  will not be created'
-    );
-    return;
-  }
+	if (!tenants) {
+		console.warn(
+			'Warning: tenants not found, Integration Entity Setting  will not be created'
+		);
+		return;
+	}
 
-  const integrationEntitySettings: IntegrationEntitySettingTiedEntity[] = [];
+	const randomIntegrationEntitySettingsTiedEntity: IntegrationEntitySettingTiedEntity[] = [];
 
-  for (const tenant of tenants) {
-    const integrationTenants = await connection.manager.find(IntegrationTenant, {
-      where: [{ tenant: tenant }]
-    });
-    for (const integrationTenant of integrationTenants) {
-      const integrationEntitySettings = await connection.manager.find(IntegrationEntitySetting, {
-        where: [{ integration: integrationTenant }]
-      });
-      for (const integrationEntitySetting of integrationEntitySettings) {
+	for (const tenant of tenants) {
+		const integrationTenants = await connection.manager.find(
+			IntegrationTenant,
+			{
+				where: [{ tenant: tenant }]
+			}
+		);
+		for (const integrationTenant of integrationTenants) {
+			const integrationEntitySettings = await connection.manager.find(
+				IntegrationEntitySetting,
+				{
+					where: [{ integration: integrationTenant }]
+				}
+			);
+			for (const integrationEntitySetting of integrationEntitySettings) {
+				const integrationEntitySettingTiedEntity = new IntegrationEntitySettingTiedEntity();
 
-        const integrationEntitySetting = new IntegrationEntitySettingTiedEntity();
+				integrationEntitySettingTiedEntity.integrationEntitySetting = integrationEntitySetting;
+				integrationEntitySettingTiedEntity.sync = faker.random.boolean();
+				//todo: need to understand real values here
+				integrationEntitySettingTiedEntity.entity =
+					'entity-' + faker.random.number(40);
 
-        integrationEntitySetting.integrationEntitySetting = integrationEntitySetting;
-        integrationEntitySetting.sync = faker.random.boolean();
-        //todo: need to understand real values here
-        integrationEntitySetting.entity = 'entity-' + faker.random.number(40);
+				randomIntegrationEntitySettingsTiedEntity.push(
+					integrationEntitySettingTiedEntity
+				);
+			}
+		}
+	}
 
-        integrationEntitySettings.push(integrationEntitySetting);
-      }
-    }
-  }
-
-  await connection.manager.save(integrationEntitySettings);
+	await connection.manager.save(randomIntegrationEntitySettingsTiedEntity);
 };
