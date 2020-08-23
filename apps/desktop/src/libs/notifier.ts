0ed82851165669af4e5ce5c53b30dc432507496d
sutralia <sutralian@gmail.com>
2020-08-23 17:41:33 +0700

    feat: gauzy desktop icon

--- apps/desktop/src/libs/notifier.ts
+++ apps/desktop/src/libs/notifier.ts
@@ -2,13 +2,7 @@ import { Notification, nativeImage } from 'electron';
 import * as path from 'path';
 export default class NotificationDesktop {
 	startTimeNotification(isStart) {
-		const iconPath = path.join(
-			__dirname,
-			'..',
-			'assets',
-			'icons',
-			'icon_16x16.png'
-		);
+		const iconPath = path.join(__dirname, '..', 'icons', 'icon.png');
 		console.log(iconPath);
 		const iconNativePath = nativeImage.createFromPath(iconPath);
 		iconNativePath.resize({ width: 16, height: 16 });
