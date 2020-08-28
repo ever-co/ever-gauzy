7aa06f18697f9289a7bde050fab5c0e95d93dddb
Chetan Khandla <ckhandla94@gmail.com>
2020-08-28 22:01:30 +0530

    fix: view timelog issue and open screenshot issue

--- apps/gauzy/src/app/@shared/gallery/gallery.module.ts
+++ apps/gauzy/src/app/@shared/gallery/gallery.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NbButtonModule, NbDialogModule, NbIconModule } from '@nebular/theme';
 import { GalleryComponent } from './gallery.component';
 import { GalleryDirective } from './gallery.directive';
+import { GalleryContainerDirective } from './gallery-container.directive';
 
 @NgModule({
 	imports: [CommonModule, NbButtonModule, NbDialogModule, NbIconModule],
-	exports: [GalleryDirective, GalleryComponent],
-	declarations: [GalleryDirective, GalleryComponent]
+	exports: [GalleryDirective, GalleryContainerDirective, GalleryComponent],
+	declarations: [
+		GalleryDirective,
+		GalleryContainerDirective,
+		GalleryComponent
+	]
 })
 export class GalleryModule {}
