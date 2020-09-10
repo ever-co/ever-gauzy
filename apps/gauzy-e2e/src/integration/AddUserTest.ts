53a53ec8ff887fc249ebc3def339b62c47d1c220
YavorGrancharov <bearhold@gmail.com>
2020-09-10 09:48:46 +0300

    feat: add new user e2e tests

---
+++ apps/gauzy-e2e/src/integration/AddUserTest.ts
@@ -0,0 +1,56 @@
+import * as loginPage from '../support/Base/pages/Login.po';
+import { LoginPageData } from '../support/Base/pagedata/LoginPageData';
+import * as addUserPage from '../support/Base/pages/AddUser.po';
+import * as faker from 'faker';
+import { AddUserPageData } from '../support/Base/pagedata/AddUserPageData';
+import * as dashboradPage from '../support/Base/pages/Dashboard.po';
+
+let firstName = ' ';
+let lastName = ' ';
+let username = ' ';
+let email = ' ';
+let password = ' ';
+let imgUrl = ' ';
+
+describe('Add user test', () => {
+	before(() => {
+		firstName = faker.name.firstName();
+		lastName = faker.name.lastName();
+		username = faker.internet.userName();
+		email = faker.internet.email();
+		password = faker.internet.password();
+		imgUrl = faker.image.avatar();
+
+		cy.visit('/');
+		loginPage.verifyTitle();
+		loginPage.verifyLoginText();
+		loginPage.clearEmailField();
+		loginPage.enterEmail(LoginPageData.email);
+		loginPage.clearPasswordField();
+		loginPage.enterPassword(LoginPageData.password);
+		loginPage.clickLoginButton();
+		dashboradPage.verifyCreateButton();
+	});
+
+	it('Should be able to add new user', () => {
+		cy.visit('/#/pages/users');
+		addUserPage.addUserButtonVisible();
+		addUserPage.clickAddUserButton();
+		addUserPage.firstNameInputVisible();
+		addUserPage.enterFirstNameData(firstName);
+		addUserPage.lastNameInputVisible();
+		addUserPage.enterLastNameData(lastName);
+		addUserPage.usernameInputVisible();
+		addUserPage.enterUsernameData(username);
+		addUserPage.emailInputVisible();
+		addUserPage.enterEmailData(email);
+		addUserPage.selectUserRoleVisible();
+		addUserPage.selectUserRoleData(AddUserPageData.role);
+		addUserPage.passwordInputVisible();
+		addUserPage.enterPasswordInputData(password);
+		addUserPage.imageInputVisible();
+		addUserPage.enterImageDataUrl(imgUrl);
+		addUserPage.confirmAddButtonVisible();
+		addUserPage.clickConfirmAddButton();
+	});
+});
