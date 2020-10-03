35d6245fb8b4ad529798b2ccc3f5bb93358090aa
Chetan Khandla <ckhandla94@gmail.com>
2020-10-03 10:50:12 +0530

    fix: deepscan

--- apps/gauzy/src/app/@shared/timesheet/timesheet-filter.service.ts
+++ apps/gauzy/src/app/@shared/timesheet/timesheet-filter.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Query, resetStores, Store, StoreConfig } from '@datorama/akita';
+import { Query, Store, StoreConfig } from '@datorama/akita';
 import { ITimeLogFilters } from '@gauzy/models';
 
 export function createInitialTimesheetFilterState(): ITimeLogFilters {
