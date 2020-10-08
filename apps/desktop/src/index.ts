b9840330201bd82776d0dd56f8fed3a81515f058
sutralia <sutralian@gmail.com>
2020-10-09 00:56:48 +0700

    fix: hide gauzy window

--- apps/desktop/src/index.ts
+++ apps/desktop/src/index.ts
@@ -204,14 +204,14 @@ try {
 	app.on('activate', () => {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
-		if (gauzyWindow === null && !onWaitingServer) {
-			createGauzyWindow(gauzyWindow, serve);
+		if (gauzyWindow) {
+			gauzyWindow.show();
 		}
 	});
 
 	app.on('before-quit', (e) => {
 		const appSetting = LocalStore.getStore('appSetting');
-		if (appSetting.timerStarted) {
+		if (appSetting && appSetting.timerStarted) {
 			e.preventDefault();
 			setTimeout(() => {
 				willquit = true;
