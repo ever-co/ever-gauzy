39c6cfb21b7b3e39ae5cf83435a83c81a477d1b6
Ruslan Konviser <evereq@gmail.com>
2020-10-21 20:12:33 +0300

    feat: make basic use of Gauzy AI APIs

--- apps/api/src/app/employee-job/employee-job.service.ts
+++ apps/api/src/app/employee-job/employee-job.service.ts
@@ -2,6 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { IPagination } from '../core';
 import { EmployeeJobPost } from './employee-job.entity';
 import { getRandomEmployeeJobPosts } from './employee-job.seed';
+import fetch from 'cross-fetch';
+
+import {
+	EmployeeJobPostsDocument,
+	EmployeeJobPostsQuery
+} from '@gauzy/integration-ai';
+import {
+	ApolloClient,
+	HttpLink,
+	InMemoryCache,
+	gql
+} from '@apollo/client/core';
 
 @Injectable()
 export class EmployeeJobPostService {
@@ -9,10 +21,57 @@ export class EmployeeJobPostService {
 		where,
 		relations
 	}): Promise<IPagination<EmployeeJobPost>> {
-		// TODO: we should check here if we can connect to Gauzy AI.
-		// If we can't connect to Gauzy AI, we should use Random Seeds (only if it's development environment, not in production)
+		let jobs;
 
-		const jobs = await getRandomEmployeeJobPosts();
+		// TODO: we should check here if we have Gauzy AI endpoint in the tenant settings.
+		// If we don't have, we should use Random Seeds. Only if it's development environment, not in production!!!
+		// In production it should just return null if Gauzy AI not configured
+
+		// TODO: replace true with check of .env setting for GauzyAIGraphQLEndpoint
+		if (true) {
+			// TODO: move all below into @gauzy/integration-ai lib as separate service there.
+			// But only that part related to GraphQL call of Gauzy AI API, not other logic.
+
+			const client = new ApolloClient({
+				typeDefs: EmployeeJobPostsDocument,
+				link: new HttpLink({
+					uri: 'http://localhost:3005/graphql', // TODO: use here endpoint from .env
+					fetch
+				}),
+				cache: new InMemoryCache()
+			});
+
+			// TODO: use Query saved in SDK, not hard-code it here. Note: we may add much more fields to that query as we need more info!
+
+			const result = await client.query<EmployeeJobPostsQuery>({
+				query: gql`
+					query employeeJobPosts {
+						employeeJobPosts {
+							edges {
+								node {
+									id
+									employee {
+										name
+									}
+									jobPost {
+										title
+									}
+								}
+							}
+						}
+					}
+				`
+			});
+
+			jobs = result.data.employeeJobPosts.edges;
+
+			// TODO: this jobs contains tons of GraphQL related fields. We should convert all that into Gauzy EmployeeJobPost and JobPost entities!
+			// I.e. here should be mapping, we don't want to return result of GraphQL query here as is!
+		} else {
+			jobs = await getRandomEmployeeJobPosts();
+		}
+
+		console.log(JSON.stringify(jobs));
 
 		return {
 			items: jobs,
