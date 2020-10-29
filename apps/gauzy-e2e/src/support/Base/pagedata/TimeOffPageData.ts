f00506d1f7fa6937dd66c34d31505c5a44b1ca0d
Yavor Grancharov <bearhold@gmail.com>
2020-10-29 17:11:08 +0200

    feat: time off e2e tests

--- apps/gauzy-e2e/src/support/Base/pagedata/TimeOffPageData.ts
+++ apps/gauzy-e2e/src/support/Base/pagedata/TimeOffPageData.ts
@@ -1,3 +1,5 @@
 export const TimeOffPageData = {
-	timeOffPolicy: 'Default Policy'
+	defaultPolicy: 'Default Policy',
+	defaultDescription: 'Going to the sea',
+	defaultHoliday: 'Нова Година'
 };
--- apps/gauzy-e2e/src/support/Base/pageobjects/TimeOffPageObject.ts
+++ apps/gauzy-e2e/src/support/Base/pageobjects/TimeOffPageObject.ts
@@ -1,7 +1,23 @@
 export const TimeOffPage = {
-	requestButtonCss: 'div.mb-3 > div.col-3 > button[status="primary"]',
-	employeeSelectorDropdownCss: 'ga-employee-selector.employees',
-	checkEmployeeMultyselectCss: '.ng-dropdown-panel .ng-option',
-	saveSelectedUsersButtonCss: 'div.form-group > button[status="success"]',
-	cancelAddUsersButtonCss: 'div.form-group > button[status="danger"]'
+	requestButtonCss: 'div.col-3 > button[status="primary"]',
+	employeeDropdownCss:
+		'ga-employee-selector.employees > ng-select[role="listbox"]',
+	employeeDropdownOptionCss: 'div.ng-option[role="option"]',
+	timeOffPolicyDropdownCss: '[formControlName="policy"]',
+	timeOffPolicyDropdownOptionCss: '.option-list nb-option',
+	startDateInputCss: '[formControlName="start"]',
+	endDateInputCss: '[formControlName="end"]',
+	descriptionInputCss: '[formControlName="description"]',
+	saveRequestButtonCss:
+		'nb-card-footer.text-right > button[status="success"]',
+	addHolidayButtonCss: 'div.col-3 > button[status="info"]',
+	selectHolidayDropdownOptionCss: '.option-list nb-option',
+	selectEmloyeeCss: 'button[class="select-button placeholder"]',
+	selectEmployeeDropdownOptionCss: '.option-list nb-option',
+	startHolidayDateCss: '[formControlName="start"]',
+	endHolidayDateCss: '[formControlName="end"]',
+	saveHolidayButtonCss: 'nb-card-footer > button[status="success"]',
+	selectTableRowCss: 'table > tbody > tr.ng2-smart-row',
+	deleteTimeOfRequestCss: 'div.actions-container > button[status="danger"]',
+	confirmDeleteTimeOfButtonCss: 'nb-card-footer > button[status="danger"]'
 };
