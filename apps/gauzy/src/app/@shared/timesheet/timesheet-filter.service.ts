e783ef78bf28425a35fdc2b44c29b5da2dbb1e53
Rahul Rathore <rahulrathore576@gmail.com>
2020-10-30 17:49:17 +0530

    fix: timer on off and timesheets filters issue

--- apps/gauzy/src/app/@shared/timesheet/timesheet-filter.service.ts
+++ apps/gauzy/src/app/@shared/timesheet/timesheet-filter.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Query, Store, StoreConfig } from '@datorama/akita';
 import { ITimeLogFilters } from '@gauzy/models';
+import * as moment from 'moment';
+
+export const ActivityLevel = {
+	start: 0,
+	end: 100
+};
 
 export function initialTimesheetFilterState(): ITimeLogFilters {
 	return {
+		activityLevel: ActivityLevel,
 		employeeIds: [],
 		source: [],
 		logType: [],
 		projectIds: [],
-		startDate: new Date(),
-		endDate: new Date(),
-		date: new Date()
+		startDate: new Date(
+			moment().startOf('day').format('YYYY-MM-DD HH:mm:ss')
+		),
+		endDate: new Date(moment().endOf('day').format('YYYY-MM-DD HH:mm:ss')),
+		date: new Date(moment().startOf('day').format('YYYY-MM-DD HH:mm:ss'))
 	};
 }
 
