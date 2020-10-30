f6fd6839b61d92af770de473ba68b27c673b5a3c
Chetan Khandla <ckhandla94@gmail.com>
2020-10-30 22:07:20 +0530

    fix: issue in save preset

--- apps/gauzy/src/app/@shared/pipes/index.ts
+++ apps/gauzy/src/app/@shared/pipes/index.ts
@@ -1,5 +1,11 @@
 import { TimeFormatPipe } from './time-format.pipe';
 import { DurationFormatPipe } from './duration-format.pipe';
 import { UtcToLocalPipe } from './utc-to-local.pipe';
+import { ReplacePipe } from './replace.pipe';
 
-export const Pipes = [TimeFormatPipe, DurationFormatPipe, UtcToLocalPipe];
+export const Pipes = [
+	TimeFormatPipe,
+	DurationFormatPipe,
+	UtcToLocalPipe,
+	ReplacePipe
+];
--- apps/gauzy/src/app/pages/jobs/matching/matching/matching.component.ts
+++ apps/gauzy/src/app/pages/jobs/matching/matching/matching.component.ts
@@ -91,6 +91,7 @@ export class MatchingComponent implements OnInit {
 						await this.getEmployeeCriterions();
 					} else {
 						this.selectedEmployeeId = null;
+						this.criterions = [];
 					}
 					this.checkForEmptyCriterion();
 				});
@@ -125,6 +126,8 @@ export class MatchingComponent implements OnInit {
 		this.jobPresetService.createJobPreset(request).then((jobPreset) => {
 			this.jobPresets = this.jobPresets.concat([jobPreset]);
 			this.criterionForm.jobPresetId = jobPreset.id;
+
+			this.checkForEmptyCriterion();
 		});
 	}
 
@@ -139,6 +142,8 @@ export class MatchingComponent implements OnInit {
 						this.criterions = jobPresetCriterions;
 					});
 			}
+		} else {
+			this.criterions = [];
 		}
 		this.hasAnyChanges = false;
 		this.checkForEmptyCriterion();
@@ -165,7 +170,10 @@ export class MatchingComponent implements OnInit {
 	}
 
 	async checkForEmptyCriterion() {
-		if (this.criterions.length === 0) {
+		if (
+			(this.selectedEmployeeId || this.criterionForm.jobPresetId) &&
+			this.criterions.length === 0
+		) {
 			this.addNewCriterion();
 		}
 	}
