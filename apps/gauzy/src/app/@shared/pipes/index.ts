cc32139380c69c8cf8f1793198cf1401f489bd57
Chetan Khandla <ckhandla94@gmail.com>
2020-11-01 17:42:52 +0530

    fix: deepscan

--- apps/gauzy/src/app/@shared/pipes/index.ts
+++ apps/gauzy/src/app/@shared/pipes/index.ts
@@ -2,7 +2,7 @@ import { TimeFormatPipe } from './time-format.pipe';
 import { DurationFormatPipe } from './duration-format.pipe';
 import { UtcToLocalPipe } from './utc-to-local.pipe';
 import { ReplacePipe } from './replace.pipe';
-import { Nl2BrPipe, SafeHtmlPipe, TruncatePipe } from './text.pipe';
+import { Nl2BrPipe, SafeHtmlPipe } from './text.pipe';
 
 export const Pipes = [
 	TimeFormatPipe,
