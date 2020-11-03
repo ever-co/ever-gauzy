878844c9f502cd75b085b9b39bf63672bf41e22f
YavorGrancharov <bearhold@gmail.com>
2020-11-03 08:34:47 +0200

    feat: started timesheets e2e tests

---
+++ apps/gauzy-e2e/src/support/Base/pageobjects/TimesheetsPageObject.ts
@@ -0,0 +1,7 @@
+export const TimesheetsPage = {
+	addTimeButtonCss: 'div.text-left > button[status="primary"]',
+	viewEmployeeTimeCss: 'div.action-button > button[status="primary"]',
+	editEmployeeTimeCss: 'div.action-button > button[status="success"]',
+	deleteEmployeeTimeCss: 'div.action-button > button[status="danger"]',
+	closeAddTimeLogPopoverCss: 'g[data-name="close"]'
+};
