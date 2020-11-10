53aedd0dec0027441facefd185de34666c8e1b6d
Chetan Khandla <ckhandla94@gmail.com>
2020-11-10 17:40:29 +0530

    fix: change curd service extends in report

--- apps/api/src/app/reports/report.controller.ts
+++ apps/api/src/app/reports/report.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { CrudController } from '../core';
+import { TenantPermissionGuard } from '../shared/guards/auth/tenant-permission.guard';
 import { Report } from './report.entity';
 import { ReportService } from './report.service';
 
+@ApiTags('Report')
+@UseGuards(AuthGuard('jwt'), TenantPermissionGuard)
 @Controller()
 export class ReportController extends CrudController<Report> {
 	constructor(reportService: ReportService) {
--- apps/api/src/app/reports/report.module.ts
+++ apps/api/src/app/reports/report.module.ts
@@ -4,9 +4,10 @@ import { ReportCategory } from './report-category.entity';
 import { Report } from './report.entity';
 import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
+import { TenantModule } from '../tenant/tenant.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Report, ReportCategory])],
+	imports: [TypeOrmModule.forFeature([Report, ReportCategory]), TenantModule],
 	controllers: [ReportController],
 	providers: [ReportService]
 })
