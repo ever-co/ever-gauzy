1cc2e46e93521329507e12d74f34546d356eab37
YavorGrancharov <bearhold@gmail.com>
2020-11-13 13:23:58 +0200

    feat: organization equipment e2e tests

---
+++ apps/gauzy-e2e/src/support/Base/pagedata/OrganizationEquipmentPageData.ts
@@ -0,0 +1,11 @@
+export const OrganizationEquipmentPageData = {
+	name: 'Car',
+	type: 'Vehicle',
+	sn: 12345678,
+	year: 2015,
+	cost: 8900,
+	period: 365,
+	requestName: 'BMW',
+	policy: 'Default policy',
+	description: 'Shared between all employees'
+};
