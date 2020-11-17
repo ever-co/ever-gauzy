f2b93b44942bbb8637ff959c43c4884fcdf7d871
Yavor Grancharov <bearhold@gmail.com>
2020-11-17 12:18:17 +0200

    feat: organization recurring expenses e2e tests

---
+++ apps/gauzy-e2e/src/support/Base/pageobjects/OrganizationRecurringExpensesPageObject.ts
@@ -0,0 +1,13 @@
+export const OrganizationRecurringExpensesPage = {
+	addButtonCss: 'div.body-header button[status="success"]',
+	expenseDropdownCss: '#positionInput',
+	dropdownOptionCss: 'div.ng-option',
+	valueInputCss: '#valueInput',
+	saveButtonCss: 'div.actions > button[status="success"]',
+	settingsButtonCss: 'g[data-name="settings-2"]',
+	editButtonCss: 'div.block-settings g[data-name="edit"]',
+	deleteButtonCss: 'g[data-name="close"]',
+	deleteOnlyThisRadioButtonCss: 'nb-radio[value="current"]',
+	confirmDeleteExpenseButtonCss: 'nb-card-footer > button[status="danger"]',
+	toastrMessageCss: 'nb-toast.ng-trigger'
+};
