5f2c3b26aac6f847488575449d895150bdea0b8f
YavorGrancharov <bearhold@gmail.com>
2020-11-20 15:05:23 +0200

    feat: invoices e2e tests

---
+++ apps/gauzy-e2e/src/integration/InvoicesTest.ts
@@ -0,0 +1,172 @@
+import * as loginPage from '../support/Base/pages/Login.po';
+import { LoginPageData } from '../support/Base/pagedata/LoginPageData';
+import * as invoicesPage from '../support/Base/pages/Invoices.po';
+import { InvoicesPageData } from '../support/Base/pagedata/InvoicesPageData';
+import * as organizationTagsUserPage from '../support/Base/pages/OrganizationTags.po';
+import { OrganizationTagsPageData } from '../support/Base/pagedata/OrganizationTagsPageData';
+import * as dashboradPage from '../support/Base/pages/Dashboard.po';
+import { CustomCommands } from '../support/commands';
+import * as faker from 'faker';
+import { ContactsLeadsPageData } from '../support/Base/pagedata/ContactsLeadsPageData';
+import * as contactsLeadsPage from '../support/Base/pages/ContactsLeads.po';
+import * as organizationProjectsPage from '../support/Base/pages/OrganizationProjects.po';
+import { OrganizationProjectsPageData } from '../support/Base/pagedata/OrganizationProjectsPageData';
+
+let email = ' ';
+let fullName = ' ';
+let country = ' ';
+let city = ' ';
+let postcode = ' ';
+let street = ' ';
+let website = ' ';
+let ssendEmail = ' ';
+
+describe('Invoices test', () => {
+	before(() => {
+		email = faker.internet.email();
+		fullName = faker.name.firstName() + ' ' + faker.name.lastName();
+		country = faker.address.country();
+		city = faker.address.city();
+		postcode = faker.address.zipCode();
+		street = faker.address.streetAddress();
+		website = faker.internet.url();
+		ssendEmail = faker.internet.email();
+
+		CustomCommands.login(loginPage, LoginPageData, dashboradPage);
+	});
+	it('Should be able to add new invoice', () => {
+		CustomCommands.addProject(
+			organizationProjectsPage,
+			OrganizationProjectsPageData
+		);
+		CustomCommands.addTag(
+			organizationTagsUserPage,
+			OrganizationTagsPageData
+		);
+		CustomCommands.addContact(
+			fullName,
+			email,
+			country,
+			city,
+			postcode,
+			street,
+			website,
+			contactsLeadsPage,
+			ContactsLeadsPageData
+		);
+		cy.visit('/#/pages/accounting/invoices');
+		invoicesPage.gridBtnExists();
+		invoicesPage.gridBtnClick(1);
+		invoicesPage.addButtonVisible();
+		invoicesPage.clickAddButton();
+		invoicesPage.tagsDropdownVisible();
+		invoicesPage.clickTagsDropdwon();
+		invoicesPage.selectTagFromDropdown(0);
+		invoicesPage.clickCardBody();
+		invoicesPage.discountInputVisible();
+		invoicesPage.enterDiscountData(InvoicesPageData.discountValue);
+		invoicesPage.discountTypeDropdownVisible();
+		invoicesPage.clickDiscountDropdown();
+		invoicesPage.selectDiscountTypeFromDropdown(
+			InvoicesPageData.discountType
+		);
+		invoicesPage.contactDropdownVisible();
+		invoicesPage.clickContactDropdown();
+		invoicesPage.selectContactFromDropdwon(0);
+		invoicesPage.taxInputVisible();
+		invoicesPage.enterTaxData(InvoicesPageData.taxValue);
+		invoicesPage.taxTypeDropdownVisible();
+		invoicesPage.clickTaxTypeDropdown();
+		invoicesPage.selectTaxTypeFromDropdown(InvoicesPageData.taxType);
+		invoicesPage.invoiceTypeDropdownVisible();
+		invoicesPage.clickInvoiceTypeDropdown();
+		invoicesPage.selectInvoiceTypeFromDropdown(
+			InvoicesPageData.invoiceType
+		);
+		invoicesPage.employeeDropdownVisible();
+		invoicesPage.clickEmployeeDropdown();
+		invoicesPage.selectEmployeeFromDropdown(0);
+		invoicesPage.clickKeyboardButtonByKeyCode(9);
+		invoicesPage.generateItemsButtonVisible();
+		invoicesPage.clickGenerateItemsButton();
+		invoicesPage.saveAsDraftButtonVisible();
+		invoicesPage.clickSaveAsDraftButton(InvoicesPageData.saveAsDraftButton);
+	});
+	it('Should be able to edit invoice', () => {
+		invoicesPage.waitMessageToHide();
+		invoicesPage.addButtonVisible();
+		invoicesPage.clickAddButton();
+		invoicesPage.backButtonVisible();
+		invoicesPage.clickBackButton();
+		invoicesPage.selectTableRow(0);
+		invoicesPage.editButtonVisible();
+		invoicesPage.clickEditButton(InvoicesPageData.editButton);
+		invoicesPage.discountInputVisible();
+		invoicesPage.enterDiscountData(InvoicesPageData.discountValue);
+		invoicesPage.discountTypeDropdownVisible();
+		invoicesPage.clickDiscountDropdown();
+		invoicesPage.selectDiscountTypeFromDropdown(
+			InvoicesPageData.discountType
+		);
+		invoicesPage.contactDropdownVisible();
+		invoicesPage.clickContactDropdown();
+		invoicesPage.selectContactFromDropdwon(0);
+		invoicesPage.taxInputVisible();
+		invoicesPage.enterTaxData(InvoicesPageData.taxValue);
+		invoicesPage.taxTypeDropdownVisible();
+		invoicesPage.clickTaxTypeDropdown();
+		invoicesPage.selectTaxTypeFromDropdown(InvoicesPageData.taxType);
+		invoicesPage.saveAsDraftButtonVisible();
+		invoicesPage.clickSaveAsDraftButton(InvoicesPageData.saveAsDraftButton);
+	});
+	it('Should be able to duplicate invoice', () => {
+		invoicesPage.waitMessageToHide();
+		invoicesPage.selectTableRow(0);
+		invoicesPage.actionButtonVisible();
+		invoicesPage.clickActionButtonByText(InvoicesPageData.duplicateButton);
+		invoicesPage.waitMessageToHide();
+		invoicesPage.backButtonVisible();
+		invoicesPage.clickBackButton();
+	});
+	it('Should be able to send invoice', () => {
+		invoicesPage.selectTableRow(0);
+		invoicesPage.actionButtonVisible();
+		invoicesPage.clickActionButtonByText(InvoicesPageData.sendButton);
+		invoicesPage.confirmButtonVisible();
+		invoicesPage.clickConfirmButton();
+	});
+	it('Should be able to view invoice', () => {
+		invoicesPage.waitMessageToHide();
+		invoicesPage.selectTableRow(0);
+		invoicesPage.actionButtonVisible();
+		invoicesPage.clickActionButtonByText(InvoicesPageData.viewButton);
+		invoicesPage.backButtonVisible();
+		invoicesPage.clickBackButton();
+	});
+	it('Should be able to send invoice by email', () => {
+		cy.on('uncaught:exception', (err, runnable) => {
+			return false;
+		});
+		invoicesPage.selectTableRow(0);
+		invoicesPage.actionButtonVisible();
+		invoicesPage.clickActionButtonByText(InvoicesPageData.emailButton);
+		invoicesPage.emailInputVisible();
+		invoicesPage.enterEmailData(ssendEmail);
+		invoicesPage.confirmButtonVisible();
+		invoicesPage.clickConfirmButton();
+	});
+	it('Should be able to set invoice status', () => {
+		invoicesPage.waitMessageToHide();
+		invoicesPage.selectTableRow(0);
+		invoicesPage.clickActionButtonByText(InvoicesPageData.setStatusButton);
+		invoicesPage.setStatusFromDropdown(InvoicesPageData.status);
+	});
+	it('Should be able to delete invoice', () => {
+		invoicesPage.waitMessageToHide();
+		invoicesPage.selectTableRow(0);
+		invoicesPage.deleteButtonVisible();
+		invoicesPage.clickDeleteButton();
+		invoicesPage.confirmDeleteButtonVisible();
+		invoicesPage.clickConfirmDeleteButton();
+	});
+});
---
+++ apps/gauzy-e2e/src/support/Base/pages/Invoices.po.ts
@@ -0,0 +1,213 @@
+import {
+	enterInput,
+	verifyElementIsVisible,
+	clickButton,
+	clearField,
+	clickKeyboardBtnByKeycode,
+	clickButtonByIndex,
+	clickElementByText,
+	waitElementToHide,
+	clickButtonByText
+} from '../utils/util';
+import { InvoicesPage } from '../pageobjects/InvoicesPageObject';
+
+export const gridBtnExists = () => {
+	verifyElementIsVisible(InvoicesPage.gridButtonCss);
+};
+
+export const gridBtnClick = (index) => {
+	clickButtonByIndex(InvoicesPage.gridButtonCss, index);
+};
+
+export const addButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.addButtonCss);
+};
+
+export const clickAddButton = () => {
+	clickButton(InvoicesPage.addButtonCss);
+};
+
+export const tagsDropdownVisible = () => {
+	verifyElementIsVisible(InvoicesPage.addTagsDropdownCss);
+};
+
+export const clickTagsDropdwon = () => {
+	clickButton(InvoicesPage.addTagsDropdownCss);
+};
+
+export const selectTagFromDropdown = (index) => {
+	clickButtonByIndex(InvoicesPage.tagsDropdownOption, index);
+};
+
+export const clickCardBody = () => {
+	clickButton(InvoicesPage.cardBodyCss);
+};
+
+export const waitMessageToHide = () => {
+	waitElementToHide(InvoicesPage.toastrMessageCss);
+};
+
+export const discountInputVisible = () => {
+	verifyElementIsVisible(InvoicesPage.discountInputCss);
+};
+
+export const enterDiscountData = (data) => {
+	clearField(InvoicesPage.discountInputCss);
+	enterInput(InvoicesPage.discountInputCss, data);
+};
+
+export const discountTypeDropdownVisible = () => {
+	verifyElementIsVisible(InvoicesPage.discountTypeDropdownCss);
+};
+
+export const clickDiscountDropdown = () => {
+	clickButton(InvoicesPage.discountTypeDropdownCss);
+};
+
+export const selectDiscountTypeFromDropdown = (text) => {
+	clickElementByText(InvoicesPage.dropdownOptionCss, text);
+};
+
+export const contactDropdownVisible = () => {
+	verifyElementIsVisible(InvoicesPage.organizationContactDropdownCss);
+};
+
+export const clickContactDropdown = () => {
+	clickButton(InvoicesPage.organizationContactDropdownCss);
+};
+
+export const selectContactFromDropdwon = (index) => {
+	clickButtonByIndex(InvoicesPage.contactOptionCss, index);
+};
+
+export const taxInputVisible = () => {
+	verifyElementIsVisible(InvoicesPage.taxInputCss);
+};
+
+export const enterTaxData = (data) => {
+	clearField(InvoicesPage.taxInputCss);
+	enterInput(InvoicesPage.taxInputCss, data);
+};
+
+export const taxTypeDropdownVisible = () => {
+	verifyElementIsVisible(InvoicesPage.taxTypeDropdownCss);
+};
+
+export const clickTaxTypeDropdown = () => {
+	clickButton(InvoicesPage.taxTypeDropdownCss);
+};
+
+export const selectTaxTypeFromDropdown = (text) => {
+	clickElementByText(InvoicesPage.dropdownOptionCss, text);
+};
+
+export const invoiceTypeDropdownVisible = () => {
+	verifyElementIsVisible(InvoicesPage.invoiceTypeDropdownCss);
+};
+
+export const clickInvoiceTypeDropdown = () => {
+	clickButton(InvoicesPage.invoiceTypeDropdownCss);
+};
+
+export const selectInvoiceTypeFromDropdown = (text) => {
+	clickElementByText(InvoicesPage.dropdownOptionCss, text);
+};
+
+export const employeeDropdownVisible = () => {
+	verifyElementIsVisible(InvoicesPage.selectEmloyeeCss);
+};
+export const clickEmployeeDropdown = () => {
+	clickButton(InvoicesPage.selectEmloyeeCss);
+};
+
+export const selectEmployeeFromDropdown = (index) => {
+	clickButtonByIndex(InvoicesPage.dropdownOptionCss, index);
+};
+
+export const clickKeyboardButtonByKeyCode = (keycode) => {
+	clickKeyboardBtnByKeycode(keycode);
+};
+
+export const generateItemsButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.generateItemsButtonCss);
+};
+
+export const clickGenerateItemsButton = () => {
+	clickButton(InvoicesPage.generateItemsButtonCss);
+};
+
+export const saveAsDraftButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.saveAsDraftButtonCss);
+};
+
+export const clickSaveAsDraftButton = (text) => {
+	clickButtonByText(text);
+};
+
+export const tableRowVisible = () => {
+	verifyElementIsVisible(InvoicesPage.tableRowCss);
+};
+
+export const selectTableRow = (index) => {
+	clickButtonByIndex(InvoicesPage.tableRowCss, index);
+};
+
+export const actionButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.infoButtonCss);
+};
+
+export const clickActionButtonByText = (text) => {
+	clickElementByText(InvoicesPage.infoButtonCss, text);
+};
+
+export const backButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.backButtonCss);
+};
+
+export const clickBackButton = () => {
+	clickButton(InvoicesPage.backButtonCss);
+};
+
+export const confirmButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.confirmButtonCss);
+};
+
+export const clickConfirmButton = () => {
+	clickButton(InvoicesPage.confirmButtonCss);
+};
+
+export const emailInputVisible = () => {
+	verifyElementIsVisible(InvoicesPage.emailInputCss);
+};
+
+export const enterEmailData = (data) => {
+	enterInput(InvoicesPage.emailInputCss, data);
+};
+
+export const editButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.editButtonCss);
+};
+
+export const clickEditButton = (text) => {
+	clickElementByText(InvoicesPage.editButtonCss, text);
+};
+
+export const deleteButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.deleteButtonCss);
+};
+
+export const clickDeleteButton = () => {
+	clickButton(InvoicesPage.deleteButtonCss);
+};
+
+export const confirmDeleteButtonVisible = () => {
+	verifyElementIsVisible(InvoicesPage.confirmDeleteButtonCss);
+};
+
+export const clickConfirmDeleteButton = () => {
+	clickButton(InvoicesPage.confirmDeleteButtonCss);
+};
+
+export const setStatusFromDropdown = (text) => {
+	clickElementByText(InvoicesPage.dropdownOptionCss, text);
+};
