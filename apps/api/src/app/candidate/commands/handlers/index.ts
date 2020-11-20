56f207c4f7ecc3a439325d7dc3bd63fbed5997db
Rahul Rathore <rahulrathore576@gmail.com>
2020-11-20 21:38:50 +0530

    feat: #2427 command and handlers for candidate module

--- apps/api/src/app/candidate/commands/handlers/index.ts
+++ apps/api/src/app/candidate/commands/handlers/index.ts
@@ -1,7 +1,9 @@
 import { CandidateBulkCreateHandler } from './candidate.bulk.create.handler';
 import { CandidateCreateHandler } from './candidate.create.handler';
+import { CandidateUpdateHandler } from './candidate.update.handler';
 
 export const CommandHandlers = [
+	CandidateBulkCreateHandler,
 	CandidateCreateHandler,
-	CandidateBulkCreateHandler
+	CandidateUpdateHandler
 ];
