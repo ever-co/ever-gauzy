183ca038f761a43100aac1b8efa1f5ed69193256
Rahul Rathore <rahulrathore576@gmail.com>
2020-11-26 12:11:19 +0530

    fix: deepscan

--- apps/gauzy/src/app/onboarding/onboarding-complete/onboarding-complete.component.ts
+++ apps/gauzy/src/app/onboarding/onboarding-complete/onboarding-complete.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IRolePermission, IUser } from '@gauzy/models';
+import { IUser } from '@gauzy/models';
 import { TranslateService } from '@ngx-translate/core';
 import { NgxPermissionsService } from 'ngx-permissions';
 import { UsersService } from '../../@core/services';
