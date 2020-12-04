f869fe544d98a39cf3e87e979f2157c8221dd60d
Ruslan Konviser <evereq@gmail.com>
2020-12-04 16:14:18 +0200

    Update country.seed.ts

--- apps/api/src/app/country/country.seed.ts
+++ apps/api/src/app/country/country.seed.ts
@@ -1,54 +1,7 @@
 import { Connection } from 'typeorm';
 import { Country } from './country.entity';
-// import * as fs from 'fs';
-// import * as path from 'path';
 import { ICountry } from '@gauzy/models';
 import { DEFAULT_COUNTRY_ENTITY } from './country';
-// import { environment as env } from '@env-api/environment';
-
-/* export const createCountries = async (
-	connection: Connection
-): Promise<ICountry[]> => {
-	return await new Promise<ICountry[]>((resolve, reject) => {
-		const baseDir = env.isElectron
-			? path.resolve(env.gauzyUserPath, ...['src', 'app', 'country'])
-			: path.resolve('.', ...['apps', 'api', 'src', 'app', 'country']);
-
-		fs.readFile(
-			path.join(baseDir, 'country.json'),
-			'utf8',
-			async (err, json) => {
-				if (err) {
-					console.log(
-						'Error reading country json file from disk:',
-						err
-					);
-					reject(null);
-					return;
-				}
-				try {
-					const countries: ICountry[] = [];
-					const entries = JSON.parse(json);
-					for (const key of Object.keys(entries)) {
-						if (entries.hasOwnProperty(key)) {
-							const country: ICountry = {
-								isoCode: key,
-								country: entries[key]
-							};
-							countries.push(country);
-						}
-					}
-					await insertCountry(connection, countries);
-					resolve(countries);
-				} catch (err) {
-					console.log('Error parsing country JSON string:', err);
-					reject(null);
-					return;
-				}
-			}
-		);
-	});
-}; */
 
 export const createCountries = async (
 	connection: Connection
