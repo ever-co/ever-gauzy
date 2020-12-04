c011af7cb4aa361f6761c0cd0bcf41145536df2a
Mahesh-Umachagi <maheshmumachagi@gmail.com>
2020-12-04 23:12:22 +0530

    Updated Import/Export Page

--- apps/gauzy/src/app/pages/import-export/import-export-routing.module.ts
+++ apps/gauzy/src/app/pages/import-export/import-export-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExportComponent } from './export/export.component';
 import { ImportExportComponent } from './import-export.component';
+import { ImportComponent } from './import/import.component';
 
 const routes: Routes = [
 	{
 		path: '',
-		component: ImportExportComponent,
-		children: [
-			{
-				path: 'export',
-				loadChildren: () =>
-					import('./export/export.module').then((m) => m.ExportModule)
-			},
-			{
-				path: 'import',
-				loadChildren: () =>
-					import('./import/import.module').then((m) => m.ImportModule)
-			}
-		]
+		component: ImportExportComponent
+	},
+	{
+		path: 'export',
+		loadChildren: () =>
+			import('./export/export.module').then((m) => m.ExportModule)
+	},
+	{
+		path: 'import',
+		loadChildren: () =>
+			import('./import/import.module').then((m) => m.ImportModule)
 	}
 ];
 
