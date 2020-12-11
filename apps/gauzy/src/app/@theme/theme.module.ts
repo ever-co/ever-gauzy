beb4b0f6406175a0925be76e2e4617caa6c495d7
PavelDenchev <pav.denchev@gmail.com>
2020-12-11 13:54:08 +0200

    feat: added project selector in site header

--- apps/gauzy/src/app/@theme/theme.module.ts
+++ apps/gauzy/src/app/@theme/theme.module.ts
@@ -50,6 +50,7 @@ import { OrganizationsService } from '../@core/services/organizations.service';
 import { TimeTrackerModule } from '../@shared/time-tracker/time-tracker.module';
 import { LanguagesService } from '../@core/services/languages.service';
 import { LayoutSelectorComponent } from './components/layout-selector/layout-selector.component';
+import { ProjectSelectModule } from '../@shared/project-select/project-select.module';
 
 export function HttpLoaderFactory(http: HttpClient) {
 	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -72,6 +73,7 @@ const NB_MODULES = [
 	NbEvaIconsModule,
 	HeaderSelectorsModule,
 	EmployeeSelectorsModule,
+	ProjectSelectModule,
 	TimeTrackerModule,
 	TranslateModule.forChild({
 		loader: {
