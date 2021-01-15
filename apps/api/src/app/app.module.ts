eb38c2c1a7082fe58354187bd5e46a85029647c8
Ruslan Konviser <evereq@gmail.com>
2021-01-15 16:46:43 +0200

    fix: updates related to Sentry integration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -49,8 +49,6 @@ import { EquipmentModule } from './equipment/equipment.module';
 import { EmployeeLevelModule } from './organization_employee-level/organization-employee-level.module';
 import { ExportAllModule } from './export_import/export-all.module';
 import { ImportAllModule } from './export_import/import/import-all.module';
-import * as Sentry from '@sentry/node';
-import * as Tracing from '@sentry/tracing';
 import { SentryModule } from '@ntegral/nestjs-sentry';
 import { environment } from '@env-api/environment';
 import { LogLevel } from '@sentry/types';
@@ -126,6 +124,8 @@ import { EmployeeProposalTemplateModule } from './employee-proposal-template/emp
 import { CustomSmtpModule } from './custom-smtp/custom-smtp.module';
 import { FeatureModule } from './feature/feature.module';
 import * as unleash from 'unleash-client';
+import { Integrations as SentryIntegrations } from '@sentry/node';
+import { Integrations as TrackingIntegrations } from '@sentry/tracing';
 
 const { unleashConfig } = environment;
 
@@ -146,11 +146,11 @@ const sentryIntegrations = [];
 
 sentryIntegrations.push(
 	// enable HTTP calls tracing
-	new Sentry.Integrations.Http({ tracing: true })
+	new SentryIntegrations.Http({ tracing: true })
 );
 
 if (process.env.DB_TYPE === 'postgres') {
-	sentryIntegrations.push(new Tracing.Integrations.Postgres());
+	sentryIntegrations.push(new TrackingIntegrations.Postgres());
 }
 
 @Module({
