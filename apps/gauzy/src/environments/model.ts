8698419de85b60277e1f92b2f624eb7a86739756
Ruslan Konviser <evereq@gmail.com>
2021-01-19 01:23:56 +0200

    feat: some more improvements related to docker builds

--- apps/gauzy/src/environments/model.ts
+++ apps/gauzy/src/environments/model.ts
@@ -2,6 +2,8 @@ export interface Environment {
 	production: boolean;
 
 	API_BASE_URL: string;
+	CLIENT_BASE_URL: string;
+
 	COMPANY_NAME: string;
 	COMPANY_SITE: string;
 	COMPANY_LINK: string;
@@ -10,8 +12,10 @@ export interface Environment {
 	COMPANY_FACEBOOK_LINK: string;
 	COMPANY_TWITTER_LINK: string;
 	COMPANY_LINKEDIN_LINK: string;
-	CLOUDINARY_CLOUD_NAME: string;
-	CLOUDINARY_API_KEY: string;
+
+	CLOUDINARY_CLOUD_NAME?: string;
+	CLOUDINARY_API_KEY?: string;
+
 	GOOGLE_AUTH_LINK: string;
 	FACEBOOK_AUTH_LINK: string;
 	LINKEDIN_AUTH_LINK: string;
@@ -19,13 +23,18 @@ export interface Environment {
 	TWITTER_AUTH_LINK: string;
 	MICROSOFT_AUTH_LINK: string;
 	AUTH0_AUTH_LINK: string;
+
 	NO_INTERNET_LOGO: string;
+
 	SENTRY_DSN?: string;
+
 	HUBSTAFF_REDIRECT_URI?: string;
+
 	IS_ELECTRON: boolean;
 
 	GOOGLE_MAPS_API_KEY: string;
 	GOOGLE_PLACE_AUTOCOMPLETE: boolean;
+
 	DEFAULT_LATITUDE: number;
 	DEFAULT_LONGITUDE: number;
 	DEFAULT_CURRENCY: string;
