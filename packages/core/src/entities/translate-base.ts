a9c8123a215a79b6d9535766c8e0f602ea0ff93f
Rahul Rathore <rahulrathore576@gmail.com>
2021-01-21 15:01:07 +0530

    fix: #1740 updated translate base

--- packages/core/src/entities/translate-base.ts
+++ packages/core/src/entities/translate-base.ts
@@ -8,6 +8,17 @@ export abstract class TranslationBase
 	languageCode: string;
 }
 
+export interface TranslateInput {
+	key: string;
+	alias: string;
+}
+
+export interface TranslatePropertyInput {
+	prop: string;
+	propsTranslate: Array<TranslateInput>;
+	propAsArr?: Array<string>;
+}
+
 export abstract class TranslatableBase
 	extends TenantOrganizationBase
 	implements ITranslatable<TranslationBase> {
@@ -30,5 +41,73 @@ export abstract class TranslatableBase
 
 		delete this.translations;
 		return this;
+	}
+
+	/*
+	 * translate product object keeping all root elements
+	 * and adding translated prop up to two nested levels
+	 */
+	translateNested(
+		languageCode: string,
+		translatePropsInput: Array<TranslatePropertyInput>
+	): any {
+		let element: TranslatableBase = this;
+		let currentInputProp: TranslatePropertyInput;
+		let propsTranslateKeys: TranslateInput[];
+		let elementPropTranslations: ITranslation<TranslationBase>[];
+
+		let result: any = {};
+
+		let inputProps: Array<TranslatePropertyInput> = translatePropsInput.map(
+			(translateObj: TranslatePropertyInput) => {
+				return {
+					...translateObj,
+					propAsArr: translateObj.prop.split('.')
+				};
+			}
+		);
+
+		Object.keys(this).forEach((prop: string) => {
+			if (prop == 'translations') prop = 'root';
+
+			currentInputProp = inputProps.find(
+				(el: TranslatePropertyInput) => el.propAsArr[0] == prop
+			);
+
+			if (!currentInputProp) {
+				result[prop] = element[prop];
+			} else {
+				let inputKeys = inputProps.find(
+					(el: TranslatePropertyInput) => el.propAsArr[0] == prop
+				).propAsArr;
+
+				if (prop == 'root') {
+					elementPropTranslations = element.translations;
+				} else if (inputKeys.length == 1) {
+					elementPropTranslations =
+						element[inputKeys[0]].translations;
+				} else if (inputKeys.length == 2) {
+					elementPropTranslations =
+						element[inputKeys[0]][inputKeys[1]].translations;
+				}
+
+				let elementPropTranslation =
+					elementPropTranslations.find(
+						(translation: ITranslation<TranslationBase>) =>
+							translation.languageCode == languageCode
+					) || null;
+
+				if (elementPropTranslations && elementPropTranslation) {
+					propsTranslateKeys = currentInputProp.propsTranslate || [];
+
+					propsTranslateKeys.forEach((translateKeyInput) => {
+						result[translateKeyInput.alias] =
+							elementPropTranslation[translateKeyInput.key] || '';
+					});
+				}
+			}
+		});
+
+		return result;
 	}
 }
