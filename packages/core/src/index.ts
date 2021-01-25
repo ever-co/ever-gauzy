86d7d31bd284d15e5fbaa727f811b23809cdbb69
Rahul Rathore <rahulrathore576@gmail.com>
2021-01-25 20:19:01 +0530

    fix: #1740 build configuration

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,27 +1,2 @@
-import { bootstrap } from './bootstrap';
 export { bootstrap } from './bootstrap';
-
 export * from './logger/index';
-
-bootstrap({
-	apiConfigOptions: {
-		port: 3001,
-		middleware: [],
-		graphqlConfigOptions: {
-			path: 'graphql',
-			playground: true,
-			debug: true,
-			apolloServerPlugins: []
-		}
-	},
-	dbConnectionOptions: {
-		type: 'postgres',
-		port: 5432,
-		synchronize: true,
-		logging: true,
-		database: 'plugin-dev',
-		username: 'postgres',
-		password: 'root'
-	},
-	plugins: []
-});
