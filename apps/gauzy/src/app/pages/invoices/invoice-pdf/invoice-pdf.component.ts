455c7b6cbf3880e0df708622f90d667f9d230844
Rahul Rathore <rahulrathore576@gmail.com>
2021-02-21 18:23:27 +0530

    fix: invoice download view

--- apps/gauzy/src/app/pages/invoices/invoice-pdf/invoice-pdf.component.ts
+++ apps/gauzy/src/app/pages/invoices/invoice-pdf/invoice-pdf.component.ts
@@ -1,19 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { IInvoice, InvoiceTypeEnum } from '@gauzy/contracts';
+import { Component, Input, OnInit } from '@angular/core';
+import { IInvoice } from '@gauzy/contracts';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
-import * as pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-import { EmployeesService } from '../../../@core/services';
-import { ExpensesService } from '../../../@core/services/expenses.service';
-import { OrganizationProjectsService } from '../../../@core/services/organization-projects.service';
-import { ProductService } from '../../../@core/services/product.service';
-import { TasksService } from '../../../@core/services/tasks.service';
-import { generatePdf } from '../../../@shared/invoice/generate-pdf';
+import { tap } from 'rxjs/operators';
+import { InvoicesService } from '../../../@core/services';
 import { TranslationBaseComponent } from '../../../@shared/language-base/translation-base.component';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
 	selector: 'ga-invoice-pdf',
-	template: `<iframe id="iframe" class="pdfDoc"></iframe>`,
+	template: `<iframe
+		type="application/pdf"
+		id="iframe"
+		class="pdfDoc"
+	></iframe>`,
 	styles: [
 		`
 			::ng-deep .pdf-preview-card {
@@ -31,102 +31,31 @@ export class InvoicePdfComponent
 	extends TranslationBaseComponent
 	implements OnInit {
 	@Input() invoice: IInvoice;
-	@Output() docDef = new EventEmitter();
-	docDefinition: any;
-	translatedText: any;
 
 	constructor(
-		private employeeService: EmployeesService,
-		private projectService: OrganizationProjectsService,
-		private taskService: TasksService,
-		private productService: ProductService,
-		private expensesService: ExpensesService,
+		private readonly invoicesService: InvoicesService,
 		readonly translateService: TranslateService
 	) {
 		super(translateService);
 	}
 
 	ngOnInit() {
-		this.loadPdf();
+		this.loadInvoicePdf();
 	}
 
-	async loadPdf() {
-		pdfMake.vfs = pdfFonts.pdfMake.vfs;
-		let docDefinition;
-		let service;
+	async loadInvoicePdf() {
+		const { id: invoiceId } = this.invoice;
+		this.invoicesService
+			.downloadInvoicePdf(invoiceId)
+			.pipe(
+				tap((data) => this.embeddedPdfToIframe(data)),
+				untilDestroyed(this)
+			)
+			.subscribe();
+	}
 
-		switch (this.invoice.invoiceType) {
-			case InvoiceTypeEnum.BY_EMPLOYEE_HOURS:
-				service = this.employeeService;
-				break;
-			case InvoiceTypeEnum.BY_PROJECT_HOURS:
-				service = this.projectService;
-				break;
-			case InvoiceTypeEnum.BY_TASK_HOURS:
-				service = this.taskService;
-				break;
-			case InvoiceTypeEnum.BY_PRODUCTS:
-				service = this.productService;
-				break;
-			case InvoiceTypeEnum.BY_EXPENSES:
-				service = this.expensesService;
-				break;
-			default:
-				break;
-		}
-
-		this.translatedText = {
-			description: this.getTranslation(
-				'INVOICES_PAGE.INVOICE_ITEM.DESCRIPTION'
-			),
-			quantity: this.getTranslation(
-				'INVOICES_PAGE.INVOICE_ITEM.QUANTITY'
-			),
-			price: this.getTranslation('INVOICES_PAGE.INVOICE_ITEM.PRICE'),
-			totalValue: this.getTranslation(
-				'INVOICES_PAGE.INVOICE_ITEM.TOTAL_VALUE'
-			),
-			item: this.getTranslation('INVOICES_PAGE.INVOICE_ITEM.ITEM'),
-			invoice: this.getTranslation('INVOICES_PAGE.INVOICE'),
-			estimate: this.getTranslation('INVOICES_PAGE.ESTIMATE'),
-			number: this.getTranslation('INVOICES_PAGE.NUMBER'),
-			from: this.getTranslation('INVOICES_PAGE.FROM'),
-			to: this.getTranslation('INVOICES_PAGE.TO'),
-			date: this.getTranslation('INVOICES_PAGE.DATE'),
-			dueDate: this.getTranslation('INVOICES_PAGE.DUE_DATE'),
-			discountValue: this.getTranslation(
-				'INVOICES_PAGE.INVOICES_SELECT_DISCOUNT_VALUE'
-			),
-			discountType: this.getTranslation('INVOICES_PAGE.DISCOUNT_TYPE'),
-			taxValue: this.getTranslation('INVOICES_PAGE.TAX_VALUE'),
-			taxType: this.getTranslation('INVOICES_PAGE.TAX_TYPE'),
-			currency: this.getTranslation('INVOICES_PAGE.CURRENCY'),
-			terms: this.getTranslation('INVOICES_PAGE.INVOICES_SELECT_TERMS'),
-			paid: this.getTranslation('INVOICES_PAGE.PAID'),
-			yes: this.getTranslation('INVOICES_PAGE.YES'),
-			no: this.getTranslation('INVOICES_PAGE.NO'),
-			alreadyPaid: this.getTranslation('INVOICES_PAGE.ALREADY_PAID'),
-			amountDue: this.getTranslation('INVOICES_PAGE.AMOUNT_DUE')
-		};
-
-		docDefinition = await generatePdf(
-			this.invoice,
-			this.invoice.fromOrganization,
-			this.invoice.toContact,
-			service,
-			this.translatedText
-		);
-
-		this.docDef.emit(docDefinition);
-
-		this.docDefinition = docDefinition;
-
-		const pdfDocGenerator = pdfMake.createPdf(docDefinition);
-		pdfDocGenerator.getDataUrl((dataUrl) => {
-			const iframe = document.querySelector(
-				'#iframe'
-			) as HTMLIFrameElement;
-			iframe.src = dataUrl;
-		});
+	embeddedPdfToIframe(data) {
+		var file = window.URL.createObjectURL(data);
+		document.querySelector('iframe').src = file;
 	}
 }
