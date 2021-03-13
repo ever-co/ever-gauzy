0203f8722eaeee6b597ec4cc8c70a87ab6984c09
Ruslan Konviser <evereq@gmail.com>
2021-03-13 15:31:24 +0200

    fix: multiple fixes in regards to env vars naming / usage

--- apps/api/src/plugin-config.ts
+++ apps/api/src/plugin-config.ts
@@ -3,7 +3,7 @@ import {
 	DEFAULT_API_PORT,
 	DEFAULT_GRAPHQL_API_PATH,
 	DEFAULT_API_HOST,
-	DEFAULT_BASE_URL
+	DEFAULT_API_BASE_URL
 } from '@gauzy/common';
 import { ConnectionOptions } from 'typeorm';
 import * as path from 'path';
@@ -14,7 +14,7 @@ export const pluginConfig: IPluginConfig = {
 	apiConfigOptions: {
 		host: process.env.HOST || DEFAULT_API_HOST,
 		port: process.env.PORT || DEFAULT_API_PORT,
-		baseUrl: process.env.BASE_URL || DEFAULT_BASE_URL,
+		baseUrl: process.env.API_BASE_URL || DEFAULT_API_BASE_URL,
 		middleware: [],
 		graphqlConfigOptions: {
 			path: DEFAULT_GRAPHQL_API_PATH,
--- apps/gauzy/src/app/app.component.ts
+++ apps/gauzy/src/app/app.component.ts
@@ -55,15 +55,15 @@ export class AppComponent implements OnInit, AfterViewInit {
 	}
 
 	private loadChatwoot(d, t) {
-		var BASE_URL = 'https://app.chatwoot.com';
+		var chatwootBaseUrl = 'https://app.chatwoot.com';
 		var g = d.createElement(t),
 			s = d.getElementsByTagName(t)[0];
-		g.src = BASE_URL + '/packs/js/sdk.js';
+		g.src = chatwootBaseUrl + '/packs/js/sdk.js';
 		s.parentNode.insertBefore(g, s);
 		g.onload = function () {
 			window['chatwootSDK'].run({
 				websiteToken: environment.CHATWOOT_SDK_TOKEN,
-				baseUrl: BASE_URL
+				baseUrl: chatwootBaseUrl
 			});
 		};
 	}
