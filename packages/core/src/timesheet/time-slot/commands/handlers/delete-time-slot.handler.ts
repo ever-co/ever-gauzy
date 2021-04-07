f3b15d0385a13d52e9b3b0e88ee01b946988f236
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-04-07 20:13:52 +0530

    fix: timeslot delete command

--- packages/core/src/timesheet/time-slot/commands/handlers/delete-time-slot.handler.ts
+++ packages/core/src/timesheet/time-slot/commands/handlers/delete-time-slot.handler.ts
@@ -4,6 +4,8 @@ import { Repository, In } from 'typeorm';
 import { TimeSlot } from '../../../time-slot.entity';
 import { DeleteTimeSpanCommand } from '../../../time-log/commands/delete-time-span.command';
 import { DeleteTimeSlotCommand } from '../delete-time-slot.command';
+import { ITimeSlot, PermissionsEnum } from '@gauzy/contracts';
+import { RequestContext } from './../../../../core/context/request-context';
 
 @CommandHandler(DeleteTimeSlotCommand)
 export class DeleteTimeSlotHandler
@@ -15,37 +17,49 @@ export class DeleteTimeSlotHandler
 	) {}
 
 	public async execute(command: DeleteTimeSlotCommand): Promise<boolean> {
-		const { ids } = command;
+		const { ids } = command;		
+		let employeeIds: string[] = [];
+		if (
+			!RequestContext.hasPermission(PermissionsEnum.CHANGE_SELECTED_EMPLOYEE)
+		) {
+			const user = RequestContext.currentUser();
+			employeeIds = [user.employeeId];
+		} 
+		const query = this.timeSlotRepository.createQueryBuilder();
+		query.innerJoin(`${query.alias}.employee`, 'employee');
+		query.innerJoinAndSelect(`${query.alias}.timeLogs`, 'timeLogs');
+		query.innerJoinAndSelect(`${query.alias}.screenshots`, 'screenshots');
+		query.andWhere(`"${query.alias}"."id" IN (:...ids)`, { ids });
 
-		const timeSlots = await this.timeSlotRepository.find({
-			where: { id: In(ids) }
-		});
-		for (let i = 0; i < ids.length; i++) {
-			const timeSlot = await this.timeSlotRepository.findOne({
-				where: {
-					startedAt: timeSlots[i].startedAt
-				},
-				relations: ['timeLogs']
-			});
+		if (employeeIds.length) {
+			query.andWhere(
+				`"${query.alias}"."employeeId" IN (:...employeeId)`,
+				{ employeeId: employeeIds }
+			);
+		}
+
+		const timeSlots = await query.getMany();
+		if (!timeSlots.length) {
+			return true;
+		}
+
+		await this.timeSlotRepository.remove(timeSlots);
+		/* timeSlots.forEach(async (timeSlot: ITimeSlot) => {
 			if (timeSlot && timeSlot.timeLogs.length > 0) {
 				const deleteSlotPromise = timeSlot.timeLogs.map(
 					async (timeLog) => {
 						await this.commandBus.execute(
 							new DeleteTimeSpanCommand(
-								{
-									start: timeSlot.startedAt,
-									end: timeSlot.stoppedAt
-								},
+								{ start: timeSlot.startedAt, end: timeSlot.stoppedAt },
 								timeLog
 							)
 						);
 						return;
 					}
 				);
-
 				await Promise.all(deleteSlotPromise);
 			}
-		}
+		}); */
 		return true;
 	}
 }
