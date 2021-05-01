8f7da9971b06aeab9a48a59e7392a45ac20597dc
Ruslan Konviser <evereq@gmail.com>
2021-05-01 19:44:09 +0300

    Update default-users.ts

--- packages/core/src/user/default-users.ts
+++ packages/core/src/user/default-users.ts
@@ -11,18 +11,6 @@ export const DEFAULT_SUPER_ADMINS = [
 	}
 ];
 
-// TODO: just remove this one and use DEFAULT_SUPER_ADMINS instead
-export const BASIC_SUPER_ADMINS = [
-	{
-		email: 'admin@ever.co',
-		password: 'admin',
-		firstName: 'Super',
-		lastName: 'Admin',
-		imageUrl: 'assets/images/avatar-default.svg',
-		preferredLanguage: LanguagesEnum.ENGLISH
-	}
-];
-
 export const DEFAULT_ADMINS = [
 	{
 		email: 'local.admin@ever.co',
