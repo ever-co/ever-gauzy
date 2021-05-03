4017feca56e1db021eae6558f5f714251c244c26
Ajay Khandla <ajay.khandla97@gmail.com>
2021-05-03 22:06:57 +0530

    fix: deep scan

--- packages/core/src/user/delete-all-data/delete-all-data.module.ts
+++ packages/core/src/user/delete-all-data/delete-all-data.module.ts
@@ -2,11 +2,10 @@
 // MIT License, see https://github.com/xmlking/ngx-starter-kit/blob/develop/LICENSE
 // Copyright (c) 2018 Sumanth Chinthagunta
 
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RouterModule } from 'nest-router';
-import { Employee, Organization, Tag, User, UserOrganization } from 'core/entities/internal';
 import { DeleteAllDataService } from './delete-all-data.service';
 import { SharedModule } from 'shared/shared.module';
 import { coreEntities } from 'core';
