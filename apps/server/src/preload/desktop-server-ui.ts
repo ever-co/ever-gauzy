fce82c0f6ab5e394c711fe0c7b96f80700df6f34
sutralia <sutralian@gmail.com>
2021-07-04 23:36:58 +0700

    feat: gauzy server initial server

--- apps/server/src/preload/desktop-server-ui.ts
+++ apps/server/src/preload/desktop-server-ui.ts
@@ -1,31 +1,40 @@
-import { fork } from 'child_process';
-import { LocalStore } from '@gauzy/desktop-libs';
+import { spawn } from 'child_process';
 import path from 'path';
+import { app } from 'electron';
+import os from 'os';
+const appPath = app.getPath('userData');
 
 const runServerUI = () => {
-    const { uiPath } = prepareServerUi();
-    const uiService = fork(uiPath, { silent: true, detached: true});
-    uiService.stdout.on('data', (data) => {
-        console.log('SERVER UI STATE LOGS -> ', data.toString());
-        uiService.unref();
-        process.exit(0);
-    });
+    try {
+        const { uiPath } = prepareServerUi();
+        console.log('ui path', uiPath);
+        const uiService = spawn(uiPath, { detached: true});
+        uiService.stdout.on('data', (data) => {
+            console.log('SERVER UI STATE LOGS -> ', data.toString());
+        });
+    } catch (error) {
+        console.log('error on runserverui', error);
+    }
 }
 
 const prepareServerUi = (): {
     uiPath: string
 } => {
-    // const {
-    //     apiBaseUrl
-    // } = LocalStore.getStore('configs');
+    let appName:string = '';
+    switch (os.platform()) {
+        case 'win32':
+            appName = 'gauzy-server.exe';
+            break;
+        case 'darwin':
+            appName = 'gauzy-server';
+            break;
+        default:
+            break;
+    }
     return {
-        uiPath: path.join(__dirname, 'ui-server.js')
+        uiPath: path.join(appPath, appName)
     };
 }
-
-export default function () {
-    console.log('Before Run Api Server');
-    runServerUI();
-}
+runServerUI();
 
 
