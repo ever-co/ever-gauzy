27760041ffd242c67ffc348e148aca9e5248bf14
sutralia <sutralian@gmail.com>
2021-08-01 23:34:46 +0700

    fix: gauzy server on windows

--- apps/server/src/preload/ui-server.ts
+++ apps/server/src/preload/ui-server.ts
@@ -8,12 +8,13 @@ const url = require('url');
 console.log('server started');
 
 async function runServer() {
+    const portUi = process.env.uiPort ? Number(process.env.uiPort) : 8084;
     http.createServer(function (req, res) {
         file.serve(req, res);
         console.log('server ui started');
         console.log(url.parse(req.url, true).query);
-    }).listen(8084, '0.0.0.0', () => {
-        console.log('listen ui on port 8084');
+    }).listen(portUi, '0.0.0.0', () => {
+        console.log(`listen ui on port ${portUi}`);
     })
     return true;
 }
