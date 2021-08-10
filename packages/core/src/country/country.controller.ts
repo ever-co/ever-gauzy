4b595be2acd922922359fc1b905a12f7c6fdb7e4
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-08-10 13:32:05 +0530

    fix: currency & country should not be extend crud

--- packages/core/src/country/country.controller.ts
+++ packages/core/src/country/country.controller.ts
@@ -1,15 +1,30 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IPagination } from '@gauzy/contracts';
 import { Public } from './../shared/decorators';
-import { CrudController } from './../core/crud';
 import { Country } from './country.entity';
 import { CountryService } from './country.service';
 
 @ApiTags('Country')
 @Controller()
 @Public()
-export class CountryController extends CrudController<Country> {
-	constructor(private readonly countryService: CountryService) {
-		super(countryService);
+export class CountryController {
+	constructor(
+		private readonly countryService: CountryService
+	) {}
+
+	@ApiOperation({ summary: 'Find all countries.' })
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Found countries',
+		type: Country
+	})
+	@ApiResponse({
+		status: HttpStatus.NOT_FOUND,
+		description: 'Record not found'
+	})
+	@Get()
+	async findAll(): Promise<IPagination<Country>> {
+		return this.countryService.findAll();
 	}
 }
--- packages/core/src/currency/currency.controller.ts
+++ packages/core/src/currency/currency.controller.ts
@@ -1,17 +1,30 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IPagination } from '@gauzy/contracts';
 import { Public } from './../shared/decorators';
-import { CrudController } from './../core/crud';
 import { Currency } from './currency.entity';
 import { CurrencyService } from './currency.service';
 
 @ApiTags('Currency')
 @Controller()
 @Public()
-export class CurrencyController extends CrudController<Currency> {
+export class CurrencyController {
 	constructor(
 		private readonly currencyService: CurrencyService
-	) {
-		super(currencyService);
+	) { }
+
+	@ApiOperation({ summary: 'Find all currencies.' })
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Found currencies',
+		type: Currency
+	})
+	@ApiResponse({
+		status: HttpStatus.NOT_FOUND,
+		description: 'Record not found'
+	})
+	@Get()
+	async findAll(): Promise<IPagination<Currency>> {
+		return this.currencyService.findAll();
 	}
 }
