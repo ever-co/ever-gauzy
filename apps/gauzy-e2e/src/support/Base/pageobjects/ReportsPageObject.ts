11a5fec3525a1873f3789cd774347076030243d5
Yavor <bearhold@gmail.com>
2021-08-18 11:25:40 +0300

    feat: reports additional e2e tests with gherkin

--- apps/gauzy-e2e/src/support/Base/pageobjects/ReportsPageObject.ts
+++ apps/gauzy-e2e/src/support/Base/pageobjects/ReportsPageObject.ts
@@ -2,5 +2,20 @@ export const ReportsPage = {
 	headerTextCss: 'nb-card-header > h4',
 	subheaderTextCss: 'div.mb-4 > h5',
 	titleCss: 'div.align-items-center > h6',
-	checkboxCss: 'input.native-input'
+	checkboxCss: 'input.native-input',
+	sidebarBtnCss: 'span.menu-title',
+	totalHoursCss:
+		'ga-daily-statistics > div.row > div.col-sm-4 > nb-card > nb-card-body > div.h1',
+	sliderBtnCss:
+		'div[class="col-auto filter-item ml-auto pl-1 ng-star-inserted"] > button[status="basic"]',
+	sliderCss: 'ng5-slider.ng5-slider',
+	timeAndActivityProjectCss:
+		'div[class="col-sm-2 project-name"] > span.ng-star-inserted',
+	amountOwedEmployeeCss:
+		'div[class="col-sm-3 employee-name"] > ngx-avatar > div[class="row align-items-center"] > div.col > div.ng-star-inserted',
+	projectsBudgetsProjectCss:
+		'div[class="col-3 project-name"] > span.ng-star-inserted',
+	clientsBudgetsClientCss:
+		'div[class="col-3 client-name"] > span.ng-star-inserted',
+	progressContainerCss: 'div.progress-container > div.progress-value > span.ng-star-inserted'
 };
--- apps/gauzy-e2e/src/support/Base/pages/Reports.po.ts
+++ apps/gauzy-e2e/src/support/Base/pages/Reports.po.ts
@@ -1,4 +1,13 @@
-import { verifyText, verifyStateByIndex } from '../utils/util';
+import {
+	verifyText,
+	verifyStateByIndex,
+	verifyElementIsVisible,
+	clickElementByText,
+	clickButton,
+	triggerSlider,
+	verifyTextNotExisting,
+	forceClickElementByText
+} from '../utils/util';
 import { ReportsPage } from '../pageobjects/ReportsPageObject';
 
 export const verifyHeader = (text) => {
@@ -15,4 +24,56 @@ export const verifyTitle = (text) => {
 
 export const verifyCheckboxState = (index, state) => {
 	verifyStateByIndex(ReportsPage.checkboxCss, index, state);
+};
+
+export const sidebarBtnVidible = () => {
+	verifyElementIsVisible(ReportsPage.sidebarBtnCss);
+};
+
+export const clickSidebarBtn = (text) => {
+	clickElementByText(ReportsPage.sidebarBtnCss, text);
+};
+
+export const clickInnerSidebarBtn = (text) => {
+	forceClickElementByText(ReportsPage.sidebarBtnCss, text);
+};
+
+export const activityLevelBtnVisible = () => {
+	verifyElementIsVisible(ReportsPage.sliderBtnCss);
+};
+
+export const clickActivityLevelBtn = () => {
+	clickButton(ReportsPage.sliderBtnCss);
+};
+
+export const sliderVisible = () => {
+	verifyElementIsVisible(ReportsPage.sliderCss);
+};
+
+export const changeSliderValue = () => {
+	triggerSlider(ReportsPage.sliderCss);
+};
+
+export const verifyTimeLogged = (time) => {
+	verifyTextNotExisting(ReportsPage.totalHoursCss, time);
+};
+
+export const verifyTimeAndActivityProject = (project) => {
+	verifyText(ReportsPage.timeAndActivityProjectCss, project);
+};
+
+export const verifyEmployeeWorked = (employee) => {
+	verifyText(ReportsPage.amountOwedEmployeeCss, employee);
+};
+
+export const verifyProjectBudgetsProject = (project) => {
+	verifyText(ReportsPage.projectsBudgetsProjectCss, project);
+};
+
+export const verifyClientsBudgetsClient = (client) => {
+	verifyText(ReportsPage.clientsBudgetsClientCss, client);
+};
+
+export const verifyClientsBudgetsProgress = (progress) => {
+	verifyTextNotExisting(ReportsPage.progressContainerCss, progress);
 };
