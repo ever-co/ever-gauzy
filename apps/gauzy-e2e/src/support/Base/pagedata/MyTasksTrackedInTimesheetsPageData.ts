45dfc2b04099c5f108082af7a6ebd5a25db25886
dokata99 <72043010+dokata99@users.noreply.github.com>
2021-08-30 15:13:42 +0300

    Feat: My Tasks Tracked in Timesheets test

---
+++ apps/gauzy-e2e/src/support/Base/pagedata/MyTasksTrackedInTimesheetsPageData.ts
@@ -0,0 +1,10 @@
+export const MyTasksTrackedInTimesheetsPageData = {
+	defaultTaskProject: 'Gauzy Web Site',
+	defaultTaskTitle: 'Feature: All Integrations should work with useHash',
+	editTaskTitle: 'Feature: Invite users with emails',
+	defaultTaskEstimateDays: 1,
+	defaultTaskEstimateHours: 8,
+	defauleStatus: 'Todo',
+	defaultTaskEstimateMinutes: 59,
+	defaultTaskDescription: 'Very important task'
+};
