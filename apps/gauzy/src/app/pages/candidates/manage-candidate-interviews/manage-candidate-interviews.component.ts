d97b7d670d6196eaadc18304ce62709bd7dd0272
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-09-14 17:37:28 +0530

    fix: manage candidate interview page

--- apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/manage-candidate-interviews.component.ts
+++ apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/manage-candidate-interviews.component.ts
@@ -1,14 +1,14 @@
 import { ICandidateInterview, IOrganization } from '@gauzy/contracts';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { TranslationBaseComponent } from '../../../@shared/language-base/translation-base.component';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { first, filter } from 'rxjs/operators';
+import { first, filter, tap } from 'rxjs/operators';
+import { Subject } from 'rxjs/internal/Subject';
 import { NbDialogService } from '@nebular/theme';
-import { CandidateInterviewMutationComponent } from '../../../@shared/candidate/candidate-interview-mutation/candidate-interview-mutation.component';
-import { CandidateInterviewService } from '../../../@core/services/candidate-interview.service';
-import { Store } from '../../../@core/services/store.service';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { ToastrService } from '../../../@core/services/toastr.service';
+import { TranslationBaseComponent } from '../../../@shared/language-base';
+import { CandidateInterviewMutationComponent } from '../../../@shared/candidate/candidate-interview-mutation/candidate-interview-mutation.component';
+import { CandidateInterviewService, Store, ToastrService } from '../../../@core/services';
+
 @UntilDestroy({ checkProperties: true })
 @Component({
 	selector: 'ga-manage-candidate-interviews',
@@ -17,38 +17,51 @@ import { ToastrService } from '../../../@core/services/toastr.service';
 })
 export class ManageCandidateInterviewsComponent
 	extends TranslationBaseComponent
-	implements OnInit, OnDestroy {
+	implements AfterViewInit, OnInit, OnDestroy {
+
+	interviews$: Subject<any> = new Subject();
+	loading: boolean;
 	tabs: any[];
-	interviewList: ICandidateInterview[];
+	interviews: ICandidateInterview[] = [];
 	organization: IOrganization;
+
 	constructor(
-		readonly translateService: TranslateService,
-		private dialogService: NbDialogService,
-		private toastrService: ToastrService,
-		public candidateInterviewService: CandidateInterviewService,
-		private store: Store
+		public readonly translateService: TranslateService,
+		private readonly dialogService: NbDialogService,
+		private readonly toastrService: ToastrService,
+		public readonly candidateInterviewService: CandidateInterviewService,
+		private readonly store: Store
 	) {
 		super(translateService);
 	}
+	
 	ngOnInit() {
-		this.loadTabs();
+		this._loadTabs();
 		this._applyTranslationOnTabs();
-		this.store.selectedOrganization$
+	}
+
+	ngAfterViewInit() {
+		this.interviews$
 			.pipe(
-				filter((organization) => !!organization),
+				tap(() => this._getInterviews()),
 				untilDestroyed(this)
 			)
-			.subscribe((organization: IOrganization) => {
-				if (organization) {
-					this.organization = organization;
-					this.loadInterviews();
-				}
-			});
+			.subscribe();
+		this.store.selectedOrganization$
+			.pipe(
+				filter((organization: IOrganization) => !!organization),
+				tap((organization: IOrganization) => this.organization = organization),
+				tap(() => this.interviews$.next()),
+				untilDestroyed(this)
+			)
+			.subscribe();
 	}
+
 	getRoute(tab: string): string {
 		return `/pages/employees/candidates/interviews/${tab}`;
 	}
-	loadTabs() {
+
+	private _loadTabs() {
 		this.tabs = [
 			{
 				title: this.getTranslation(
@@ -73,53 +86,53 @@ export class ManageCandidateInterviewsComponent
 			}
 		];
 	}
-	async add() {
+
+	async addInterview() {
 		const dialog = this.dialogService.open(
 			CandidateInterviewMutationComponent,
 			{
 				context: {
-					header: this.getTranslation(
-						'CANDIDATES_PAGE.EDIT_CANDIDATE.INTERVIEW.SCHEDULE_INTERVIEW'
-					),
+					header: this.getTranslation('CANDIDATES_PAGE.EDIT_CANDIDATE.INTERVIEW.SCHEDULE_INTERVIEW'),
 					isCalendar: true,
-					interviewList: this.interviewList
+					interviewList: this.interviews
 				}
 			}
 		);
 		const data = await dialog.onClose.pipe(first()).toPromise();
 		if (data) {
-			this.toastrService.success(
-				`TOASTR.MESSAGE.CANDIDATE_EDIT_CREATED`,
-				{
-					name: data.title
-				}
-			);
-			this.loadInterviews();
+			this.toastrService.success( `TOASTR.MESSAGE.CANDIDATE_EDIT_CREATED`, {
+				name: data.title
+			});
 		}
+		this.interviews$.next();
 	}
-	async loadInterviews() {
+	
+	private async _getInterviews() {
 		const { tenantId } = this.store.user;
 		const { id: organizationId } = this.organization;
-		const res = await this.candidateInterviewService.getAll(
-			[
+
+		this.interviews = (
+			await this.candidateInterviewService.getAll([
 				'feedbacks',
 				'interviewers',
 				'technologies',
 				'personalQualities',
 				'candidate'
-			],
-			{ organizationId, tenantId }
-		);
-		if (res) {
-			this.interviewList = res.items;
-		}
+			], {
+				organizationId,
+				tenantId
+			})
+		).items;
 	}
+
 	private _applyTranslationOnTabs() {
 		this.translateService.onLangChange
-			.pipe(untilDestroyed(this))
-			.subscribe(() => {
-				this.loadTabs();
-			});
+			.pipe(
+				tap(() => this._loadTabs()),
+				untilDestroyed(this)
+			)
+			.subscribe();
 	}
+
 	ngOnDestroy() {}
 }
