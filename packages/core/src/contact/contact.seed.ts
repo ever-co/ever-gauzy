e87794719995b5230a53e06d6d8cf9ba301ba3a1
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-10-12 10:51:21 +0530

    fix: organization default random location

--- packages/core/src/contact/contact.seed.ts
+++ packages/core/src/contact/contact.seed.ts
@@ -1,34 +1,49 @@
 import { Connection } from 'typeorm';
 import * as faker from 'faker';
-import { IContact, ICountry, IOrganization, ITenant } from '@gauzy/contracts';
-import { Contact, Country } from './../core/entities/internal';
+import { IContact, IOrganization, ITenant } from '@gauzy/contracts';
+import { Contact } from './../core/entities/internal';
 
 export const createRandomContacts = async (
 	connection: Connection,
 	tenant: ITenant,
 	organizations: IOrganization[],
 	noOfRandomContacts: number
-): Promise<Contact[]> => {
-	const countries: ICountry[] = await connection.manager.find(Country);
+): Promise<IContact[]> => {
 	const contacts: IContact[] = [];
 	for (let i = 0; i < noOfRandomContacts; i++) {
-		for (const organization of organizations) {
-			const contact: IContact = new Contact();
-			contact.firstName = faker.name.firstName();
-			contact.lastName = faker.name.lastName();
-			contact.website = faker.internet.url();
-			contact.address = faker.address.streetAddress();
-			contact.address2 = faker.address.secondaryAddress();
-			contact.city = faker.address.city();
-			contact.country = faker.random.arrayElement(countries).isoCode;
-			contact.name = contact.firstName + ' ' + contact.lastName;
-			contact.longitude = +faker.address.longitude();
-			contact.latitude = +faker.address.latitude();
-			contact.postcode = faker.address.zipCode();
-			contact.organization = organization;
-			contact.tenant = tenant;
+		for await (const organization of organizations) {
+			const contact: IContact = getRandomContact(
+				tenant,
+				organization
+			);
+
+			// organization primary contact location
+			organization.contact = contact;
+			await connection.manager.save(organization);
+
 			contacts.push(contact);
 		}
 	}
 	return await connection.manager.save(contacts);
 };
+
+export const getRandomContact = (
+	tenant: ITenant,
+	organization: IOrganization
+) => {
+	const contact: IContact = new Contact();
+	contact.firstName = faker.name.firstName();
+	contact.lastName = faker.name.lastName();
+	contact.website = faker.internet.url();
+	contact.address = faker.address.streetAddress();
+	contact.address2 = faker.address.secondaryAddress();
+	contact.city = faker.address.city();
+	contact.country = faker.address.countryCode();
+	contact.name = contact.firstName + ' ' + contact.lastName;
+	contact.longitude = +faker.address.longitude();
+	contact.latitude = +faker.address.latitude();
+	contact.postcode = faker.address.zipCode();
+	contact.organization = organization;
+	contact.tenant = tenant;
+	return contact;
+}
