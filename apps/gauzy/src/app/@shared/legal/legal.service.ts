675d1c67293e26244400711420d0176b52a0a8c2
Chirag Parmar <chiraginfotech93@gmail.com>
2021-10-16 11:48:03 +0530

    feat: get dynamic content for term and policy page

--- apps/gauzy/src/app/@shared/legal/legal.service.ts
+++ apps/gauzy/src/app/@shared/legal/legal.service.ts
@@ -2,14 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  	providedIn: 'root'
+	providedIn: 'root'
 })
 export class LegalService {
 	constructor(
 		private readonly http: HttpClient
-	) {}
-
-	getJsonFromUrl(url: string) {
-		return this.http.get<any>(url);
+	) { }
+	/**
+	 * Load content from iubenda
+	 * 
+	 * @param url https://www.iubenda.com/api/privacy-policy/18120170
+	 */
+	getContentFromFromUrl(url: string) {
+		return this.http
+			.get(url)
+			.pipe()
+			.toPromise();
 	}
 }
