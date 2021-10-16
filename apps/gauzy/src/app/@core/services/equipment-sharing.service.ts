d443d5d64fdc8c87dc5a577a306a4a985ab54fb0
Chirag Parmar <chiraginfotech93@gmail.com>
2021-10-16 18:02:11 +0530

    fix: removed deprecate code from organization module services

--- apps/gauzy/src/app/@core/services/equipment-sharing.service.ts
+++ apps/gauzy/src/app/@core/services/equipment-sharing.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { first } from 'rxjs/operators';
+import { firstValueFrom } from 'rxjs';
 import { IEquipmentSharing, IEquipmentSharingRequest } from '@gauzy/contracts';
 import { API_PREFIX } from '../constants/app.constants';
 
@@ -11,80 +11,80 @@ export class EquipmentSharingService {
 	constructor(private http: HttpClient) {}
 
 	getAll(): Promise<IEquipmentSharing[]> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.get<IEquipmentSharing[]>(`${this.EQUIPMENT_SHARING_URL}`)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 
 	getByOrganizationId(id): Promise<IEquipmentSharing[]> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.get<IEquipmentSharing[]>(
 				`${this.EQUIPMENT_SHARING_URL}/organization/` + id
 			)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 
 	getByAuthorUserId(id): Promise<IEquipmentSharing[]> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.get<IEquipmentSharing[]>(
 				`${this.EQUIPMENT_SHARING_URL}/employee/` + id
 			)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 
 	delete(id: string): Promise<any> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.delete(`${this.EQUIPMENT_SHARING_URL}/${id}`)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 
 	create(
 		equipmentSharing: IEquipmentSharingRequest,
 		id: string
 	): Promise<IEquipmentSharing> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.post<IEquipmentSharing>(
 				`${this.EQUIPMENT_SHARING_URL}/organization/${id}`,
 				equipmentSharing
 			)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 
 	update(
 		id: string,
 		equipmentSharing: IEquipmentSharingRequest
 	): Promise<IEquipmentSharing> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.put<IEquipmentSharing>(
 				`${this.EQUIPMENT_SHARING_URL}/${id}`,
 				equipmentSharing
 			)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 
 	approval(id): Promise<IEquipmentSharing> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.put<IEquipmentSharing>(
 				`${this.EQUIPMENT_SHARING_URL}/approval/${id}`,
 				null
 			)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 
 	refuse(id): Promise<IEquipmentSharing> {
-		return this.http
+		return firstValueFrom(
+			this.http
 			.put<IEquipmentSharing>(
 				`${this.EQUIPMENT_SHARING_URL}/refuse/${id}`,
 				null
 			)
-			.pipe(first())
-			.toPromise();
+		);
 	}
 }
