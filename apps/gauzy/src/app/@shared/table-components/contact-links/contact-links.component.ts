7eb039319cc8339aae1cad881b292f1a8b040bac
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-11-01 11:47:48 +0530

    fix: #2638 view button on contact page

--- apps/gauzy/src/app/@shared/table-components/contact-links/contact-links.component.ts
+++ apps/gauzy/src/app/@shared/table-components/contact-links/contact-links.component.ts
@@ -3,11 +3,13 @@ import { Router } from '@angular/router';
 
 @Component({
 	selector: 'ngx-contact-links',
-	template: `<ng-container *ngIf="value">
-		<a *ngIf="value?.name" (click)="navigateToContact()" class="link-text">
-			{{ value.name }}
-		</a>
-	</ng-container>`,
+	template: `
+		<ng-container *ngIf="value">
+			<a *ngIf="value?.name" (click)="navigateToContact()" class="link-text">
+				{{ value.name }}
+			</a>
+		</ng-container>
+	`,
 	styles: [
 		`
 			.link-text {
@@ -24,29 +26,19 @@ import { Router } from '@angular/router';
 export class ContactLinksComponent {
 	@Input()
 	rowData: any;
+
 	@Input()
 	value: any;
 
-	constructor(private readonly router: Router) { }
+	constructor(
+		private readonly _router: Router
+	) {}
 
 	navigateToContact() {
-		this.router.navigate([`/pages/contacts/view/${this.value.id}`]);
-		// if (this.value.contactType == ContactType.CLIENT) {
-		// 	this.router.navigate([`/pages/contacts/view/${this.value.id}`], {
-		// 		queryParams: { id: this.value.id }
-		// 	});
-		// }
-
-		// if (this.value.contactType == ContactType.CUSTOMER) {
-		// 	this.router.navigate([`/pages/contacts/customers`], {
-		// 		queryParams: { id: this.value.id }
-		// 	});
-		// }
-
-		// if (this.value.contactType == ContactType.LEAD) {
-		// 	this.router.navigate([`/pages/contacts/leads`], {
-		// 		queryParams: { id: this.value.id }
-		// 	});
-		// }
+		if (!this.value) {
+			return;
+		}
+		
+		this._router.navigate([`/pages/contacts/view/${this.value.id}`, ]);
 	}
 }
