4fb47ea2cd1040a048272bd53c6626d2f3886dd7
Ruslan Konviser <evereq@gmail.com>
2021-12-15 17:35:35 +0200

    fix: seed of initial random data

--- packages/core/src/organization-team/default-organization-teams.ts
+++ packages/core/src/organization-team/default-organization-teams.ts
@@ -5,35 +5,25 @@ export const DEFAULT_ORGANIZATION_TEAMS = [
 			'admin@ever.co',
 			'ruslan@ever.co',
 			'alish@ever.co',
-			'blagovest@ever.co',
-			'elvis@ever.co',
-			'hristo@ever.co',
-			'alex@ever.co',
-			'pavel@ever.co',
-			'yavor@ever.co',
-			'tsvetelina@ever.co',
 			'julia@ever.co'
 		],
 		manager: ['ruslan@ever.co']
 	},
 	{
 		name: 'Contractors',
-		defaultMembers: [
-			'dimana@ever.co',
-			'deko898@hotmail.com',
-			'muiz@smooper.xyz',
+		defaultMembers: [			
 			'ckhandla94@gmail.com'
 		],
-		manager: ['ruslan@ever.co', 'rachit@ever.co']
+		manager: ['ruslan@ever.co']
 	},
 	{
 		name: 'Designers',
-		defaultMembers: ['julia@ever.co', 'yordan@ever.co'],
+		defaultMembers: ['julia@ever.co'],
 		manager: []
 	},
 	{
 		name: 'QA',
-		defaultMembers: ['julia@ever.co', 'yordan@ever.co'],
+		defaultMembers: ['julia@ever.co'],
 		manager: []
 	}
 ];
