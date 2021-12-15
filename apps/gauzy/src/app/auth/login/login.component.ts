20cf068293ae1af23966cbd1804cfc4257700889
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-12-15 15:55:29 +0530

    fix: created demo credentials block for demo server

--- apps/gauzy/src/app/auth/login/login.component.ts
+++ apps/gauzy/src/app/auth/login/login.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NbAuthService, NbLoginComponent, NB_AUTH_OPTIONS } from '@nebular/auth';
 import { ElectronService } from 'ngx-electron';
+import { RolesEnum } from '@gauzy/contracts/';
 import { environment } from './../../../environments/environment';
+import { FormGroupDirective } from '@angular/forms';
 
 @Component({
 	selector: 'ngx-login',
@@ -10,9 +12,12 @@ import { environment } from './../../../environments/environment';
 	styleUrls: ['./login.component.scss']
 })
 export class NgxLoginComponent extends NbLoginComponent implements OnInit {
+
+	@ViewChild('form') private readonly form: FormGroupDirective;
 	
 	environment = environment;
-	isShown: boolean = false ;
+	isShown: boolean = false;
+	RolesEnum = RolesEnum;
 
 	constructor(
 		public readonly electronService: ElectronService,
@@ -25,15 +30,48 @@ export class NgxLoginComponent extends NbLoginComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		if (this.environment.DEMO) {
-			this.user.email = 'admin@ever.co';
-			this.user.password = 'admin';
-		}
+		this.autoFillCredential();
 	}
 
 	collapseDemo() {
 		if (this.environment.DEMO) {
 			this.isShown = !this.isShown;
+		}
+	}
+
+	/**
+	 * Autofill Super Admin Credential By Default
+	 */
+	autoFillCredential() {
+		if (this.environment.DEMO) {
+			this.user.email = this.environment.DEMO_SUPER_ADMIN_EMAIL;
+			this.user.password = this.environment.DEMO_SUPER_ADMIN_PASSWORD;
+		}
+	}
+
+	/**
+	 * Automatic Login For Demo Server
+	 * 
+	 * @param role 
+	 */
+	autoLogin(role: RolesEnum) {
+		if (this.environment.DEMO) {
+			switch (role) {
+				case RolesEnum.SUPER_ADMIN:
+					this.autoFillCredential();
+					break;
+				case RolesEnum.ADMIN:
+					this.user.email = this.environment.DEMO_ADMIN_EMAIL;
+					this.user.password = this.environment.DEMO_ADMIN_PASSWORD;
+					break;
+				case RolesEnum.EMPLOYEE:
+					this.user.email = this.environment.DEMO_EMPLOYEE_EMAIL;
+					this.user.password = this.environment.DEMO_EMPLOYEE_PASSWORD;
+					break;
+				default:
+					break;
+			}
+			this.form.ngSubmit.emit();
 		}
 	}
 }
