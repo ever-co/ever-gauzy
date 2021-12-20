4ee8c2b3a2dcab6e2be9ce6e532f9df2196ad08d
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-12-20 17:51:26 +0530

    feat: #3479 role class create/update and duplicate validators

--- packages/core/src/role/role.controller.ts
+++ packages/core/src/role/role.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, HttpStatus, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put, Query, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { IPagination, IRole, PermissionsEnum } from '@gauzy/contracts';
 import { RoleService } from './role.service';
 import { CrudController } from './../core/crud';
 import { Role } from './role.entity';
-import { ParseJsonPipe } from './../shared/pipes';
+import { ParseJsonPipe, UUIDValidationPipe } from './../shared/pipes';
 import { PermissionGuard, TenantPermissionGuard } from './../shared/guards';
 import { Permissions } from './../shared/decorators';
+import { CreateRoleDTO } from './dto';
+import { UpdateResult } from 'typeorm';
 
 @ApiTags('Role')
 @UseGuards(TenantPermissionGuard)
@@ -47,6 +49,42 @@ export class RoleController extends CrudController<Role> {
 	@Get()
 	async findAll(): Promise<IPagination<IRole>> {
 		return this.roleService.findAll();
+	}
+
+	/**
+	 * Create role
+	 * 
+	 * @param entity 
+	 * @returns 
+	 */
+	@UseGuards(PermissionGuard)
+	@Permissions(PermissionsEnum.CHANGE_ROLES_PERMISSIONS)
+	@HttpCode(HttpStatus.CREATED)
+	@Post()
+	@UsePipes(new ValidationPipe({ transform: true }))
+	async create(
+		@Body() entity: CreateRoleDTO
+	): Promise<IRole> {
+		return this.roleService.create(entity);
+	}
+
+	/**
+	 * Update role
+	 * 
+	 * @param id 
+	 * @param entity 
+	 * @returns 
+	 */
+	@UseGuards(PermissionGuard)
+	@Permissions(PermissionsEnum.CHANGE_ROLES_PERMISSIONS)
+	@HttpCode(HttpStatus.ACCEPTED)
+	@Put(':id')
+	@UsePipes(new ValidationPipe({ transform: true }))
+	async update(
+		@Param('id', UUIDValidationPipe) id: string,
+		@Body() entity: CreateRoleDTO
+	): Promise<UpdateResult | IRole> {
+		return await this.roleService.update(id, entity);
 	}
 
 	@ApiOperation({ summary: 'Import role from self hosted to gauzy cloud hosted in bulk' })
--- packages/core/src/shared/decorators/validations/index.ts
+++ packages/core/src/shared/decorators/validations/index.ts
@@ -1,1 +1,2 @@
 export * from './match.decorator';
+export * from './is-role-already.decorator';
