bc271577373026fdb8e99eaeda4724fba1dc4efd
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-12-20 14:46:41 +0530

    fix: export only DTO class

--- packages/core/src/user/dto/index.ts
+++ packages/core/src/user/dto/index.ts
@@ -1,3 +1,3 @@
-export * from './create-user.dto';
-export * from './register-user.dto';
-export * from './login-user.dto';
+export { CreateUserDTO } from './create-user.dto';
+export { RegisterUserDTO } from './register-user.dto';
+export { LoginUserDTO } from './login-user.dto';
