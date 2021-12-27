8b4cddbb79486a24a0939987a0f94cd8137a8bdb
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-12-27 12:54:16 +0530

    fix: login & register removed white space in credentials

--- packages/core/src/user/dto/create-user.dto.ts
+++ packages/core/src/user/dto/create-user.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty } from "class-validator";
 import { IUserCreateInput } from "@gauzy/contracts";
+import { Transform, TransformFnParams } from "class-transformer";
 
 /**
  * Create User DTO validation
@@ -10,12 +11,15 @@ export class CreateUserDTO implements IUserCreateInput {
     @ApiProperty({ type: () => String })
     @IsNotEmpty()
     @IsEmail()
+    @Transform((params: TransformFnParams) => params.value.trim())
     readonly email: string;
 
     @ApiProperty({ type: () => String })
+    @Transform((params: TransformFnParams) => params.value.trim())
     readonly firstName: string;
 
     @ApiProperty({ type: () => String })
+    @Transform((params: TransformFnParams) => params.value.trim())
     readonly lastName: string;
 
     @ApiProperty({ type: () => String })
