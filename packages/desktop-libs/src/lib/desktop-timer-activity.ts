6cc765e6700fc177715811fafb380464438b7ac2
RAHUL RATHORE <rahulrathore576@gmail.com>
2021-12-29 18:45:27 +0530

    fix: return  back promise code

--- packages/desktop-libs/src/lib/desktop-timer-activity.ts
+++ packages/desktop-libs/src/lib/desktop-timer-activity.ts
@@ -17,12 +17,17 @@ export const TimerData = {
 			eventId: query.eventId
 		});
 		if (eventExist.length === 0) {
-			await knex('window-events').insert(query);
+			await knex('window-events').insert(query).then(true);
 		} else {
 			delete query.updated_at;
 			delete query.activityId;
 			// delete query.timerId;
-			await knex('window-events').where({ eventId: query.eventId }).update(query);
+			await knex('window-events')
+				.where({
+					eventId: query.eventId
+				})
+				.update(query)
+				.then(true);
 		}
 	},
 	updateWindowEventUpload: async (knex, data) => {
@@ -32,38 +37,63 @@ export const TimerData = {
 			})
 			.update({
 				activityId: data.activityId
-			});
+			})
+			.then((res) => res);
 	},
 	deleteWindowEventAfterSended: async (knex, data) => {
 		return await knex('window-events')
 			.whereIn('id', data.activityIds)
-			.del();
+			.del()
+			.then((res) => res);
 	},
 	updateTimerUpload: async (knex, data) => {
 		return await knex('timer')
 			.where({
 				id: data.id
 			})
-			.update(data);
+			.update(data)
+			.then((res) => res);
 	},
+
 	getTimer: async (knex, id) => {
-		return await knex('timer').where({ id: id });
+		return await knex('timer')
+			.where({
+				id: id
+			})
+			.then((res) => res);
 	},
+
 	getAfk: async (knex, timerId) => {
-		return await knex('afk-events').where({ timerId: timerId });
+		return await knex('afk-events')
+			.where({
+				timerId: timerId
+			})
+			.then((res) => res);
 	},
+
 	deleteAfk: async (knex, data) => {
-		return await knex('afk-events').where({ id: data.idAfk }).del();
+		return await knex('afk-events')
+			.where({
+				id: data.idAfk
+			})
+			.del()
+			.then((res) => res);
 	},
+
 	getWindowEvent: async (knex, timerId) => {
-		return await knex('window-events').where({ timerId: timerId });
+		return await knex('window-events')
+			.where({
+				timerId: timerId
+			})
+			.then((res) => res);
 	},
+
 	insertAfkEvent: async (knex, query) => {
 		const eventExist = await knex('afk-events').where({
 			eventId: query.eventId
 		});
 		if (eventExist.length === 0) {
-			await knex('afk-events').insert(query);
+			await knex('afk-events').insert(query).then(true);
 		} else {
 			delete query.updated_at;
 			delete query.timeSlotId;
@@ -72,35 +102,44 @@ export const TimerData = {
 				.where({
 					eventId: query.eventId
 				})
-				.update(query);
+				.update(query)
+				.then(true);
 		}
 	},
+
 	getLastTimer: async (knex, appInfo) => {
-		return await knex('timer')
+		const result = await knex('timer')
 			.where({
 				projectId: appInfo.projectId,
 				userId: appInfo.employeeId
 			})
 			.orderBy('created_at', 'desc')
 			.limit(1);
+		return result;
 	},
+
 	getLastCaptureTimeSlot: async (knex, info) => {
-		return await knex('timer')
+		const result = await knex('timer')
 			.where('userId', info.employeeId)
 			.where((qb) => qb.orWhereNotNull("timeSlotId"))
 			.orderBy('created_at', 'desc')
 			.limit(1);
+		return result;
 	},
+
 	saveFailedRequest: async (knex, value) => {
-		return await knex('failed-request').insert(
-			{
-				type: value.type,
-				params: value.params,
-				errorMessage: value.message,
-				created_at: moment(),
-				updated_at: moment()
-			},
-			['id']
-		);
+		const result = await knex('failed-request')
+			.insert(
+				{
+					type: value.type,
+					params: value.params,
+					errorMessage: value.message,
+					created_at: moment(),
+					updated_at: moment()
+				},
+				['id']
+			)
+			.then((res) => res);
+		return result;
 	}
 };
