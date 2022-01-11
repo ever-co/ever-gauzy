0105c90d9bcf964bd121f8b11dd820f4115dab56
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-01-11 14:04:01 +0530

    fix: #3939 deleted employee manage on invoice add page

--- apps/gauzy/src/app/pages/invoices/table-components/invoice-employees-selector.component.ts
+++ apps/gauzy/src/app/pages/invoices/table-components/invoice-employees-selector.component.ts
@@ -1,29 +1,29 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IEmployee, IOrganization } from '@gauzy/contracts';
-import { EmployeesService } from '../../../@core/services';
 import { DefaultEditor } from 'ng2-smart-table';
-import { Store } from '../../../@core/services/store.service';
 import { filter, tap } from 'rxjs/operators';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { isNotEmpty } from '@gauzy/common-angular';
+import { EmployeesService, Store } from '../../../@core/services';
+
 @UntilDestroy({ checkProperties: true })
 @Component({
 	template: `
 		<nb-select
 			fullWidth
-			placeholder="{{ 'INVOICES_PAGE.SELECT_EMPLOYEE' | translate }}"
+			[placeholder]="'INVOICES_PAGE.SELECT_EMPLOYEE' | translate"
 			[(ngModel)]="employee"
 			(selectedChange)="selectEmployee($event)"
 		>
 			<nb-option *ngFor="let employee of employees" [value]="employee">
 				<img
-					src="{{ employee.user.imageUrl }}"
+					[src]="employee.user.imageUrl"
 					alt="Smiley face"
 					height="40"
 					width="40"
 					style="margin-right:10px"
 				/>
-				{{ employee.user.firstName }}
-				{{ employee.user.lastName }}
+				{{ employee.fullName }}
 			</nb-option>
 		</nb-select>
 	`,
@@ -32,13 +32,14 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 export class InvoiceEmployeesSelectorComponent
 	extends DefaultEditor
 	implements OnInit, OnDestroy {
+
 	employee: IEmployee;
-	employees: IEmployee[];
+	employees: IEmployee[] = [];
 	organization: IOrganization;
 
 	constructor(
-		readonly employeeService: EmployeesService,
-		private store: Store
+		private readonly employeeService: EmployeesService,
+		private readonly store: Store
 	) {
 		super();
 	}
@@ -48,26 +49,37 @@ export class InvoiceEmployeesSelectorComponent
 			.pipe(
 				filter((organization) => !!organization),
 				tap((organization) => (this.organization = organization)),
-				tap(() => this._loadEmployees()),
+				tap(() => this._getWorkingEmployees()),
 				untilDestroyed(this)
 			)
 			.subscribe();
 	}
 
-	private _loadEmployees() {
-		const tenantId = this.store.user.tenantId;
+	/**
+	 * Get working employees of the selected month
+	 */
+	 private async _getWorkingEmployees(): Promise<void> {
+		const { tenantId } = this.store.user;
 		const { id: organizationId } = this.organization;
-		this.employeeService
-			.getAll(['user'], { organizationId, tenantId })
-			.pipe(untilDestroyed(this))
-			.subscribe(({ items }) => {
-				this.employees = items;
-				if (this.employees.length) {
-					this.employee = this.employees.find(
-						(e) => e.id === this.cell.newValue.id
-					);
-				}
-			});
+		const { selectedDate } = this.store;
+
+		const { items = [] } = await this.employeeService.getWorking(
+			organizationId,
+			tenantId,
+			selectedDate,
+			true
+		);
+		this.employees = items;
+		this.preSelectedEmployee();
+	}
+
+	preSelectedEmployee() {
+		const { newValue } = this.cell;
+		if (isNotEmpty(this.employees)) {
+			this.employee = this.employees.find(
+				(employee: IEmployee) => employee.id === newValue.id
+			);
+		}
 	}
 
 	selectEmployee($event) {
