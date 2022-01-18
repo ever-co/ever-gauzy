1f4d76c78cb46c7ada0b2894be0fc637ca2ae817
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-01-18 13:15:16 +0530

    fix: removed gauzy server auto generated environments files

--- apps/server/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-
-    export const environment = {
-        production: true,
-        AWHost: 'http://localhost:5600',
-        API_DEFAULT_PORT: 5620,
-        GAUZY_UI_DEFAULT_PORT: 5621,
-        SENTRY_DSN: ''
-    }
-    
