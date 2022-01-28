43ceec8ae84cd1a4de3d693c76d35c6c79b44082
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-01-28 13:02:13 +0530

    fix: update organization DTOs

--- packages/core/src/organization/dto/update-organization.dto.ts
+++ packages/core/src/organization/dto/update-organization.dto.ts
@@ -1,7 +1,18 @@
-import { IOrganizationUpdateInput } from "@gauzy/contracts";
+import { CurrenciesEnum, IOrganizationUpdateInput } from "@gauzy/contracts";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { CreateOrganizationDTO } from "./create-organization.dto";
 
 export class UpdateOrganizationDTO extends CreateOrganizationDTO 
     implements IOrganizationUpdateInput {
 
+    @ApiProperty({ type: () => String, required: true })
+	@IsOptional()
+	@IsString()
+	readonly name: string;
+
+    @ApiProperty({ type: () => String, enum: CurrenciesEnum })
+    @IsOptional()
+	@IsEnum(CurrenciesEnum)
+	readonly currency: CurrenciesEnum;
 }
