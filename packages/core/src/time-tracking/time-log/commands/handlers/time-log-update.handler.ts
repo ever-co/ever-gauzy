203baefdc4cd71b0c05e8eba5f9f592e1cabfc08
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-02-07 18:03:56 +0530

    fix: #3978  update time log & timesheet issue

--- packages/core/src/time-tracking/time-log/commands/handlers/time-log-update.handler.ts
+++ packages/core/src/time-tracking/time-log/commands/handlers/time-log-update.handler.ts
@@ -1,6 +1,7 @@
 import { ICommandHandler, CommandBus, CommandHandler } from '@nestjs/cqrs';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, In } from 'typeorm';
+import { Repository, In, SelectQueryBuilder } from 'typeorm';
+import * as moment from 'moment';
 import { ITimeLog, ITimesheet, TimeLogSourceEnum } from '@gauzy/contracts';
 import { TimeLog } from './../../time-log.entity';
 import { TimeLogUpdateCommand } from '../time-log-update.command';
@@ -11,6 +12,7 @@ import {
 import { TimeSlotService } from '../../../time-slot/time-slot.service';
 import { UpdateEmployeeTotalWorkedHoursCommand } from '../../../../employee/commands';
 import { RequestContext } from './../../../../core/context';
+import { TimeSlot } from './../../../../core/entities/internal';
 
 @CommandHandler(TimeLogUpdateCommand)
 export class TimeLogUpdateHandler
@@ -18,6 +20,9 @@ export class TimeLogUpdateHandler
 	constructor(
 		@InjectRepository(TimeLog)
 		private readonly timeLogRepository: Repository<TimeLog>,
+
+		@InjectRepository(TimeSlot)
+		private readonly timeSlotRepository: Repository<TimeSlot>,
 
 		private readonly commandBus: CommandBus,
 		private readonly timeSlotService: TimeSlotService
@@ -64,34 +69,49 @@ export class TimeLogUpdateHandler
 			...(timesheet ? { timesheetId: timesheet.id } : {})
 		});
 
-		timeLog = await this.timeLogRepository.findOne(timeLog.id);
-		const { timesheetId } = timeLog;
-
 		const timeSlots = this.timeSlotService.generateTimeSlots(
 			timeLog.startedAt,
 			timeLog.stoppedAt
 		);
-		
+
+		timeLog = await this.timeLogRepository.findOne(timeLog.id);
+		const { timesheetId } = timeLog;
+
 		if (needToUpdateTimeSlots) {
 			const startTimes = timeSlots
 				.filter((timeslot) => {
 					return (
 						updateTimeSlots.filter(
-							(newSlot) =>
-								newSlot.startedAt.getTime() ===
-								timeslot.startedAt.getTime()
+							(newSlot) => moment(newSlot.startedAt).isSame(
+								timeslot.startedAt
+							)
 						).length === 0
 					);
 				})
 				.map((timeslot) => new Date(timeslot.startedAt));
 
 			if (startTimes.length > 0) {
-				await this.timeSlotService.delete({
-					organizationId,
-					tenantId,
-					employeeId: timeLog.employeeId,
-					startedAt: In(startTimes)
+				/**
+				 * Removed Deleted TimeSlots
+				 */
+				const timeSlots = await this.timeSlotRepository.find({
+					where: (qb: SelectQueryBuilder<TimeSlot>) => {
+						qb.andWhere(`"${qb.alias}"."organizationId" = :organizationId`, {
+							organizationId
+						});
+						qb.andWhere(`"${qb.alias}"."tenantId" = :tenantId`, {
+							tenantId
+						});
+						qb.andWhere(`"${qb.alias}"."employeeId" = :employeeId`, {
+							employeeId
+						});
+						qb.andWhere(`"${qb.alias}"."startedAt" IN (:...startTimes)`, {
+							startTimes
+						});
+					},
+					relations: ['screenshots']
 				});
+				await this.timeSlotRepository.remove(timeSlots);
 			}
 
 			if (!manualTimeSlot && timeLog.source === TimeLogSourceEnum.BROWSER) {
@@ -103,19 +123,28 @@ export class TimeLogUpdateHandler
 						tenantId,
 						keyboard: 0,
 						mouse: 0,
-						overall: 0
+						overall: 0,
+						timeLogId: timeLog.id
 					}))
 					.filter((slot) => slot.tenantId && slot.organizationId);
-
-				const timeSlots = await this.timeSlotService.bulkCreate(
+				/**
+				 * Assign regenerated TimeSlot enties for existed TimeLog
+				 */
+				await this.timeSlotService.bulkCreate(
 					updateTimeSlots,
 					employeeId,
 					organizationId
 				);
-				timeLog.timeSlots = timeSlots;
 			}
 
-			this.timeLogRepository.save(timeLog);
+			/**
+			 * Update TimeLog Entry
+			 */
+			try {
+				await this.timeLogRepository.save(timeLog);
+			} catch (error) {
+				console.error('Error while updating TimeLog', error);
+			}
 
 			/**
 			 * RECALCULATE timesheet activity  
