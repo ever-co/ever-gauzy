a5a1ca22661966377f1e2c9450e36f4b1a5160f2
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-02-11 19:45:57 +0530

    fix: #4136 role permission RAW query migration

--- packages/core/src/role-permission/role-permission.seed.ts
+++ packages/core/src/role-permission/role-permission.seed.ts
@@ -2,12 +2,12 @@
 // MIT License, see https://github.com/alexitaylor/angular-graphql-nestjs-postgres-starter-kit/blob/master/LICENSE
 // Copyright (c) 2019 Alexi Taylor
 
+import { Connection } from 'typeorm';
 import { IRole, ITenant, IRolePermission, PermissionsEnum } from '@gauzy/contracts';
-import { Brackets, Connection, SelectQueryBuilder, WhereExpressionBuilder } from 'typeorm';
-import { isNotEmpty } from '@gauzy/common';
+import { isEmpty, isNotEmpty } from '@gauzy/common';
+import { v4 as uuidV4 } from 'uuid';
 import { DEFAULT_ROLE_PERMISSIONS } from './default-role-permissions';
 import { RolePermission } from './role-permission.entity';
-import { Role, Tenant } from './../core/entities/internal';
 
 export const createRolePermissions = async (
 	connection: Connection,
@@ -57,12 +57,17 @@ export const reloadRolePermissions = async (
 		PermissionsEnum.ACCESS_DELETE_ALL_DATA
 	];
 
-	const tenants = await connection.getRepository(Tenant).find();
+	/**
+	 * GET all tenants in the system
+	 */
+	const tenants = await connection.manager.query(`SELECT * FROM tenant`);
 	for await (const tenant of tenants) {
-		const rolePermissions: IRolePermission[] = [];
-		const roles = await connection.getRepository(Role).find({
-			tenant
-		});
+		const tenantId = tenant.id;
+		/**
+		 * GET all roles for specific tenant
+		 */
+		const roles = await connection.manager.query(`SELECT * FROM "role" WHERE "role"."tenantId" = $1`, [tenantId]);
+
 		for await (const { role: roleEnum, defaultEnabledPermissions } of DEFAULT_ROLE_PERMISSIONS) {
 			const permissions = defaultEnabledPermissions.filter(
 				(permission) => isDemo ? !deniedPermissions.includes(permission) : true
@@ -70,41 +75,61 @@ export const reloadRolePermissions = async (
 			const role = roles.find((dbRole) => dbRole.name === roleEnum);
 			if (isNotEmpty(permissions)) {
 				for await (const permission of permissions) {
-					const existPermission = await connection.getRepository(RolePermission).findOne({
-						join: {
-							alias: 'role_permission',
-							innerJoin: {
-								role: 'role_permission.role'
-							}
-						},
-						where: (query: SelectQueryBuilder<RolePermission>) => {
-							query.andWhere(
-								new Brackets((qb: WhereExpressionBuilder) => { 
-									qb.andWhere(`"${query.alias}"."tenantId" =:tenantId`, { tenantId: tenant.id });
-									qb.andWhere(`"${query.alias}"."permission" =:permission`, { permission });
-									qb.andWhere(`"role"."name" =:roleEnum`, { roleEnum });
-								})
-							);
-						}
-					});
-					if (!existPermission && role) {
+					const existPermission = await connection.manager.query(
+						`SELECT DISTINCT 
+							"distinctAlias"."role_permission_id"
+						FROM (
+							SELECT 
+								"role_permission"."id" AS "role_permission_id",
+								"role_permission"."tenantId" AS "role_permission_tenantId",
+								"role_permission"."permission" AS "role_permission_permission",
+								"role_permission"."roleId" AS "role_permission_roleId"
+							FROM "role_permission" "role_permission"
+							INNER JOIN "role" "role" 
+								ON "role"."id"="role_permission"."roleId" 
+							WHERE (
+								"role_permission"."tenantId" = $1 AND
+								"role_permission"."permission" = $2 AND
+								"role"."name" = $3
+							)
+						) 
+						"distinctAlias" ORDER BY "role_permission_id" ASC LIMIT 1`, [
+							tenantId,
+							permission,
+							roleEnum
+						]
+					);
+					if (isEmpty(existPermission) && role) {
 						console.log('Unauthorized access blocked permission', {
 							permission,
 							enabled: true,
 							role,
 							tenant
 						});
-						const rolePermission = new RolePermission({
+
+						const payload = [
+							tenantId,
 							permission,
-							enabled: true,
-							role,
-							tenant
-						});
-						rolePermissions.push(rolePermission);
+							true,
+							role.id
+						];
+						if (connection.options.type === 'sqlite') {
+							payload.push(uuidV4());
+							await connection.manager.query(`
+								INSERT INTO "role_permission" ("tenantId", "permission", "enabled", "roleId", "id") 
+								VALUES($1, $2, $3, $4, $5)`,
+								payload
+							);
+						} else {
+							await connection.manager.query(`
+								INSERT INTO "role_permission" ("tenantId", "permission", "enabled", "roleId") 
+								VALUES($1, $2, $3, $4)`,
+								payload
+							);
+						}
 					}
 				}
 			}
 		}
-		await connection.getRepository(RolePermission).save(rolePermissions);
 	}
 }
