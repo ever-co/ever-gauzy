f731060bd0c610b3afc8d79536999158740722d6
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-02-11 19:21:45 +0200

    Implemented dto validations for creating invoice estimate history

---
+++ packages/core/src/invoice-estimate-history/dto/create-invoice-estimate-history.dto.ts
@@ -0,0 +1,3 @@
+import { InvoiceEstimeHistoryDTO } from "./invoice-estimate-history.dto";
+
+export class CreateInvoiceEstimateHistoryDTO extends InvoiceEstimeHistoryDTO {}
--- packages/core/src/invoice-estimate-history/dto/invoice-estimate-history.dto.ts
+++ packages/core/src/invoice-estimate-history/dto/invoice-estimate-history.dto.ts
@@ -1,32 +1,24 @@
+import { IInvoice, IUser } from "@gauzy/contracts";
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+import { TenantOrganizationBaseDTO } from "core/dto";
 
-export abstract class InvoiceEstimeHistoryDTO {
+export abstract class InvoiceEstimeHistoryDTO extends TenantOrganizationBaseDTO {
 
-    // @ApiPropertyOptional({ type: () => String })
-    // @IsString()
-    // readonly action: string;
+    @ApiPropertyOptional({ type: () => String, readOnly : true })
+    @IsString()
+    readonly action: string;
 
-    // @ApiPropertyOptional({ type: () => Object })
-    // readonly user: IUser;
+    @ApiPropertyOptional({ type: () => Object, readOnly : true })
+    readonly user: IUser;
 
-    // @ApiPropertyOptional({ type: () => String })
-    // readonly userId: string;
+    @ApiPropertyOptional({ type: () => String, readOnly : true })
+    readonly userId: string;
 
-    // @ApiPropertyOptional({ type: () => Object })
-    // readonly invoice: IInvoice;
+    @ApiPropertyOptional({ type: () => Object, readOnly : true })
+    readonly invoice: IInvoice;
 
-    // @ApiPropertyOptional({ type: () => String })
-    // readonly invoiceId: string;
-
-    // @ApiPropertyOptional({ type: () => String })
-    // readonly organizationId: string;
-
-    // @ApiPropertyOptional({ type: () => Object })
-    // readonly organization: IOrganization;
-
-    // @ApiPropertyOptional({ type: () => String })
-    // readonly tenantId: string;
-
-    // @ApiPropertyOptional({ type: () => String })
-    // readonly tenant: ITenant;
+    @ApiPropertyOptional({ type: () => String, readOnly : true })
+    readonly invoiceId: string;
 
 }
