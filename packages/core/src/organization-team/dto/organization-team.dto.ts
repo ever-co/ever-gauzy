4b59ad10e785bb99a5c875e855fa745c090f217d
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-02-11 15:31:35 +0200

    Fix required review changes

--- packages/core/src/organization-team/dto/organization-team.dto.ts
+++ packages/core/src/organization-team/dto/organization-team.dto.ts
@@ -5,21 +5,21 @@ import { TenantOrganizationBaseDTO } from "core/dto";
 
 export abstract class OrganizationTeamDTO extends TenantOrganizationBaseDTO {
 
-    @ApiProperty({ type: () => String, isArray: true })
+    @ApiProperty({ type: () => String, readOnly: true })
     @IsString()
     @IsNotEmpty()
     readonly name: string;
 
-    @ApiProperty({ type: () => String, isArray: true })
+    @ApiProperty({ type: () => String, isArray: true, readOnly: true })
     @IsArray()
     @ArrayNotEmpty()
     readonly members: string[];
 
-    @ApiProperty({ type: () => String, isArray: true })
+    @ApiProperty({ type: () => String, isArray: true , readOnly: true})
     @IsOptional()
     readonly managers: string[];
 
-    @ApiProperty({ type: () => Object, isArray: true })
+    @ApiProperty({ type: () => Object, isArray: true, readOnly: true })
     @IsOptional()
     readonly tags: ITag[];
 
