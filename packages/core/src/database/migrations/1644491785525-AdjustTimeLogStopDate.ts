307add6fdfb43dc4464d8e7897de2f20ac602e90
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-02-13 15:02:35 +0530

    fix: #4136  RAW query for TimeLog

--- packages/core/src/database/migrations/1644491785525-AdjustTimeLogStopDate.ts
+++ packages/core/src/database/migrations/1644491785525-AdjustTimeLogStopDate.ts
@@ -1,38 +1,78 @@
 import { MigrationInterface, QueryRunner, SelectQueryBuilder } from "typeorm";
+import { chain } from 'underscore';
 import * as moment from 'moment';
 import { isEmpty, isNotEmpty } from "@gauzy/common";
-import { TimeLog, TimeSlot } from "./../../core/entities/internal";
+import { TimeSlot } from "./../../core/entities/internal";
     
 export class AdjustTimeLogStopDate1644491785525 implements MigrationInterface {
 
     name = 'AdjustTimeLogStopDate1644491785525';
     
     public async up(queryRunner: QueryRunner): Promise<any> {
-        const timeLogs = await queryRunner.connection.getRepository(TimeLog).find({
-            where: (query: SelectQueryBuilder<TimeLog>) => {
-                query.andWhere(`"${query.alias}"."stoppedAt" IS NULL`);
-            },
-            relations: ['timeSlots']
-        });
-        for await (const timeLog of timeLogs) {
+        const timelogs = await queryRunner.connection.manager.query(`
+            SELECT 
+                "time_log"."id" AS "time_log_id",
+                "time_slot"."id" AS "time_slot_id"
+            FROM "time_log"
+            LEFT JOIN "time_slot_time_logs" 
+                ON "time_slot_time_logs"."timeLogId" = "time_log"."id" 
+            LEFT JOIN "time_slot"
+                ON "time_slot"."id" = "time_slot_time_logs"."timeSlotId"
+            WHERE 
+                "time_log"."stoppedAt" IS NULL
+        `);
+        const timeLogs = chain(timelogs).groupBy((log: any) => log.time_log_id).value();
+        for await (const [timeLogId, timeSlots] of Object.entries(timeLogs)) {
+            const timeSlotsIds = timeSlots.map(
+                (timeSlot: any) => timeSlot.time_slot_id
+            ).filter(Boolean);
+            const [timeLog] = await queryRunner.connection.manager.query(`
+                SELECT * FROM "time_log" WHERE "time_log"."id" = $1 LIMIT 1`, 
+                [
+                    timeLogId
+                ]
+            );
             const logDifference = moment().diff(moment(timeLog.startedAt), 'minutes');
             if (
-                isEmpty(timeLog.timeSlots) &&
+                isEmpty(timeSlotsIds) &&
                 logDifference > 10
             ) {
-                await queryRunner.connection.getRepository(TimeLog).save({
-                    id: timeLog.id,
-                    stoppedAt: timeLog.startedAt,
-                    ...timeLog,
-                });
-            } else if (isNotEmpty(timeLog.timeSlots)) {
-                const [lastTimeSlot] = timeLog.timeSlots.reverse();
-                const slotDifference = moment().diff(moment(lastTimeSlot.stoppedAt), 'minutes');
+                await queryRunner.connection.manager.query(`
+                    UPDATE "time_log" SET 
+                        "stoppedAt" = $1, 
+                        "updatedAt" = CURRENT_TIMESTAMP 
+                    WHERE 
+                        "id" IN($2) 
+                    RETURNING "updatedAt"`, 
+                    [
+                        timeLog.startedAt,
+                        timeLog.id
+                    ]
+                );
+            } else if (isNotEmpty(timeSlotsIds)) {
+                const [lastTimeSlot] = await queryRunner.connection.manager.query(`
+                    SELECT * FROM 
+                        "time_slot" 
+                    WHERE 
+                        "time_slot"."id" IN ('${timeSlotsIds.join("','")}') 
+                    ORDER BY 
+                        "time_slot"."startedAt" DESC
+                `);
+                const stoppedAt = moment(lastTimeSlot.startedAt).add(10, 'minutes').toDate();
+                const slotDifference = moment().diff(moment(stoppedAt), 'minutes');
                 if (slotDifference > 10) {
-                    await queryRunner.connection.getRepository(TimeLog).save({
-                        id: timeLog.id,
-                        stoppedAt: timeLog.startedAt
-                    });
+                    await queryRunner.connection.manager.query(`
+                        UPDATE "time_log" SET
+                            "stoppedAt" = $1,
+                            "updatedAt" = CURRENT_TIMESTAMP 
+                        WHERE 
+                            "id" IN($2) 
+                        RETURNING "updatedAt"`,
+                        [
+                            stoppedAt,
+                            timeLog.id
+                        ]
+                    );
                 }
             }
         }
@@ -51,8 +91,7 @@ export class AdjustTimeLogStopDate1644491785525 implements MigrationInterface {
                 query.orWhere(`"${query.alias}"."duration" > :duration`, {
                     duration: 600
                 });
-            },
-            relations: ['timeLogs']
+            }
         });
 
         for await (const timeSlot of timeSlots) {
