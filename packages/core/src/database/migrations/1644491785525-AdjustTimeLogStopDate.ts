72facf34c65649223f469e338f2909f924afd5de
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-02-14 12:32:12 +0530

    fix: #4136 migration for SQLite format

--- packages/core/src/database/migrations/1644491785525-AdjustTimeLogStopDate.ts
+++ packages/core/src/database/migrations/1644491785525-AdjustTimeLogStopDate.ts
@@ -1,8 +1,7 @@
-import { MigrationInterface, QueryRunner, SelectQueryBuilder } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 import { chain } from 'underscore';
 import * as moment from 'moment';
 import { isEmpty, isNotEmpty } from "@gauzy/common";
-import { TimeSlot } from "./../../core/entities/internal";
     
 export class AdjustTimeLogStopDate1644491785525 implements MigrationInterface {
 
@@ -19,20 +18,24 @@ export class AdjustTimeLogStopDate1644491785525 implements MigrationInterface {
             LEFT JOIN "time_slot"
                 ON "time_slot"."id" = "time_slot_time_logs"."timeSlotId"
             WHERE 
-                "time_log"."stoppedAt" IS NULL
+                "time_log"."stoppedAt" IS NULL OR
+                "time_log"."stoppedAt" = ''
         `);
         const timeLogs = chain(timelogs).groupBy((log: any) => log.time_log_id).value();
         for await (const [timeLogId, timeSlots] of Object.entries(timeLogs)) {
-            const timeSlotsIds = timeSlots.map(
-                (timeSlot: any) => timeSlot.time_slot_id
-            ).filter(Boolean);
+            const timeSlotsIds = timeSlots
+                .map(
+                    (timeSlot: any) => timeSlot.time_slot_id
+                )
+                .filter(Boolean);
             const [timeLog] = await queryRunner.connection.manager.query(`
                 SELECT * FROM "time_log" WHERE "time_log"."id" = $1 LIMIT 1`, 
                 [
                     timeLogId
                 ]
             );
-            const logDifference = moment().diff(moment(timeLog.startedAt), 'minutes');
+
+            const logDifference = moment().diff(moment.utc(timeLog.startedAt), 'minutes');
             if (
                 isEmpty(timeSlotsIds) &&
                 logDifference > 10
@@ -42,8 +45,7 @@ export class AdjustTimeLogStopDate1644491785525 implements MigrationInterface {
                         "stoppedAt" = $1, 
                         "updatedAt" = CURRENT_TIMESTAMP 
                     WHERE 
-                        "id" IN($2) 
-                    RETURNING "updatedAt"`, 
+                        "id" IN($2)`, 
                     [
                         timeLog.startedAt,
                         timeLog.id
@@ -58,16 +60,22 @@ export class AdjustTimeLogStopDate1644491785525 implements MigrationInterface {
                     ORDER BY 
                         "time_slot"."startedAt" DESC
                 `);
-                const stoppedAt = moment(lastTimeSlot.startedAt).add(10, 'minutes').toDate();
-                const slotDifference = moment().diff(moment(stoppedAt), 'minutes');
+
+                let stoppedAt: any;
+                if (queryRunner.connection.options.type === 'sqlite') {
+                    stoppedAt = moment.utc(lastTimeSlot.startedAt).add(10, 'minutes').format('YYYY-MM-DD HH:mm:ss.SSS');
+                } else {
+                    stoppedAt = moment.utc(lastTimeSlot.startedAt).add(10, 'minutes').toDate();
+                }
+                
+                const slotDifference = moment.utc(moment()).diff(stoppedAt, 'minutes');                
                 if (slotDifference > 10) {
                     await queryRunner.connection.manager.query(`
                         UPDATE "time_log" SET
                             "stoppedAt" = $1,
                             "updatedAt" = CURRENT_TIMESTAMP 
                         WHERE 
-                            "id" IN($2) 
-                        RETURNING "updatedAt"`,
+                            "id" IN($2)`,
                         [
                             stoppedAt,
                             timeLog.id
@@ -76,32 +84,38 @@ export class AdjustTimeLogStopDate1644491785525 implements MigrationInterface {
                 }
             }
         }
-
-        const timeSlots = await queryRunner.connection.getRepository(TimeSlot).find({
-            where: (query: SelectQueryBuilder<TimeSlot>) => {
-                query.orWhere(`"${query.alias}"."overall" < :overall`, {
-                    overall: 0
-                });
-                query.orWhere(`"${query.alias}"."keyboard" < :keyboard`, {
-                    keyboard: 0
-                });
-                query.orWhere(`"${query.alias}"."mouse" < :mouse`, {
-                    mouse: 0
-                });
-                query.orWhere(`"${query.alias}"."duration" > :duration`, {
-                    duration: 600
-                });
-            }
-        });
-
+        
+        const timeSlots = await queryRunner.connection.manager.query(`
+            SELECT * FROM 
+                "time_slot" 
+            WHERE 
+                "time_slot"."overall" < $1 OR 
+                "time_slot"."keyboard" < $2 OR
+                "time_slot"."mouse" < $3 OR
+                "time_slot"."duration" > $4
+        `, [ 0, 0, 0, 600 ]);
         for await (const timeSlot of timeSlots) {
-            await queryRunner.connection.getRepository(TimeSlot).save({
-                id: timeSlot.id,
-                duration: (timeSlot.duration < 0) ? 0 : (timeSlot.duration > 600) ? 600 : timeSlot.duration,
-                overall: (timeSlot.overall < 0) ? 0 : (timeSlot.overall > 600) ? 600 : timeSlot.overall,
-                keyboard: (timeSlot.keyboard < 0) ? 0 : (timeSlot.keyboard > 600) ? 600 : timeSlot.keyboard,
-                mouse: (timeSlot.mouse < 0) ? 0 : (timeSlot.mouse > 600) ? 600 : timeSlot.mouse
-            });
+            const duration = (timeSlot.duration < 0) ? 0 : (timeSlot.duration > 600) ? 600 : timeSlot.duration;
+            const overall = (timeSlot.overall < 0) ? 0 : (timeSlot.overall > 600) ? 600 : timeSlot.overall;
+            const keyboard = (timeSlot.keyboard < 0) ? 0 : (timeSlot.keyboard > 600) ? 600 : timeSlot.keyboard;
+            const mouse = (timeSlot.mouse < 0) ? 0 : (timeSlot.mouse > 600) ? 600 : timeSlot.mouse;
+            await queryRunner.connection.manager.query(`
+                UPDATE "time_slot" SET
+                    "duration" = $1,
+                    "overall" = $2,
+                    "keyboard" = $3,
+                    "mouse" = $4,
+                    "updatedAt" = CURRENT_TIMESTAMP
+                WHERE 
+                    "id" IN($5)`,
+                [
+                    duration,
+                    overall,
+                    keyboard,
+                    mouse,
+                    timeSlot.id
+                ]
+            );
         }
     }
     
