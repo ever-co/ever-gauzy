7f0fc6cf227fc71648484d42885aaf89fb8de056
Adolphe Kifungo <akifungo@gmail.com>
2022-02-22 19:04:58 +0200

    fix: position of currency in all invoice/estimate ngSmartTable

--- apps/gauzy/src/app/pages/invoices/table-components/invoice-total-value.component.ts
+++ apps/gauzy/src/app/pages/invoices/table-components/invoice-total-value.component.ts
@@ -2,7 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'ga-invoice-amount',
-	template: `<span >{{ rowData?.currency }} {{ value }}</span>`,
+	template: `<span>{{
+		value
+			| currency: rowData?.currency
+			| position: rowData?.fromOrganization?.currencyPosition
+	}}</span>`
 })
 export class InvoiceEstimateTotalValueComponent {
 	@Input() value: Date;
