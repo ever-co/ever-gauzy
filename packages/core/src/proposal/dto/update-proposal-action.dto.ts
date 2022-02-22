2ed8988f69a4ee1fe39428ca3cdd27536c165b32
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-02-22 07:55:12 +0200

    fix: review changes

--- packages/core/src/proposal/dto/update-proposal-action.dto.ts
+++ packages/core/src/proposal/dto/update-proposal-action.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { UpdateProposalDTO } from ".";
+import { ProposalStatusEnum } from "@gauzy/contracts";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsNotEmpty, IsOptional } from "class-validator";
 
-export class UpdateProposalActionDTO extends PartialType(UpdateProposalDTO) {}
+export class UpdateProposalActionDTO {
+
+    @ApiProperty({ type: () => String, enum: ProposalStatusEnum })
+    @IsOptional()
+    @IsNotEmpty()
+    @IsEnum(ProposalStatusEnum)
+    readonly status: ProposalStatusEnum;
+
+}
