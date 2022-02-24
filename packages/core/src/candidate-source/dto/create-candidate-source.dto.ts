8405c0a52987bf73fda0d029de26c238a530ecc1
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-02-24 18:33:10 +0530

    fix: improved import class

--- packages/core/src/candidate-source/dto/create-candidate-source.dto.ts
+++ packages/core/src/candidate-source/dto/create-candidate-source.dto.ts
@@ -1,12 +1,12 @@
 import { ICandidateSource } from "@gauzy/contracts";
 import { ApiProperty } from "@nestjs/swagger";
-import { CandidateFeatureDTO } from "candidate/dto";
 import { IsNotEmpty, IsString } from "class-validator";
+import { CandidateFeatureDTO } from "./../../candidate/dto";
+
 export class CreateCandidateSourceDTO extends CandidateFeatureDTO implements ICandidateSource {
 
     @ApiProperty({ type: () => String, readOnly: true })
     @IsNotEmpty()
     @IsString()
     readonly name: string;
-
 }
