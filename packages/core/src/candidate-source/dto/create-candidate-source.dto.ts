b84e75e2e81464005ed1ccdbecb7394dbb9cf7f4
Miracle Kasigwa <miraclekasigwa@gmail.com>
2022-02-24 11:23:28 +0200

    fix: #3479 review required changes

--- packages/core/src/candidate-source/dto/create-candidate-source.dto.ts
+++ packages/core/src/candidate-source/dto/create-candidate-source.dto.ts
@@ -1,6 +1,6 @@
 import { ICandidateSource } from "@gauzy/contracts";
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 import { TenantOrganizationBaseDTO } from "core/dto";
 
 export class CreateCandidateSourceDTO extends TenantOrganizationBaseDTO implements ICandidateSource {
@@ -10,8 +10,8 @@ export class CreateCandidateSourceDTO extends TenantOrganizationBaseDTO implemen
     @IsString()
     readonly name: string;
 
-    @ApiPropertyOptional({ type: () => String, readOnly: true })
-    @IsOptional()
+    @ApiProperty({ type: () => String, readOnly: true })
+    @IsNotEmpty()
     @IsString()
     readonly candidateId: string;
 }
