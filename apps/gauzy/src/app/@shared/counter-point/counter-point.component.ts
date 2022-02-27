1ead2a6033d2c6ec075d364e62e3fd06ba9aa1c8
Adolphe Kifungo <akifungo@gmail.com>
2022-02-28 01:54:48 +0200

    fix: dark theme issue and remove unused imports

--- apps/gauzy/src/app/@shared/counter-point/counter-point.component.ts
+++ apps/gauzy/src/app/@shared/counter-point/counter-point.component.ts
@@ -1,33 +1,46 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+	Component,
+	Input,
+	OnChanges,
+	OnInit,
+	SimpleChanges
+} from '@angular/core';
 import { Point } from './point/point.class';
 import { progressStatus } from '@gauzy/common-angular';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { NbThemeService } from '@nebular/theme';
 
 @Component({
 	selector: 'gauzy-counter-point',
 	templateUrl: './counter-point.component.html',
 	styleUrls: ['./counter-point.component.scss']
 })
-export class CounterPointComponent implements OnChanges {
+export class CounterPointComponent implements OnInit, OnChanges {
 	progressStatus = progressStatus;
 	@Input() total: any;
 	@Input() value: number;
 	@Input() color: string;
 	@Input() progress: boolean = false;
 	points: Point[] = [];
-	DEFAULT_COLOR = '#D8E3ED66';
-	constructor() {}
+	DEFAULT_COLOR: string = '#D8E3ED66';
+	constructor(private themeService: NbThemeService) {}
+
+	ngOnInit(): void {
+		this.themeService.getJsTheme().subscribe((theme) => {
+			this.DEFAULT_COLOR = theme.variables.bg3.toString();
+			if (!this.progress) this.generateColorizedPoints();
+		});
+	}
 
 	ngOnChanges(changes: SimpleChanges): void {
-		if(!this.progress) this.generateColorizedPoints();
+		if (!this.progress) this.generateColorizedPoints();
 	}
 
 	generateColorizedPoints() {
 		const points: Point[] = [];
-    let total = this.total === 0 ? 86400: this.total;
-    let value = this.value;
+		let total = this.total === 0 ? 86400 : this.total;
+		let value = this.value;
 		if (total > 24) {
-			value = value / total * 24;
+			value = (value / total) * 24;
 			total = 24;
 		}
 		for (let i = 0; i < total; i++) {
