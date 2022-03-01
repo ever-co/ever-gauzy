0a27a2fca7367221e457ddc7614e1d301b5980cd
Adolphe Kifungo <akifungo@gmail.com>
2022-03-01 07:47:00 +0200

    refactor: improve grid ui pagination

--- apps/gauzy/src/app/@shared/pagination/pagination.module.ts
+++ apps/gauzy/src/app/@shared/pagination/pagination.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '../translate/translate.module';
 import { PaginationComponent } from './pagination.component';
+import { NbIconModule } from '@nebular/theme';
 
 @NgModule({
 	declarations: [PaginationComponent],
 	imports: [
 		CommonModule,
-		TranslateModule
+		TranslateModule,
+    NbIconModule
 	],
 	exports: [PaginationComponent]
-	
+
 })
 export class PaginationModule {}
