2d4a5978b374d3f5c919de537bf67264aabc0b42
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-03-02 14:12:58 +0530

    fix: expense create/update DTO validation

--- packages/core/src/expense/dto/update-expense.dto.ts
+++ packages/core/src/expense/dto/update-expense.dto.ts
@@ -1,17 +1,3 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString } from "class-validator";
 import { CreateExpenseDTO } from "./create-expense.dto";
 
-export class UpdateExpenseDTO extends CreateExpenseDTO {
-
-    @ApiProperty({ type: () => String, readOnly: true })
-    @IsOptional()
-    @IsString()
-    readonly vendorId: string;
-
-    @ApiProperty({ type: () => String, readOnly: true })
-    @IsOptional()
-    @IsString()
-    readonly categoryId: string;
-
-}
+export class UpdateExpenseDTO extends CreateExpenseDTO {}
