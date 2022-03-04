214ea187a6326494840bdb013bbe42689b4ca93c
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-03-04 16:10:40 +0530

    fix: deep scan

--- apps/gauzy/src/app/pages/settings/settings-routing.module.ts
+++ apps/gauzy/src/app/pages/settings/settings-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { DangerZoneComponent } from './danger-zone/danger-zone.component';
 import { SettingsComponent } from './settings.component';
 import { EmailHistoryComponent } from './email-history/email-history.component';
-import { FileStorageComponent } from './file-storage/file-storage.component';
 import { NgxPermissionsGuard } from 'ngx-permissions';
 import { PermissionsEnum } from '@gauzy/contracts';
 import { SmsGatewayComponent } from './sms-gateway/sms-gateway.component';
