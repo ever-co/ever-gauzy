840ff8aa91bf72683fb6191801bd01823528afb0
Adolphe Kifungo <akifungo@gmail.com>
2022-03-09 13:38:38 +0200

    feat: add infinite scroll directive to card component

--- apps/gauzy/src/app/@shared/card-grid/card-grid.component.ts
+++ apps/gauzy/src/app/@shared/card-grid/card-grid.component.ts
@@ -5,28 +5,28 @@ import {
 	Input,
 	TemplateRef
 } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { TranslationBaseComponent } from '../language-base/translation-base.component';
+
 import { Output, EventEmitter } from '@angular/core';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
 	selector: 'ga-card-grid',
 	templateUrl: './card-grid.component.html',
 	styleUrls: ['./card-grid.component.scss']
 })
-export class CardGridComponent extends TranslationBaseComponent
-	implements OnInit, OnDestroy {
+export class CardGridComponent
+	extends PaginationComponent
+	implements OnInit, OnDestroy
+{
 	@Input() source: any;
 	@Input() settings: any = {};
 	@Input() buttonTemplate: TemplateRef<any>;
 	@Input() cardSize: undefined | 'big';
-  @Output() onSelectedItem: EventEmitter<any> = new EventEmitter<any>();
-  selected: any = {isSelected: false, data: null};
+	@Output() onSelectedItem: EventEmitter<any> = new EventEmitter<any>();
+	selected: any = { isSelected: false, data: null };
 
-	constructor(
-		readonly translationService: TranslateService
-	) {
-		super(translationService);
+	constructor() {
+		super();
 	}
 
 	ngOnInit(): void {}
@@ -35,12 +35,17 @@ export class CardGridComponent extends TranslationBaseComponent
 		return Object.keys(this.settings.columns);
 	}
 
-  selectedItem(item){
-    this.selected =
-    this.selected.data && item.id === this.selected.data.id
-      ? { isSelected: !this.selected.isSelected, data: item }
-      : { isSelected: true, data: item };
-    this.onSelectedItem.emit(this.selected);
-  }
+	selectedItem(item) {
+		this.selected =
+			this.selected.data && item.id === this.selected.data.id
+				? { isSelected: !this.selected.isSelected, data: item }
+				: { isSelected: true, data: item };
+		this.onSelectedItem.emit(this.selected);
+	}
+
+	onScroll() {
+		this.itemsPerPage += 10;
+	}
+
 	ngOnDestroy() {}
 }
