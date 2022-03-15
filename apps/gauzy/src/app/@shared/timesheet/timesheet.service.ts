c4924d2427c754985e0fc65a089cea05888eb16d
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-03-15 10:39:03 +0530

    fix: deepscan

--- apps/gauzy/src/app/@shared/timesheet/timesheet.service.ts
+++ apps/gauzy/src/app/@shared/timesheet/timesheet.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {
 	ITimeLog,
 	IGetTimeLogInput,
