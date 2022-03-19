5faa3616a1ee97f52f84ea3cb1ce98ca6d633ba9
Adolphe Kifungo <akifungo@gmail.com>
2022-03-19 18:11:21 +0200

    fix: add comments

--- apps/gauzy/src/app/@shared/gauzy-button-action/gauzy-button-action.component.ts
+++ apps/gauzy/src/app/@shared/gauzy-button-action/gauzy-button-action.component.ts
@@ -7,18 +7,26 @@ import { ComponentEnum } from '../../@core/constants/layout.constants';
 	styleUrls: ['./gauzy-button-action.component.scss']
 })
 export class GauzyButtonActionComponent implements OnInit {
-  @Input() icon: string;
-  @Input() text: string;
-	@Input() isDisable: boolean = true;
-	@Input() buttonTemplate: any;
-	@Input() componentName: ComponentEnum;
-  @Output() addOnClick: EventEmitter<any> = new EventEmitter<any>();
-
+	// This inputs properties are bound to a DOM properties in the template
+	@Input() icon: string; // This propriety is bound nebular icon name
+	@Input() text: string; // This propriety is bound custom text in button
+	@Input() isDisable: boolean = true; // This propriety is bound disability of button
+	@Input() buttonTemplate: any; // This propriety is bound template of buttons
+	@Input() componentName: ComponentEnum; // This propriety is bound a string name of component
+	// This decorator that marks a class field as an output property and supplies configuration metadata
+	@Output() addOnClick: EventEmitter<any> = new EventEmitter<any>(); // create new instance of event emitter
+	/**
+	 * Default constructor
+	 */
 	constructor() {}
-
+	/**
+	 * not implemented
+	 */
 	ngOnInit(): void {}
-
-  onAdd(){
-    this.addOnClick.emit();
-  }
+	/**
+	 * Trigger an event on click
+	 */
+	onAdd() {
+		this.addOnClick.emit();
+	}
 }
