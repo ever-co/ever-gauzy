57ebf58cea2fd132019d4b2263e1a7f81c4c4f77
Sineastra <lord.of.light.0002@gmail.com>
2022-03-23 13:52:34 +0200

    fixed typo in code

--- apps/gauzy/src/app/app.component.ts
+++ apps/gauzy/src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 	) {}
 
 	async ngOnInit() {
-		if (environment.CHATWOOT_SDK_TOKEN) {8
+		if (environment.CHATWOOT_SDK_TOKEN) {
 			this.loadChatwoot(document, 'script');
 		}
 
