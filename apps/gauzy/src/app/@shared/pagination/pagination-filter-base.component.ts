83bde91f3e59816675f028d52f07324f717b29af
Adolphe Kifungo <akifungo@gmail.com>
2022-03-26 10:13:55 +0200

    feat: add common method

--- apps/gauzy/src/app/@shared/pagination/pagination-filter-base.component.ts
+++ apps/gauzy/src/app/@shared/pagination/pagination-filter-base.component.ts
@@ -1,37 +1,40 @@
-import { AfterViewInit, Component } from "@angular/core";
-import { UntilDestroy } from "@ngneat/until-destroy";
-import { TranslateService } from "@ngx-translate/core";
-import { isNotEmpty } from "@gauzy/common-angular";
-import { Subject } from "rxjs/internal/Subject";
-import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
-import { TranslationBaseComponent } from "../language-base/translation-base.component";
+import { AfterViewInit, Component } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { TranslateService } from '@ngx-translate/core';
+import { isNotEmpty } from '@gauzy/common-angular';
+import { Subject } from 'rxjs/internal/Subject';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { TranslationBaseComponent } from '../language-base/translation-base.component';
 
 export interface IPaginationBase {
-	totalItems?: number,
-	activePage: number,
-	itemsPerPage: number
+	totalItems?: number;
+	activePage: number;
+	itemsPerPage: number;
 }
 
 @UntilDestroy({ checkProperties: true })
 @Component({
 	template: ''
 })
-export class PaginationFilterBaseComponent extends TranslationBaseComponent implements AfterViewInit {
-    
+export class PaginationFilterBaseComponent
+	extends TranslationBaseComponent
+	implements AfterViewInit
+{
 	protected pagination: IPaginationBase = {
-        totalItems: 0,
+		totalItems: 0,
 		activePage: 1,
 		itemsPerPage: 10
 	};
-    protected subject$: Subject<any> = new Subject();
-	protected pagination$: BehaviorSubject<IPaginationBase> = new BehaviorSubject({
-		activePage: this.pagination.activePage,
-		itemsPerPage: this.pagination.itemsPerPage
-	});
+	protected subject$: Subject<any> = new Subject();
+	protected pagination$: BehaviorSubject<IPaginationBase> =
+		new BehaviorSubject({
+			activePage: this.pagination.activePage,
+			itemsPerPage: this.pagination.itemsPerPage
+		});
 
-    /*
-	* getter setter for filters 
-	*/
+	/*
+	 * getter setter for filters
+	 */
 	protected _filters: any = {};
 	set filters(val: any) {
 		this._filters = val;
@@ -40,15 +43,15 @@ export class PaginationFilterBaseComponent extends TranslationBaseComponent impl
 		return this._filters;
 	}
 
-    constructor(public readonly translateService: TranslateService) {
+	constructor(public readonly translateService: TranslateService) {
 		super(translateService);
 	}
 
-    ngAfterViewInit() {}
+	ngAfterViewInit() {}
 
-    /*
-	* refresh pagination
-	*/
+	/*
+	 * refresh pagination
+	 */
 	protected refreshPagination() {
 		this.setPagination({
 			...this.getPagination(),
@@ -56,17 +59,14 @@ export class PaginationFilterBaseComponent extends TranslationBaseComponent impl
 		});
 	}
 
-	protected setFilter(
-		filter: any, 
-		doEmit: boolean = true
-	) {
+	protected setFilter(filter: any, doEmit: boolean = true) {
 		if (isNotEmpty(filter.search)) {
 			this.filters = {
-				where: { 
-					...this.filters.where, 
-					[filter.field]: filter.search 
+				where: {
+					...this.filters.where,
+					[filter.field]: filter.search
 				}
-			}
+			};
 		} else {
 			if (`${filter.field}` in this.filters.where) {
 				delete this.filters.where[filter.field];
@@ -77,12 +77,12 @@ export class PaginationFilterBaseComponent extends TranslationBaseComponent impl
 		}
 	}
 
-    protected onPageChange(selectedPage: number) {
+	protected onPageChange(selectedPage: number) {
 		this.setPagination({
 			...this.getPagination(),
 			activePage: selectedPage
 		});
-        this.subject$.next(true);
+		this.subject$.next(true);
 	}
 
 	protected getPagination(): IPaginationBase {
@@ -94,5 +94,13 @@ export class PaginationFilterBaseComponent extends TranslationBaseComponent impl
 
 		const { activePage, itemsPerPage } = this.getPagination();
 		this.pagination$.next({ activePage, itemsPerPage });
+	}
+
+	onUpdateOption($event: number) {
+		this.pagination.itemsPerPage = $event;
+		this.setPagination({
+			...this.getPagination(),
+			itemsPerPage: this.pagination.itemsPerPage
+		});
 	}
 }
