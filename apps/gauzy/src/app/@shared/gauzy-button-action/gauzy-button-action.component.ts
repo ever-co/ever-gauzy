649bc4ff816c117c2f9e497a5eac214ed50bd279
Adolphe Kifungo <akifungo@gmail.com>
2022-03-26 09:41:31 +0200

    fix: remove unused imports

--- apps/gauzy/src/app/@shared/gauzy-button-action/gauzy-button-action.component.ts
+++ apps/gauzy/src/app/@shared/gauzy-button-action/gauzy-button-action.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ComponentEnum } from '../../@core/constants/layout.constants';
 
 @Component({
