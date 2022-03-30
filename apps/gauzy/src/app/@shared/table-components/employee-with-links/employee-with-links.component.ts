ae7a625ee63aa384137f05bd16636413f425b3ee
Adolphe Kifungo <akifungo@gmail.com>
2022-03-30 11:38:52 +0200

    feat: grouping employees  🐾

--- apps/gauzy/src/app/@shared/table-components/employee-with-links/employee-with-links.component.ts
+++ apps/gauzy/src/app/@shared/table-components/employee-with-links/employee-with-links.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ViewCell } from 'ng2-smart-table';
 import { Router } from '@angular/router';
 import { ISelectedEmployee } from '@gauzy/contracts';
@@ -8,15 +8,38 @@ import { Store } from '../../../@core/services/store.service';
 @Component({
 	selector: 'ngx-employee-with-links',
 	templateUrl: './employee-with-links.component.html',
-  styleUrls: ['./employee-with-links.component.scss']
+	styleUrls: ['./employee-with-links.component.scss']
 })
-export class EmployeeWithLinksComponent implements ViewCell {
+export class EmployeeWithLinksComponent implements ViewCell, OnInit {
 	@Input()
 	rowData: any;
 	@Input()
 	value: any;
+	employees: any[] = [];
 
 	constructor(private store: Store, private readonly router: Router) {}
+
+	ngOnInit(): void {
+		this.initializeGrouping();
+	}
+
+	initializeGrouping() {
+    const GROUP = 3;
+		let count = 0;
+		let group: any[] = [];
+
+		for (let employee of this.value) {
+			if (count % GROUP === 0) {
+				group.push(employee);
+				this.employees.push(group);
+				group = [];
+			} else {
+				group.push(employee);
+			}
+			count++;
+		}
+		this.employees = this.employees.reverse();
+	}
 
 	selectEmployee(
 		employee: ISelectedEmployee,
