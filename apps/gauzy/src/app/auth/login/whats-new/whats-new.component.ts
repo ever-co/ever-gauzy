01e3c75909d140131c330d2f6940885294843092
Sineastra <lord.of.light.0002@gmail.com>
2022-04-04 13:06:35 +0300

    reverted whats new to develop

--- apps/gauzy/src/app/auth/login/whats-new/whats-new.component.ts
+++ apps/gauzy/src/app/auth/login/whats-new/whats-new.component.ts
@@ -1,8 +1,4 @@
-import { Component } from "@angular/core";
-import { ChangelogComponent } from "../../../@theme/components/theme-sidebar/changelog/changelog.component";
-import { untilDestroyed } from "@ngneat/until-destroy";
-import { tap } from "rxjs/operators";
-import { IChangelog } from "@gauzy/contracts";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
@@ -10,13 +6,8 @@ import { IChangelog } from "@gauzy/contracts";
 	templateUrl: './whats-new.component.html',
 	styleUrls: ['./whats-new.component.scss'],
 })
-export class NgxWhatsNewComponent extends ChangelogComponent {
-	learnMore: string | null
-
-	ngOnInit () {
-		super.ngOnInit();
-		this.items$.subscribe(x => {
-			console.log(x)
-		})
-	}
+export class NgxWhatsNewComponent {
+	dataMock: any[] = [1,2,3];
+	showDate: boolean = true;
+	@Input() dark: boolean
 }
