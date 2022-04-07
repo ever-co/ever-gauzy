27512aece09658625439d81df9f4394bc9051033
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-04-07 17:23:30 +0530

    fix: added unit of time for timesheet menu

--- apps/gauzy/src/app/pages/employees/timesheet/timesheet-routing.module.ts
+++ apps/gauzy/src/app/pages/employees/timesheet/timesheet-routing.module.ts
@@ -16,13 +16,21 @@ const routes: Routes = [
 		children: [
 			{
 				path: 'daily',
-				loadChildren: () =>
-					import('./daily/daily.module').then((m) => m.DailyModule)
+				data: {
+					datePicker: {
+						unitOfTime: 'day'
+					}
+				},
+				loadChildren: () => import('./daily/daily.module').then((m) => m.DailyModule)
 			},
 			{
 				path: 'weekly',
-				loadChildren: () =>
-					import('./weekly/weekly.module').then((m) => m.WeeklyModule)
+				data: {
+					datePicker: {
+						unitOfTime: 'week'
+					}
+				},
+				loadChildren: () => import('./weekly/weekly.module').then((m) => m.WeeklyModule)
 			},
 			{
 				path: 'calendar',
@@ -41,6 +49,9 @@ const routes: Routes = [
 					},
 					selectors: {
 						project: false
+					},
+					datePicker: {
+						unitOfTime: 'month'
 					}
 				},
 				loadChildren: () => import('./approvals/approvals.module').then((m) => m.ApprovalsModule)
