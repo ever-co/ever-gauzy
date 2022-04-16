d7c2bdc25d52819b5f82cbbdfe2ce7b8b0d4434b
Adolphe Kifungo <akifungo@gmail.com>
2022-04-16 08:01:05 +0200

    fix: project count on project team component

--- apps/gauzy/src/app/@shared/table-components/project/project.component.ts
+++ apps/gauzy/src/app/@shared/table-components/project/project.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ViewCell } from 'ng2-smart-table';
 import { OrganizationsService } from '../../../@core/services/organizations.service';
-import { IOrganization } from '../../../../../../../packages/contracts/dist/organization.model';
 import { firstValueFrom } from 'rxjs';
+import { IOrganization } from '@gauzy/contracts';
 
 @Component({
 	selector: 'ngx-project',
@@ -10,10 +10,12 @@ import { firstValueFrom } from 'rxjs';
 	styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit, ViewCell {
+	@Input()
 	value: any;
+	@Input()
 	rowData: any;
 	organization: Promise<IOrganization> | null = null;
-	employees: any[] = [];
+	count: number;
 	constructor(private readonly organizationService: OrganizationsService) {}
 
 	ngOnInit(): void {
@@ -25,15 +27,17 @@ export class ProjectComponent implements OnInit, ViewCell {
 	}
 
 	init() {
+		let count: number = 0;
 		if (this.rowData.employeesMergedTeams) {
 			const buffers = this.rowData.employeesMergedTeams[1];
-			if (buffers) {
+			if (buffers.length > 0) {
 				for (let buffer of buffers) {
-					for (let member of buffer.members) {
-						this.employees.push(member.employee);
-					}
+					count += buffer.members ? buffer.members.length : 0;
 				}
+			} else {
+				count = this.rowData.members.length;
 			}
 		}
+		this.count = count;
 	}
 }
