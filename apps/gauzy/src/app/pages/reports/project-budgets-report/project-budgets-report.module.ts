e7ab5c869f7e1b8794a0adfc0aa715ed8a7bc452
Sineastra <lord.of.light.0002@gmail.com>
2022-04-20 18:26:06 +0300

    feat: update project budget design

--- apps/gauzy/src/app/pages/reports/project-budgets-report/project-budgets-report.module.ts
+++ apps/gauzy/src/app/pages/reports/project-budgets-report/project-budgets-report.module.ts
@@ -17,6 +17,7 @@ import { ProgressStatusModule } from '../../../@shared/progress-status/progress-
 import { HeaderTitleModule } from '../../../@shared/components/header-title/header-title.module';
 import { DateRangeTitleModule } from '../../../@shared/components/date-range-title/date-range-title.module';
 import { GauzyFiltersModule } from '../../../@shared/timesheet/gauzy-filters/gauzy-filters.module';
+import { OrganizationModule } from "../../../@shared/report/organization/organization.module";
 
 @NgModule({
 	declarations: [ProjectBudgetsReportComponent],
@@ -34,7 +35,8 @@ import { GauzyFiltersModule } from '../../../@shared/timesheet/gauzy-filters/gau
 		ProgressStatusModule,
 		HeaderTitleModule,
 		DateRangeTitleModule,
-    	GauzyFiltersModule
-	]
+		GauzyFiltersModule,
+		OrganizationModule,
+	],
 })
 export class ProjectBudgetsReportModule {}
