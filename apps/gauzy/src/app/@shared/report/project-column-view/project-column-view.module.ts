7bcd3914ffd4f220f28fcc3ae2b31c83a5ebc8ab
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-04-22 11:14:10 +0530

    fix: rename project view column component

---
+++ apps/gauzy/src/app/@shared/report/project-column-view/project-column-view.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from "@ngx-translate/core";
+import { SharedModule } from '../../shared.module';
+import { ProjectColumnViewComponent } from "./project-column-view.component";
+
+@NgModule({
+	declarations: [ProjectColumnViewComponent],
+	exports: [ProjectColumnViewComponent],
+	imports: [
+		CommonModule,
+		SharedModule,
+		TranslateModule,
+	],
+})
+export class ProjectColumnViewModule {}
