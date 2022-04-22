2199e12c97e6d368a8d68906b5130db98493017e
Adolphe Kifungo <akifungo@gmail.com>
2022-04-22 09:43:02 +0200

    feat: redesign button cancel

--- apps/gauzy/src/app/@shared/alert-modal/alert-modal.component.ts
+++ apps/gauzy/src/app/@shared/alert-modal/alert-modal.component.ts
@@ -9,7 +9,8 @@ export interface AlertOptions {
 
 @Component({
 	selector: 'ga-alert-modal',
-	templateUrl: './alert-modal.component.html'
+	templateUrl: './alert-modal.component.html',
+  styleUrls: ['./alert-modal.component.scss']
 })
 export class AlertModalComponent implements OnInit {
 	alertOptions: AlertOptions;
