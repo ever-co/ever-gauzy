e13b14e94ba83e5ab02b953910295d145916c2a2
Adolphe Kifungo <akifungo@gmail.com>
2022-04-29 10:45:48 +0200

    fix: unused import

--- apps/gauzy/src/app/pages/positions/positions.component.ts
+++ apps/gauzy/src/app/pages/positions/positions.component.ts
@@ -9,7 +9,7 @@ import { NbDialogService } from '@nebular/theme';
 import { TranslateService } from '@ngx-translate/core';
 import { distinctUntilChange } from '@gauzy/common-angular';
 import { LocalDataSource } from 'ng2-smart-table';
-import { firstValueFrom, filter, tap, distinctUntilChanged } from 'rxjs';
+import { firstValueFrom, filter, tap } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import {
 	OrganizationPositionsService,
