ea2f51ce6de3fd4cf731f1f4c5f6f4e2951e8221
Adolphe Kifungo <akifungo@gmail.com>
2022-05-03 10:54:10 +0200

    fix: padding left

--- apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/interview-panel/table-components/criterions/criterions.component.ts
+++ apps/gauzy/src/app/pages/candidates/manage-candidate-interviews/interview-panel/table-components/criterions/criterions.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
 	selector: 'ga-interview-criterions',
 	template: `
-		<ul style="padding-left:18px; margin:0;">
+		<ul style="padding-left: 10px; margin:0;">
 			<li *ngFor="let tech of this.rowData.technologies">
 				<span> {{ tech.name }} </span>
 			</li>
