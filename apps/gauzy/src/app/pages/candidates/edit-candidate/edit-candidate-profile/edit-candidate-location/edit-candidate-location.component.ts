8d28e12c84902ed3d5f4d0bd29a0d3367cd14b0b
Adolphe Kifungo <akifungo@gmail.com>
2022-05-04 07:53:50 +0200

    fix: overflow on all edit candidate pages

--- apps/gauzy/src/app/pages/candidates/edit-candidate/edit-candidate-profile/edit-candidate-location/edit-candidate-location.component.ts
+++ apps/gauzy/src/app/pages/candidates/edit-candidate/edit-candidate-profile/edit-candidate-location/edit-candidate-location.component.ts
@@ -3,6 +3,14 @@ import { Component } from '@angular/core';
 	selector: 'ga-edit-candidate-location',
 	template: `
 		<ga-employee-location [isCandidate]="true"></ga-employee-location>
-	`
+	`,
+	styles: [
+		`
+			:host {
+				overflow-y: overlay;
+				max-height: calc(100vh - 28rem);
+			}
+		`
+	]
 })
 export class EditCandidateLocationComponent {}
