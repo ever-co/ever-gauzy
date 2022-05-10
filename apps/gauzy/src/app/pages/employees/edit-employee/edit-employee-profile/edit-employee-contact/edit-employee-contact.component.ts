e452fa24b0063b2b23e124eb06fd65aaf2c66ae3
Adolphe Kifungo <akifungo@gmail.com>
2022-05-10 10:45:36 +0200

    fix: firefox overflow not working property on edit employee components

--- apps/gauzy/src/app/pages/employees/edit-employee/edit-employee-profile/edit-employee-contact/edit-employee-contact.component.ts
+++ apps/gauzy/src/app/pages/employees/edit-employee/edit-employee-profile/edit-employee-contact/edit-employee-contact.component.ts
@@ -20,7 +20,7 @@ import { ToastrService } from 'apps/gauzy/src/app/@core/services/toastr.service'
   styles: [
 		`
 			:host {
-        overflow-y: overlay;
+        overflow-y: auto;
 				max-height: calc(100vh - 28rem);
 			}
 		`
