129c70dafd4ade862856ad64323a74b9ae818353
Adolphe Kifungo <akifungo@gmail.com>
2022-05-11 12:25:09 +0200

    feat: using nb accordion

--- apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.module.ts
+++ apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarMenuComponent } from './sidebar-menu.component';
 import { SharedModule } from '../shared.module';
+import { NbAccordionModule } from '@nebular/theme';
 
 @NgModule({
 	declarations: [SidebarMenuComponent],
-	imports: [CommonModule, SharedModule],
+	imports: [CommonModule, SharedModule, NbAccordionModule],
 	exports: [SidebarMenuComponent]
 })
 export class SidebarMenuModule {}
