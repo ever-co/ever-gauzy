96203476a7b16a3395932c34e6b12e9000c86742
Adolphe Kifungo <akifungo@gmail.com>
2022-05-11 12:25:09 +0200

    feat: using nb accordion

--- apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.component.ts
+++ apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.component.ts
@@ -1,16 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NbMenuItem } from '@nebular/theme';
+import { FeatureEnum, PermissionsEnum } from '@gauzy/contracts';
+
+interface IMenuItem extends NbMenuItem {
+	data: {
+		translationKey: string; //Translation key for the title, mandatory for all items
+		permissionKeys?: PermissionsEnum[]; //Check permissions and hide item if any given permission is not present
+		featureKey?: FeatureEnum; //Check permissions and hide item if any given permission is not present
+		withOrganizationShortcuts?: boolean; //Declare if the sidebar item has organization level shortcuts
+		hide?: () => boolean; //Hide the menu item if this returns true
+	};
+}
 
 @Component({
-  selector: 'gauzy-sidebar-menu',
-  templateUrl: './sidebar-menu.component.html',
-  styleUrls: ['./sidebar-menu.component.scss']
+	selector: 'gauzy-sidebar-menu',
+	templateUrl: './sidebar-menu.component.html',
+	styleUrls: ['./sidebar-menu.component.scss']
 })
 export class SidebarMenuComponent implements OnInit {
-  @Input() menu: any;
-  constructor() { }
+	@Input() menu: IMenuItem[];
+	constructor() {}
 
-  ngOnInit(): void {
-    console.log(this.menu)
-  }
-
+	ngOnInit(): void {}
 }
