1c5beb8247cee054147efc6b86dbf93128c54261
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-05-13 12:47:34 +0530

    feat: #4480 at least one field validate for pipeline search form

---
+++ apps/gauzy/src/app/@core/validators/at-least-one-field.validator.ts
@@ -0,0 +1,15 @@
+import { FormGroup } from '@angular/forms';
+
+export function AtLeastOneFieldValidator(group: FormGroup): {[key: string]: any} {
+	let isAtLeastOne = false;
+	if (group && group.controls) {
+		for (const control in group.controls) {
+			if (group.controls.hasOwnProperty(control) && group.controls[control].valid && group.controls[control].value) {
+				isAtLeastOne = true;
+				break;
+			}
+		}
+	}
+	console.log(isAtLeastOne, group);
+	return isAtLeastOne ? null : { requiredAtLeastOne: true };
+}
