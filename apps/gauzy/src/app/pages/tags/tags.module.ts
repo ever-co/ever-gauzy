6f5ae0ae3f6b28c080b1561df0761331b8d66e97
Adolphe Kifungo <akifungo@gmail.com>
2022-05-23 12:36:14 +0200

    feat: integrate pagination component

--- apps/gauzy/src/app/pages/tags/tags.module.ts
+++ apps/gauzy/src/app/pages/tags/tags.module.ts
@@ -28,6 +28,7 @@ import { CardGridModule } from '../../@shared/card-grid/card-grid.module';
 import { TranslateModule } from '../../@shared/translate/translate.module';
 import { HeaderTitleModule } from '../../@shared/components/header-title/header-title.module';
 import { GauzyButtonActionModule } from '../../@shared/gauzy-button-action/gauzy-button-action.module';
+import { PaginationModule } from '../../@shared/pagination/pagination.module';
 
 @NgModule({
 	imports: [
@@ -56,7 +57,8 @@ import { GauzyButtonActionModule } from '../../@shared/gauzy-button-action/gauzy
 		TranslateModule,
 		NbSpinnerModule,
 		HeaderTitleModule,
-		GauzyButtonActionModule
+		GauzyButtonActionModule,
+		PaginationModule
 	],
 	declarations: [TagsComponent, TagsColorComponent]
 })
