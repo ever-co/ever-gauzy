ad57254bb862a85ba143616ee7a864099b9f3293
Adolphe Kifungo <akifungo@gmail.com>
2022-05-26 13:44:45 +0200

    fix: organization with tags

--- apps/gauzy/src/app/@shared/table-components/organization-with-tags/organization-with-tags.component.ts
+++ apps/gauzy/src/app/@shared/table-components/organization-with-tags/organization-with-tags.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-import { NotesWithTagsComponent } from '..';
+import { NbThemeService } from '@nebular/theme';
+import { TranslateService } from '@ngx-translate/core';
+import { PictureNameTagsComponent } from '../picture-name-tags/picture-name-tags.component';
 
 @Component({
 	selector: 'gauzy-organization-with-tags',
 	templateUrl: './organization-with-tags.component.html',
 	styleUrls: ['./organization-with-tags.component.scss']
 })
-export class OrganizationWithTagsComponent extends NotesWithTagsComponent {}
+export class OrganizationWithTagsComponent extends PictureNameTagsComponent {
+	constructor(
+		readonly themeService: NbThemeService,
+		readonly translateService: TranslateService
+	) {
+		super(themeService, translateService);
+	}
+}
