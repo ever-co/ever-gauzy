475186598f2a2af7b65f2390affd225ffe461b66
Chirag Parmar <chiraginfotech93@gmail.com>
2022-06-04 12:47:47 +0530

    Fix: #4595 Country selector should be Combo box (Searchable)

--- apps/gauzy/src/app/@shared/country/country.component.ts
+++ apps/gauzy/src/app/@shared/country/country.component.ts
@@ -14,9 +14,10 @@ import {
 	NG_VALUE_ACCESSOR
 } from '@angular/forms';
 import { ICountry } from '@gauzy/contracts';
-import { UntilDestroy } from '@ngneat/until-destroy';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CountryService } from '../../@core/services/country.service';
 import { TranslationBaseComponent } from '../language-base/translation-base.component';
 
@@ -24,6 +25,7 @@ import { TranslationBaseComponent } from '../language-base/translation-base.comp
 @Component({
 	selector: 'ga-country',
 	templateUrl: './country.component.html',
+	styleUrls: ['./country.component.scss'],
 	providers: [
 		{
 			provide: NG_VALUE_ACCESSOR,
@@ -40,8 +42,9 @@ export class CountryComponent
 	@Input() formControl: FormControl = new FormControl();
 	@Output() selectChange = new EventEmitter<string>();
 	@Output() optionChange = new EventEmitter<ICountry>();
-
+	
 	countries$: Observable<ICountry[]> = this.countryService.countries$;
+	private _countries: ICountry[] = [];
 
 	onChange: any = () => {};
 	onTouched: any = () => {};
@@ -67,14 +70,28 @@ export class CountryComponent
 		this.countryService.find$.next(true);
 	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.countries$
+			.pipe(
+				tap((countries: ICountry[]) => (this._countries = countries)),
+				tap(() => this.onSelectChange(this.country)),				
+				untilDestroyed(this)
+			)
+			.subscribe();
+	}
 
 	ngAfterViewInit() {
 		this.cdr.detectChanges();
 	}
-
-	onSelectChange($event: string) {
-		this.selectChange.emit($event);
+	
+	onSelectChange(value: string) {
+		if (value && this._countries.length > 0) {
+			const country = this._countries.find(
+				(country: ICountry) => country.isoCode === value
+			);
+			this.country = country.isoCode;
+			this.onOptionChange(country);
+		}
 	}
 
 	onOptionChange($event: ICountry) {
@@ -94,5 +111,12 @@ export class CountryComponent
 
 	registerOnTouched(fn: () => void): void {
 		this.onTouched = fn;
+	}
+
+	searchCountry(term: string, item: any) {
+		return (
+			item.isoCode.toLowerCase().includes(term.toLowerCase()) ||
+			item.country.toLowerCase().includes(term.toLowerCase())
+		);
 	}
 }
