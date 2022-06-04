e05c50444faa2c28557f1cd33725d3de4315b292
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-06-04 15:48:49 +0530

    fix: formatting

--- apps/gauzy/src/app/@theme/components/header/selectors/employee/employee.module.ts
+++ apps/gauzy/src/app/@theme/components/header/selectors/employee/employee.module.ts
@@ -5,12 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { EmployeeSelectorComponent } from './employee.component';
 import { EmployeesService, EmployeeStore } from './../../../../../@core/services';
 import { TranslateModule } from './../../../../../@shared/translate/translate.module';
-import { SharedModule } from 'apps/gauzy/src/app/@shared/shared.module';
+import { SharedModule } from './../../../../../@shared/shared.module';
 
 const COMPONENTS = [EmployeeSelectorComponent];
 
 @NgModule({
-	imports: [CommonModule, NgSelectModule, FormsModule, TranslateModule, SharedModule],
+	imports: [
+		CommonModule,
+		NgSelectModule,
+		FormsModule,
+		TranslateModule,
+		SharedModule
+	],
 	exports: [...COMPONENTS],
 	declarations: [...COMPONENTS],
 	providers: [EmployeesService, EmployeeStore]
