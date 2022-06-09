c1352ab163bd129b559c122bb29e8d373df8afa3
Adolphe Kifungo <akifungo@gmail.com>
2022-06-09 11:31:08 +0200

    feat: add button add

--- apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.module.ts
+++ apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarMenuComponent } from './sidebar-menu.component';
 import { SharedModule } from '../shared.module';
-import { NbAccordionModule, NbTooltipModule } from '@nebular/theme';
+import {
+	NbAccordionModule,
+	NbButtonModule,
+	NbTooltipModule
+} from '@nebular/theme';
 import { MenuItemComponent } from './menu-items/concrete/menu-item/menu-item.component';
 import { ChildrenMenuItemComponent } from './menu-items/concrete/children-menu-item/children-menu-item.component';
 import { TooltipDirective } from './tooltip.directive';
@@ -14,7 +18,13 @@ import { TooltipDirective } from './tooltip.directive';
 		ChildrenMenuItemComponent,
 		TooltipDirective
 	],
-	imports: [CommonModule, SharedModule, NbAccordionModule, NbTooltipModule],
+	imports: [
+		CommonModule,
+		SharedModule,
+		NbAccordionModule,
+		NbTooltipModule,
+		NbButtonModule
+	],
 	exports: [SidebarMenuComponent]
 })
 export class SidebarMenuModule {}
