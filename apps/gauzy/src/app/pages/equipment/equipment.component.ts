47e917d1c096fb0c6034c4810e7302af2b7fd399
Adolphe Kifungo <akifungo@gmail.com>
2022-06-09 18:08:39 +0200

    fix: unused import

--- apps/gauzy/src/app/pages/equipment/equipment.component.ts
+++ apps/gauzy/src/app/pages/equipment/equipment.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import {
 	IEquipment,
 	ComponentLayoutStyleEnum,
