18446f507dfef8ee1b8a906558ea7049e06dbf16
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-06-10 16:53:59 +0530

    fix: #4671 product type generics DTO's inherited

--- packages/core/src/product-type/dto/product-type.dto.ts
+++ packages/core/src/product-type/dto/product-type.dto.ts
@@ -1,12 +1,10 @@
-import { LanguagesEnum, ProductTypesIconsEnum, TranslatePropertyInput } from "@gauzy/contracts";
+import { ProductTypesIconsEnum } from "@gauzy/contracts";
 import { ApiPropertyOptional } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsEnum, IsOptional, ValidateNested } from "class-validator";
+import { IsEnum, IsOptional } from "class-validator";
 import { Product, ProductTypeTranslation } from "./../../core/entities/internal";
-import { TenantOrganizationBaseDTO } from "./../../core/dto";
-import { ProductTypeTranslationDTO } from "./product-type-translation.dto";
+import { TranslatableBaseDTO } from "./../../core/dto";
 
-export class ProductTypeDTO extends TenantOrganizationBaseDTO {
+export class ProductTypeDTO extends TranslatableBaseDTO<ProductTypeTranslation[]> {
     
     @ApiPropertyOptional({ type: () => String, enum: ProductTypesIconsEnum })
     @IsOptional()
@@ -14,18 +12,5 @@ export class ProductTypeDTO extends TenantOrganizationBaseDTO {
     readonly icon: ProductTypesIconsEnum;
 
     @ApiPropertyOptional({ type: () => Array, isArray: true })
-    @ValidateNested({ each: true })
-    @Type(() => ProductTypeTranslationDTO)
-    readonly translations: ProductTypeTranslation[];
-
-    @ApiPropertyOptional({ type: () => Array, isArray: true })
     readonly products: Product[];
-
-    @ApiPropertyOptional({ type: () => Function })
-    @IsOptional()
-    readonly translate: (language: LanguagesEnum) => string;
-
-    @ApiPropertyOptional({ type: () => Function })
-    @IsOptional()
-    readonly translateNested: (language: LanguagesEnum, pros: TranslatePropertyInput[]) => any;
 }
