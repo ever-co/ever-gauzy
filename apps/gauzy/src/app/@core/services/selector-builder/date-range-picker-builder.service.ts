3e00a68fa85af2f71cbf2b172b7f9e6356e3cf2c
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-06-16 16:34:28 +0530

    fix: date picker range builder via service

--- apps/gauzy/src/app/@core/services/selector-builder/date-range-picker-builder.service.ts
+++ apps/gauzy/src/app/@core/services/selector-builder/date-range-picker-builder.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import moment from 'moment';
-import { isNotEmpty } from '@gauzy/common-angular';
+import * as moment from 'moment';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { isNotEmpty } from '@gauzy/common-angular';
+import { IDateRangePicker } from '@gauzy/contracts';
 
 export interface IDatePickerConfig {
 	readonly unitOfTime: moment.unitOfTime.Base,
@@ -27,11 +28,33 @@ export class DateRangePickerBuilderService {
 	private _datePickerConfig$: BehaviorSubject<IDatePickerConfig> = new BehaviorSubject(DEFAULT_DATE_PICKER_CONFIG);
 	public datePickerConfig$: Observable<IDatePickerConfig> = this._datePickerConfig$.asObservable();
 
+	public _datePickerRange$: BehaviorSubject<IDateRangePicker> = new BehaviorSubject({
+		startDate: moment().startOf(DEFAULT_DATE_PICKER_CONFIG.unitOfTime).toDate(),
+		endDate: moment().endOf(DEFAULT_DATE_PICKER_CONFIG.unitOfTime).toDate()
+	});
+	public datePickerRange$: Observable<IDateRangePicker> = this._datePickerRange$.asObservable();
+
 	constructor() {}
 
+	/**
+	 * Override date range picker default configuration
+	 * 
+	 * @param options 
+	 */
 	setDatePickerConfig(options: IDatePickerConfig) {
 		if (isNotEmpty(options)) {
 			this._datePickerConfig$.next(options);
+		}
+	}
+
+	/**
+	 * Override date range picker default values
+	 * 
+	 * @param options 
+	 */
+	setDateRangePicker(options: IDateRangePicker) {
+		if (isNotEmpty(options)) {
+			this._datePickerRange$.next(options);
 		}
 	}
 }
