da1810cea6abb403c8176fb8ee31570964077dd8
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-06-16 11:17:02 +0530

    fix: #4734 start & end date before DTO validation

--- packages/core/src/shared/dto/date-range-query.dto.ts
+++ packages/core/src/shared/dto/date-range-query.dto.ts
@@ -1,6 +1,7 @@
 import { IDateRangePicker } from "@gauzy/contracts";
 import { ApiPropertyOptional } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
+import { IsBeforeDate } from "./../../shared/decorators/validations";
 import { TenantOrganizationBaseDTO } from "./../../core/dto";
 
 /**
@@ -11,6 +12,9 @@ export class DateRangeQueryDTO extends TenantOrganizationBaseDTO implements IDat
     @ApiPropertyOptional({ type: () => Date })
     @IsOptional()
     @IsString()
+    @IsBeforeDate(DateRangeQueryDTO, (it) => it.endDate, {
+        message: "Start date must be before to the end date"
+    })
     readonly startDate: Date;
 
     @ApiPropertyOptional({ type: () => Date })
