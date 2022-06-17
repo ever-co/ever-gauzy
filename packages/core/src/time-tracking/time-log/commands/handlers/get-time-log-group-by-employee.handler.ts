a800b0721f2b7dfb75d074e504f16a19b559cd51
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-06-17 14:32:56 +0530

    fix: #4736 average total worked & activity for employees

--- packages/core/src/time-tracking/time-log/commands/handlers/get-time-log-group-by-employee.handler.ts
+++ packages/core/src/time-tracking/time-log/commands/handlers/get-time-log-group-by-employee.handler.ts
@@ -1,11 +1,12 @@
 import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
-import { chain } from 'underscore';
+import { chain, pluck, reduce } from 'underscore';
 import * as moment from 'moment';
 import {
 	IReportDayGroupByEmployee,
 	ITimeLog,
 	ITimeSlot
 } from '@gauzy/contracts';
+import { ArraySum } from '@gauzy/common';
 import { GetTimeLogGroupByEmployeeCommand } from '../get-time-log-group-by-employee.command';
 
 @CommandHandler(GetTimeLogGroupByEmployeeCommand)
@@ -21,6 +22,19 @@ export class GetTimeLogGroupByEmployeeHandler
 		const dailyLogs: any = chain(timeLogs)
 			.groupBy((log) => log.employeeId)
 			.map((byEmployeeLogs: ITimeLog[]) => {
+				/**
+				* calculate avarage duration for specific employee.
+				*/
+				const avgDuration = reduce(pluck(byEmployeeLogs, 'duration'), ArraySum, 0);
+				/**
+				* calculate average activity for specific date range.
+				*/
+				const slots: ITimeSlot[] = chain(byEmployeeLogs).pluck('timeSlots').flatten(true).value();
+				const avgActivity = (
+					(reduce(pluck(slots, 'overall'), ArraySum, 0) * 100) / 
+					(reduce(pluck(slots, 'duration'), ArraySum, 0))
+				) || 0;
+
 				const employee =
 					byEmployeeLogs.length > 0
 						? byEmployeeLogs[0].employee
@@ -34,66 +48,39 @@ export class GetTimeLogGroupByEmployeeHandler
 						const byProject = chain(byDateLogs)
 							.groupBy('projectId')
 							.map((byProjectLogs: ITimeLog[]) => {
-								const sum = byProjectLogs.reduce(
-									(iteratee: any, log: any) => {
-										return iteratee + log.duration;
-									},
-									0
-								);
-
-								const timeSlots: ITimeSlot[] = chain(
-									byProjectLogs
-								)
-								.pluck('timeSlots')
-								.flatten(true)
-								.value();
+								/**
+								* calculate avarage duration of the employee
+								*/
+								const sum = reduce(pluck(byProjectLogs, 'duration'), ArraySum, 0);
+								/**
+								* calculate average activity of the employee
+								*/
+								const slots: ITimeSlot[] = chain(byProjectLogs).pluck('timeSlots').flatten(true).value();
 								/**
 								* Calculate Average activity of the employee
 								*/
-								const overallSum = timeSlots.reduce(
-									(
-										iteratee: any,
-										timeSlot: ITimeSlot
-									) => {
-										return (
-											iteratee +
-											timeSlot.overall
-										);
-									},
-									0
+								const avgActivity = (
+									(reduce(pluck(slots, 'overall'), ArraySum, 0) * 100) / 
+									(reduce(pluck(slots, 'duration'), ArraySum, 0))
 								) || 0;
-								const durationSum = timeSlots.reduce(
-									(
-										iteratee: any,
-										timeSlot: ITimeSlot
-									) => {
-										return (
-											iteratee +
-											timeSlot.duration
-										);
-									},
-									0
-								) || 0;
-								const avgActivity = ((overallSum * 100) / (durationSum)) || 0;
 								const project =
 									byProjectLogs.length > 0
 										? byProjectLogs[0].project
 										: null;
-
 								const task =
 									byProjectLogs.length > 0
 										? byProjectLogs[0].task
 										: null;
-
 								return {
 									task,
 									project,
 									sum,
-									activity: avgActivity.toFixed(2)
+									activity: parseFloat(
+										parseFloat(avgActivity + '').toFixed(2)
+									)
 								};
 							})
 							.value();
-
 						return {
 							date,
 							projectLogs: byProject
@@ -101,7 +88,14 @@ export class GetTimeLogGroupByEmployeeHandler
 					})
 					.value();
 
-				return { employee, logs: byDate };
+				return {
+					employee,
+					logs: byDate,
+					sum: avgDuration || null,
+					activity: parseFloat(
+						parseFloat(avgActivity + '').toFixed(2)
+					)
+				};
 			})
 			.value();
 
