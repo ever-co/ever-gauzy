0de536c851107930577f95ec1d629e7c372536ab
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-06-22 15:05:46 +0530

    fix: #4805 default dummy project image using name entity

--- packages/core/src/organization-project/organization-project.subscriber.ts
+++ packages/core/src/organization-project/organization-project.subscriber.ts
@@ -1,4 +1,5 @@
 import { EntitySubscriberInterface, EventSubscriber, InsertEvent, UpdateEvent } from "typeorm";
+import { getDummyImage } from "./../core/utils";
 import { OrganizationProject } from "./organization-project.entity";
 
 @EventSubscriber()
@@ -14,9 +15,13 @@ export class OrganizationProjectSubscriber implements EntitySubscriberInterface<
     * Called before organization project insertion.
     */
     beforeInsert(event: InsertEvent<OrganizationProject>) {
-        if (event) {
+        if (event.entity) {
             const { entity } = event;
             entity.membersCount = (entity.members) ? entity.members.length : 0;
+            if (!entity.imageUrl && entity.name) {
+                const name = entity.name.toLowerCase().split(' ').slice(0, 2).map((elem) => elem[0]).join('');
+                entity.imageUrl = getDummyImage(330, 300, name.toUpperCase());
+            }
         }
     }
 
