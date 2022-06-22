6c44d48990bc49d2d930ff3ee40b7bc1aa33b7bf
Adolphe Kifungo <akifungo@gmail.com>
2022-06-22 16:48:34 +0200

    fix: grouping algorithm of project organization employee

--- apps/gauzy/src/app/@shared/table-components/project-organization-employees/project-organization-employees.component.ts
+++ apps/gauzy/src/app/@shared/table-components/project-organization-employees/project-organization-employees.component.ts
@@ -22,12 +22,14 @@ export class ProjectOrganizationEmployeesComponent implements OnInit, ViewCell {
 		if (this.rowData.members.length > 0) {
 			const size: number = this.rowData.members.length;
 			const half: number = size / 2;
-			this.employeesFirstHalf = this.rowData.members.splice(
-				0,
-				Math.ceil(half)
-			);
-			this.employeesLastHalf = this.rowData.members;
-      this.employeesFirstHalf[0]
+			for (let i = 0; i < size; i++) {
+				if (i < Math.ceil(half)) {
+					this.employeesFirstHalf[i] = this.rowData.members[i];
+				} else {
+					this.employeesLastHalf[i - Math.ceil(half)] =
+						this.rowData.members[i];
+				}
+			}
 		}
 	}
 
