2fc011623cd996242cbc3f0d2ec35028f7a19b80
Adolphe Kifungo <akifungo@gmail.com>
2022-06-25 15:51:24 +0200

    feat: improve imageUrl getter

--- apps/gauzy/src/app/pages/inventory/components/table-components/image-row.component.ts
+++ apps/gauzy/src/app/pages/inventory/components/table-components/image-row.component.ts
@@ -66,13 +66,20 @@ export class ImageRowComponent implements ViewCell {
 	get imageUrl() {
 		if (this.rowData.imageUrl) {
 			return this.rowData.imageUrl;
-		} else if (
-			this.rowData.featuredImage &&
-			this.rowData.featuredImage.url
-		) {
+		}
+		if (this.rowData.featuredImage && this.rowData.featuredImage.url) {
 			return this.rowData.featuredImage.url;
-		} else if (this.rowData.url) {
+		}
+		if (this.rowData.url) {
 			return this.rowData.url;
 		}
+
+		if (!this.value) return false;
+
+		if (this.value.imageUrl) return this.value.imageUrl;
+
+		if (this.value.url) return this.value.url;
+
+		if (typeof this.value == 'string') return this.value;
 	}
 }
