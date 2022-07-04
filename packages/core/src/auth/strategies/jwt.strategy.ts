18b8f71691d735d3f14923b4295dea3880c4feb2
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-07-04 18:04:44 +0530

    fix: #4720 JWT refresh token strategy guard

--- packages/core/src/auth/strategies/jwt.strategy.ts
+++ packages/core/src/auth/strategies/jwt.strategy.ts
@@ -2,24 +2,29 @@ import { environment as env } from '@gauzy/config';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from './auth.service';
+import { AuthService } from './../auth.service';
 import { IUser } from '@gauzy/contracts';
+import { JwtPayload } from 'jsonwebtoken';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-	constructor(private readonly authService: AuthService) {
+
+	constructor(
+		private readonly authService: AuthService
+	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			secretOrKey: env.JWT_SECRET
 		});
 	}
 
-	async validate(payload, done: Function) {
+	async validate(payload: JwtPayload, done: Function) {
 		try {
+			const { id, thirdPartyId } = payload;
 			// We use this to also attach the user object to the request context.
 			const user: IUser = await this.authService.getAuthenticatedUser(
-				payload.id,
-				payload.thirdPartyId
+				id,
+				thirdPartyId
 			);
 
 			if (!user) {
