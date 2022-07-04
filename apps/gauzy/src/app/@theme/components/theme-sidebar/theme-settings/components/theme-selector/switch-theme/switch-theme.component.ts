de3935fe28a4eb6e369346a15e2cf4d337426097
Adolphe Kifungo <akifungo@gmail.com>
2022-07-04 22:51:37 +0200

    feat: switching to OS theme color

--- apps/gauzy/src/app/@theme/components/theme-sidebar/theme-settings/components/theme-selector/switch-theme/switch-theme.component.ts
+++ apps/gauzy/src/app/@theme/components/theme-sidebar/theme-settings/components/theme-selector/switch-theme/switch-theme.component.ts
@@ -9,15 +9,39 @@ import { Store } from '../../../../../../../@core/services/store.service';
 	styleUrls: ['./switch-theme.component.scss']
 })
 export class SwitchThemeComponent extends ThemeSelectorComponent {
-
-  @Input() hasText: boolean = true;
+	@Input() hasText: boolean = true;
 
 	constructor(readonly themeService: NbThemeService, readonly store: Store) {
 		super(themeService, store);
+		// Listerning event and switching to current OS color theme
+		window
+			.matchMedia('(prefers-color-scheme: dark)')
+			.addEventListener('change', (event) => {
+				event.matches
+					? this.isDark.state
+						? null
+						: this.switchTheme()
+					: !this.isDark.state
+					? null
+					: this.switchTheme();
+			});
 		this.ngOnInit();
+		this.getPreferColorOsScheme();
 	}
 
 	switchTheme() {
 		this.reverseTheme();
+	}
+	/**
+	 * get current OS color and switching to it.
+	 */
+	public getPreferColorOsScheme() {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			this.isDark.state ? null : this.switchTheme();
+		}
+
+		if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+			this.isDark.state ? this.switchTheme() : null;
+		}
 	}
 }
