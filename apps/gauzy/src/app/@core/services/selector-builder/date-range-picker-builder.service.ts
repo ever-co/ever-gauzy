2d1af8fccd1050440849c3020d1ccb1172a8b92b
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-07-06 12:43:13 +0530

    fix: #4737 default selectors dates

--- apps/gauzy/src/app/@core/services/selector-builder/date-range-picker-builder.service.ts
+++ apps/gauzy/src/app/@core/services/selector-builder/date-range-picker-builder.service.ts
@@ -24,37 +24,42 @@ export const DEFAULT_DATE_PICKER_CONFIG: IDatePickerConfig = {
 	providedIn: 'root'
 })
 export class DateRangePickerBuilderService {
-	
+
 	private _datePickerConfig$: BehaviorSubject<IDatePickerConfig> = new BehaviorSubject(DEFAULT_DATE_PICKER_CONFIG);
 	public datePickerConfig$: Observable<IDatePickerConfig> = this._datePickerConfig$.asObservable();
 
-	public _datePickerRange$: BehaviorSubject<IDateRangePicker> = new BehaviorSubject({
+	public dates$: BehaviorSubject<IDateRangePicker> = new BehaviorSubject({
 		startDate: moment().startOf(DEFAULT_DATE_PICKER_CONFIG.unitOfTime).toDate(),
 		endDate: moment().endOf(DEFAULT_DATE_PICKER_CONFIG.unitOfTime).toDate()
 	});
-	public datePickerRange$: Observable<IDateRangePicker> = this._datePickerRange$.asObservable();
 
 	constructor() {}
 
 	/**
 	 * Override date range picker default configuration
-	 * 
-	 * @param options 
+	 *
+	 * @param options
 	 */
 	setDatePickerConfig(options: IDatePickerConfig) {
 		if (isNotEmpty(options)) {
+
+			const { unitOfTime } = options;
+			const startDate = moment().startOf(unitOfTime).toDate();
+			const endDate = moment().endOf(unitOfTime).toDate();
+			this.setDateRangePicker({ startDate, endDate });
+
 			this._datePickerConfig$.next(options);
 		}
 	}
 
 	/**
 	 * Override date range picker default values
-	 * 
-	 * @param options 
+	 *
+	 * @param options
 	 */
 	setDateRangePicker(options: IDateRangePicker) {
 		if (isNotEmpty(options)) {
-			this._datePickerRange$.next(options);
+			this.dates$.next(options);
 		}
 	}
 }
