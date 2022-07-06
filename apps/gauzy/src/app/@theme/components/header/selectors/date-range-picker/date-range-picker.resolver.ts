cdfc716540032ce6e6f3219aa586671dc2f783dc
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-07-06 18:06:12 +0530

    fix: #4737 date range picker dates route resolver

---
+++ apps/gauzy/src/app/@theme/components/header/selectors/date-range-picker/date-range-picker.resolver.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
+import { of as observableOf } from 'rxjs';
+import { Observable } from "rxjs/internal/Observable";
+import * as moment from 'moment';
+import { IDateRangePicker } from "@gauzy/contracts";
+import { IDatePickerConfig } from "./../../../../../@core/services/selector-builder/date-range-picker-builder.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DateRangePickerResolver implements Resolve<Observable<IDateRangePicker>> {
+
+    resolve(route: ActivatedRouteSnapshot): Observable<IDateRangePicker>  {
+        const { unitOfTime } = route.data.datePicker as IDatePickerConfig;
+
+        const date_start = route.queryParams.date || new Date();
+        const date_end = route.queryParams.date_end;
+
+        const start_date = moment(date_start).startOf(unitOfTime);
+        const end_date = moment(date_end || start_date).endOf(unitOfTime);
+
+        return observableOf({
+            startDate: start_date.toDate(),
+            endDate: end_date.toDate()
+        });
+    }
+}
