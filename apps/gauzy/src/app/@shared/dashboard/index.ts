cc5814ee6d9d129fc6e6c0a3015be99028ef9641
Adolphe Kifungo <akifungo@gmail.com>
2022-07-14 18:39:29 +0200

    feat: export all new components from dashboard

--- apps/gauzy/src/app/@shared/dashboard/index.ts
+++ apps/gauzy/src/app/@shared/dashboard/index.ts
@@ -1,2 +1,4 @@
 export * from './profit-history/profit-history.component';
 export * from './records-history/records-history.component';
+export * from './widget/widget.component';
+export * from './widget-layout/widget-layout.component';
