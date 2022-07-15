f51dab7d5b6b97a85bb6b21fc6c0e1cf1319fc72
Adolphe Kifungo <akifungo@gmail.com>
2022-07-15 15:46:02 +0200

    feat: implement draggable interface

--- apps/gauzy/src/app/@shared/dashboard/widget/widget.component.ts
+++ apps/gauzy/src/app/@shared/dashboard/widget/widget.component.ts
@@ -1,13 +1,37 @@
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Draggable } from '../interfaces/draggable.interface';
 
 @Component({
 	selector: 'ga-widget',
 	templateUrl: './widget.component.html',
 	styleUrls: ['./widget.component.scss']
 })
-export class WidgetComponent implements OnInit {
-	@Input()
-	templateRef: TemplateRef<HTMLElement>;
+export class WidgetComponent implements OnInit, Draggable {
+	private _templateRef: TemplateRef<HTMLElement>;
+	private _position: number;
+	private _title: string;
+
 	constructor() {}
+  
 	ngOnInit(): void {}
+
+	@Input()
+	set templateRef(value: TemplateRef<HTMLElement>) {
+		this._templateRef = value;
+	}
+	get templateRef(): TemplateRef<HTMLElement> {
+		return this._templateRef;
+	}
+	set title(value: string) {
+		this._title = value;
+	}
+	get title(): string {
+		return this._title;
+	}
+	get position(): number {
+		return this._position;
+	}
+	set position(value: number) {
+		this._position = value;
+	}
 }
