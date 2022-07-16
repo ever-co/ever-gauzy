ad10d068d07bb6ba3834297d16e8bd03abf69d11
Adolphe Kifungo <akifungo@gmail.com>
2022-07-16 13:22:07 +0200

    feat: implements new interfaces and add popover with options

--- apps/gauzy/src/app/@shared/dashboard/widget/widget.component.ts
+++ apps/gauzy/src/app/@shared/dashboard/widget/widget.component.ts
@@ -1,18 +1,24 @@
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Collapsable } from '../interfaces/collapsable.interface';
 import { Draggable } from '../interfaces/draggable.interface';
+import { Expandable } from '../interfaces/expandable.interface';
 
 @Component({
 	selector: 'ga-widget',
 	templateUrl: './widget.component.html',
 	styleUrls: ['./widget.component.scss']
 })
-export class WidgetComponent implements OnInit, Draggable {
+export class WidgetComponent
+	implements OnInit, Draggable, Expandable, Collapsable
+{
 	private _templateRef: TemplateRef<HTMLElement>;
 	private _position: number;
 	private _title: string;
+	private _collapsed: boolean = false;
+	hide: boolean = false;
 
 	constructor() {}
-  
+
 	ngOnInit(): void {}
 
 	@Input()
@@ -33,5 +39,17 @@ export class WidgetComponent implements OnInit, Draggable {
 	}
 	set position(value: number) {
 		this._position = value;
+	}
+	get isExpand(): boolean {
+		return !this._collapsed;
+	}
+	set isExpand(value: boolean) {
+		this._collapsed = !value;
+	}
+	get isCollapse(): boolean {
+		return this._collapsed;
+	}
+	set isCollapse(value: boolean) {
+		this._collapsed = value;
 	}
 }
--- apps/gauzy/src/app/@shared/dashboard/widget/widget.module.ts
+++ apps/gauzy/src/app/@shared/dashboard/widget/widget.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WidgetComponent } from './widget.component';
 import { NgxDraggableDomModule } from 'ngx-draggable-dom';
+import { NbButtonModule, NbIconModule, NbPopoverModule } from '@nebular/theme';
 
 @NgModule({
 	declarations: [WidgetComponent],
-	imports: [CommonModule, NgxDraggableDomModule],
+	imports: [
+		CommonModule,
+		NgxDraggableDomModule,
+		NbIconModule,
+		NbPopoverModule,
+		NbButtonModule
+	],
 	exports: [WidgetComponent]
 })
 export class WidgetModule {}
