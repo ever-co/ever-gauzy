c6a99afd803f25ba1e26b9d1de78dd505c445a9b
Adolphe Kifungo <akifungo@gmail.com>
2022-07-16 22:01:14 +0200

    fix: scope of methods in Layout draggable object

--- apps/gauzy/src/app/@shared/dashboard/interfaces/layout-with-draggable-object.abstract.ts
+++ apps/gauzy/src/app/@shared/dashboard/interfaces/layout-with-draggable-object.abstract.ts
@@ -6,7 +6,7 @@ import { Subject } from 'rxjs/internal/Subject';
 export abstract class LayoutWithDraggableObject {
 	protected draggableObject: TemplateRef<HTMLElement>[] = [];
 	private _event: Subject<any> = new Subject<any>();
-	drop(event: CdkDragDrop<number>): void {
+	protected drop(event: CdkDragDrop<number>): void {
 		moveItemInArray(
 			this.draggableObject,
 			event.previousContainer.data,
