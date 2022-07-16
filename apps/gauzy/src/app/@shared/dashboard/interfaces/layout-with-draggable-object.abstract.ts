5cb5cd71e3627485e76373da5901802f878e97fa
Adolphe Kifungo <akifungo@gmail.com>
2022-07-16 19:42:48 +0200

    feat: add new method to abstract class Layout draggable object

--- apps/gauzy/src/app/@shared/dashboard/interfaces/layout-with-draggable-object.abstract.ts
+++ apps/gauzy/src/app/@shared/dashboard/interfaces/layout-with-draggable-object.abstract.ts
@@ -1,13 +1,23 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { TemplateRef } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Subject } from 'rxjs/internal/Subject';
 
 export abstract class LayoutWithDraggableObject {
 	protected draggableObject: TemplateRef<HTMLElement>[] = [];
+	private _event: Subject<any> = new Subject<any>();
 	drop(event: CdkDragDrop<number>): void {
 		moveItemInArray(
 			this.draggableObject,
 			event.previousContainer.data,
 			event.container.data
 		);
+	}
+	protected onDragEnded(event: Object) {
+		this._event.next(event);
+	}
+
+	protected get event(): Observable<any> {
+		return this._event.asObservable();
 	}
 }
