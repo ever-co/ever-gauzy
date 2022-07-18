ece1a9b2e9a6407486ad7109097ebf49b4e9a953
Adolphe Kifungo <akifungo@gmail.com>
2022-07-18 13:38:34 +0200

    feat: improve widget service

--- apps/gauzy/src/app/@shared/dashboard/widget/widget.service.ts
+++ apps/gauzy/src/app/@shared/dashboard/widget/widget.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, TemplateRef } from '@angular/core';
+import { GuiDrag } from '../interfaces/gui-drag.abstract';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class WidgetService {
-	private _widgets: any[] = [];
+	private _widgetsRef: TemplateRef<HTMLElement>[] = [];
+	private _widgets: GuiDrag[] = [];
 
 	constructor() {}
 
-	public get widgets(): any[] {
+	public get widgetsRef(): TemplateRef<HTMLElement>[] {
+		return this._widgetsRef;
+	}
+	public set widgetsRef(value: TemplateRef<HTMLElement>[]) {
+		this._widgetsRef = value;
+	}
+
+	public get widgets(): GuiDrag[] {
 		return this._widgets;
 	}
-	public set widgets(value: any[]) {
+	public set widgets(value: GuiDrag[]) {
 		this._widgets = value;
 	}
 }
