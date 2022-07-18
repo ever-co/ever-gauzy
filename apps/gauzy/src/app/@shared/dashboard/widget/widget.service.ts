7d6991bcde4f5533a1f053c03a4dfd59af105bd7
Adolphe Kifungo <akifungo@gmail.com>
2022-07-18 21:13:13 +0200

    feat: improve widget service with dashboard persistance inheritance

--- apps/gauzy/src/app/@shared/dashboard/widget/widget.service.ts
+++ apps/gauzy/src/app/@shared/dashboard/widget/widget.service.ts
@@ -1,19 +1,33 @@
 import { Injectable, TemplateRef } from '@angular/core';
+import { DashboardPersistance } from '../interfaces/dashboard-persistance.abstract';
 import { GuiDrag } from '../interfaces/gui-drag.abstract';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class WidgetService {
+export class WidgetService extends DashboardPersistance {
 	private _widgetsRef: TemplateRef<HTMLElement>[] = [];
 	private _widgets: GuiDrag[] = [];
 
-	constructor() {}
+	constructor() {
+		super();
+		this._KEY = '_dashboard';
+	}
 
 	public get widgetsRef(): TemplateRef<HTMLElement>[] {
 		return this._widgetsRef;
 	}
 	public set widgetsRef(value: TemplateRef<HTMLElement>[]) {
+		const buffers: GuiDrag[] = [];
+		this.widgetsRef.forEach((widgetRef: TemplateRef<HTMLElement>) => {
+			this.widgets.forEach((widget: GuiDrag) => {
+				if (widgetRef === widget.templateRef) {
+					buffers.push(widget);
+				}
+			});
+		});
+		this.widgets = buffers;
+		this.serialize(this.widgets);
 		this._widgetsRef = value;
 	}
 
