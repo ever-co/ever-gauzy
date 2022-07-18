08930ce92fca380dad1cbf70c1cf9871c1192b5b
Adolphe Kifungo <akifungo@gmail.com>
2022-07-18 10:19:30 +0200

    feat: use widget service to remember widget positions

--- apps/gauzy/src/app/@shared/dashboard/widget-layout/widget-layout.component.ts
+++ apps/gauzy/src/app/@shared/dashboard/widget-layout/widget-layout.component.ts
@@ -1,5 +1,7 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { LayoutWithDraggableObject } from '../interfaces/layout-with-draggable-object.abstract';
+import { WidgetService } from '../widget/widget.service';
 
 @Component({
 	selector: 'ga-widget-layout',
@@ -15,13 +17,27 @@ export class WidgetLayoutComponent
 		this.draggableObject = value;
 	}
 
-	constructor() {
+	constructor(private readonly widgetService: WidgetService) {
 		super();
+	}
+
+	protected drop(event: CdkDragDrop<number>): void {
+		moveItemInArray(
+			this.draggableObject,
+			event.previousContainer.data,
+			event.container.data
+		);
+		this.widgetService.widgets = this.draggableObject;
 	}
 
 	ngOnInit(): void {}
 
 	get widgets() {
+		if (
+			this.widgetService.widgets.length > 0 &&
+			this.draggableObject !== this.widgetService.widgets
+		)
+			this.draggableObject = this.widgetService.widgets;
 		return this.draggableObject;
 	}
 }
