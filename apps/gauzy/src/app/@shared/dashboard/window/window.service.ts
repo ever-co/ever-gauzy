06833041eb85d839acd40e3a82508373da4362c5
Adolphe Kifungo <akifungo@gmail.com>
2022-07-18 21:45:15 +0200

    feat: improve window service with dashboard persistance inheritance

--- apps/gauzy/src/app/@shared/dashboard/window/window.service.ts
+++ apps/gauzy/src/app/@shared/dashboard/window/window.service.ts
@@ -1,17 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, TemplateRef } from '@angular/core';
+import { DashboardPersistance } from '../interfaces/dashboard-persistance.abstract';
+import { GuiDrag } from '../interfaces/gui-drag.abstract';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class WindowService {
-	private _windows: any[] = [];
+export class WindowService extends DashboardPersistance {
+	private _windowsRef: TemplateRef<HTMLElement>[] = [];
+	private _windows: GuiDrag[] = [];
 
-	constructor() {}
+	constructor() {
+		super();
+		this._KEY = '_window';
+	}
 
-	public get windows(): any[] {
+	public get windowsRef(): any[] {
+		return this._windowsRef;
+	}
+	public set windowsRef(value: any[]) {
+		const buffers: GuiDrag[] = [];
+		this.windowsRef.forEach((windowsRef: TemplateRef<HTMLElement>) => {
+			this.windows.forEach((window: GuiDrag) => {
+				if (windowsRef === window.templateRef) {
+					buffers.push(window);
+				}
+			});
+		});
+		this.windows = buffers;
+		this.serialize(this.windows);
+		this._windowsRef = value;
+	}
+	public get windows(): GuiDrag[] {
 		return this._windows;
 	}
-	public set windows(value: any[]) {
+	public set windows(value: GuiDrag[]) {
 		this._windows = value;
 	}
 }
