2c835b16f5c54d1c0d215372b99dde71816c1ac7
Adolphe Kifungo <akifungo@gmail.com>
2022-07-18 10:26:19 +0200

    feat: use window service to remember widget positions

--- apps/gauzy/src/app/@shared/dashboard/window-layout/window-layout.component.ts
+++ apps/gauzy/src/app/@shared/dashboard/window-layout/window-layout.component.ts
@@ -1,5 +1,7 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { LayoutWithDraggableObject } from '../interfaces/layout-with-draggable-object.abstract';
+import { WindowService } from '../window/window.service';
 
 @Component({
 	selector: 'ga-window-layout',
@@ -15,13 +17,27 @@ export class WindowLayoutComponent
 		this.draggableObject = value;
 	}
 
-	constructor() {
+	protected drop(event: CdkDragDrop<number, number, any>): void {
+		moveItemInArray(
+			this.draggableObject,
+			event.previousContainer.data,
+			event.container.data
+		);
+		this.windowService.windows = this.draggableObject;
+	}
+
+	constructor(private windowService: WindowService) {
 		super();
 	}
 
 	ngOnInit(): void {}
 
 	get windows() {
+		if (
+			this.windowService.windows.length > 0 &&
+			this.draggableObject !== this.windowService.windows
+		)
+			this.draggableObject = this.windowService.windows;
 		return this.draggableObject;
 	}
 }
