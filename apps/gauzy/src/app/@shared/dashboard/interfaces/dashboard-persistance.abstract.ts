67df1f615c6a27e972965d04db69e55625910a7c
Adolphe Kifungo <akifungo@gmail.com>
2022-07-19 12:51:51 +0200

    feat: improve dashboard persistance abstract class

--- apps/gauzy/src/app/@shared/dashboard/interfaces/dashboard-persistance.abstract.ts
+++ apps/gauzy/src/app/@shared/dashboard/interfaces/dashboard-persistance.abstract.ts
@@ -1,28 +1,44 @@
+import { Observable } from 'rxjs/internal/Observable';
+import { Subject } from 'rxjs/internal/Subject';
 import { GuiDrag } from './gui-drag.abstract';
 import { Serializable } from './serializable.interface';
 
 export abstract class DashboardPersistance implements Serializable<GuiDrag> {
 	protected _KEY: string;
 	protected _data: GuiDrag[];
+	private _notifier$: Subject<Object> = new Subject();
 
 	public serialize(values: GuiDrag[]): void {
 		if (values.length === 0) return;
-		const toJson: Partial<GuiDrag>[] = values.map(
-			(value: Partial<GuiDrag>) => {
-				return {
-					position: value.position,
-					isCollapse: value.isCollapse,
-					isExpand: value.isExpand,
-					hide: value.hide,
-					title: value.title
-				};
-			}
-		);
-		localStorage.setItem(this._KEY, JSON.stringify(toJson));
+		if (this._KEY)
+			localStorage.setItem(
+				this._KEY,
+				JSON.stringify(this.toJson(values))
+			);
 	}
+
+	protected toJson(values: GuiDrag[]): Partial<GuiDrag>[] {
+		return values.map((value: Partial<GuiDrag>) => {
+			return {
+				position: value.position,
+				isCollapse: value.isCollapse,
+				isExpand: value.isExpand,
+				hide: value.hide,
+				title: value.title
+			};
+		});
+	}
+
 	public deSerialize(): Partial<GuiDrag>[] {
 		return JSON.parse(
 			localStorage.getItem(this._KEY)
 		) as Partial<GuiDrag>[];
+	}
+
+	public get notifier$(): Observable<Object> {
+		return this._notifier$.asObservable();
+	}
+	public set notifier$(value: Object) {
+		this._notifier$.next(value);
 	}
 }
