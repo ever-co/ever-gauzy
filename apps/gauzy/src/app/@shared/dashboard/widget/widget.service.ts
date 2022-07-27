647982642de7d7f1adfa77524c115ad2bbef91af
Adolphe Kifungo <akifungo@gmail.com>
2022-07-27 21:36:50 +0200

    feat: improve widget service

--- apps/gauzy/src/app/@shared/dashboard/widget/widget.service.ts
+++ apps/gauzy/src/app/@shared/dashboard/widget/widget.service.ts
@@ -18,15 +18,19 @@ export class WidgetService {
 	private _persistanceTakers: PersistanceTakers;
 	private _localStorage: BackupStrategy;
 	private _widgets$: Subject<Partial<GuiDrag[]>>;
+	private _strategy: BackupStrategy;
 
 	constructor(private readonly store: Store) {
 		this._layoutPersistance = new LayoutPersistance();
 		this._localStorage = new LocalstorageStrategy();
-		this._persistanceTakers = new PersistanceTakers();
+		this._persistanceTakers = new PersistanceTakers(
+			this._layoutPersistance
+		);
 		this._widgets$ = new Subject();
 		this._widgets$
 			.pipe(
 				tap((widgets: GuiDrag[]) => (this.widgets = widgets)),
+				tap(() => (this._layoutPersistance.state = this.widgets)),
 				filter(() => this.widgetsRef.length === 0),
 				tap(() => {
 					this.retrieve().length === 0
@@ -42,6 +46,7 @@ export class WidgetService {
 	public get widgetsRef(): TemplateRef<HTMLElement>[] {
 		return this._widgetsRef;
 	}
+
 	public set widgetsRef(value: TemplateRef<HTMLElement>[]) {
 		this._widgetsRef = value;
 		this.sorting();
@@ -74,36 +79,31 @@ export class WidgetService {
 	public get widgets(): GuiDrag[] {
 		return this._widgets;
 	}
+
 	public set widgets(value: GuiDrag[]) {
 		this._widgets = value;
 	}
+
 	public save(): void {
 		if (this.widgets.length === 0) return;
-		this._layoutPersistance.state = this.widgets;
-		this._persistanceTakers.strategy = this._localStorage;
-		this._persistanceTakers.addPersistance(this._layoutPersistance.save());
-		this.store.widgets =
-			this._persistanceTakers.strategy.serialize() as Partial<GuiDrag>[];
+		this._persistanceTakers.backup();
+		this._strategy = this._localStorage;
+		this._strategy.serializables = this.widgets;
+		this.store.widgets = this._strategy.serialize() as Partial<GuiDrag>[];
 	}
 
 	public retrieve(): Partial<GuiDrag>[] {
-		this._persistanceTakers.strategy = this._localStorage;
-		return this._persistanceTakers.strategy.deSerialize(
-			this.store.widgets,
-			this.widgets
-		);
+		this._strategy = this._localStorage;
+		return this._strategy.deSerialize(this.store.widgets, this.widgets);
 	}
 
 	public undoDrag() {
-		this._persistanceTakers.strategy = this._localStorage;
 		this._persistanceTakers.undo();
-		if (this._persistanceTakers.lastPersistance) {
-			this.widgets =
-				this._persistanceTakers.lastPersistance.restore() as GuiDrag[];
-			this.sortingReverse();
-			this.store.widgets =
-				this._persistanceTakers.strategy.serialize() as Partial<GuiDrag>[];
-		}
+		this.widgets = this._layoutPersistance.state as GuiDrag[];
+		this.sortingReverse();
+		this._strategy = this._localStorage;
+		this._strategy.serializables = this.widgets;
+		this.store.widgets = this._strategy.serialize() as Partial<GuiDrag>[];
 	}
 
 	public set widgets$(value: Partial<GuiDrag[]>) {
