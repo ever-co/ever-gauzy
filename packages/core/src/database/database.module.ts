fc3673059c1844cb9b8863222dea91eee4d370fa
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-07-27 10:43:13 +0530

    fix: no more need to define database connection name

--- packages/core/src/database/database.module.ts
+++ packages/core/src/database/database.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@gauzy/config';
-import { DEFAULT_DB_CONNECTION } from '@gauzy/common';
 
 /**
  * Import and provide base typeorm related classes.
@@ -12,7 +11,6 @@ import { DEFAULT_DB_CONNECTION } from '@gauzy/common';
 	imports: [
 		TypeOrmModule.forRootAsync({
 			imports: [ConfigModule],
-			name: DEFAULT_DB_CONNECTION,
 			useFactory: async (configService: ConfigService) => {
 				const { dbConnectionOptions } = configService.config;
 				return dbConnectionOptions;
