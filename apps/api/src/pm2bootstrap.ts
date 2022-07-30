68394b91d3fd57d33d94870efdae13143ca49250
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-07-30 21:57:30 +0530

    fix: import dotenv using ES6

--- apps/api/src/pm2bootstrap.ts
+++ apps/api/src/pm2bootstrap.ts
@@ -1,4 +1,6 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
 const pm2 = require('pm2');
 
 const PRIVATE_KEY = process.env.KEYMETRICS_SECRET_KEY || '';
--- packages/config/src/default-configuration.ts
+++ packages/config/src/default-configuration.ts
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 import {
 	DEFAULT_API_HOST,
