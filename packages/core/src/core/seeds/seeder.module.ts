3e80f09b70e5996082be8477f4ae89364dddcb68
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-08-02 22:40:50 +0530

    fix: #4973 database connections for API & SEEDER

--- packages/core/src/core/seeds/seeder.module.ts
+++ packages/core/src/core/seeds/seeder.module.ts
@@ -1,14 +1,16 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { getDynamicPluginsModules } from '@gauzy/plugin';
+import { ConfigModule } from '@gauzy/config';
 import { SeedDataService } from './seed-data.service';
-import { DatabaseModule } from '../../database/database.module';
+import { DatabaseModule } from './../../database/database.module';
+
 /**
  * Import and provide seeder classes.
  *
  * @module
  */
 @Module({
-	imports: [],
+	imports: [ConfigModule],
 	providers: [SeedDataService],
 	exports: [SeedDataService]
 })
@@ -19,7 +21,7 @@ export class SeederModule {
 			providers: [],
 			imports: [
 				...getDynamicPluginsModules(),
-				DatabaseModule
+				DatabaseModule.forSeeder()
 			],
 			exports: []
 		} as DynamicModule;
