3ed6fbf2b1d5f64cb361ac9f3b7526da56a6f771
Adolphe Kifungo <akifungo@gmail.com>
2022-08-15 16:26:02 +0200

    feat: remove inheritence

--- apps/gauzy/src/app/@shared/card-grid/card-grid.component.ts
+++ apps/gauzy/src/app/@shared/card-grid/card-grid.component.ts
@@ -1,37 +1,27 @@
-import {
-	Component,
-	OnDestroy,
-	OnInit,
-	Input,
-	TemplateRef
-} from '@angular/core';
+import { Component, OnDestroy, OnInit, Input } from '@angular/core';
 
 import { Output, EventEmitter } from '@angular/core';
-import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
 	selector: 'ga-card-grid',
 	templateUrl: './card-grid.component.html',
 	styleUrls: ['./card-grid.component.scss']
 })
-export class CardGridComponent extends PaginationComponent 
-	implements OnInit, OnDestroy {
-
+export class CardGridComponent implements OnInit, OnDestroy {
 	@Input() source: any;
-	@Input() buttonTemplate: TemplateRef<any>;
-	@Input() cardSize: undefined | 'big';
 	@Output() onSelectedItem: EventEmitter<any> = new EventEmitter<any>();
+	@Output() scroll: EventEmitter<any> = new EventEmitter<any>();
 	selected: any = { isSelected: false, data: null };
 
 	/*
-	* Getter & Setter for dynamic columns settings
-	*/
+	 * Getter & Setter for dynamic columns settings
+	 */
 	_settings: any = {};
 	get settings(): any {
 		return this._settings;
 	}
 	@Input() set settings(settings: any) {
-		this.setColumns(settings.columns)
+		this.setColumns(settings.columns);
 		this._settings = settings;
 	}
 
@@ -40,9 +30,7 @@ export class CardGridComponent extends PaginationComponent
 	 */
 	columns: any = [];
 
-	constructor() {
-		super();
-	}
+	constructor() {}
 
 	ngOnInit(): void {}
 
@@ -53,13 +41,13 @@ export class CardGridComponent extends PaginationComponent
 	getKeys() {
 		return Object.keys(this.settings.columns);
 	}
-	
+
 	setColumns(columns: []) {
-        this.columns = columns;
-    }
+		this.columns = columns;
+	}
 	getColumns() {
-        return this.columns;
-    }
+		return this.columns;
+	}
 
 	selectedItem(item) {
 		this.selected =
@@ -70,7 +58,7 @@ export class CardGridComponent extends PaginationComponent
 	}
 
 	onScroll() {
-		this.itemsPerPage += 10;
+		this.scroll.emit();
 	}
 
 	getValue(row: any, key: string) {
