354415158098412eba122a281987e360171c1f04
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-08-26 13:49:02 +0530

    fix: permissions & roles metadata constant

--- packages/core/src/shared/decorators/user.decorator.ts
+++ packages/core/src/shared/decorators/user.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const UserDecorator = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
-        return request.user;
+        return request.user || {};
     },
 );
