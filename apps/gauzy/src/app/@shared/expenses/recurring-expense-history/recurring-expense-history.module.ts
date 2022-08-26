f81e356dca225edb7d2c283c35b078ab4e93a214
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-08-26 11:15:07 +0530

    fix: #5063 recurring expense block currency position

--- apps/gauzy/src/app/@shared/expenses/recurring-expense-history/recurring-expense-history.module.ts
+++ apps/gauzy/src/app/@shared/expenses/recurring-expense-history/recurring-expense-history.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { NbIconModule } from '@nebular/theme';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { ThemeModule } from '../../../@theme/theme.module';
+import { SharedModule } from '../../shared.module';
 import { TableComponentsModule } from '../../table-components/table-components.module';
 import { TranslateModule } from '../../translate/translate.module';
 import { RecurringExpenseHistoryComponent } from './recurring-expense-history.component';
@@ -12,7 +13,8 @@ import { RecurringExpenseHistoryComponent } from './recurring-expense-history.co
 		Ng2SmartTableModule,
 		TableComponentsModule,
 		NbIconModule,
-		TranslateModule
+		TranslateModule,
+		SharedModule
 	],
 	exports: [RecurringExpenseHistoryComponent],
 	declarations: [RecurringExpenseHistoryComponent],
