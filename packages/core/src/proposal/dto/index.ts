b6143a9d68d3828b08b1c29a8301b3bcd813a1bd
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-08-29 18:35:44 +0530

    fix: updated proposal DTO's validation for create/update method

--- packages/core/src/proposal/dto/index.ts
+++ packages/core/src/proposal/dto/index.ts
@@ -1,4 +1,3 @@
 
 export { CreateProposalDTO } from "./create-proposal.dto";
 export { UpdateProposalDTO } from "./update-proposal.dto";
-export { UpdateProposalActionDTO } from "./update-proposal-action.dto";
