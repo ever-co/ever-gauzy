6b9a82535e82173cc1897dc39ed0ecb550a7b0ae
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-09-05 15:22:24 +0530

    fix: #5052 product type create & mapped command

---
+++ packages/core/src/product-type/commands/product-type.create.command.ts
@@ -0,0 +1,11 @@
+import { ICommand } from '@nestjs/cqrs';
+import { IProductTypeTranslatable, LanguagesEnum } from '@gauzy/contracts';
+
+export class ProductTypeCreateCommand implements ICommand {
+	static readonly type = '[Product Type] Create';
+
+	constructor(
+		public readonly input: IProductTypeTranslatable,
+		public readonly Language: LanguagesEnum
+	) {}
+}
