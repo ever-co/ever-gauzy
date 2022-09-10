95624c0a103b32d037f491471996518fad19f2c0
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-09-10 19:02:36 +0530

    fix: added missing employee type for find option method

--- packages/core/src/employee/commands/employee.get.command.ts
+++ packages/core/src/employee/commands/employee.get.command.ts
@@ -1,8 +1,11 @@
 import { ICommand } from '@nestjs/cqrs';
 import { FindOneOptions } from 'typeorm';
+import { Employee } from './../employee.entity';
 
 export class EmployeeGetCommand implements ICommand {
 	static readonly type = '[Employee] Get';
 
-	constructor(public readonly input: FindOneOptions) {}
+	constructor(
+		public readonly input: FindOneOptions<Employee>
+	) {}
 }
