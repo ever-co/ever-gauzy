5551e39a175bf7ec9f7ceef2678999be5816a7ce
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-09-14 12:53:51 +0530

    fix: build issue

--- packages/core/src/public-share/invoice/commands/public-invoice-update.command.ts
+++ packages/core/src/public-share/invoice/commands/public-invoice-update.command.ts
@@ -1,11 +1,11 @@
-import { IEstimateEmail, IInvoiceUpdateInput } from '@gauzy/contracts';
+import { IInvoice, IInvoiceUpdateInput } from '@gauzy/contracts';
 import { ICommand } from '@nestjs/cqrs';
 
 export class PublicInvoiceUpdateCommand implements ICommand {
 	static readonly type = '[Public Invoice] Update';
 
 	constructor(
-		public readonly params: IEstimateEmail,
+		public readonly params: IInvoice,
 		public readonly entity: IInvoiceUpdateInput
 	) {}
 }
