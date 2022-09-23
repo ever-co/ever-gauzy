c4f6cc27501c70df91be836a8afa5994909c5635
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-09-23 10:00:47 +0530

    fix: feature menu component missing routes permissions

--- apps/gauzy/src/app/pages/settings/feature/feature-routing.module.ts
+++ apps/gauzy/src/app/pages/settings/feature/feature-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgxPermissionsGuard } from 'ngx-permissions';
+import { PermissionsEnum } from '@gauzy/contracts';
 import { FeatureToggleComponent } from '../../../@shared/feature-toggle/feature-toggle.component';
 import { FeatureComponent } from './feature.component';
+
+export function redirectTo() {
+	return '/pages/dashboard';
+}
 
 const routes: Routes = [
 	{
 		path: '',
 		component: FeatureComponent,
+		canActivate: [NgxPermissionsGuard],
+		data: {
+			permissions: {
+				only: [PermissionsEnum.ALL_ORG_VIEW],
+				redirectTo
+			},
+			selectors: {
+				project: false,
+				employee: false,
+				organization: false,
+				date: false
+			}
+		},
 		children: [
 			{
 				path: '',
