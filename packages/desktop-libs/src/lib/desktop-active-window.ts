6611bba2241a6ad12e7a6f20ceacf33688a4edbc
Adolphe Kifungo <akifungo@gmail.com>
2022-09-30 20:58:27 +0200

    feat: detect window change under condition

--- packages/desktop-libs/src/lib/desktop-active-window.ts
+++ packages/desktop-libs/src/lib/desktop-active-window.ts
@@ -25,9 +25,6 @@ export class DesktopActiveWindow extends EventEmitter {
 	 */
 	private _ACTIVE_WINDOW_POLLING_INTERVAL: number = 1000;
 
-	private _windows = [];
-
-
 	constructor() {
 		super();
 		this._pollingTimerId = null;
@@ -56,29 +53,20 @@ export class DesktopActiveWindow extends EventEmitter {
 	 */
 	public start(): boolean {
 		if (this.active) return false;
-		this._pollingTimerId = setInterval(async () => {
-			try {
-				const window = await activeWindow();
-				// Detect changes
-				if (window && window.owner && (window.owner.path !== this._currentApplication.data.executable)
-					|| (window.title !== this._currentApplication.data.title)
-					|| (window.url !== this._currentApplication.data.url)) {
-					this.applyNewWindow(window);
-				}
-			} catch (e) {
-				console.log('Error occurred during active window poll', e);
-			}
-		}, this._ACTIVE_WINDOW_POLLING_INTERVAL);
+		this._pollingTimerId = setInterval(async () =>
+				await this.getWindow(),
+			this._ACTIVE_WINDOW_POLLING_INTERVAL
+		);
 		return true;
 	}
 
 	/**
 	 * Stops the active window polling
 	 */
-	public stop(): void {
+	public async stop() {
 		if (!this._pollingTimerId) return;
+		await this.getWindow(true);
 		clearInterval(this._pollingTimerId);
-		this._windows = [];
 		this._pollingTimerId = null;
 	}
 
@@ -95,8 +83,28 @@ export class DesktopActiveWindow extends EventEmitter {
 		this._currentApplication.data.executable = window.owner.path;
 		this._currentApplication.data.name = window.owner.name;
 		this._currentApplication.data.url = window.url;
-		this._windows.push({...this._currentApplication.toObject()});
-		this.emit('updated', this._windows);
+		this.emit('updated', this._currentApplication.toObject());
 		this._currentApplication.timestamp = now.format();
+	}
+
+	/**
+	 * Get current under anyway condition
+	 * @param anyway if true update current window anyway
+	 * @private
+	 */
+	private async getWindow(anyway: boolean = false) {
+		try {
+			const window = await activeWindow();
+			// Detect changes
+			if (window && window.owner
+				&& (window.owner.path !== this._currentApplication.data.executable)
+				|| (window.title !== this._currentApplication.data.title)
+				|| (window.url !== this._currentApplication.data.url)
+				|| anyway) {
+				this.applyNewWindow(window);
+			}
+		} catch (e) {
+			console.log('Error occurred during active window poll', e);
+		}
 	}
 }
