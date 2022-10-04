79991fd0d508f9e7d0633f519462e612d51f79a9
Adolphe Kifungo <akifungo@gmail.com>
2022-10-04 10:48:45 +0200

    feat: keep  all interfaces and contexts together

---
+++ packages/desktop-libs/src/lib/contexts/index.ts
@@ -0,0 +1,2 @@
+export * from './current-application';
+export * from './data-application';
---
+++ packages/desktop-libs/src/lib/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from './i-current-application';
+export * from './i-data-application';
+export * from './serializable';
