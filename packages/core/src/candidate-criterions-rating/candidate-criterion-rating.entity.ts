21b9d83053d5645ae229789acc0c087f1f0a8196
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-10-07 11:12:05 +0530

    fix: candidate criterion rating controller permissions

--- packages/core/src/candidate-criterions-rating/candidate-criterion-rating.entity.ts
+++ packages/core/src/candidate-criterions-rating/candidate-criterion-rating.entity.ts
@@ -1,7 +1,11 @@
 import { Column, Entity, Index, ManyToOne, RelationId } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-import { ICandidateCriterionsRating, ICandidateFeedback, ICandidatePersonalQualities, ICandidateTechnologies } from '@gauzy/contracts';
+import {
+	ICandidateCriterionsRating,
+	ICandidateFeedback,
+	ICandidatePersonalQualities,
+	ICandidateTechnologies
+} from '@gauzy/contracts';
 import {
 	CandidateFeedback,
 	CandidatePersonalQualities,
@@ -10,18 +14,22 @@ import {
 } from '../core/entities/internal';
 
 @Entity('candidate_criterion_rating')
-export class CandidateCriterionsRating
-	extends TenantOrganizationBaseEntity
+export class CandidateCriterionsRating extends TenantOrganizationBaseEntity
 	implements ICandidateCriterionsRating {
+
 	@ApiProperty({ type: () => String })
 	@Column()
 	rating: number;
+
 	/*
     |--------------------------------------------------------------------------
-    | @ManyToOne 
+    | @ManyToOne
     |--------------------------------------------------------------------------
     */
 
+	/**
+	 * Candidate Technologies
+	 */
 	@ApiProperty({ type: () => CandidateTechnologies })
 	@ManyToOne(() => CandidateTechnologies, (quality) => quality.criterionsRatings, {
 		onDelete: 'CASCADE'
@@ -30,11 +38,13 @@ export class CandidateCriterionsRating
 
 	@ApiProperty({ type: () => String })
 	@RelationId((it: CandidateCriterionsRating) => it.technology)
-	@IsString()
 	@Index()
 	@Column({ nullable: true })
 	technologyId?: string;
 
+	/**
+	 * Candidate Personal Qualities
+	 */
 	@ApiProperty({ type: () => CandidatePersonalQualities })
 	@ManyToOne(() => CandidatePersonalQualities, (quality) => quality.criterionsRatings, {
 		onDelete: 'CASCADE'
@@ -43,11 +53,13 @@ export class CandidateCriterionsRating
 
 	@ApiProperty({ type: () => String })
 	@RelationId((it: CandidateCriterionsRating) => it.personalQuality)
-	@IsString()
 	@Index()
 	@Column({ nullable: true })
 	personalQualityId?: string;
 
+	/**
+	 * Candidate Feedback
+	 */
 	@ApiProperty({ type: () => CandidateFeedback })
 	@ManyToOne(() => CandidateFeedback, (feedback) => feedback.criterionsRating, {
 		onDelete: 'CASCADE'
@@ -56,7 +68,6 @@ export class CandidateCriterionsRating
 
 	@ApiProperty({ type: () => String })
 	@RelationId((it: CandidateCriterionsRating) => it.feedback)
-	@IsString()
 	@Index()
 	@Column({ nullable: true })
 	feedbackId?: string;
