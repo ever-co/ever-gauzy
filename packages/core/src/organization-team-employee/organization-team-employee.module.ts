06d0ae07fce00708c5a274fad44d50a89c9dedd6
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-10-18 09:37:36 +0530

    feat: removed organization team member

--- packages/core/src/organization-team-employee/organization-team-employee.module.ts
+++ packages/core/src/organization-team-employee/organization-team-employee.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RouterModule } from 'nest-router';
+import { UserModule } from './../user/user.module';
+import { TenantModule } from './../tenant/tenant.module';
+import { OrganizationTeamEmployeeController } from './organization-team-employee.controller';
 import { OrganizationTeamEmployee } from './organization-team-employee.entity';
 import { OrganizationTeamEmployeeService } from './organization-team-employee.service';
 
 @Module({
 	imports: [
+		RouterModule.forRoutes([
+			{ path: '/organization-team-employee', module: OrganizationTeamEmployeeModule }
+		]),
 		TypeOrmModule.forFeature([
 			OrganizationTeamEmployee
-		])
+		]),
+		TenantModule,
+		UserModule
+	],
+	controllers: [
+		OrganizationTeamEmployeeController
 	],
 	providers: [
 		OrganizationTeamEmployeeService
