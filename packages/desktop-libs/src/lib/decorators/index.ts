1e1de74f3148bdb5266ad35e600d88a47f24d02f
Adolphe Kifungo <akifungo@gmail.com>
2022-11-08 18:07:47 +0200

    feat: expose classes out of package

--- packages/desktop-libs/src/lib/decorators/index.ts
+++ packages/desktop-libs/src/lib/decorators/index.ts
@@ -1,3 +1,5 @@
 export * from './abstracts/base-power-manager-decorator';
 export * from './concretes/power-manager-prevent-display-sleep';
 export * from './concretes/power-manager-detect-inactivity';
+export * from './concretes/dialog-confirm-upgrade-download';
+export * from './concretes/dialog-confirm-install-download';
