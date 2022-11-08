f4e4e549852f5df4a0a40d373f71ec49746b5fba
Adolphe Kifungo <akifungo@gmail.com>
2022-11-08 18:06:43 +0200

    feat: create base decorator for dialog class

---
+++ packages/desktop-libs/src/lib/decorators/abstracts/base-desktop-dialog-decorator.ts
@@ -0,0 +1,27 @@
+import { MessageBoxOptions } from 'electron';
+import { IDesktopDialog } from '../../interfaces';
+
+
+export abstract class BaseDesktopDialogDecorator implements IDesktopDialog {
+	private readonly _dialogWrapper: IDesktopDialog;
+
+	constructor(dialog: IDesktopDialog) {
+		this._dialogWrapper = dialog;
+	}
+
+	public get options(): MessageBoxOptions {
+		return this._dialogWrapper.options;
+	}
+
+	public set options(value: MessageBoxOptions) {
+		this._dialogWrapper.options = value;
+	}
+
+	public show(): Promise<any> {
+		return this._dialogWrapper.show();
+	}
+
+	public close(): void {
+		this._dialogWrapper.close();
+	}
+}
