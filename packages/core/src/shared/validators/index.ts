db1ecd68b31fc07c4691ad7173ade7123fca8535
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-11-17 21:45:32 +0530

    fix: organization team already exist validation constraints

--- packages/core/src/shared/validators/index.ts
+++ packages/core/src/shared/validators/index.ts
@@ -6,5 +6,6 @@ export * from './is-expense-category-exist.decorator';
 export * from './is-organization-belongs-to-user.decorator';
 export * from './is-role-already-exist.decorator';
 export * from './is-role-should-exist.decorator';
+export * from './is-team-already-exist.decorator';
 export * from './is-tenant-belongs-to-user.decorator';
 export * from './match.decorator';
