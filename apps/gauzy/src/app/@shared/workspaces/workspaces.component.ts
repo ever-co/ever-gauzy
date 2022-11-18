4d26cd108aabd878beed89545b7fe6b5b6180b81
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-11-18 16:43:47 +0530

    fix: duplicate workspace during profile image update

--- apps/gauzy/src/app/@shared/workspaces/workspaces.component.ts
+++ apps/gauzy/src/app/@shared/workspaces/workspaces.component.ts
@@ -1,12 +1,14 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { ITenant, IUser } from '@gauzy/contracts';
+import { IUser } from '@gauzy/contracts';
 import { NbMenuItem } from '@nebular/theme';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
-import { Subject } from 'rxjs/internal/Subject';
+import { distinctUntilChange } from '@gauzy/common-angular';
 import { debounceTime, filter, tap } from 'rxjs/operators';
 import { Store } from '../../@core/services';
 import { TranslationBaseComponent } from '../language-base';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Observable } from 'rxjs/internal/Observable';
 
 interface IWorkSpace {
 	id: string;
@@ -20,14 +22,15 @@ interface IWorkSpace {
 	templateUrl: './workspaces.component.html',
 	styleUrls: ['./workspaces.component.scss']
 })
-export class WorkspacesComponent extends TranslationBaseComponent implements 
-	AfterViewInit, OnInit {
-	
-	workspaces: IWorkSpace[] = [];
+export class WorkspacesComponent extends TranslationBaseComponent
+	implements AfterViewInit, OnInit {
+
+	public _workspaces$: BehaviorSubject<IWorkSpace[]> = new BehaviorSubject([]);
+	public workspaces$: Observable<IWorkSpace[]> = this._workspaces$.asObservable();
+
 	selected: IWorkSpace;
 	contextMenus: NbMenuItem[];
 	user: IUser;
-	workspaces$: Subject<any> = new Subject();
 
 	constructor(
 		public readonly translateService: TranslateService,
@@ -42,32 +45,34 @@ export class WorkspacesComponent extends TranslationBaseComponent implements
 	}
 
 	ngAfterViewInit() {
-		this.workspaces$
-			.pipe(
-				debounceTime(300),
-				tap(() => this.getWorkspaces()),
-				untilDestroyed(this)
-			)
-			.subscribe();
 		this.store.user$
 			.pipe(
+				debounceTime(100),
+				distinctUntilChange(),
 				filter((user: IUser) => !!user),
 				tap((user: IUser) => (this.user = user)),
-				tap(() => this.workspaces$.next(true)),
+				tap(() => this.getWorkspaces()),
 				untilDestroyed(this)
 			)
 			.subscribe();
 	}
 
 	getWorkspaces() {
-		const tenant: ITenant = this.user.tenant;
-		const defaultWorkspace = {
+		const { tenant } = this.user;
+		const workspace = {
 			id: tenant.id,
 			imgUrl: tenant.logo,
 			isOnline: true
 		}
-		this.workspaces.push(defaultWorkspace);
-		this.selected = defaultWorkspace;
+		const workspaces = this._workspaces$.getValue();
+		const index = workspaces.find((workspace: IWorkSpace) => workspace.id === tenant.id);
+		if (!index) {
+			this._workspaces$.next([
+				...workspaces,
+				workspace
+			]);
+			this.selected = workspace;
+		}
 	}
 
 	/**
@@ -83,9 +88,9 @@ export class WorkspacesComponent extends TranslationBaseComponent implements
 	}
 
 	/**
-	 * On Change Workspace
-	 * 
-	 * @param workSpace 
+	 * On Change workspace
+	 *
+	 * @param workspace
 	 */
 	onChangeWorkspace(workspace: IWorkSpace) {
 		this.selected = workspace;
@@ -111,7 +116,7 @@ export class WorkspacesComponent extends TranslationBaseComponent implements
 
 	/**
 	 * Create new workspace
-	 * 
+	 *
 	 */
 	add() {
 		// TODO
