ba9ec3b4300a0ee8cd82aa75e249ddbb3a2ffb25
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-12-07 21:04:01 +0530

    fix: SQLITE_CONSTRAINT: UNIQUE constraint failed

--- packages/core/src/equipment-sharing/equipment-sharing.seed.ts
+++ packages/core/src/equipment-sharing/equipment-sharing.seed.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { faker } from '@ever-co/faker';
 import { addDays } from 'date-fns';
-import { IEmployee, IEquipment, IEquipmentSharing, IOrganization, ITenant } from '@gauzy/contracts';
+import { IEmployee, IEquipmentSharing, IOrganization, ITenant } from '@gauzy/contracts';
 import { EquipmentSharing } from './equipment-sharing.entity';
 import { Equipment } from './../core/entities/internal';
 
@@ -9,25 +9,38 @@ export const createDefaultEquipmentSharing = async (
 	dataSource: DataSource,
 	tenant: ITenant,
 	organization: IOrganization,
-	defaultEmployees,
+	defaultEmployees: IEmployee[],
 	noOfEquipmentSharingPerTenant: number
-): Promise<EquipmentSharing[]> => {
+): Promise<IEquipmentSharing[]> => {
 	const { id: tenantId } = tenant;
+	const { id: organizationId } = organization;
+
 	let equipmentSharings: EquipmentSharing[] = [];
 	const equipments = await dataSource.manager.findBy(Equipment, {
-		tenantId
+		tenantId,
+		organizationId
 	});
-	equipmentSharings = await dataOperation(
-		dataSource,
-		equipmentSharings,
-		noOfEquipmentSharingPerTenant,
-		equipments,
-		defaultEmployees,
-		tenant,
-		organization
-	);
+	for (let i = 0; i < noOfEquipmentSharingPerTenant; i++) {
+		for await (const equipment of equipments) {
+			const sharing = new EquipmentSharing();
+			sharing.name = faker.company.companyName();
+			sharing.equipment = equipment;
+			sharing.shareRequestDay = faker.date.recent(30);
+			sharing.shareStartDay = faker.date.future(0.5);
+			sharing.shareEndDay = addDays(
+				sharing.shareStartDay,
+				faker.datatype.number(15)
+			);
+			sharing.status = faker.datatype.number({ min: 1, max: 3 });
+			sharing.employees = [faker.random.arrayElement(defaultEmployees)];
+			sharing.organization = organization;
+			sharing.tenant = tenant;
 
-	return await dataSource.manager.save(equipmentSharings);
+			const equipmentSharing = await dataSource.manager.save(sharing);
+			equipmentSharings.push(equipmentSharing);
+		}
+	}
+	return equipmentSharings;
 };
 
 export const createRandomEquipmentSharing = async (
@@ -36,59 +49,42 @@ export const createRandomEquipmentSharing = async (
 	tenantOrganizationsMap: Map<ITenant, IOrganization[]>,
 	organizationEmployeesMap: Map<IOrganization, IEmployee[]>,
 	noOfEquipmentSharingPerTenant: number
-): Promise<EquipmentSharing[]> => {
+): Promise<IEquipmentSharing[]> => {
 	let equipmentSharings: EquipmentSharing[] = [];
+
 	for await (const tenant of tenants) {
 		const organizations = tenantOrganizationsMap.get(tenant);
 		for await (const organization of organizations) {
-			const { id: tenantId } = tenant;
 			const { id: organizationId } = organization;
+			const { id: tenantId } = tenant;
+
+			const employees = organizationEmployeesMap.get(organization);
 			const equipments = await dataSource.manager.findBy(Equipment, {
 				tenantId,
 				organizationId
 			});
-			const employees = organizationEmployeesMap.get(organization);
-			equipmentSharings = await dataOperation(
-				dataSource,
-				equipmentSharings,
-				noOfEquipmentSharingPerTenant,
-				equipments,
-				employees,
-				tenant,
-				null
-			);
+			for (let i = 0; i < noOfEquipmentSharingPerTenant; i++) {
+				for await (const equipment of equipments) {
+
+					const sharing = new EquipmentSharing();
+					sharing.name = faker.company.companyName();
+					sharing.equipment = equipment;
+					sharing.shareRequestDay = faker.date.recent(30);
+					sharing.shareStartDay = faker.date.future(0.5);
+					sharing.shareEndDay = addDays(
+						sharing.shareStartDay,
+						faker.datatype.number(15)
+					);
+					sharing.status = faker.datatype.number({ min: 1, max: 3 });
+					sharing.employees = [faker.random.arrayElement(employees)];
+					sharing.organization = organization;
+					sharing.tenant = tenant;
+
+					const equipmentSharing = await dataSource.manager.save(sharing);
+					equipmentSharings.push(equipmentSharing);
+				}
+			}
 		}
 	}
-	return equipmentSharings;
-};
-
-const dataOperation = async (
-	dataSource: DataSource,
-	equipmentSharings: IEquipmentSharing[],
-	noOfEquipmentSharingPerTenant: number,
-	equipments: IEquipment[],
-	employees: IEmployee[],
-	tenant: ITenant,
-	organization: IOrganization
-) => {
-	for (let i = 0; i < noOfEquipmentSharingPerTenant; i++) {
-		const sharing = new EquipmentSharing();
-		sharing.name = faker.company.companyName();
-		sharing.equipment = faker.random.arrayElement(equipments);
-		sharing.equipmentId = sharing.equipment.id;
-		sharing.shareRequestDay = faker.date.recent(30);
-		sharing.shareStartDay = faker.date.future(0.5);
-		sharing.shareEndDay = addDays(
-			sharing.shareStartDay,
-			faker.datatype.number(15)
-		);
-		sharing.status = faker.datatype.number({ min: 1, max: 3 });
-		// sharing.teams =[faker.random.arrayElement(teams)];
-		sharing.employees = [faker.random.arrayElement(employees)];
-		sharing.organization = organization;
-		sharing.tenant = tenant;
-		equipmentSharings.push(sharing);
-	}
-	await dataSource.manager.save(equipmentSharings);
 	return equipmentSharings;
 };
