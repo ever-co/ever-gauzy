5be3ee0cdb57f61ffd0d988ebecd6d0004a2ff12
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-12-08 16:51:17 +0530

    fix: organization contact DTO (swagger documentation)

--- packages/core/src/organization-contact/dto/organization-contact.dto.ts
+++ packages/core/src/organization-contact/dto/organization-contact.dto.ts
@@ -18,52 +18,52 @@ import { TenantOrganizationBaseDTO } from "./../../core/dto";
 
 export class OrganizationContactDTO extends TenantOrganizationBaseDTO {
 
-    @ApiProperty({ type: () => String, readOnly: true  })
-    @IsNotEmpty()
-    @IsString()
-    readonly name: string;
+	@ApiProperty({ type: () => String })
+	@IsNotEmpty()
+	@IsString()
+	readonly name: string;
 
-    @ApiPropertyOptional({ type: () => String, readOnly: true  })
-    @IsOptional()
-    @IsEmail()
-    @Transform((params: TransformFnParams) => params.value ? params.value.trim() : null)
-    readonly primaryEmail: string;
+	@ApiPropertyOptional({ type: () => String })
+	@IsOptional()
+	@IsEmail()
+	@Transform((params: TransformFnParams) => params.value ? params.value.trim() : null)
+	readonly primaryEmail: string;
 
-    @ApiPropertyOptional({ type: () => String, readOnly: true })
+	@ApiPropertyOptional({ type: () => String })
 	@IsOptional()
 	@IsString()
 	readonly primaryPhone: string;
 
-    @ApiPropertyOptional({ type: () => String, enum: ContactOrganizationInviteStatus, readOnly: true })
+	@ApiPropertyOptional({ type: () => String, enum: ContactOrganizationInviteStatus })
 	@IsOptional()
 	@IsEnum(ContactOrganizationInviteStatus)
 	readonly inviteStatus: ContactOrganizationInviteStatus;
 
-    @ApiPropertyOptional({ type: () => String, readOnly: true })
+	@ApiPropertyOptional({ type: () => String })
 	@IsOptional()
 	@IsString()
 	readonly notes: string;
 
-    @ApiProperty({ type: () => String, enum: ContactType, readOnly: true })
+	@ApiProperty({ type: () => String, enum: ContactType })
 	@IsEnum(ContactType)
 	readonly contactType: ContactType;
 
-    @ApiPropertyOptional({ type: () => String, maxLength: 500, readOnly: true })
+	@ApiPropertyOptional({ type: () => String, maxLength: 500 })
 	@IsOptional()
 	@MaxLength(500)
 	readonly imageUrl: string;
 
-    @ApiPropertyOptional({ type: () => Number, readOnly: true })
+	@ApiPropertyOptional({ type: () => Number })
 	@IsOptional()
 	@IsNumber()
 	readonly budget: number;
 
-    @ApiPropertyOptional({ type: () => String, enum: OrganizationContactBudgetTypeEnum, readOnly: true })
+	@ApiPropertyOptional({ type: () => String, enum: OrganizationContactBudgetTypeEnum })
 	@IsOptional()
 	@IsEnum(OrganizationContactBudgetTypeEnum)
 	readonly budgetType: OrganizationContactBudgetTypeEnum;
 
-    @ApiProperty({ type: () => String, readOnly: true })
+	@ApiPropertyOptional({ type: () => String })
 	@IsOptional()
 	readonly createdBy: string;
 }
