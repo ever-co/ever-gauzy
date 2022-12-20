16b4651e5f50a6abb726c9b368c205905858ff1f
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-12-20 16:08:58 +0530

    fix: blank teams retrieve for admin roles

--- packages/core/src/organization-team/organization-team.service.ts
+++ packages/core/src/organization-team/organization-team.service.ts
@@ -220,28 +220,29 @@ export class OrganizationTeamService extends TenantAwareCrudService<Organization
 		const tenantId = RequestContext.currentTenantId();
 		const employeeId = RequestContext.currentEmployeeId();
 
-		const query = this.repository.createQueryBuilder(this.alias).setFindOptions(options);
-		query.andWhere(`"${query.alias}"."tenantId" = :tenantId`, { tenantId });
+		const query = this.repository.createQueryBuilder(this.alias);
+		if (isNotEmpty(options)) { query.setFindOptions(options); }
 
-		// Sub Query to get only employee assigned teams
-		query.andWhere((cb: SelectQueryBuilder<OrganizationTeam>) => {
-			const subQuery = cb.subQuery().select('"team"."organizationTeamId"').from('organization_team_employee', 'team');
-			subQuery.andWhere(`"${query.alias}"."tenantId" = :tenantId`, { tenantId });
+		// If employee has login and don't have permission to change employee
+		if (employeeId && !RequestContext.hasPermission(
+			PermissionsEnum.CHANGE_SELECTED_EMPLOYEE
+		)) {
+			// Sub Query to get only employee assigned teams
+			query.andWhere((cb: SelectQueryBuilder<OrganizationTeam>) => {
+				const subQuery = cb.subQuery().select('"team"."organizationTeamId"').from('organization_team_employee', 'team');
+				subQuery.andWhere(`"${query.alias}"."tenantId" = :tenantId`, { tenantId });
 
-			if (isNotEmpty(options.where)) {
-				const { organizationId } = options.where;
-				subQuery.andWhere(`"${query.alias}"."organizationId" = :organizationId`, { organizationId });
-			}
+				if (isNotEmpty(options) && isNotEmpty(options.where)) {
+					const { organizationId } = options.where;
+					subQuery.andWhere(`"${query.alias}"."organizationId" = :organizationId`, { organizationId });
+				}
 
-			// If employee has login and don't have permission to change employee
-			if (employeeId && !RequestContext.hasPermission(
-				PermissionsEnum.CHANGE_SELECTED_EMPLOYEE
-			)) {
 				subQuery.andWhere('"team"."employeeId" = :employeeId', { employeeId });
-			}
-			return '"organization_team"."id" IN ' + subQuery.distinct(true).getQuery();
-		});
+				return '"organization_team"."id" IN ' + subQuery.distinct(true).getQuery();
+			});
+		}
 
+		query.andWhere(`"${query.alias}"."tenantId" = :tenantId`, { tenantId });
 		const [items, total] = await query.getManyAndCount();
 		return { items, total };
 	}
