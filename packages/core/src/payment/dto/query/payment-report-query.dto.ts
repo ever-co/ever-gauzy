b0d20f4475a8905de30656f1eaab55712ea75ba1
RAHUL RATHORE <rahulrathore576@gmail.com>
2022-12-25 15:28:08 +0530

    fix: payment reports API improvement

--- packages/core/src/payment/dto/query/payment-report-query.dto.ts
+++ packages/core/src/payment/dto/query/payment-report-query.dto.ts
@@ -11,7 +11,7 @@ export class PaymentReportQueryDTO extends IntersectionType(
     RelationsQueryDTO
 ) implements IGetPaymentInput {
 
-    @ApiPropertyOptional({ enum: ReportGroupFilterEnum, readOnly: true })
+    @ApiPropertyOptional({ enum: ReportGroupFilterEnum })
     @IsOptional()
     @IsEnum(ReportGroupFilterEnum)
     readonly groupBy: ReportGroupFilterEnum;
