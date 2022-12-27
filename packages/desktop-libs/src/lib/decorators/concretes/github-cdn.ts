586de6b94e7eb73e3a430fd4c7e5a894fcb897e2
Adolphe Kifungo <akifungo@gmail.com>
2022-12-28 00:55:50 +0200

    fix: filter *-dev* versions

--- packages/desktop-libs/src/lib/decorators/concretes/github-cdn.ts
+++ packages/desktop-libs/src/lib/decorators/concretes/github-cdn.ts
@@ -1,17 +1,42 @@
-import { IDesktopCdnUpdate } from "../../interfaces/i-desktop-cdn-update";
-import { BaseCdnDecorator } from "../abstracts/base-cdn-decorator";
+import { promisify } from 'node:util';
+import childProcess from 'node:child_process';
+import { IDesktopCdnUpdate } from '../../interfaces/i-desktop-cdn-update';
+import { BaseCdnDecorator } from '../abstracts/base-cdn-decorator';
+import { app } from 'electron';
 
-export class GithubCdn
-    extends BaseCdnDecorator
-    implements IDesktopCdnUpdate {
-    private _CDN_HOST: string = 'https://github.com'
-    constructor(update: IDesktopCdnUpdate) {
-        super(update);
+export class GithubCdn extends BaseCdnDecorator implements IDesktopCdnUpdate {
+	private _CDN_HOST: string = 'https://github.com';
+
+	constructor(update: IDesktopCdnUpdate) {
+		super(update);
 		(async () => {
-			const res: any = await this.tagName();
-			this.url = res
-				? `${this._CDN_HOST}/${this.config.owner}/${this.config.repository}/${this.config.typeRelease}/download/${res.tag_name}`
-				: null;
+			const latests = await this._remoteGitTags();
+			const tag: string = latests[0];
+			this.url = `${this._CDN_HOST}/${this.config.owner}/${this.config.repository}/${this.config.typeRelease}/download/${tag}`;
 		})();
-    }
+	}
+
+	private async _remoteGitTags() {
+		const execFile = promisify(childProcess.execFile);
+		const tags = [];
+		const { stdout } = await execFile('git', [
+			'ls-remote',
+			'--tags',
+			'--sort=-v:refname',
+			`${this._CDN_HOST}/${this.config.owner}/${this.config.repository}`
+		]);
+
+		for (const line of stdout.trim().split('\n')) {
+			const [, tagReference] = line.split('\t');
+			const regex = new RegExp('-', 'g');
+			const tagName = tagReference
+				.replace(/^refs\/tags\//, '')
+				.replace(/\^{}$/, '');
+			if (!regex.test(tagName)) {
+				tags.push(tagName);
+			}
+		}
+
+		return tags;
+	}
 }
