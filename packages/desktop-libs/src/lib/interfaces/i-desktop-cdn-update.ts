2f3514bab6a4317749e99bee8f65a82b62210f1e
Adolphe Kifungo <akifungo@gmail.com>
2023-01-03 08:30:18 +0200

    feat: support prerelease as idesktopCdnUpdate behaviour

--- packages/desktop-libs/src/lib/interfaces/i-desktop-cdn-update.ts
+++ packages/desktop-libs/src/lib/interfaces/i-desktop-cdn-update.ts
@@ -4,4 +4,5 @@ import IUpdaterConfig from './i-updater-config';
 export interface IDesktopCdnUpdate extends IDesktopUpdate {
 	tagName(): Promise<string>;
 	get config(): IUpdaterConfig;
+	get isPrerelease(): boolean;
 }
--- packages/desktop-libs/src/lib/interfaces/index.ts
+++ packages/desktop-libs/src/lib/interfaces/index.ts
@@ -1,3 +1,5 @@
+import { from } from 'form-data';
+
 export * from './i-current-application';
 export * from './i-data-application';
 export * from './serializable';
@@ -10,3 +12,5 @@ export * from './i-desktop-cdn-update';
 export * from './i-updater-config';
 export * from './i-desktop-open-dialog';
 export * from './i-local-update-server';
+export * from './i-updater-config-digital-ocean';
+export * from './i-desktop-github-update';
