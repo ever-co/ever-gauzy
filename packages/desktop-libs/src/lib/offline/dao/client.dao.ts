f4ba2b126c126ffcc37542ffac787309d859160c
Adolphe Kifungo <akifungo@gmail.com>
2023-01-04 16:02:01 +0200

    feat: implement client and tag dao

--- packages/desktop-libs/src/lib/offline/dao/client.dao.ts
+++ packages/desktop-libs/src/lib/offline/dao/client.dao.ts
@@ -1,24 +1,51 @@
-import { DAO, IClientFilterOption } from '../../interfaces';
-import { ClientTO } from '../dto';
+import { DAO, IClientFilterOption, IDatabaseProvider } from '../../interfaces';
+import { ProviderFactory } from '../databases';
+import { ClientTO, TABLE_NAME_CLIENTS } from '../dto';
 
 export class ClientDAO implements DAO<ClientTO> {
-	findAll(): Promise<ClientTO[]> {
-		throw new Error('Method not implemented.');
-	}
-	save(value: ClientTO): Promise<void> {
-		throw new Error('Method not implemented.');
-	}
-	findOneById(id: number): Promise<ClientTO> {
-		throw new Error('Method not implemented.');
-	}
-	update(id: number, value: Partial<ClientTO>): Promise<void> {
-		throw new Error('Method not implemented.');
-	}
-	delete(value: Partial<ClientTO>): Promise<void> {
-		throw new Error('Method not implemented.');
+	private _provider: IDatabaseProvider;
+
+	constructor() {
+		this._provider = new ProviderFactory();
 	}
 
-	findOneByOptions(options: IClientFilterOption): Promise<ClientTO> {
-		throw new Error('Method not implemented.');
+	public async findAll(): Promise<ClientTO[]> {
+		return await this._provider
+			.connection<ClientTO>(TABLE_NAME_CLIENTS)
+			.select('*');
+	}
+	public async save(value: ClientTO): Promise<void> {
+		await this._provider
+			.connection<ClientTO>(TABLE_NAME_CLIENTS)
+			.insert(value);
+	}
+	public async findOneById(id: number): Promise<ClientTO> {
+		return await this._provider
+			.connection<ClientTO>(TABLE_NAME_CLIENTS)
+			.select('*')
+			.where('id', id)[0];
+	}
+	public async update(id: number, value: Partial<ClientTO>): Promise<void> {
+		await this._provider
+			.connection<ClientTO>(TABLE_NAME_CLIENTS)
+			.where('id', '=', id)
+			.update(value);
+	}
+	public async delete(value: Partial<ClientTO>): Promise<void> {
+		await this._provider
+			.connection(TABLE_NAME_CLIENTS)
+			.where('id', '=', value.id)
+			.del();
+	}
+
+	public async findOneByOptions(
+		options: IClientFilterOption
+	): Promise<ClientTO[]> {
+		const { clienId, remoteId, name } = options;
+		return this._provider
+			.connection(TABLE_NAME_CLIENTS)
+			.where('id', '=', clienId)
+			.orWhere('remoteId', '=', remoteId)
+			.orWhere('name', '=', name);
 	}
 }
--- packages/desktop-libs/src/lib/offline/dao/tag.dao.ts
+++ packages/desktop-libs/src/lib/offline/dao/tag.dao.ts
@@ -1,20 +1,38 @@
-import { DAO } from "../../interfaces";
-import { TagTO } from "../dto";
+import { DAO, IDatabaseProvider } from '../../interfaces';
+import { ProviderFactory } from '../databases';
+import { TABLE_NAME_TAGS, TagTO } from '../dto';
 
 export class TagDAO implements DAO<TagTO> {
-    findAll(): Promise<TagTO[]> {
-        throw new Error("Method not implemented.");
-    }
-    save(value: TagTO): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    findOneById(id: number): Promise<TagTO> {
-        throw new Error("Method not implemented.");
-    }
-    update(id: number, value: Partial<TagTO>): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    delete(value: Partial<TagTO>): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+	private _provider: IDatabaseProvider;
+
+	constructor() {
+		this._provider = new ProviderFactory();
+	}
+
+	public async findAll(): Promise<TagTO[]> {
+		return await this._provider
+			.connection<TagTO>(TABLE_NAME_TAGS)
+			.select('*');
+	}
+	public async save(value: TagTO): Promise<void> {
+		await this._provider.connection<TagTO>(TABLE_NAME_TAGS).insert(value);
+	}
+	public async findOneById(id: number): Promise<TagTO> {
+		return await this._provider
+			.connection<TagTO>(TABLE_NAME_TAGS)
+			.select('*')
+			.where('id', id)[0];
+	}
+	public async update(id: number, value: Partial<TagTO>): Promise<void> {
+		await this._provider
+			.connection<TagTO>(TABLE_NAME_TAGS)
+			.where('id', '=', id)
+			.update(value);
+	}
+	public async delete(value: Partial<TagTO>): Promise<void> {
+		await this._provider
+			.connection(TABLE_NAME_TAGS)
+			.where('id', '=', value.id)
+			.del();
+	}
 }
