bed9f8823a5b1b8e2f3f5d5442d4ca16d8633689
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-01-05 15:30:08 +0530

    fix: authentication code expire time managed by environment

--- packages/config/src/environments/environment.prod.ts
+++ packages/config/src/environments/environment.prod.ts
@@ -30,11 +30,13 @@ export const environment: IEnvironment = {
 	 * Email verification options
 	 */
 	JWT_VERIFICATION_TOKEN_SECRET: process.env.JWT_VERIFICATION_TOKEN_SECRET || 'verificationSecretKey',
+	JWT_VERIFICATION_TOKEN_EXPIRATION_TIME: parseInt(process.env.JWT_VERIFICATION_TOKEN_EXPIRATION_TIME) || 86400 * 7, // default verification expire token time (7 days)
+	EMAIL_CONFIRMATION_URL: process.env.EMAIL_CONFIRMATION_URL || 'https://app.gauzy.co/#/auth/confirm-email',
 
-	// default verification expire token time (7 days)
-	JWT_VERIFICATION_TOKEN_EXPIRATION_TIME: parseInt(process.env.JWT_VERIFICATION_TOKEN_EXPIRATION_TIME) || 86400 * 7,
-
-	EMAIL_CONFIRMATION_URL: process.env.EMAIL_CONFIRMATION_URL || 'http://localhost:4200/#/auth/confirm-email',
+	/**
+	 * Password Less Authentication Configuration
+	 */
+	AUTHENTICATION_CODE_EXPIRATION_TIME: parseInt(process.env.AUTHENTICATION_CODE_EXPIRATION_TIME) || 600,
 
 	/**
 	 * Throttler (Rate Limiting) Options
