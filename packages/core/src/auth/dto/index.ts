2ca3097b6c8fbf5f4379757638b64083c90403ae
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-01-11 15:47:14 +0530

    fix: has permissions DTO for request validation

--- packages/core/src/auth/dto/index.ts
+++ packages/core/src/auth/dto/index.ts
@@ -1,4 +1,5 @@
+export * from './confirm-email.dto';
+export * from './has-permissions-query.dto';
 export * from './has-role-query.dto';
 export * from './refresh-token.dto';
 export * from './two-factor-dto';
-export * from './confirm-email.dto';
