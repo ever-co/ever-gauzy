3c01372c6f74ca29aded86cc430e895c950f8a07
rahul-rathore-576 <rahul-rathore-576@users.noreply.github.com>
2023-01-17 10:43:57 +0000

    [MegaLinter] Apply linters fixes

--- megalinter-reports/updated_sources/packages/core/src/statuses/default-global-statuses.ts
+++ megalinter-reports/updated_sources/packages/core/src/statuses/default-global-statuses.ts
@@ -2,36 +2,55 @@ import { IStatus, TaskStatusEnum } from '@gauzy/contracts';
 
 export const DEFAULT_GLOBAL_STATUSES: IStatus[] = [
 	{
-		name: TaskStatusEnum.TODO,
-		value: TaskStatusEnum.TODO,
+		name: TaskStatusEnum.OPEN,
+		value: TaskStatusEnum.OPEN,
 		description:
 			'The issue/task has been reported and is waiting for the team to action it.',
 		icon: null,
-		color: '#ffab2d',
+		color: '#f7f9fc',
 		isSystem: true,
 	},
 	{
 		name: TaskStatusEnum.IN_PROGRESS,
 		value: TaskStatusEnum.IN_PROGRESS,
 		description:
-			'This issue is being actively worked on at the moment by the assignee.',
+			'This issue/task is being actively worked on at the moment by the assignee.',
 		icon: null,
 		color: '#0095ff',
 		isSystem: true,
 	},
 	{
-		name: TaskStatusEnum.FOR_TESTING,
-		value: TaskStatusEnum.FOR_TESTING,
-		description: 'It needs peer review before being considered done.',
+		name: TaskStatusEnum.READY_FOR_REVIEW,
+		value: TaskStatusEnum.READY_FOR_REVIEW,
+		description:
+			'At this point the merge request / pull request is ready to be reviewed for issue/task.',
 		icon: null,
-		color: '#222b45',
+		color: '#0095ff',
+		isSystem: true,
+	},
+	{
+		name: TaskStatusEnum.IN_REVIEW,
+		value: TaskStatusEnum.IN_REVIEW,
+		description:
+			'It needs peer review issue/task before being considered done.',
+		icon: null,
+		color: '#0095ff',
+		isSystem: true,
+	},
+	{
+		name: TaskStatusEnum.BLOCKED,
+		value: TaskStatusEnum.BLOCKED,
+		description:
+			'The issue/task is missing information, wait for customer decision, etc.',
+		icon: null,
+		color: '#ff0000',
 		isSystem: true,
 	},
 	{
 		name: TaskStatusEnum.COMPLETED,
 		value: TaskStatusEnum.COMPLETED,
 		description:
-			'The issue is considered finished. The resolution is correct.',
+			'The issue/task is considered finished. The resolution is correct.',
 		icon: null,
 		color: '#00d68f',
 		isSystem: true,
