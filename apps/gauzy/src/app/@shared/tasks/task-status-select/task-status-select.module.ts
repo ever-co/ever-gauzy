8a4577f1b3ec2abcef4a2b8918d8f498e65bd146
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-01-19 22:27:16 +0530

    fix: #5377 task status selector

--- apps/gauzy/src/app/@shared/tasks/task-status-select/task-status-select.module.ts
+++ apps/gauzy/src/app/@shared/tasks/task-status-select/task-status-select.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { TaskStatusSelectComponent } from './task-status-select.component';
 import { FormsModule } from '@angular/forms';
+import { TaskStatusSelectComponent } from './task-status-select.component';
+import { TranslateModule } from '../../translate/translate.module';
+import { SharedModule } from '../../shared.module';
+import { StatusesService } from '../../../@core/services';
 
 @NgModule({
 	declarations: [
@@ -16,7 +18,11 @@ import { FormsModule } from '@angular/forms';
 		CommonModule,
 		FormsModule,
 		TranslateModule,
-		NgSelectModule
+		NgSelectModule,
+		SharedModule
+	],
+	providers: [
+		StatusesService
 	]
 })
-export class TaskStatusSelectModule {}
+export class TaskStatusSelectModule { }
