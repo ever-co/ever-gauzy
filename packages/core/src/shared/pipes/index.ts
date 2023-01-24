d17a3e0655eddbdd3ea8d56c2250922dbf5e4fdc
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-01-24 15:37:49 +0530

    feat: #5377 abstract validation transform pipe

--- packages/core/src/shared/pipes/index.ts
+++ packages/core/src/shared/pipes/index.ts
@@ -2,3 +2,4 @@ export * from './uuid-validation.pipe';
 export * from './parse-json.pipe';
 export * from './bulk-body-load-transform.pipe';
 export * from './column-numeric-transformer.pipe';
+export * from './abstract-validation.pipe';
