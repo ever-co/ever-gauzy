b8932c4062b5b0d638172d36f06519b4cf19ea4f
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-01-24 21:05:00 +0530

    feat: #5377 base crud controller using validation without methods

--- packages/core/src/tasks/priorities/priority.controller.ts
+++ packages/core/src/tasks/priorities/priority.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, UseGuards } from '@nestjs/common';
+import {
+	IBasePerTenantAndOrganizationEntityModel,
+	IPaginationParam,
+	ITaskPriorityCreateInput,
+	ITaskPriorityUpdateInput
+} from '@gauzy/contracts';
+import { CrudFactory, PaginationParams } from './../../core/crud';
 import { TenantPermissionGuard } from './../../shared/guards';
-import { CrudController } from './../../core/crud';
-import { TaskPriorityService } from './priority.service';
+import { CountQueryDTO } from './../../shared/dto';
 import { TaskPriority } from './priority.entity';
+import { TaskPriorityService } from './priority.service';
 
 @UseGuards(TenantPermissionGuard)
 @Controller()
-export class TaskPriorityController extends CrudController<TaskPriority> {
+export class TaskPriorityController extends
+	CrudFactory<TaskPriority, ITaskPriorityCreateInput, ITaskPriorityUpdateInput, IPaginationParam, IBasePerTenantAndOrganizationEntityModel>(
+		TaskPriority,
+		TaskPriority,
+		PaginationParams,
+		CountQueryDTO
+	) {
 
 	constructor(
 		protected readonly taskPriorityService: TaskPriorityService
--- packages/core/src/tasks/sizes/size.controller.ts
+++ packages/core/src/tasks/sizes/size.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, UseGuards, } from '@nestjs/common';
+import {
+	IBasePerTenantAndOrganizationEntityModel,
+	IPaginationParam,
+	ITaskSizeCreateInput,
+	ITaskSizeUpdateInput
+} from '@gauzy/contracts';
+import { CrudFactory, PaginationParams } from './../../core/crud';
+import { CountQueryDTO } from './../../shared/dto';
 import { TenantPermissionGuard } from './../../shared/guards';
-import { CrudController } from './../../core/crud';
 import { TaskSizeService } from './size.service';
 import { TaskSize } from './size.entity';
 
 @UseGuards(TenantPermissionGuard)
 @Controller()
-export class TaskSizeController extends CrudController<TaskSize> {
+export class TaskSizeController extends
+	CrudFactory<TaskSize, ITaskSizeCreateInput, ITaskSizeUpdateInput, IPaginationParam, IBasePerTenantAndOrganizationEntityModel>(
+		TaskSize,
+		TaskSize,
+		PaginationParams,
+		CountQueryDTO
+	) {
 
 	constructor(
 		protected readonly taskSizeService: TaskSizeService
