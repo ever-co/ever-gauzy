43afda2ee360b9aa4451728f6f0bb2032ba8090d
rahul-rathore-576 <rahul-rathore-576@users.noreply.github.com>
2023-01-28 09:13:35 +0000

    [MegaLinter] Apply linters automatic fixes

--- packages/core/src/tasks/statuses/status.controller.ts
+++ packages/core/src/tasks/statuses/status.controller.ts
@@ -26,16 +26,8 @@ export class TaskStatusController extends CrudFactory<
 	ITaskStatusUpdateInput,
 	IPaginationParam,
 	ITaskStatusFindInput
->(
-	CreateStatusDTO,
-	UpdatesStatusDTO,
-	PaginationParams,
-	CountQueryDTO
-) {
-	constructor(
-		private readonly queryBus: QueryBus,
-		protected readonly taskStatusService: TaskStatusService
-	) {
+>(CreateStatusDTO, UpdatesStatusDTO, PaginationParams, CountQueryDTO) {
+	constructor(private readonly queryBus: QueryBus, protected readonly taskStatusService: TaskStatusService) {
 		super(taskStatusService);
 	}
 
@@ -54,9 +46,7 @@ export class TaskStatusController extends CrudFactory<
 	@HttpCode(HttpStatus.OK)
 	@Get()
 	@UsePipes(new ValidationPipe({ whitelist: true }))
-	async findTaskStatuses(
-		@Query() params: StatusQuerDTO
-	): Promise<IPagination<ITaskStatus>> {
+	async findTaskStatuses(@Query() params: StatusQuerDTO): Promise<IPagination<ITaskStatus>> {
 		return await this.queryBus.execute(new FindStatusesQuery(params));
 	}
 }
