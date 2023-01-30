1cf87e7b290566527588647002c879d9d2ac7293
Adolphe Kifungo <akifungo@gmail.com>
2023-01-30 19:43:11 +0200

    feat: optimize sql query with query builder

--- packages/desktop-libs/src/lib/desktop-wakatime.ts
+++ packages/desktop-libs/src/lib/desktop-wakatime.ts
@@ -1,15 +1,11 @@
 export const metaData = {
 	getActivity: (knex, date) => {
-		return [];
-		//  knex
-		// 	.raw(
-		// 		`
-		//     SELECT * from heartbeats where DATETIME(time, 'unixepoch') <= datetime(?) and DATETIME(time, 'unixepoch') > DATETIME(?);
-		// `,
-		// 		[date.end, date.start]
-		// 	)
-		// 	.then((res) => res)
-		// 	.catch((error) => console.log(error));
+		return knex
+			.select('*')
+			.from('heartbeats')
+			.whereBetween('time', [date.end, date.start])
+			.then((res) => res)
+			.catch((error) => console.log(error));
 	},
 	removeActivity: (knex, data) => {
 		return knex('heartbeats')
