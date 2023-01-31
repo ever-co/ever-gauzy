f8e502fe78bea4ee20b0f357d6e7404bb4b41661
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-01-31 12:02:12 +0530

    feat(@gauzy/common): cloudinary filesystem configuration contract

--- packages/common/src/interfaces/index.ts
+++ packages/common/src/interfaces/index.ts
@@ -1,13 +1,14 @@
 export * from './hooks';
 
-export * from './ILogger';
-export * from './IGraphql';
-export * from './IPlugin';
 export * from './IConfig';
+export * from './IGraphql';
+export * from './ILogger';
+export * from './IPlugin';
 
-export * from './IAwsConfig';
-export * from './IWasabiConfig';
+export * from './IAppIntegrationConfig';
 export * from './IAuth0Config';
+export * from './IAwsConfig';
+export * from './ICloudinaryConfig';
 export * from './IFacebookConfig';
 export * from './IFiverrConfig';
 export * from './IGithubConfig';
@@ -17,6 +18,6 @@ export * from './ILinkedinIConfig';
 export * from './IMicrosoftConfig';
 export * from './ISMTPConfig';
 export * from './ITwitterConfig';
-export * from './IUpworkConfig';
 export * from './IUnleashConfig';
-export * from './IAppIntegrationConfig';
+export * from './IUpworkConfig';
+export * from './IWasabiConfig';
