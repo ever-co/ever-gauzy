f5fe7a04c75b17b156122f1138c8c1be1f10c2d5
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-02-03 18:45:10 +0530

    fix: #5377 tags improvement

--- apps/gauzy/src/app/@shared/tags/tags-color-input/tags-color-input.component.ts
+++ apps/gauzy/src/app/@shared/tags/tags-color-input/tags-color-input.component.ts
@@ -6,13 +6,17 @@ import {
 	EventEmitter,
 	OnDestroy
 } from '@angular/core';
-import { ITag, IOrganization } from '@gauzy/contracts';
+import { ITag, IOrganization, PermissionsEnum } from '@gauzy/contracts';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { filter, tap } from 'rxjs/operators';
-import { Store, TagsService } from '../../../@core/services';
-import { PictureNameTagsComponent } from '../../table-components';
+import { filter, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
+import { Subject } from 'rxjs/internal/Subject';
 import { NbThemeService } from '@nebular/theme';
 import { TranslateService } from '@ngx-translate/core';
+import * as randomColor from 'randomcolor';
+import { distinctUntilChange } from '@gauzy/common-angular';
+import { Store, TagsService } from '../../../@core/services';
+import { PictureNameTagsComponent } from '../../table-components';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
@@ -20,27 +24,47 @@ import { TranslateService } from '@ngx-translate/core';
 	templateUrl: './tags-color-input.component.html',
 	styleUrls: ['./tags-color-input.component.scss']
 })
-export class TagsColorInputComponent
-	extends PictureNameTagsComponent
-	implements OnInit, OnDestroy
-{
-	tags: ITag[] = [];
-	loading: boolean;
+export class TagsColorInputComponent extends PictureNameTagsComponent
+	implements OnInit, OnDestroy {
+
+	public subject$: Subject<boolean> = new Subject();
+	public hasAddTag$: Observable<boolean>;
+	public tags: ITag[] = [];
+	public loading: boolean;
 	private organization: IOrganization;
 
-	@Input('isOrgLevel')
-	isOrgLevel: boolean = false;
+	/*
+	* Getter & Setter selected tags
+	*/
+	_selectedTags: ITag[] = [];
+	get selectedTags(): ITag[] {
+		return this._selectedTags;
+	}
+	@Input() set selectedTags(value: ITag[]) {
+		this._selectedTags = value;
+	}
 
-	@Input('isTenantLevel')
-	isTenantLevel: boolean = false;
+	/*
+	* Getter & Setter for check organization level
+	*/
+	_isOrgLevel: boolean = false;
+	get isOrgLevel(): boolean {
+		return this._isOrgLevel;
+	}
+	@Input() set isOrgLevel(value: boolean) {
+		this._isOrgLevel = value;
+	}
 
-	@Input('selectedTags')
-	selectedTags: ITag[];
-
-	selectedTagIds: string[];
-
-	@Output()
-	selectedTagsEvent = new EventEmitter<ITag[]>();
+	/*
+	* Getter & Setter for check tenant level
+	*/
+	_isTenantLevel: boolean = false;
+	get isTenantLevel(): boolean {
+		return this._isTenantLevel;
+	}
+	@Input() set isTenantLevel(value: boolean) {
+		this._isTenantLevel = value;
+	}
 
 	/*
 	* Getter & Setter for multiple selection
@@ -64,74 +88,108 @@ export class TagsColorInputComponent
 		this._label = value;
 	}
 
+	/*
+	* Getter & Setter for dynamic add tag option
+	*/
+	_addTag: boolean = true;
+	get addTag(): boolean {
+		return this._addTag;
+	}
+	@Input() set addTag(value: boolean) {
+		this._addTag = value;
+	}
+
+	@Output() selectedTagsEvent = new EventEmitter<ITag[]>();
+
 	constructor(
 		private readonly tagsService: TagsService,
 		private readonly store: Store,
-		readonly themeService: NbThemeService,
-		readonly translateService: TranslateService
+		public readonly themeService: NbThemeService,
+		public readonly translateService: TranslateService
 	) {
 		super(themeService, translateService);
 	}
 
-	async onChange(currentSelection: string[]) {
-		const selectedTags = this.tags.filter((tag) =>
-			currentSelection.includes(tag.id)
+	ngOnInit(): void {
+		this.hasAddTag$ = this.store.userRolePermissions$.pipe(
+			map(() =>
+				this.store.hasPermission(PermissionsEnum.ORG_TAGS_EDIT)
+			)
 		);
-		this.selectedTagsEvent.emit(selectedTags);
-	}
-
-	addTag = async (tagName: string) => {
-		const { tenantId } = this.store.user;
-		const { id: organizationId } = this.organization;
-
-		const tag: ITag = {
-			name: tagName,
-			color: '#' + Math.floor(Math.random() * 16777215).toString(16),
-			description: ''		
-		};
-
-		if (this.isTenantLevel || this.isOrgLevel) { tag['tenantId'] = tenantId; }
-		if (this.isOrgLevel) { tag['organizationId'] = organizationId; }
-
-		this.loading = true;
-		return await this.tagsService.insertTag(tag)
-			.finally(() => {
-				this.loading = false
-			});
-	};
-
-	ngOnInit() {
-		this.store.selectedOrganization$
+		this.subject$
 			.pipe(
-				filter((organization: IOrganization) => !!organization),
-				tap((organization: IOrganization) => this.organization = organization),
-				tap(() => this.getAllTags()),
+				tap(() => this.getTagsByLevel()),
 				untilDestroyed(this)
 			)
 			.subscribe();
-		this.selectedTagIds = this.selectedTagIds? this.selectedTags?.map((tag: ITag) => tag.id): null;
+		this.store.selectedOrganization$
+			.pipe(
+				distinctUntilChange(),
+				filter((organization: IOrganization) => !!organization),
+				tap((organization: IOrganization) => this.organization = organization),
+				tap(() => this.subject$.next(true)),
+				untilDestroyed(this)
+			)
+			.subscribe();
 	}
 
-	async getAllTags() {
+	/**
+	 * Get tags by level
+	 *
+	 * @returns
+	 */
+	async getTagsByLevel() {
+		if (!this.organization) {
+			return;
+		}
 		const { tenantId } = this.store.user;
 		const { id: organizationId } = this.organization;
 
 		if (this.isOrgLevel) {
-			const { items } = await this.tagsService.getTagsByLevel(
-				{ organizationId, tenantId },
-				['organization']
-			);
+			const { items } = await this.tagsService.getTagsByLevel({
+				organizationId,
+				tenantId
+			});
 			this.tags = items;
 		}
 
 		if (this.isTenantLevel) {
-			const { items } = await this.tagsService.getTagsByLevel(
-				{ tenantId },
-				['tenant']
-			);
+			const { items } = await this.tagsService.getTagsByLevel({
+				tenantId
+			});
 			this.tags = items;
 		}
 	}
 
-	ngOnDestroy() {}
+	/**
+	 * Create new tag
+	 *
+	 * @param name
+	 * @returns
+	 */
+	createNewTag = async (name: ITag['name']) => {
+		if (!name) {
+			return;
+		}
+		this.loading = true;
+
+		const { tenantId } = this.store.user;
+		const { id: organizationId } = this.organization;
+
+		try {
+			return await this.tagsService.insertTag({
+				name: name,
+				color: randomColor(),
+				description: '',
+				tenantId,
+				...(this.isOrgLevel ? { organizationId } : {}),
+			});
+		} catch (error) {
+			console.log('Error while creating tags', error);
+		} finally {
+			this.loading = false;
+		}
+	};
+
+	ngOnDestroy(): void { }
 }
