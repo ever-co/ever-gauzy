37bed9c750b1f22fb25756729c744d8a5d178a3c
adkif <adkif@users.noreply.github.com>
2023-02-09 10:34:28 +0000

    [MegaLinter] Apply linters automatic fixes

--- packages/desktop-libs/src/lib/offline/databases/migrations/20230208165526_alter-table-timers.ts
+++ packages/desktop-libs/src/lib/offline/databases/migrations/20230208165526_alter-table-timers.ts
@@ -2,15 +2,15 @@ import { TABLE_NAME_TIMERS } from '../../../offline';
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.alterTable(TABLE_NAME_TIMERS, (table: Knex.TableBuilder) => {
-        table.boolean('isStartedOffline').defaultTo(false);
-        table.boolean('isStoppedOffline').defaultTo(false);
-    });
+	await knex.schema.alterTable(TABLE_NAME_TIMERS, (table: Knex.TableBuilder) => {
+		table.boolean('isStartedOffline').defaultTo(false);
+		table.boolean('isStoppedOffline').defaultTo(false);
+	});
 }
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.alterTable(TABLE_NAME_TIMERS, (table: Knex.TableBuilder) => {
-        table.dropColumn('isStartedOffline');
-        table.dropColumn('isStoppedOffline');
-    });
+	await knex.schema.alterTable(TABLE_NAME_TIMERS, (table: Knex.TableBuilder) => {
+		table.dropColumn('isStartedOffline');
+		table.dropColumn('isStoppedOffline');
+	});
 }
