42b336cb120c2c2d618fa0619eb716b8f195288c
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-02-14 16:23:39 +0530

    refactor(DTO): #5693 for delete team member

--- packages/core/src/organization-team/dto/delete-team-query.dto.ts
+++ packages/core/src/organization-team/dto/delete-team-query.dto.ts
@@ -1,12 +1,12 @@
 import { IntersectionType, PickType } from "@nestjs/swagger";
 import { IBasePerTenantAndOrganizationEntityModel, IRelationalEmployee } from "@gauzy/contracts";
 import { EmployeeFeatureDTO } from "./../../employee/dto";
-import { DeleteQueryDTO } from "./../../shared/dto";
+import { TenantOrganizationBaseDTO } from "../../core/dto";
 
 /**
  * Delete team member query DTO
  */
-export class DeleteTeamMemberQueryDTO extends IntersectionType(
-    DeleteQueryDTO,
+export class DeleteTeamQueryDTO extends IntersectionType(
+    PickType(TenantOrganizationBaseDTO, ['organizationId', 'tenantId']),
     PickType(EmployeeFeatureDTO, ['employeeId']),
 ) implements IBasePerTenantAndOrganizationEntityModel, IRelationalEmployee { }
--- packages/core/src/organization-team/dto/index.ts
+++ packages/core/src/organization-team/dto/index.ts
@@ -2,3 +2,4 @@
 export * from "./create-organization-team.dto";
 export * from "./update-organization-team.dto";
 export * from "./organization-team-statistic.dto";
+export * from './delete-team-query.dto';
