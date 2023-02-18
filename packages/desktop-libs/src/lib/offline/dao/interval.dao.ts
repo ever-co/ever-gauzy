aa6a6e320b7d821bad266b702fd8da9133421263
Adolphe Kifungo <akifungo@gmail.com>
2023-02-18 23:04:35 +0200

    fix: missing timeslots to delete

--- packages/desktop-libs/src/lib/offline/dao/interval.dao.ts
+++ packages/desktop-libs/src/lib/offline/dao/interval.dao.ts
@@ -144,7 +144,7 @@ export class IntervalDAO implements DAO<IntervalTO> {
 				.connection<IntervalTO>(TABLE_NAME_INTERVALS)
 				.select('remoteId')
 				.where('employeeId', user.employeeId)
-				.where((qb) => qb.andWhereBetween('startedAt', [startedAt, stoppedAt]).andWhere('synced', true))
+				.where((qb) => qb.andWhereBetween('stoppedAt', [startedAt, stoppedAt]).andWhere('synced', true))
 			await this.deleteLocallyIdlesTime(startedAt, stoppedAt, user);
 			return remotesIds;
 		} catch (error) {
@@ -168,7 +168,7 @@ export class IntervalDAO implements DAO<IntervalTO> {
 				.connection<IntervalTO>(TABLE_NAME_INTERVALS)
 				.select('id')
 				.where('employeeId', user.employeeId)
-				.where((qb) => qb.andWhereBetween('startedAt', [startedAt, stoppedAt]));
+				.where((qb) => qb.andWhereBetween('stoppedAt', [startedAt, stoppedAt]));
 			await this._provider
 				.connection<IntervalTO>(TABLE_NAME_INTERVALS)
 				.whereIn('id', subQuery.map(({ id }) => id))
