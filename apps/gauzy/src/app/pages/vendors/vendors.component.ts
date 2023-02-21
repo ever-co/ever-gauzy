71a01a40a507edfb85714385517e84cf872ca433
lawphilenz <lawphilenz@gmail.com>
2023-02-21 14:58:08 +0800

    Revert "fix: #5703 no header title on vendors"
    This reverts commit 4213e26f0b14e808e997550ca32461eb9792c60a.

--- apps/gauzy/src/app/pages/vendors/vendors.component.ts
+++ apps/gauzy/src/app/pages/vendors/vendors.component.ts
@@ -180,12 +180,10 @@ export class VendorsComponent
 				perPage: pagination ? pagination : this.minItemPerPage
 			},
 			actions: false,
-			hideSubHeader: true,
 			columns: {
 				logo: {
 					title: this.getTranslation('ORGANIZATIONS_PAGE.IMAGE'),
 					type: 'custom',
-					width: '5%',
 					renderComponent: CompanyLogoComponent
 				},
 				name: {
