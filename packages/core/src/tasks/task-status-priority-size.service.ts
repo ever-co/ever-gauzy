42bb8d515beb10390bbc7e0f3ccc46d98048818b
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-02-23 12:11:09 +0530

    fix: #5377 improved query for get task related things

--- packages/core/src/tasks/task-status-priority-size.service.ts
+++ packages/core/src/tasks/task-status-priority-size.service.ts
@@ -54,8 +54,9 @@ export class TaskStatusPrioritySizeService<BaseEntity extends TenantBaseEntity> 
 					bck.andWhere(`"${qb.alias}"."organizationId" IS NULL`);
 					bck.andWhere(`"${qb.alias}"."tenantId" IS NULL`);
 					bck.andWhere(`"${qb.alias}"."projectId" IS NULL`);
+					bck.andWhere(`"${qb.alias}"."organizationTeamId" IS NULL`);
 					bck.andWhere(`"${qb.alias}"."isSystem" = :isSystem`, {
-						isSystem: true,
+						isSystem: true
 					});
 				})
 			);
@@ -81,7 +82,7 @@ export class TaskStatusPrioritySizeService<BaseEntity extends TenantBaseEntity> 
 		 */
 		if (isNotEmpty(tenantId)) {
 			query.andWhere(`"${query.alias}"."tenantId" = :tenantId`, {
-				tenantId: RequestContext.currentTenantId(),
+				tenantId: RequestContext.currentTenantId()
 			});
 		} else {
 			query.andWhere(`"${query.alias}"."tenantId" IS NULL`);
@@ -91,7 +92,7 @@ export class TaskStatusPrioritySizeService<BaseEntity extends TenantBaseEntity> 
 		 */
 		if (isNotEmpty(organizationId)) {
 			query.andWhere(`"${query.alias}"."organizationId" = :organizationId`, {
-				organizationId,
+				organizationId
 			});
 		} else {
 			query.andWhere(`"${query.alias}"."organizationId" IS NULL`);
@@ -101,7 +102,7 @@ export class TaskStatusPrioritySizeService<BaseEntity extends TenantBaseEntity> 
 		 */
 		if (isNotEmpty(projectId)) {
 			query.andWhere(`"${query.alias}"."projectId" = :projectId`, {
-				projectId,
+				projectId
 			});
 		} else {
 			query.andWhere(`"${query.alias}"."projectId" IS NULL`);
@@ -112,7 +113,7 @@ export class TaskStatusPrioritySizeService<BaseEntity extends TenantBaseEntity> 
 		 */
 		if (isNotEmpty(organizationTeamId)) {
 			query.andWhere(`"${query.alias}"."organizationTeamId" = :organizationTeamId`, {
-				organizationTeamId,
+				organizationTeamId
 			});
 		} else {
 			query.andWhere(`"${query.alias}"."organizationTeamId" IS NULL`);
