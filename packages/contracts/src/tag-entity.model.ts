0a3e98681d4ca1bcc4f866486035f069feea269d
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-02-23 16:44:01 +0530

    fix: default global tags enum

--- packages/contracts/src/tag-entity.model.ts
+++ packages/contracts/src/tag-entity.model.ts
@@ -3,6 +3,7 @@ import { IBasePerTenantAndOrganizationEntityModel } from './base-entity.model';
 export interface ITag extends IBasePerTenantAndOrganizationEntityModel {
 	name: string;
 	color: string;
+	icon?: string;
 	description?: string;
 	isSystem?: boolean;
 }
@@ -17,4 +18,18 @@ export interface ITagCreateInput extends ITag { }
 
 export interface ITagUpdateInput extends Partial<ITagCreateInput> {
 	id?: string;
+}
+
+/**
+ * Default task tags
+ */
+export enum TagEnum {
+	MOBILE = 'Mobile',
+	FRONTEND = 'Frontend',
+	BACKEND = 'Backend',
+	WEB = 'WEB',
+	UI_UX = 'UI/UX',
+	FULL_STACK = 'Full-Stack',
+	TABLET = 'Tablet',
+	BUG = 'Bug'
 }
