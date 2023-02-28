31c0a364ff30a7c4a6a12c025d25160cfe00e89b
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-02-28 18:52:30 +0530

    wip: support cloudinary with multer storage

--- packages/core/src/core/file-storage/providers/cloudinary.provider.ts
+++ packages/core/src/core/file-storage/providers/cloudinary.provider.ts
@@ -1,11 +1,15 @@
 import { Request } from 'express';
 import * as multer from 'multer';
+import * as moment from 'moment';
 import * as fs from 'fs';
+import { join, resolve } from 'path';
 import { UploadApiErrorResponse, UploadApiResponse, v2 as cloudinary } from 'cloudinary';
-import { environment } from '@gauzy/config';
+import { environment, getConfig } from '@gauzy/config';
 import { FileStorageOption, FileStorageProviderEnum, FileSystem, UploadedFile } from "@gauzy/contracts";
 import { ICloudinaryConfig } from '@gauzy/common';
 import { Provider } from './provider';
+
+const config = getConfig();
 
 export class CloudinaryProvider extends Provider<CloudinaryProvider> {
 
@@ -28,33 +32,63 @@ export class CloudinaryProvider extends Provider<CloudinaryProvider> {
     handler({
         dest,
         filename,
-        prefix
+        prefix = 'file'
     }: FileStorageOption): multer.StorageEngine {
-        return multer.diskStorage({
+        const diskStorage = multer.diskStorage({
             destination: (_req: Request, file: Express.Multer.File, callback) => {
-                console.log({ file }, 'destination express multer file');
+                // A string or function that determines the destination path for uploaded
+                let dir: string;
+                if (dest instanceof Function) {
+                    dir = dest(file);
+                } else {
+                    dir = dest;
+                }
+
+                // In "screenshots" folder we will temporarily upload image before uploading to cloudinary
+                const fullPath = join(this.config.rootPath, dir);
+                console.log({ fullPath }, file.path);
+
+                // Creating screenshots folder if not already present.
+                if (!fs.existsSync(fullPath)) {
+                    fs.mkdirSync(fullPath, { recursive: true });
+                }
+                callback(null, fullPath);
             },
             filename: (_req: Request, file: Express.Multer.File, callback) => {
-                console.log({ file }, 'filename express multer file');
+                console.log(file.path, 'File Path');
+
+                // A file extension, or filename extension, is a suffix at the end of a file.
+                const extension = file.originalname.split('.').pop();
+
+                // A function that determines the name of the uploaded file.
+                let fileName: string;
+                if (filename) {
+                    if (typeof filename === 'string') {
+                        fileName = filename;
+                    } else {
+                        fileName = filename(file, extension);
+                    }
+                } else {
+                    fileName = `${prefix}-${moment().unix()}-${parseInt('' + Math.random() * 1000, 10)}.${extension}`;
+                }
+                callback(null, fileName);
             }
         });
+        console.log(diskStorage);
+        return diskStorage;
     }
 
     async deleteFile(file: string): Promise<void> { }
 
-    url(path: string): string {
-        try {
-            return path;
-        } catch (error) {
-            console.log('Error while retrieving singed URL:', error);
+    url(fileURL: string): string {
+        if (fileURL && fileURL.startsWith('http')) {
+            return fileURL;
         }
+        return fileURL ? `${this.config.baseUrl}/${fileURL}` : null;
     }
-    path(path: string): string {
-        try {
-            return path;
-        } catch (error) {
-            console.log('Error while retrieving singed URL:', error);
-        }
+
+    path(filePath: string): string {
+        return filePath ? `${this.config.rootPath}/${filePath}` : null;
     }
 
     /**
@@ -62,7 +96,8 @@ export class CloudinaryProvider extends Provider<CloudinaryProvider> {
      */
     setConfig() {
         this.config = {
-            rootPath: '',
+            rootPath: config.assetOptions.assetPublicPath || resolve(process.cwd(), 'apps', 'api', 'public'),
+            baseUrl: environment.baseUrl + '/public',
             ...cloudinary.config({
                 cloud_name: environment.cloudinaryConfig.cloudName,
                 api_key: environment.cloudinaryConfig.apiKey,
@@ -77,13 +112,33 @@ export class CloudinaryProvider extends Provider<CloudinaryProvider> {
     }
 
     async putFile(file: any, path: string = ''): Promise<UploadedFile> {
+        console.log('Cloudinary File Upload', file, path);
         return new Promise((resolve, reject) => {
-            cloudinary.uploader.upload(file, (error: UploadApiErrorResponse, result: UploadApiResponse) => {
+            cloudinary.uploader.upload(this.path(path), (error: UploadApiErrorResponse, result: UploadApiResponse) => {
+                console.log(error, result);
                 if (error) {
                     return reject(error);
                 }
+                // Image has been successfully uploaded on cloudinary so we dont need local image file anymore
+                // Remove file from local uploads folder
+                fs.unlinkSync(this.path(path));
                 return resolve({ url: result.url, id: result.public_id } as any);
             });
         });
+    }
+
+    /**
+     * Map uploaded file for provider
+     *
+     * @param file
+     * @returns
+     */
+    mapUploadedFile(file: any): UploadedFile {
+        const separator = process.platform === 'win32' ? '\\' : '/';
+        if (file.path) {
+            file.key = file.path.replace(this.config.rootPath + separator, '');
+        }
+        file.url = this.url(file.key);
+        return file;
     }
 }
