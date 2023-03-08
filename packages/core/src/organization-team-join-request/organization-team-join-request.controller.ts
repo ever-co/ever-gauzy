ac999b802d05d43be09306718d208f389dcc609b
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-03-08 12:01:54 +0530

    fix: removed crud controller for organization team join request

--- packages/core/src/organization-team-join-request/organization-team-join-request.controller.ts
+++ packages/core/src/organization-team-join-request/organization-team-join-request.controller.ts
@@ -1,32 +1,26 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import {
-	IPaginationParam,
-	IOrganizationTeamJoinRequest,
-	IOrganizationTeamJoinRequestFindInput,
-	IOrganizationTeamJoinRequestUpdateInput,
-	IOrganizationTeamJoinRequestCreateInput
-} from '@gauzy/contracts';
+import { IOrganizationTeamJoinRequest, IPagination } from '@gauzy/contracts';
 import { Public } from '@gauzy/common';
-import { CrudFactory, PaginationParams } from './../core/crud';
-import { CountQueryDTO } from './../shared/dto';
+import { PaginationParams } from './../core/crud';
 import { OrganizationTeamJoinRequest } from './organization-team-join-request.entity';
 import { OrganizationTeamJoinRequestService } from './organization-team-join-request.service';
 
 @ApiTags('OrganizationTeamJoinRequest')
 @Controller()
-export class OrganizationTeamJoinRequestController extends CrudFactory<
-	OrganizationTeamJoinRequest,
-	IPaginationParam,
-	IOrganizationTeamJoinRequestCreateInput,
-	IOrganizationTeamJoinRequestUpdateInput,
-	IOrganizationTeamJoinRequestFindInput
->(PaginationParams, OrganizationTeamJoinRequest, OrganizationTeamJoinRequest, CountQueryDTO) {
+export class OrganizationTeamJoinRequestController {
 
 	constructor(
 		private readonly _organizationTeamJoinRequestService: OrganizationTeamJoinRequestService
-	) {
-		super(_organizationTeamJoinRequestService);
+	) { }
+
+	@HttpCode(HttpStatus.OK)
+	@Get()
+	@UsePipes(new ValidationPipe())
+	async findAll(
+		@Query() params: PaginationParams<OrganizationTeamJoinRequest>
+	): Promise<IPagination<IOrganizationTeamJoinRequest>> {
+		return await this._organizationTeamJoinRequestService.findAll(params);
 	}
 
 	/**
@@ -36,9 +30,9 @@ export class OrganizationTeamJoinRequestController extends CrudFactory<
 	 * @returns
 	 */
 	@HttpCode(HttpStatus.CREATED)
-	@Public()
 	@Post()
 	@UsePipes(new ValidationPipe({ whitelist: true }))
+	@Public()
 	async create(
 		@Body() entity: OrganizationTeamJoinRequest
 	): Promise<IOrganizationTeamJoinRequest> {
