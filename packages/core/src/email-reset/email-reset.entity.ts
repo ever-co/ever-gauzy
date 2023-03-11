51f2daf37205e132ae87c97d17e54a45e0191b9e
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-03-11 15:14:56 +0530

    feat: #5895 email reset event listener

--- packages/core/src/email-reset/email-reset.entity.ts
+++ packages/core/src/email-reset/email-reset.entity.ts
@@ -2,17 +2,14 @@ import {
 	Entity,
 	Index,
 	Column,
-	AfterLoad,
 	ManyToOne,
 	JoinColumn,
-	RelationId,
-	BeforeInsert,
+	RelationId
 } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
-import * as moment from 'moment';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail } from 'class-validator';
+import { Exclude } from 'class-transformer';
 import { IEmailReset, IUser } from '@gauzy/contracts';
-import { environment } from '@gauzy/config';
 import { TenantBaseEntity, User } from '../core/entities/internal';
 
 @Entity('email_reset')
@@ -30,22 +27,21 @@ export class EmailReset extends TenantBaseEntity implements IEmailReset {
 	@Column()
 	oldEmail: string;
 
-	@ApiProperty({ type: () => Number, required: true })
-	@IsNotEmpty()
+	@Exclude({ toPlainOnly: true })
 	@Index()
 	@Column()
 	code: number;
 
-	@ApiProperty({ type: () => String })
 	@Index()
+	@Exclude({ toPlainOnly: true })
 	@Column({ nullable: true })
 	token: string;
 
-	@ApiProperty({ type: () => 'timestamptz' })
+	@Exclude({ toPlainOnly: true })
 	@Column({ nullable: true })
-	expiredAt?: Date;
+	expiredAt: Date;
 
-	expired?: boolean;
+	isExpired: boolean;
 
 	/*
 	|--------------------------------------------------------------------------
@@ -66,24 +62,4 @@ export class EmailReset extends TenantBaseEntity implements IEmailReset {
 	@Index()
 	@Column({ nullable: true })
 	userId?: IUser['id'];
-
-	/**
-	 * Called after entity is loaded.
-	 */
-	@AfterLoad()
-	afterLoadEntity?() {
-		const createdAt = moment(this.createdAt, 'YYYY-MM-DD HH:mm:ss');
-		const expiredAt = moment(moment(), 'YYYY-MM-DD HH:mm:ss');
-		this.expired = expiredAt.diff(createdAt, 'minutes') > 10;
-	}
-
-	/**
-	 * Called before entity is inserted.
-	 */
-	@BeforeInsert()
-	beforeInsertEntity() {
-		this.expiredAt = moment(this.createdAt)
-			.add(environment.EMAIL_RESET_EXPIRATION_TIME, 'seconds')
-			.toDate();
-	}
 }
---
+++ packages/core/src/email-reset/email-reset.subscriber.ts
@@ -0,0 +1,49 @@
+import { EntitySubscriberInterface, EventSubscriber, InsertEvent, LoadEvent } from 'typeorm';
+import * as moment from 'moment';
+import { environment } from '@gauzy/config';
+import { EmailReset } from './email-reset.entity';
+
+@EventSubscriber()
+export class EmailResetSubscriber implements EntitySubscriberInterface<EmailReset> {
+    /**
+     * Indicates that this subscriber only listen to EmailReset events.
+     */
+    listenTo() {
+        return EmailReset;
+    }
+
+    /**
+     * Called after entity is loaded from the database.
+     *
+     * @param entity
+     * @param event
+     */
+    afterLoad(
+        entity: EmailReset,
+        event?: LoadEvent<EmailReset>
+    ): void | Promise<any> {
+        try {
+            if ('expiredAt' in entity) {
+                entity.isExpired = entity.expiredAt ? moment(entity.expiredAt).isBefore(moment()) : false;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    /**
+     * Called before entity is inserted to the database.
+     *
+     * @param event
+     */
+    beforeInsert(event: InsertEvent<EmailReset>): void | Promise<any> {
+        try {
+            if (event.entity) {
+                const entity = event.entity;
+                entity.expiredAt = moment(new Date()).add(environment.EMAIL_RESET_EXPIRATION_TIME, 'seconds').toDate()
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
