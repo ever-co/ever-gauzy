c25e8fbacf8da156070083bb89cb93d57d98f8a9
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-03-14 12:34:15 +0530

    fix: #5741 request organization team join command handler

---
+++ packages/core/src/organization-team-join-request/commands/handlers/organization-team-join-request.create.handler.ts
@@ -0,0 +1,24 @@
+import { BadRequestException } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { IOrganizationTeamJoinRequest } from '@gauzy/contracts';
+import { OrganizationTeamJoinRequestService } from '../../organization-team-join-request.service';
+import { OrganizationTeamJoinRequestCreateCommand } from '../organization-team-join-request.create.command';
+
+@CommandHandler(OrganizationTeamJoinRequestCreateCommand)
+export class OrganizationTeamJoinRequestCreateHandler implements ICommandHandler<OrganizationTeamJoinRequestCreateCommand> {
+
+	constructor(
+		private readonly _organizationTeamJoinRequestService: OrganizationTeamJoinRequestService
+	) { }
+
+	public async execute(
+		command: OrganizationTeamJoinRequestCreateCommand
+	): Promise<IOrganizationTeamJoinRequest> {
+		try {
+			const { input, languageCode } = command;
+			return await this._organizationTeamJoinRequestService.create(input, languageCode);
+		} catch (error) {
+			throw new BadRequestException(error);
+		}
+	}
+}
