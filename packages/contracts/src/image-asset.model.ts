0ed3ff3bd86ff3a883de9db9807ef610bb1952d9
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-03-20 16:43:06 +0530

    fix: alter [table migration] for image asset

--- packages/contracts/src/image-asset.model.ts
+++ packages/contracts/src/image-asset.model.ts
@@ -1,9 +1,16 @@
+import { FileStorageProviderEnum } from "./file-provider";
 import { IBasePerTenantAndOrganizationEntityModel } from "./base-entity.model";
 
 export interface IImageAsset extends IBasePerTenantAndOrganizationEntityModel {
 	name: string;
 	url: string;
+	thumb?: string;
+	fullUrl?: string;
+	thumbUrl?: string;
 	width: number;
 	height: number;
+	size?: number;
 	isFeatured: boolean;
+	externalProviderId?: string;
+	storageProvider?: FileStorageProviderEnum;
 }
--- packages/core/src/image-asset/image-asset.entity.ts
+++ packages/core/src/image-asset/image-asset.entity.ts
@@ -1,42 +1,95 @@
-import { IEquipment, IImageAsset, IWarehouse } from '@gauzy/contracts';
-import { ApiProperty } from '@nestjs/swagger';
+import { FileStorageProviderEnum, IEquipment, IImageAsset, IWarehouse } from '@gauzy/contracts';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+import { IsBoolean, IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Column, Entity, ManyToMany, OneToMany } from 'typeorm';
+import { ColumnNumericTransformerPipe } from './../shared/pipes';
 import {
 	Product,
 	TenantOrganizationBaseEntity,
 	Equipment,
 	Warehouse
-} from '../core/entities/internal';
+} from './../core/entities/internal';
 
 @Entity('image_asset')
 export class ImageAsset extends TenantOrganizationBaseEntity
 	implements IImageAsset {
 
-	@ApiProperty({ type: () => String })
+	@ApiPropertyOptional({ type: () => String })
+	@IsOptional()
+	@IsString()
 	@Column({ nullable: true })
 	name: string;
 
 	@ApiProperty({ type: () => String })
+	@IsNotEmpty()
+	@IsString()
 	@Column()
-	url: string;
+	url: string
 
-	@ApiProperty({ type: () => Number })
+	@ApiProperty({ type: () => String })
+	@IsString()
+	@IsOptional()
+	@Column({ nullable: true })
+	thumb?: string;
+
+	@ApiPropertyOptional({ type: () => Number, default: 0 })
+	@IsOptional()
+	@IsNumber()
 	@Column({ default: 0 })
 	width: number;
 
-	@ApiProperty({ type: () => Number })
+	@ApiPropertyOptional({ type: () => Number, default: 0 })
+	@IsOptional()
+	@IsNumber()
 	@Column({ default: 0 })
 	height: number;
 
+	@ApiPropertyOptional({ type: () => Number })
+	@IsOptional()
+	@IsNumber()
+	@Column({
+		nullable: true,
+		type: 'numeric',
+		transformer: new ColumnNumericTransformerPipe()
+	})
+	size?: number;
+
 	@ApiProperty({ type: () => Boolean })
+	@IsBoolean()
 	@Column({ default: false })
 	isFeatured: boolean;
 
+	@ApiPropertyOptional({ type: () => String })
+	@IsOptional()
+	@Exclude({ toPlainOnly: true })
+	@Column({ nullable: true })
+	externalProviderId?: string;
+
+	@ApiPropertyOptional({ type: () => String, enum: FileStorageProviderEnum })
+	@Exclude({ toPlainOnly: true })
+	@Column({
+		type: 'simple-enum',
+		nullable: true,
+		enum: FileStorageProviderEnum
+	})
+	storageProvider?: FileStorageProviderEnum;
+
+	@ApiPropertyOptional({ type: () => 'timestamptz', required: false })
+	@IsOptional()
+	@IsDateString()
+	@Column({ nullable: true })
+	deletedAt?: Date;
+
+	/** Additional fields */
+	fullUrl?: string;
+	thumbUrl?: string;
+
 	/*
-    |--------------------------------------------------------------------------
-    | @OneToMany
-    |--------------------------------------------------------------------------
-    */
+	|--------------------------------------------------------------------------
+	| @OneToMany
+	|--------------------------------------------------------------------------
+	*/
 
 	/**
 	 * Product
@@ -66,10 +119,10 @@ export class ImageAsset extends TenantOrganizationBaseEntity
 	warehouses?: IWarehouse[];
 
 	/*
-    |--------------------------------------------------------------------------
-    | @ManyToMany
-    |--------------------------------------------------------------------------
-    */
+	|--------------------------------------------------------------------------
+	| @ManyToMany
+	|--------------------------------------------------------------------------
+	*/
 
 	/**
 	 * Product
