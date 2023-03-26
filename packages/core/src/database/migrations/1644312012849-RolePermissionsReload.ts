ba84addab02a43ee8f3d70238b9e2ee19573f514
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-03-26 11:14:17 +0530

    fix: used common role permissions utility methods for migrating permissions

--- packages/core/src/database/migrations/1644312012849-RolePermissionsReload.ts
+++ packages/core/src/database/migrations/1644312012849-RolePermissionsReload.ts
@@ -1,13 +1,17 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-import { reloadRolePermissions } from "./../../role-permission/role-permission.seed";
+import { RolePermissionUtils } from "./../../role-permission/utils";
 
 export class RolePermissionsReload1644312012849 implements MigrationInterface {
 
     name = 'RolePermissionsReload1644312012849';
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await reloadRolePermissions(queryRunner.connection);
+        try {
+            await RolePermissionUtils.migrateRolePermissions(queryRunner);
+        } catch (error) {
+            console.log(`Error while migrating missing role permisions`, error);
+        }
     }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {}
+    public async down(queryRunner: QueryRunner): Promise<any> { }
 }
