2b2eb2cfbc3a18bd5160ae918063fd180e8eeb3a
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-03-28 14:23:43 +0200

    feat: exports all services

---
+++ apps/desktop-timer/src/app/services/index.ts
@@ -0,0 +1,6 @@
+export * from './error-client.service';
+export * from './error-handler.service';
+export * from './error-server.service';
+export * from './native-notification.service';
+export * from './notification.service';
+export * from './toastr-notification.service';
