cde20ed9e8cefabbb4715ce46b5bb41d06375597
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-03-29 12:03:55 +0530

    feat: #5377 model contract for issue type entity

---
+++ packages/contracts/src/issue-type.model.ts
@@ -0,0 +1,21 @@
+import { IBasePerTenantAndOrganizationEntityModel } from './base-entity.model';
+import { IRelationalOrganizationProject } from './organization-projects.model';
+import { IRelationalOrganizationTeam } from './organization-team.model';
+
+export interface IIssueType extends IBasePerTenantAndOrganizationEntityModel, IRelationalOrganizationTeam, IRelationalOrganizationProject {
+	name: string;
+	value: string;
+	description?: string;
+	icon?: string;
+	color?: string;
+	isSystem?: boolean;
+	fullIconUrl?: string;
+}
+
+export interface IIssueTypeCreateInput extends Omit<IIssueType, 'isSystem'>, Omit<IIssueType, 'value'> { }
+
+export interface IIssueTypeUpdateInput extends Partial<IIssueTypeCreateInput> {
+	id?: string;
+}
+
+export interface IIssueTypeFindInput extends IBasePerTenantAndOrganizationEntityModel, Pick<IIssueType, 'projectId' | 'organizationTeamId'> { }
