8dd585b3ada7d80763aecea1edb57af822d59c50
rahul-rocket <rahul-rocket@users.noreply.github.com>
2023-04-01 12:20:40 +0000

    [MegaLinter] Apply linters automatic fixes

--- apps/gauzy/src/app/pages/jobs/search/search/search.component.ts
+++ apps/gauzy/src/app/pages/jobs/search/search/search.component.ts
@@ -1,10 +1,4 @@
-
-import {
-	Component,
-	OnDestroy,
-	OnInit,
-	AfterViewInit
-} from '@angular/core';
+import { Component, OnDestroy, OnInit, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, combineLatest, Subject, Subscription, timer } from 'rxjs';
@@ -28,7 +22,10 @@ import { NbTabComponent } from '@nebular/theme';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
 import { EmployeeLinksComponent } from './../../../../@shared/table-components';
-import { IPaginationBase, PaginationFilterBaseComponent } from '../../../../@shared/pagination/pagination-filter-base.component';
+import {
+	IPaginationBase,
+	PaginationFilterBaseComponent
+} from '../../../../@shared/pagination/pagination-filter-base.component';
 import { JobService, Store, ToastrService } from './../../../../@core/services';
 import { StatusBadgeComponent } from './../../../../@shared/status-badge';
 import { API_PREFIX } from './../../../../@core/constants';
@@ -43,9 +40,7 @@ import { JobTitleDescriptionDetailsComponent } from '../../table-components';
 	templateUrl: './search.component.html',
 	styleUrls: ['./search.component.scss']
 })
-export class SearchComponent extends PaginationFilterBaseComponent
-	implements OnInit, OnDestroy, AfterViewInit {
-
+export class SearchComponent extends PaginationFilterBaseComponent implements OnInit, OnDestroy, AfterViewInit {
 	loading: boolean = false;
 	autoRefresh: boolean = false;
 	settingsSmartTable: any;
@@ -81,19 +76,22 @@ export class SearchComponent extends PaginationFilterBaseComponent
 	public selectedEmployeeId: ISelectedEmployee['id'];
 
 	/*
-	* Search Tab Form
-	*/
+	 * Search Tab Form
+	 */
 	public form: FormGroup = SearchComponent.buildForm(this.fb);
 	static buildForm(fb: FormBuilder): FormGroup {
-		return fb.group({
-			title: [],
-			jobSource: [],
-			jobType: [],
-			jobStatus: [],
-			budget: []
-		}, {
-			validators: [AtLeastOneFieldValidator]
-		});
+		return fb.group(
+			{
+				title: [],
+				jobSource: [],
+				jobType: [],
+				jobStatus: [],
+				budget: []
+			},
+			{
+				validators: [AtLeastOneFieldValidator]
+			}
+		);
 	}
 
 	constructor(
@@ -241,14 +239,9 @@ export class SearchComponent extends PaginationFilterBaseComponent
 					this.smartTableSource.refresh();
 
 					if (resp.isRedirectRequired) {
-						const proposalTemplate =
-							await this.getEmployeeDefaultProposalTemplate(
-								$event.data
-							);
+						const proposalTemplate = await this.getEmployeeDefaultProposalTemplate($event.data);
 						if (proposalTemplate) {
-							await this.copyTextToClipboard(
-								proposalTemplate.content
-							);
+							await this.copyTextToClipboard(proposalTemplate.content);
 						}
 						window.open($event.data.jobPost.url, '_blank');
 					}
@@ -319,10 +312,7 @@ export class SearchComponent extends PaginationFilterBaseComponent
 			this.smartTableSource.refresh();
 
 			if (resp.isRedirectRequired) {
-				const proposalTemplate =
-					await this.getEmployeeDefaultProposalTemplate(
-						this.selectedJob.data
-					);
+				const proposalTemplate = await this.getEmployeeDefaultProposalTemplate(this.selectedJob.data);
 				if (proposalTemplate) {
 					await this.copyTextToClipboard(proposalTemplate.content);
 				}
@@ -345,26 +335,23 @@ export class SearchComponent extends PaginationFilterBaseComponent
 			columns: {
 				...(!this.selectedEmployeeId
 					? {
-						employee: {
-							title: this.getTranslation('JOBS.EMPLOYEE'),
+							employee: {
+								title: this.getTranslation('JOBS.EMPLOYEE'),
 
-							filter: false,
-							width: '15%',
-							type: 'custom',
-							sort: false,
-							renderComponent: EmployeeLinksComponent,
-							valuePrepareFunction: (
-								cell,
-								row: IEmployeeJobPost
-							) => {
-								return {
-									name: row.employee && row.employee.user ? row.employee.user.name : null,
-									imageUrl: row.employee && row.employee.user ? row.employee.user.imageUrl : null,
-									id: row.employee ? row.employee.id : null
-								};
+								filter: false,
+								width: '15%',
+								type: 'custom',
+								sort: false,
+								renderComponent: EmployeeLinksComponent,
+								valuePrepareFunction: (cell, row: IEmployeeJobPost) => {
+									return {
+										name: row.employee && row.employee.user ? row.employee.user.name : null,
+										imageUrl: row.employee && row.employee.user ? row.employee.user.imageUrl : null,
+										id: row.employee ? row.employee.id : null
+									};
+								}
 							}
-						}
-					}
+					  }
 					: {}),
 				jobDetails: {
 					title: this.getTranslation('JOBS.JOB_DETAILS'),
@@ -382,22 +369,13 @@ export class SearchComponent extends PaginationFilterBaseComponent
 					renderComponent: StatusBadgeComponent,
 					valuePrepareFunction: (cell, row: IEmployeeJobPost) => {
 						let badgeClass;
-						if (
-							row.jobPost.jobStatus.toLowerCase() ===
-							JobPostStatusEnum.CLOSED.toLowerCase()
-						) {
+						if (row.jobPost.jobStatus.toLowerCase() === JobPostStatusEnum.CLOSED.toLowerCase()) {
 							badgeClass = 'danger';
 							cell = this.getTranslation('JOBS.STATUS_CLOSED');
-						} else if (
-							row.jobPost.jobStatus.toLowerCase() ===
-							JobPostStatusEnum.OPEN.toLowerCase()
-						) {
+						} else if (row.jobPost.jobStatus.toLowerCase() === JobPostStatusEnum.OPEN.toLowerCase()) {
 							badgeClass = 'success';
 							cell = this.getTranslation('JOBS.STATUS_OPEN');
-						} else if (
-							row.jobPost.jobStatus.toLowerCase() ===
-							JobPostStatusEnum.APPLIED.toLowerCase()
-						) {
+						} else if (row.jobPost.jobStatus.toLowerCase() === JobPostStatusEnum.APPLIED.toLowerCase()) {
 							badgeClass = 'warning';
 							cell = this.getTranslation('JOBS.STATUS_APPLIED');
 						} else {
@@ -416,8 +394,8 @@ export class SearchComponent extends PaginationFilterBaseComponent
 	}
 
 	/*
-	* Register Smart Table Source Config
-	*/
+	 * Register Smart Table Source Config
+	 */
 	setSmartTableSource() {
 		if (!this.organization) {
 			return;
@@ -471,56 +449,54 @@ export class SearchComponent extends PaginationFilterBaseComponent
 			 */
 			this.smartTableSource.setFilter(
 				[
-					...(
-						this.selectedEmployeeId ? [
-							{
-								field: 'employeeIds',
-								search: [
-									this.selectedEmployeeId
-								]
-							}
-						] : []
-					),
-					...(
-						title ? [
-							{
-								field: 'title',
-								search: title
-							}
-						] : []
-					),
-					...(
-						jobSource ? [
-							{
-								field: 'jobSource',
-								search: jobSource
-							}
-						] : []
-					),
-					...(
-						jobType ? [
-							{
-								field: 'jobType',
-								search: jobType
-							}
-						] : []
-					),
-					...(
-						jobStatus ? [
-							{
-								field: 'jobStatus',
-								search: jobStatus
-							}
-						] : []
-					),
-					...(
-						budget ? [
-							{
-								field: 'budget',
-								search: budget
-							}
-						] : []
-					),
+					...(this.selectedEmployeeId
+						? [
+								{
+									field: 'employeeIds',
+									search: [this.selectedEmployeeId]
+								}
+						  ]
+						: []),
+					...(title
+						? [
+								{
+									field: 'title',
+									search: title
+								}
+						  ]
+						: []),
+					...(jobSource
+						? [
+								{
+									field: 'jobSource',
+									search: jobSource
+								}
+						  ]
+						: []),
+					...(jobType
+						? [
+								{
+									field: 'jobType',
+									search: jobType
+								}
+						  ]
+						: []),
+					...(jobStatus
+						? [
+								{
+									field: 'jobStatus',
+									search: jobStatus
+								}
+						  ]
+						: []),
+					...(budget
+						? [
+								{
+									field: 'budget',
+									search: budget
+								}
+						  ]
+						: [])
 				],
 				false,
 				false
@@ -540,11 +516,7 @@ export class SearchComponent extends PaginationFilterBaseComponent
 			/**
 			 * Applied smart table pagination configuration
 			 */
-			this.smartTableSource.setPaging(
-				activePage,
-				itemsPerPage,
-				false
-			);
+			this.smartTableSource.setPaging(activePage, itemsPerPage, false);
 		} catch (error) {
 			this.toastrService.danger(error);
 		}
@@ -565,9 +537,7 @@ export class SearchComponent extends PaginationFilterBaseComponent
 	hideAll() {
 		const request: IVisibilityJobPostInput = {
 			hide: true,
-			...(this.selectedEmployeeId
-				? { employeeId: this.selectedEmployeeId }
-				: {})
+			...(this.selectedEmployeeId ? { employeeId: this.selectedEmployeeId } : {})
 		};
 		this.jobService.hideJob(request).then(() => {
 			this.toastrService.success('TOASTR.MESSAGE.JOB_HIDDEN');
@@ -592,5 +562,5 @@ export class SearchComponent extends PaginationFilterBaseComponent
 		this.jobs$.next(true);
 	}
 
-	ngOnDestroy(): void { }
+	ngOnDestroy(): void {}
 }
