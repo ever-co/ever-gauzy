b26c2a91c9a6e50cf0108a1bcc63b3923c6e00f2
rahul-rocket <rahul-rocket@users.noreply.github.com>
2023-04-03 17:00:00 +0000

    [MegaLinter] Apply linters automatic fixes

--- packages/core/src/database/migrations/1680539459969-UpdateEmailTemplates.ts
+++ packages/core/src/database/migrations/1680539459969-UpdateEmailTemplates.ts
@@ -1,38 +1,32 @@
-
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 import { EmailTemplateNameEnum } from '@gauzy/contracts';
 import { EmailTemplateUtils } from './../../email-template/utils';
 
 export class UpdateEmailTemplates1680539459969 implements MigrationInterface {
+	name = 'UpdateEmailTemplates1680539459969';
 
-    name = 'UpdateEmailTemplates1680539459969';
+	/**
+	 * Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async up(queryRunner: QueryRunner): Promise<any> {
+		const templates = Object.values(EmailTemplateNameEnum);
+		await Promise.all(
+			templates.map(async (template: EmailTemplateNameEnum) => {
+				try {
+					await EmailTemplateUtils.migrateEmailTemplates(queryRunner, template);
+				} catch (error) {
+					console.log(`Error while migrating missing email templates for ${template}`, error);
+				}
+			})
+		);
+	}
 
-    /**
-    * Up Migration
-    *
-    * @param queryRunner
-    */
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        const templates = Object.values(EmailTemplateNameEnum);
-        await Promise.all(
-            templates.map(
-                async (template: EmailTemplateNameEnum) => {
-                    try {
-                        await EmailTemplateUtils.migrateEmailTemplates(queryRunner, template);
-                    } catch (error) {
-                        console.log(`Error while migrating missing email templates for ${template}`, error);
-                    }
-                }
-            )
-        );
-    }
-
-    /**
-    * Down Migration
-    *
-    * @param queryRunner
-    */
-    public async down(queryRunner: QueryRunner): Promise<any> {
-
-    }
+	/**
+	 * Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async down(queryRunner: QueryRunner): Promise<any> {}
 }
