49bbf64e84774105932e3ae86a896892dab54d11
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-04-06 17:20:26 +0200

    feat: create tenat and token interceptor for desktop app

---
+++ apps/desktop/src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+	HttpRequest,
+	HttpHandler,
+	HttpEvent,
+	HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Store } from '@gauzy/desktop-timer/src/app/auth/services/store.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+	constructor(private store: Store) { }
+	intercept(
+		request: HttpRequest<any>,
+		next: HttpHandler
+	): Observable<HttpEvent<any>> {
+		const token = this.store.token;
+		if (token) {
+			request = request.clone({
+				setHeaders: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+		}
+		return next.handle(request);
+	}
+}
