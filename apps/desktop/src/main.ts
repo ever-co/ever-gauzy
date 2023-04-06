5b57d7232eccfb7ccadd9586adb1ec2513117ed9
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-04-06 11:55:37 +0200

    feat: add akita store to desktop app

--- apps/desktop/src/main.ts
+++ apps/desktop/src/main.ts
@@ -2,10 +2,21 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { enableAkitaProdMode, persistState } from '@datorama/akita';
+import { akitaConfig } from '@datorama/akita';
 
 if (environment.production) {
 	enableProdMode();
+	enableAkitaProdMode();
 }
+
+persistState({
+	key: '_gauzyStore'
+});
+
+akitaConfig({
+	resettable: true
+});
 
 platformBrowserDynamic()
 	.bootstrapModule(AppModule)
