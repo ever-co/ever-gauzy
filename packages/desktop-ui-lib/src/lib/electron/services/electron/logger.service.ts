4e0c33e492a1ce5d7113c21590f2c400482fbec1
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-04-13 00:20:48 +0200

    fix: electron log calling in logger service

--- packages/desktop-ui-lib/src/lib/electron/services/electron/logger.service.ts
+++ packages/desktop-ui-lib/src/lib/electron/services/electron/logger.service.ts
@@ -8,9 +8,11 @@ export class LoggerService {
 	private _log: any;
 
 	constructor(private readonly _electronService: ElectronService) {
-		this._log = this._electronService.remote.require('electron-log');
-		console.log = this._log.log;
-		Object.assign(console, this._log.functions);
+		if (this._electronService.isElectron) {
+			this._log = window.require('electron-log');
+			console.log = this._log.log;
+			Object.assign(console, this._log.functions);
+		}
 	}
 
 	public get log(): any {
