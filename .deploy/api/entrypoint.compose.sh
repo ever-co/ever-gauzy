a993395cf87e9e4701528946e5be035f66cecbba
Ruslan Konviser <evereq@gmail.com>
2023-04-29 23:08:46 +0200

    chore: disable for now PM2

--- .deploy/api/entrypoint.compose.sh
+++ .deploy/api/entrypoint.compose.sh
@@ -8,14 +8,14 @@ export WAIT_HOSTS=$DB_HOST:$DB_PORT
 # in Docker Compose we should wait other services start
 ./wait
 
-echo 'Version of PM2 …'
-pm2 --version
+# echo 'Version of PM2 …'
+# pm2 --version
 
-echo 'Resetting PM2 Metadata …'
-pm2 reset all
+# echo 'Resetting PM2 Metadata …'
+# pm2 reset all
 
-echo 'Updating PM2 …'
-pm2 update
+# echo 'Updating PM2 …'
+# pm2 update
 
 echo 'Starting …'
 exec "$@"
