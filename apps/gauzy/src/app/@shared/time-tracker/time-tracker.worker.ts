c9556e37deb5c90c6e129d1a10b1e74748022729
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-05-07 19:26:57 +0200

    Revert "fix: remove web worker"
    This reverts commit d1b58223009427f4bb731b1b63ad6a269174ad28.

---
+++ apps/gauzy/src/app/@shared/time-tracker/time-tracker.worker.ts
@@ -0,0 +1,22 @@
+/// <reference lib="webworker" />
+
+// Initialize interval to null
+let interval = null;
+// retrieve message post from time tracker service
+addEventListener('message', ({ data }) => {
+	// if timer running, start counter
+	if (data.isRunning) {
+		interval = setInterval(() => {
+			// Pre-increment session and duration values each 1 sec and send to time tracker service
+			postMessage({
+				session: ++data.session,
+				todayWorked: ++data.duration
+			});
+		}, 1000);
+	} else {
+		// if timer stopped clear interval
+		if (interval) {
+			clearInterval(interval);
+		}
+	}
+});
