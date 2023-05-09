6994cd56dd0c2a69f7956905a22958254bef3ae5
Ruslan K <evereq@gmail.com>
2023-05-09 18:17:09 +0200

    Update electron.env.ts

--- .scripts/electron.env.ts
+++ .scripts/electron.env.ts
@@ -15,7 +15,7 @@ if (isProd) {
     export const environment = {
         production: true,
         AWHost: 'http://localhost:5600',
-        API_DEFAULT_PORT: 5620,
+        API_DEFAULT_PORT: 3000,
         GAUZY_UI_DEFAULT_PORT: 5621,
         SCREENSHOTS_ENGINE_METHOD: 'ScreenshotDesktopLib', // ElectronDesktopCapturer || ScreenshotDesktopLib
         SENTRY_DSN: '${env.SENTRY_DSN}'
