87f796e083c9bb70136cd64a94cc5fb9a16fb71f
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-06-21 14:33:25 +0200

    refactor: project organization grid component

--- apps/gauzy/src/app/@shared/table-components/project-organization-grid/project-organization-grid.component.ts
+++ apps/gauzy/src/app/@shared/table-components/project-organization-grid/project-organization-grid.component.ts
@@ -1,14 +1,44 @@
 import { Component, Input } from '@angular/core';
 import { ViewCell } from 'ng2-smart-table';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
 	selector: 'gauzy-project-organization-grid',
 	templateUrl: './project-organization-grid.component.html',
-	styleUrls: ['./project-organization-grid.component.scss']
+	styleUrls: ['./project-organization-grid.component.scss'],
 })
 export class ProjectOrganizationGridComponent implements ViewCell {
 	@Input()
 	value: string | number;
+	private _rowData: any;
+	private _visibility$: BehaviorSubject<boolean>;
+
+	constructor() {
+		this._rowData = null;
+		this._visibility$ = new BehaviorSubject(null);
+	}
+
+	public onVisibilityChange(state: boolean): void {
+		this._visibility$.next(state);
+	}
+
+	public get visibility(): boolean {
+		if (this._visibility$.getValue() === null) {
+			this._visibility$.next(
+				this.rowData.public === null ? false : this.rowData.public
+			);
+		}
+		return this._visibility$.getValue();
+	}
+
 	@Input()
-	rowData: any;
+	public set rowData(value: any) {
+		if (value) {
+			this._rowData = value;
+		}
+	}
+
+	public get rowData() {
+		return this._rowData;
+	}
 }
