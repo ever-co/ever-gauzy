0562e08600f42404a0a6cbaaf344afaccfd59174
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-06-21 17:40:25 +0200

    fix: remove unused import

--- apps/gauzy/src/app/@shared/table-components/allow-screenshot-capture/allow-screenshot-capture.component.ts
+++ apps/gauzy/src/app/@shared/table-components/allow-screenshot-capture/allow-screenshot-capture.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ViewCell } from 'ng2-smart-table';
 import { IEmployee } from '@gauzy/contracts';
 import { BehaviorSubject, tap } from 'rxjs';
