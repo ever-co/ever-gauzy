66bcb048caf580ec4a3831be11eae656a906b29c
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-06-26 20:34:19 +0530

    fix: job browse page footer title settings

--- apps/gauzy/src/app/pages/jobs/table-components/job-table-components.module.ts
+++ apps/gauzy/src/app/pages/jobs/table-components/job-table-components.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NbIconModule, NbTooltipModule } from '@nebular/theme';
+import { NbIconModule, NbTagModule, NbTooltipModule } from '@nebular/theme';
 import { JobTitleDescriptionDetailsComponent } from './job-title-description-details/job-title-description-details.component';
 import { SharedModule } from '../../../@shared/shared.module';
 
 @NgModule({
-	imports: [CommonModule, NbIconModule, NbTooltipModule, SharedModule],
+	imports: [CommonModule, NbIconModule, NbTooltipModule, NbTagModule, SharedModule],
 	declarations: [JobTitleDescriptionDetailsComponent],
 	exports: [JobTitleDescriptionDetailsComponent]
 })
