e58bd2415e5dd79e75306412eff7fadcca226613
Badal Khatri <badal.khatri0924@gmail.com>
2023-06-27 08:54:19 +0530

    fix: seed:all

--- packages/core/src/core/seeds/seeder.module.ts
+++ packages/core/src/core/seeds/seeder.module.ts
@@ -1,8 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { getDynamicPluginsModules } from '@gauzy/plugin';
-import { ConfigModule } from '@gauzy/config';
+import { ConfigModule, environment } from '@gauzy/config';
 import { SeedDataService } from './seed-data.service';
 import { DatabaseModule } from './../../database/database.module';
+import { HeaderResolver, I18nModule } from 'nestjs-i18n';
+import { LanguagesEnum } from '@gauzy/contracts';
+import * as path from 'path';
 
 /**
  * Import and provide seeder classes.
@@ -12,7 +15,7 @@ import { DatabaseModule } from './../../database/database.module';
 @Module({
 	imports: [ConfigModule],
 	providers: [SeedDataService],
-	exports: [SeedDataService]
+	exports: [SeedDataService],
 })
 export class SeederModule {
 	static forPlugins(): DynamicModule {
@@ -21,9 +24,17 @@ export class SeederModule {
 			providers: [],
 			imports: [
 				...getDynamicPluginsModules(),
-				DatabaseModule
+				DatabaseModule,
+				I18nModule.forRoot({
+					fallbackLanguage: LanguagesEnum.ENGLISH,
+					loaderOptions: {
+						path: path.resolve(__dirname, '../../i18n/'),
+						watch: !environment.production,
+					},
+					resolvers: [new HeaderResolver(['language'])],
+				}),
 			],
-			exports: []
+			exports: [],
 		} as DynamicModule;
 	}
 }
