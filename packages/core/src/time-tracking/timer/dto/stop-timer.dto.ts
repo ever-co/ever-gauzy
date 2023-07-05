1253cf60b16e6699effbb3a556b033a0926e1135
Badal Khatri <badal.khatri0924@gmail.com>
2023-07-05 08:01:31 +0530

    fix: Deepscan issue

--- packages/core/src/time-tracking/timer/dto/stop-timer.dto.ts
+++ packages/core/src/time-tracking/timer/dto/stop-timer.dto.ts
@@ -1,6 +1,6 @@
 import { ITimerToggleInput, TimeLogSourceEnum } from '@gauzy/contracts';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum } from 'class-validator';
 import { StartTimerDTO } from './start-timer.dto';
 
 export class StopTimerDTO extends StartTimerDTO implements ITimerToggleInput {
