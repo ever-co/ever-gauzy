0ad6c32aca299f2f2d2cfebfcf422607c9b7e536
rahul-rocket <rahul-rocket@users.noreply.github.com>
2023-07-06 07:50:43 +0000

    [MegaLinter] Apply linters automatic fixes

--- packages/core/src/time-tracking/timer/dto/stop-timer.dto.ts
+++ packages/core/src/time-tracking/timer/dto/stop-timer.dto.ts
@@ -2,6 +2,6 @@ import { ITimerToggleInput } from '@gauzy/contracts';
 import { IntersectionType, PartialType, PickType } from '@nestjs/swagger';
 import { StartTimerDTO } from './start-timer.dto';
 
-export class StopTimerDTO extends IntersectionType(StartTimerDTO, PartialType(
-	PickType(StartTimerDTO, ['source'] as const) )
-) implements ITimerToggleInput { }
+export class StopTimerDTO
+	extends IntersectionType(StartTimerDTO, PartialType(PickType(StartTimerDTO, ['source'] as const)))
+	implements ITimerToggleInput {}
