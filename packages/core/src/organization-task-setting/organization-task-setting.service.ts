af68cbd65089774afa228594629b21d51a4d629d
Govindram Solanki <gram01846@gmail.com>
2023-07-24 14:04:21 +0530

    Tenant Id added to where orgTaskSetting

--- packages/core/src/organization-task-setting/organization-task-setting.service.ts
+++ packages/core/src/organization-task-setting/organization-task-setting.service.ts
@@ -4,6 +4,7 @@ import { TenantAwareCrudService } from '../core/crud';
 import { OrganizationTaskSetting } from './organization-task-setting.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { IOrganizationTaskSetting } from '@gauzy/contracts';
+import { RequestContext } from '../core/context';
 
 @Injectable()
 export class OrganizationTaskSettingService extends TenantAwareCrudService<OrganizationTaskSetting> {
@@ -24,11 +25,14 @@ export class OrganizationTaskSettingService extends TenantAwareCrudService<Organ
     async findByOrganizationId(
         organizationId: IOrganizationTaskSetting['id'],
     ): Promise<IOrganizationTaskSetting> {
+        const tenantId = RequestContext.currentTenantId();
+
         const {
             record
         } = await this.findOneOrFailByOptions({
             where: {
-                organizationId
+                organizationId,
+                tenantId
             }
         });
 
