72109f48301a64be814ca9acbcd52b243386124d
Govindram Solanki <gram01846@gmail.com>
2023-07-24 08:09:25 +0530

    New API added findByOrganizationId

--- packages/core/src/organization-task-setting/organization-task-setting.controller.ts
+++ packages/core/src/organization-task-setting/organization-task-setting.controller.ts
@@ -1,14 +1,17 @@
-import {
-	Body,
-	Controller,
-	HttpCode,
-	HttpStatus,
-	Param,
-	Put,
-	UseGuards,
-	Post,
-	UsePipes,
-	ValidationPipe,
+import
+{
+    Body,
+    Controller,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Put,
+    UseGuards,
+    Post,
+    UsePipes,
+    ValidationPipe,
+    Get,
+    Query,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CommandBus } from '@nestjs/cqrs';
@@ -18,78 +21,115 @@ import { UUIDValidationPipe } from './../shared/pipes';
 import { PermissionGuard, TenantPermissionGuard } from './../shared/guards';
 import { OrganizationTaskSettingCreateCommand, OrganizationTaskSettingUpdateCommand } from './commands';
 import { CreateOrganizationTaskSettingDTO, UpdateOrganizationTaskSettingDTO } from './dto';
+import { OrganizationTaskSetting } from './organization-task-setting.entity';
+import { OrganizationTaskSettingService } from './organization-task-setting.service';
 
 @ApiTags('OrganizationTaskSetting')
 @UseGuards(TenantPermissionGuard, PermissionGuard)
 @Permissions(PermissionsEnum.ALL_ORG_EDIT)
 @Controller()
-export class OrganizationTaskSettingController {
+export class OrganizationTaskSettingController
+{
 
-	constructor(
-		private readonly commandBus: CommandBus
-	) { }
+    constructor(
+        private readonly commandBus: CommandBus,
+        private readonly organizationTaskSettingService: OrganizationTaskSettingService
+    ) { }
 
-	/**
-	 * CREATE organization task setting
-	 *
-	 * @param body
-	 * @returns
-	 */
-	@ApiOperation({
-		summary: 'Create new record',
-	})
-	@ApiResponse({
-		status: HttpStatus.CREATED,
-		description: 'The record has been successfully created.',
-	})
-	@ApiResponse({
-		status: HttpStatus.BAD_REQUEST,
-		description:
-			'Invalid input, The response body may contain clues as to what went wrong',
-	})
-	@HttpCode(HttpStatus.CREATED)
-	@Permissions(PermissionsEnum.ALL_ORG_EDIT, PermissionsEnum.ORG_TASK_SETTING)
-	@Post()
-	@UsePipes(new ValidationPipe())
-	async create(
-		@Body() body: CreateOrganizationTaskSettingDTO
-	): Promise<IOrganizationTaskSetting> {
-		return this.commandBus.execute(
-			new OrganizationTaskSettingCreateCommand(body)
-		);
-	}
+    /**
+     * GET organization Task Setting by organizationId
+     *
+     * @param organizationId
+     * @returns
+     */
+    @ApiOperation({
+        summary: 'Find organization task setting by organizationId.'
+    })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'Found Organization Task Setting',
+        type: OrganizationTaskSetting
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: 'Record not found'
+    })
+    @Permissions(
+        PermissionsEnum.ALL_ORG_VIEW,
+        PermissionsEnum.ORG_TASK_SETTING
+    )
+    @Get('organization/:organizationId')
+    @UsePipes(new ValidationPipe())
+    async findByEmployee(
+        @Param('organizationId', UUIDValidationPipe) organizationId: IOrganizationTaskSetting['id'],
+    ): Promise<IOrganizationTaskSetting>
+    {
+        return await this.organizationTaskSettingService.findByOrganizationId(organizationId);
+    }
 
-	/**
-	 * UPDATE organization tasks setting
-	 *
-	 * @param id
-	 * @param body
-	 * @returns
-	 */
-	@ApiOperation({ summary: 'Update an existing record' })
-	@ApiResponse({
-		status: HttpStatus.CREATED,
-		description: 'The record has been successfully edited.',
-	})
-	@ApiResponse({
-		status: HttpStatus.NOT_FOUND,
-		description: 'Record not found',
-	})
-	@ApiResponse({
-		status: HttpStatus.BAD_REQUEST,
-		description:
-			'Invalid input, The response body may contain clues as to what went wrong',
-	})
-	@HttpCode(HttpStatus.OK)
-	@Permissions(PermissionsEnum.ALL_ORG_EDIT, PermissionsEnum.ORG_TASK_SETTING)
-	@Put(':id')
-	@UsePipes(new ValidationPipe())
-	async update(
-		@Param('id', UUIDValidationPipe) id: IOrganizationTaskSetting['id'],
-		@Body() body: UpdateOrganizationTaskSettingDTO
-	): Promise<IOrganizationTaskSetting> {
-		return this.commandBus.execute(
-			new OrganizationTaskSettingUpdateCommand(id, body)
-		);
-	}
+    /**
+     * CREATE organization task setting
+     *
+     * @param body
+     * @returns
+     */
+    @ApiOperation({
+        summary: 'Create new record',
+    })
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: 'The record has been successfully created.',
+    })
+    @ApiResponse({
+        status: HttpStatus.BAD_REQUEST,
+        description:
+            'Invalid input, The response body may contain clues as to what went wrong',
+    })
+    @HttpCode(HttpStatus.CREATED)
+    @Permissions(PermissionsEnum.ALL_ORG_EDIT, PermissionsEnum.ORG_TASK_SETTING)
+    @Post()
+    @UsePipes(new ValidationPipe())
+    async create(
+        @Body() body: CreateOrganizationTaskSettingDTO
+    ): Promise<IOrganizationTaskSetting>
+    {
+        return this.commandBus.execute(
+            new OrganizationTaskSettingCreateCommand(body)
+        );
+    }
+
+    /**
+     * UPDATE organization tasks setting
+     *
+     * @param id
+     * @param body
+     * @returns
+     */
+    @ApiOperation({ summary: 'Update an existing record' })
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: 'The record has been successfully edited.',
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: 'Record not found',
+    })
+    @ApiResponse({
+        status: HttpStatus.BAD_REQUEST,
+        description:
+            'Invalid input, The response body may contain clues as to what went wrong',
+    })
+    @HttpCode(HttpStatus.OK)
+    @Permissions(PermissionsEnum.ALL_ORG_EDIT, PermissionsEnum.ORG_TASK_SETTING)
+    @Put(':id')
+    @UsePipes(new ValidationPipe())
+    async update(
+        @Param('id', UUIDValidationPipe) id: IOrganizationTaskSetting['id'],
+        @Body() body: UpdateOrganizationTaskSettingDTO
+    ): Promise<IOrganizationTaskSetting>
+    {
+        return this.commandBus.execute(
+            new OrganizationTaskSettingUpdateCommand(id, body)
+        );
+    }
 }
--- packages/core/src/organization-task-setting/organization-task-setting.service.ts
+++ packages/core/src/organization-task-setting/organization-task-setting.service.ts
@@ -1,15 +1,38 @@
-import { Repository } from 'typeorm';
+import { Brackets, Repository, WhereExpressionBuilder } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { TenantAwareCrudService } from '../core/crud';
 import { OrganizationTaskSetting } from './organization-task-setting.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IOrganizationTaskSetting } from '@gauzy/contracts';
 
 @Injectable()
 export class OrganizationTaskSettingService extends TenantAwareCrudService<OrganizationTaskSetting> {
-	constructor(
-		@InjectRepository(OrganizationTaskSetting)
-		private readonly organizationTaskSettingRepository: Repository<OrganizationTaskSetting>
-	) {
-		super(organizationTaskSettingRepository);
-	}
+    constructor(
+        @InjectRepository(OrganizationTaskSetting)
+        private readonly organizationTaskSettingRepository: Repository<OrganizationTaskSetting>
+    )
+    {
+        super(organizationTaskSettingRepository);
+    }
+
+    /**
+     * Find organization task setting
+     *
+     * @param organizationId
+     * @param options
+     * @returns
+     */
+    async findByOrganizationId(
+        organizationId: IOrganizationTaskSetting['id'],
+    ): Promise<IOrganizationTaskSetting>
+    {
+        const organizationTaskSetting = await this.organizationTaskSettingRepository.findOne({
+            where: {
+                organizationId
+            }
+        });
+
+        return organizationTaskSetting;
+    }
+
 }
