30ca4346ee61c3c20762806077b0d2ba95344815
Govindram Solanki <gram01846@gmail.com>
2023-07-24 08:43:55 +0530

    Organization Task Setting UI Completed

--- apps/gauzy/src/app/@core/services/organization-task-setting.service.ts
+++ apps/gauzy/src/app/@core/services/organization-task-setting.service.ts
@@ -1,43 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {
-	IOrganizationTaskSettingCreateInput,
-	IOrganizationTaskSetting,
-	IOrganizationTaskSettingUpdateInput,
+import
+{
+    IOrganizationTaskSettingCreateInput,
+    IOrganizationTaskSetting,
+    IOrganizationTaskSettingUpdateInput,
 } from '@gauzy/contracts';
-import { firstValueFrom } from 'rxjs';
+import { Observable } from 'rxjs';
 import { API_PREFIX } from '../constants/app.constants';
 
 @Injectable({
-	providedIn: 'root',
+    providedIn: 'root',
 })
-export class OrganizationTaskSettingService {
-	private readonly API_URL = `${API_PREFIX}/organization-task-setting`;
+export class OrganizationTaskSettingService
+{
+    private readonly API_URL = `${API_PREFIX}/organization-task-setting`;
 
-	constructor(private readonly http: HttpClient) {}
+    constructor(private readonly http: HttpClient) { }
 
-	create(
-		body: IOrganizationTaskSettingCreateInput
-	): Promise<IOrganizationTaskSetting> {
-		return firstValueFrom(
-			this.http.post<IOrganizationTaskSetting>(this.API_URL, body)
-		);
-	}
+    create(
+        body: IOrganizationTaskSettingCreateInput
+    ): Observable<IOrganizationTaskSetting>
+    {
+        return this.http.post<IOrganizationTaskSetting>(this.API_URL, body)
 
-	edit(
-		body: Partial<IOrganizationTaskSettingUpdateInput>
-	): Promise<IOrganizationTaskSetting> {
-		return firstValueFrom(
-			this.http.put<IOrganizationTaskSetting>(
-				`${this.API_URL}/${body.id}`,
-				body
-			)
-		);
-	}
+    }
 
-	getById(id: IOrganizationTaskSetting['id']) {
-		return firstValueFrom(
-			this.http.get<IOrganizationTaskSetting>(`${this.API_URL}/${id}`)
-		);
-	}
+    edit(
+        body: Partial<IOrganizationTaskSettingUpdateInput>
+    ): Observable<IOrganizationTaskSetting>
+    {
+        return this.http.put<IOrganizationTaskSetting>(
+            `${this.API_URL}/${body.id}`,
+            body
+        );
+
+    }
+
+    getByOrganizationId(id: IOrganizationTaskSetting['id']): Observable<IOrganizationTaskSetting>
+    {
+        return this.http.get<IOrganizationTaskSetting>(`${this.API_URL}/organization/${id}`);
+
+    }
 }
