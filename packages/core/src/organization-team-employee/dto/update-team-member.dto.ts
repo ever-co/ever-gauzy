55b9f3a3ee79b8e892bba5de029cbdc8bfde8a32
Badal Khatri <badal.khatri0924@gmail.com>
2023-07-26 14:09:33 +0530

    Updated team member update dto

--- packages/core/src/organization-team-employee/dto/update-team-member.dto.ts
+++ packages/core/src/organization-team-employee/dto/update-team-member.dto.ts
@@ -1,12 +1,18 @@
-import { IOrganizationTeamEmployeeUpdateInput } from "@gauzy/contracts";
-import { IntersectionType, PickType } from "@nestjs/swagger";
-import { OrganizationTeamEmployee } from "./../../core/entities/internal";
-import { TenantOrganizationBaseDTO } from "./../../core/dto";
+import { IOrganizationTeamEmployeeUpdateInput } from '@gauzy/contracts';
+import { IntersectionType, PickType } from '@nestjs/swagger';
+import { OrganizationTeamEmployee } from './../../core/entities/internal';
+import { TenantOrganizationBaseDTO } from './../../core/dto';
 
 /**
  * Update team member entity DTO
  */
-export class UpdateTeamMemberDTO extends IntersectionType(TenantOrganizationBaseDTO, PickType(
-    OrganizationTeamEmployee,
-    ['isTrackingEnabled', 'organizationTeamId']
-)) implements IOrganizationTeamEmployeeUpdateInput { }
+export class UpdateTeamMemberDTO
+	extends IntersectionType(
+		TenantOrganizationBaseDTO,
+		PickType(OrganizationTeamEmployee, [
+			'isTrackingEnabled',
+			'organizationTeamId',
+			'activeTaskId',
+		])
+	)
+	implements IOrganizationTeamEmployeeUpdateInput {}
