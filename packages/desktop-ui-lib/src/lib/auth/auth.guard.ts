8a51f820e58a7a8d6046ed09c21c67af77519c3a
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-08-03 21:02:35 +0200

    fix: revert removed code.

--- packages/desktop-ui-lib/src/lib/auth/auth.guard.ts
+++ packages/desktop-ui-lib/src/lib/auth/auth.guard.ts
@@ -6,13 +6,17 @@ import {
 	RouterStateSnapshot,
 } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { AuthStrategy } from './services/auth-strategy.service';
 import { Store } from '../services';
+import { ElectronService } from '../electron/services';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 	constructor(
 		private readonly router: Router,
 		private readonly authService: AuthService,
+		private readonly electronService: ElectronService,
+		private readonly authStrategy: AuthStrategy,
 		private readonly store: Store
 	) {}
 
@@ -30,6 +34,16 @@ export class AuthGuard implements CanActivate {
 				// logged in so return true
 				return true;
 			}
+
+			// not logged in so logout from desktop timer
+			if (this.electronService.isElectron) {
+				try {
+					this.electronService.ipcRenderer.send('logout');
+				} catch (error) { }
+			}
+
+			// logout and clear local store
+			this.authStrategy.logout();
 
 			// not logged in so redirect to login page with the return url
 			this.router.navigate(['/auth/login'], {
