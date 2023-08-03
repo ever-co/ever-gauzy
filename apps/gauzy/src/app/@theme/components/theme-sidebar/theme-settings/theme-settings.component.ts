09af41bf08cb0287ee5eab4473d702d123e0714e
Govindram Solanki <gram01846@gmail.com>
2023-08-03 09:11:16 +0530

    Quick setting close on click outside

--- apps/gauzy/src/app/@theme/components/theme-sidebar/theme-settings/theme-settings.component.ts
+++ apps/gauzy/src/app/@theme/components/theme-sidebar/theme-settings/theme-settings.component.ts
@@ -1,22 +1,42 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewChecked } from '@angular/core';
 import { NbSidebarService } from '@nebular/theme';
+import { untilDestroyed } from '@ngneat/until-destroy';
+import { tap } from 'rxjs/operators';
 
 @Component({
 	selector: 'ngx-theme-settings',
 	styleUrls: ['./theme-settings.component.scss'],
 	templateUrl: './theme-settings.component.html'
 })
-export class ThemeSettingsComponent implements OnInit, OnDestroy {
+export class ThemeSettingsComponent implements OnInit, AfterViewChecked, OnDestroy {
+
+	private state: boolean;
 
 	constructor(
 		private readonly sidebarService: NbSidebarService
-	) {}
+	) { }
 
-	ngOnInit(): void {}
+	ngOnInit(): void { }
 
-	ngOnDestroy(): void {}
+	ngAfterViewChecked(): void {
+		this.sidebarService
+			.getSidebarState('settings_sidebar')
+			.pipe(
+				tap(
+					(state) =>
+						(this.state = state === 'expanded' ? true : false)
+				),
+				untilDestroyed(this)
+			)
+			.subscribe();
+	}
+
+	ngOnDestroy(): void { }
 
 	public closeSidebar() {
 		this.sidebarService.toggle(false, 'settings_sidebar');
+	}
+	public onClickOutside(event: boolean) {
+		if (!event && this.state) this.closeSidebar();
 	}
 }
