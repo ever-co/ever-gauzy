ab7c0533358845e833caad9464ed105bd63da96e
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-08-16 15:55:34 +0530

    fix: import history separate module & API routes

---
+++ packages/core/src/export-import/import-history/import-history.controller.ts
@@ -0,0 +1,41 @@
+import {
+    Controller,
+    Get,
+    HttpStatus,
+    UseGuards
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IImportHistory, IPagination, PermissionsEnum } from '@gauzy/contracts';
+import { PermissionGuard, TenantPermissionGuard } from './../../shared/guards';
+import { Permissions } from './../../shared/decorators';
+import { ImportHistoryService } from './import-history.service';
+
+@ApiTags('Import History')
+@UseGuards(TenantPermissionGuard, PermissionGuard)
+@Permissions(PermissionsEnum.IMPORT_VIEW)
+@Controller()
+export class ImportHistoryController {
+
+    constructor(
+        private readonly _importHistoryService: ImportHistoryService
+    ) { }
+
+    /**
+     *
+     * @returns
+     */
+    @ApiOperation({ summary: 'Find all imports history.' })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'Found import history'
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: 'Record not found'
+    })
+    @Permissions(PermissionsEnum.IMPORT_VIEW)
+    @Get()
+    async findAll(): Promise<IPagination<IImportHistory>> {
+        return await this._importHistoryService.findAll();
+    }
+}
