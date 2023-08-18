de526636021c9bedb6ed8775c606ef9c81aa587a
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-08-18 15:15:07 +0530

    fix: redirect navigation url

--- apps/gauzy/src/app/pages/integrations/gauzy-ai/gauzy-ai-authorize/gauzy-ai-authorize.component.ts
+++ apps/gauzy/src/app/pages/integrations/gauzy-ai/gauzy-ai-authorize/gauzy-ai-authorize.component.ts
@@ -117,7 +117,7 @@ export class GauzyAIAuthorizeComponent implements AfterViewInit, OnInit, OnDestr
 	 * Gauzy AI integration remember state API call
 	 */
 	private _redirectToGauzyAIIntegration(integrationId: string) {
-		this._router.navigate(['pages/integrations/ever-ai', integrationId]);
+		this._router.navigate(['pages/integrations/gauzy-ai', integrationId]);
 	}
 
 	/**
