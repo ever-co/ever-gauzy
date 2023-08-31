4d83cb6096b1b961a13cda53ea6b6dab633e3341
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-08-31 17:13:12 +0200

    feat: use exit confirmation dialog

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -39,7 +39,9 @@ import {
 	ServerConfig,
 	IServerConfig,
 	ILocalServer,
-	ReverseProxy
+	ReverseProxy,
+	DesktopDialog,
+	DialogStopServerExitConfirmation
 } from '@gauzy/desktop-libs';
 import {
 	createSetupWindow,
@@ -70,6 +72,7 @@ let settingsWindow: BrowserWindow;
 let splashScreen: SplashScreen;
 let tray: Tray;
 let isServerRun: boolean;
+let willQuit = false;
 
 const updater = new DesktopUpdater({
 	repository: 'ever-gauzy-server',
@@ -175,18 +178,25 @@ const { signal } = controller;
 const runServer = (isRestart) => {
 	const envVal = getEnvApi();
 	const uiPort = serverConfig.uiPort;
-
-	apiServer(
-		{
-			ui: path.join(__dirname, 'preload', 'ui-server.js'),
-			api: path.join(__dirname, 'api/main.js'),
-		},
-		envVal,
-		serverWindow,
-		uiPort,
-		isRestart,
-		signal
-	);
+	try {
+		apiServer(
+			{
+				ui: path.join(__dirname, 'preload', 'ui-server.js'),
+				api: path.join(__dirname, 'api/main.js'),
+			},
+			envVal,
+			serverWindow,
+			uiPort,
+			isRestart,
+			signal
+		);
+	} catch (error) {
+		if (error.name === 'AbortError') {
+			console.log('You exit without to stop the server');
+			return;
+		}
+		console.log('Error ', error);
+	}
 };
 
 const getEnvApi = () => {
@@ -443,6 +453,10 @@ ipcMain.on('running_state', (event, arg) => {
 	}
 	tray.setContextMenu(Menu.buildFromTemplate(trayContextMenu));
 	isServerRun = arg;
+	// Closed the server if marked.
+	if (willQuit) {
+		app.quit();
+	}
 });
 
 ipcMain.on('loading_state', (event, arg) => {
@@ -503,15 +517,30 @@ ipcMain.on('restart_and_update', () => {
 	});
 });
 
-app.on('before-quit', (e) => {
+app.on('before-quit', async (e) => {
 	e.preventDefault();
-	// Kill child processes
-	controller.abort();
-	// soft download cancellation
-	try {
-		updater.cancel();
-	} catch (e) {}
-	app.exit(0);
+	if (isServerRun) {
+		const exitConfirmationDialog = new DialogStopServerExitConfirmation(
+			new DesktopDialog(
+				'Gauzy Server',
+				TranslateService.instant('TIMER_TRACKER.DIALOG.EXIT'),
+				serverWindow
+			)
+		);
+		const button = await exitConfirmationDialog.show();
+		if (button.response === 0) {
+			// Stop the server from main
+			stopServer(false);
+			// Mark as will quit
+			willQuit = true;
+		};
+	} else {
+		// soft download cancellation
+		try {
+			updater.cancel();
+		} catch (e) { }
+		app.exit(0);
+	}
 });
 
 app.on('window-all-closed', quit);
