5a029e65b0c7b087bc3a3d7849d30d8bf7834f14
RAHUL RATHORE <rahulrathore576@gmail.com>
2023-09-05 16:46:54 +0530

    wip: signin with email for different workspace

--- apps/gauzy/src/app/auth/signin-workspaces/signin-workspaces-routing.module.ts
+++ apps/gauzy/src/app/auth/signin-workspaces/signin-workspaces-routing.module.ts
@@ -2,14 +2,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { SignInWorkspacesLayoutComponent } from './signin-workspaces.component';
 import { MultiWorkspaceOnboardingComponent } from './components/multi-workspace/multi-workspace.component';
+import { WorkspaceSigninWithEmailComponent } from './components/signin-with-email/signin-with-email.component';
 
 const routes: Routes = [
 	{
-		path: 'workspaces',
+		path: '',
 		component: SignInWorkspacesLayoutComponent,
 		children: [
 			{
-				path: '',
+				path: 'email',
+				component: WorkspaceSigninWithEmailComponent
+			},
+			{
+				path: 'workspaces',
 				component: MultiWorkspaceOnboardingComponent
 			}
 		]
