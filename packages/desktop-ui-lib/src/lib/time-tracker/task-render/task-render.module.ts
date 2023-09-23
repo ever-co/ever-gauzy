33cbdd140116adbf08f47ac7d16aed882be06d66
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-09-23 07:25:12 +0200

    feat: improve task render abstract component

--- packages/desktop-ui-lib/src/lib/time-tracker/task-render/task-render.module.ts
+++ packages/desktop-ui-lib/src/lib/time-tracker/task-render/task-render.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbProgressBarModule } from '@nebular/theme';
+import {
+	NbButtonModule,
+	NbIconModule,
+	NbProgressBarModule,
+	NbTooltipModule,
+} from '@nebular/theme';
 import { DurationFormatPipe } from '../pipes/duration-format.pipe';
 import { TaskProgressComponent } from './task-progress/task-progress.component';
 import { TaskDurationComponent } from './task-duration/task-duration.component';
 import { TaskEstimateComponent } from './task-estimate/task-estimate.component';
+import { TaskRenderCellComponent } from './task-render-cell/task-render-cell.component';
+import { DesktopDirectiveModule } from '../../directives/desktop-directive.module';
+import { TaskEstimateInputComponent } from './task-estimate/task-estimate-input/task-estimate-input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TaskDueDateComponent } from './task-due-date/task-due-date.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
 	declarations: [
@@ -12,7 +23,20 @@ import { TaskEstimateComponent } from './task-estimate/task-estimate.component';
 		TaskProgressComponent,
 		TaskDurationComponent,
 		TaskEstimateComponent,
+		TaskRenderCellComponent,
+		TaskEstimateInputComponent,
+		TaskDueDateComponent,
 	],
-	imports: [CommonModule, NbProgressBarModule],
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		NbProgressBarModule,
+		NbIconModule,
+		NbTooltipModule,
+		DesktopDirectiveModule,
+		NbButtonModule,
+		TranslateModule,
+	],
 })
-export class TaskRenderModule { }
+export class TaskRenderModule {}
