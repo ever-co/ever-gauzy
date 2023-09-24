d91e7ef31a60f603b223b809b32b629d4cc1b95b
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-09-24 19:56:22 +0200

    feat: improve tasks dialogs using dynamics status, size and priority

--- apps/gauzy/src/app/@shared/tasks/task-priority-select/task-priority-select.component.ts
+++ apps/gauzy/src/app/@shared/tasks/task-priority-select/task-priority-select.component.ts
@@ -1,12 +1,12 @@
 import {
-	Component,
-	OnInit,
-	OnDestroy,
-	Input,
-	forwardRef,
-	EventEmitter,
-	Output,
 	AfterViewInit,
+	Component,
+	EventEmitter,
+	forwardRef,
+	Input,
+	OnDestroy,
+	OnInit,
+	Output,
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
@@ -19,11 +19,15 @@ import {
 	IPagination,
 	ITaskPriority,
 	ITaskPriorityFindInput,
-	TaskPriorityEnum
+	TaskPriorityEnum,
 } from '@gauzy/contracts';
 import { distinctUntilChange, sluggable } from '@gauzy/common-angular';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { Store, TaskPrioritiesService, ToastrService } from '../../../@core/services';
+import {
+	Store,
+	TaskPrioritiesService,
+	ToastrService,
+} from '../../../@core/services';
 import { TranslationBaseComponent } from '../../language-base/translation-base.component';
 
 @UntilDestroy({ checkProperties: true })
@@ -38,86 +42,40 @@ import { TranslationBaseComponent } from '../../language-base/translation-base.c
 		},
 	],
 })
-export class TaskPrioritySelectComponent extends TranslationBaseComponent
-	implements AfterViewInit, OnInit, OnDestroy {
-
+export class TaskPrioritySelectComponent
+	extends TranslationBaseComponent
+	implements AfterViewInit, OnInit, OnDestroy
+{
 	private subject$: Subject<boolean> = new Subject();
-	public organization: IOrganization;
-	public priorities$: BehaviorSubject<ITaskPriority[]> = new BehaviorSubject([]);
-
 	/**
 	 * Default global task priorities
 	 */
-	private _priorities: Array<{ name: string; value: TaskPriorityEnum & any }> = [
+	private _priorities: Array<{
+		name: string;
+		value: TaskPriorityEnum & any;
+	}> = [
 		{
 			name: TaskPriorityEnum.URGENT,
-			value: sluggable(TaskPriorityEnum.URGENT)
+			value: sluggable(TaskPriorityEnum.URGENT),
 		},
 		{
 			name: TaskPriorityEnum.HIGH,
-			value: sluggable(TaskPriorityEnum.HIGH)
+			value: sluggable(TaskPriorityEnum.HIGH),
 		},
 		{
 			name: TaskPriorityEnum.MEDIUM,
-			value: sluggable(TaskPriorityEnum.MEDIUM)
+			value: sluggable(TaskPriorityEnum.MEDIUM),
 		},
 		{
 			name: TaskPriorityEnum.LOW,
-			value: sluggable(TaskPriorityEnum.LOW)
+			value: sluggable(TaskPriorityEnum.LOW),
 		},
 	];
-
-	/*
-	* Getter & Setter for selected organization project
-	*/
-	private _projectId: IOrganizationProject['id'];
-	get projectId(): IOrganizationProject['id'] {
-		return this._projectId;
-	}
-	@Input() set projectId(value: IOrganizationProject['id']) {
-		this._projectId = value;
-		this.subject$.next(true);
-	}
-
-	/*
-	* Getter & Setter for dynamic add tag option
-	*/
-	private _addTag: boolean = true;
-	get addTag(): boolean {
-		return this._addTag;
-	}
-	@Input() set addTag(value: boolean) {
-		this._addTag = value;
-	}
-
-	/*
-	 * Getter & Setter for dynamic placeholder
-	 */
-	private _placeholder: string;
-	get placeholder(): string {
-		return this._placeholder;
-	}
-	@Input() set placeholder(value: string) {
-		this._placeholder = value;
-	}
-
-	/*
-	 * Getter & Setter for priority
-	 */
-	private _priority: TaskPriorityEnum | string;
-	set priority(val: TaskPriorityEnum | string) {
-		this._priority = val;
-		this.onChange(val);
-		this.onTouched(val);
-	}
-	get priority(): TaskPriorityEnum | string {
-		return this._priority;
-	}
-
-	onChange: any = () => { };
-	onTouched: any = () => { };
-
-	@Output() onChanged = new EventEmitter<string>();
+	public organization: IOrganization;
+	public priorities$: BehaviorSubject<ITaskPriority[]> = new BehaviorSubject(
+		[]
+	);
+	@Output() onChanged = new EventEmitter<ITaskPriority>();
 
 	constructor(
 		public readonly translateService: TranslateService,
@@ -127,6 +85,65 @@ export class TaskPrioritySelectComponent extends TranslationBaseComponent
 	) {
 		super(translateService);
 	}
+
+	/*
+	 * Getter & Setter for selected organization project
+	 */
+	private _projectId: IOrganizationProject['id'];
+
+	get projectId(): IOrganizationProject['id'] {
+		return this._projectId;
+	}
+
+	@Input() set projectId(value: IOrganizationProject['id']) {
+		this._projectId = value;
+		this.subject$.next(true);
+	}
+
+	/*
+	 * Getter & Setter for dynamic add tag option
+	 */
+	private _addTag: boolean = true;
+
+	get addTag(): boolean {
+		return this._addTag;
+	}
+
+	@Input() set addTag(value: boolean) {
+		this._addTag = value;
+	}
+
+	/*
+	 * Getter & Setter for dynamic placeholder
+	 */
+	private _placeholder: string;
+
+	get placeholder(): string {
+		return this._placeholder;
+	}
+
+	@Input() set placeholder(value: string) {
+		this._placeholder = value;
+	}
+
+	/*
+	 * Getter & Setter for priority
+	 */
+	private _priority: ITaskPriority;
+
+	get priority(): ITaskPriority {
+		return this._priority;
+	}
+
+	set priority(val: ITaskPriority) {
+		this._priority = val;
+		this.onChange(val);
+		this.onTouched(val);
+	}
+
+	onChange: any = () => {};
+
+	onTouched: any = () => {};
 
 	ngOnInit(): void {
 		this.subject$
@@ -155,8 +172,8 @@ export class TaskPrioritySelectComponent extends TranslationBaseComponent
 			.subscribe();
 	}
 
-	writeValue(value: TaskPriorityEnum) {
-		this._priority = value;
+	writeValue(value: ITaskPriority) {
+		this.priority = value;
 	}
 
 	registerOnChange(fn: (rating: number) => void): void {
@@ -167,8 +184,8 @@ export class TaskPrioritySelectComponent extends TranslationBaseComponent
 		this.onTouched = fn;
 	}
 
-	selectPriority(event: { label: string; value: TaskPriorityEnum }) {
-		this.onChanged.emit(event ? event.value : null);
+	selectPriority(priority: ITaskPriority) {
+		this.onChanged.emit(priority);
 	}
 
 	/**
@@ -182,20 +199,26 @@ export class TaskPrioritySelectComponent extends TranslationBaseComponent
 		const { tenantId } = this.store.user;
 		const { id: organizationId } = this.organization;
 
-		this.taskPrioritiesService.get<ITaskPriorityFindInput>({
-			tenantId,
-			organizationId,
-			...(this.projectId
-				? {
-					projectId: this.projectId
-				}
-				: {}),
-		}).pipe(
-			map(({ items, total }: IPagination<ITaskPriority>) => total > 0 ? items : this._priorities),
-			tap((priorities: ITaskPriority[]) => this.priorities$.next(priorities)),
-			untilDestroyed(this)
-		)
-		.subscribe();
+		this.taskPrioritiesService
+			.get<ITaskPriorityFindInput>({
+				tenantId,
+				organizationId,
+				...(this.projectId
+					? {
+							projectId: this.projectId,
+					  }
+					: {}),
+			})
+			.pipe(
+				map(({ items, total }: IPagination<ITaskPriority>) =>
+					total > 0 ? items : this._priorities
+				),
+				tap((priorities: ITaskPriority[]) =>
+					this.priorities$.next(priorities)
+				),
+				untilDestroyed(this)
+			)
+			.subscribe();
 	}
 
 	/**
@@ -218,13 +241,13 @@ export class TaskPrioritySelectComponent extends TranslationBaseComponent
 				name,
 				...(this.projectId
 					? {
-						projectId: this.projectId
-					}
+							projectId: this.projectId,
+					  }
 					: {}),
 			});
 			const priority: ITaskPriority = await firstValueFrom(source);
-			if (priority.value) {
-				this.priority = priority.value;
+			if (priority) {
+				this.priority = priority;
 			}
 		} catch (error) {
 			this.toastrService.error(error);
@@ -233,5 +256,5 @@ export class TaskPrioritySelectComponent extends TranslationBaseComponent
 		}
 	};
 
-	ngOnDestroy(): void { }
+	ngOnDestroy(): void {}
 }
