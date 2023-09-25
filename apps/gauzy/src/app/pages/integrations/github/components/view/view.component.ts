52048df80ec4baea5f386c2d71c6d06565b41d05
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2023-09-25 13:19:52 +0530

    fix: #6734 display tables for issues of specific repository

--- apps/gauzy/src/app/pages/integrations/github/components/view/view.component.ts
+++ apps/gauzy/src/app/pages/integrations/github/components/view/view.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { debounceTime, of } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { catchError, filter, finalize, map, switchMap, tap } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { IGithubRepository, IIntegrationSetting, IIntegrationTenant, IOrganization } from '@gauzy/contracts';
 import { distinctUntilChange } from '@gauzy/common-angular';
@@ -12,27 +13,39 @@ import {
 	IntegrationsService,
 	Store
 } from './../../../../../@core/services';
+import { TranslationBaseComponent } from 'apps/gauzy/src/app/@shared/language-base';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
 	templateUrl: './view.component.html'
 })
-export class GithubViewComponent implements OnInit {
+export class GithubViewComponent extends TranslationBaseComponent implements AfterViewInit, OnInit {
 
+	public settingsSmartTable: object;
 	public organization: IOrganization;
 	public loading: boolean = true;
 	public repositories: IGithubRepository[] = [];
 	public repositories$: Observable<IGithubRepository[]>;
+	public issues$: Observable<any[]>;
+	public issues: any[] = [];
 
 	constructor(
+		public readonly _translateService: TranslateService,
 		private readonly _activatedRoute: ActivatedRoute,
 		private readonly _errorHandlingService: ErrorHandlingService,
 		private readonly _store: Store,
 		private readonly _integrationsService: IntegrationsService,
 		private readonly _githubService: GithubService,
-	) { }
+	) {
+		super(_translateService);
+	}
 
-	ngOnInit() {
+	ngOnInit(): void {
+		this._loadSmartTableSettings();
+		this._applyTranslationOnSmartTable();
+	}
+
+	ngAfterViewInit(): void {
 		this._store.selectedOrganization$
 			.pipe(
 				debounceTime(200),
@@ -83,5 +96,43 @@ export class GithubViewComponent implements OnInit {
 			}),
 			finalize(() => (this.loading = false)),
 		);
+	}
+
+	/**
+	 *
+	 */
+	private _applyTranslationOnSmartTable() {
+		this.translateService.onLangChange
+			.pipe(
+				tap(() => this._loadSmartTableSettings()),
+				untilDestroyed(this)
+			)
+			.subscribe();
+	}
+
+	/**
+	 *
+	 */
+	private _loadSmartTableSettings() {
+		this.settingsSmartTable = {
+			selectedRowIndex: -1,
+			selectMode: 'multi',
+			actions: {
+				add: false,
+				edit: false,
+				delete: false,
+				select: true
+			},
+			columns: {
+				name: {
+					title: this.getTranslation('SM_TABLE.NAME'),
+					type: 'string'
+				},
+				description: {
+					title: this.getTranslation('SM_TABLE.DESCRIPTION'),
+					type: 'string'
+				}
+			}
+		};
 	}
 }
