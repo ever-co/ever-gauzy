f282244e8f2b9a7d20472c5589cc298ca47cdf8b
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2023-10-06 13:14:12 +0530

    fix: #6924 revert provider injection

--- packages/core/src/jitsu-analytics/jitsu-analytics.module.ts
+++ packages/core/src/jitsu-analytics/jitsu-analytics.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JitsuAnalyticsService } from './jitsu-analytics.service';
+import { JitsuEventsSubscriber } from './jitsu-events-subscriber';
 
 @Module({
-	providers: [JitsuAnalyticsService],
+	providers: [JitsuAnalyticsService, JitsuEventsSubscriber],
 })
 export class JitsuAnalyticsModule { }
