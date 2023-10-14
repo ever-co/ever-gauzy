cf8d5a6d29025b62b2d8d3b6b513a574b5879f35
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2023-10-14 17:50:17 +0530

    fix: #6823 use project table for settings not integration map table

--- apps/gauzy/src/app/@core/services/integration/integration-map.service.ts
+++ apps/gauzy/src/app/@core/services/integration/integration-map.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/internal/Observable';
-import { IIntegrationMap, IIntegrationMapSyncRepository, IIntegrationSyncedRepositoryFindInput } from '@gauzy/contracts';
-import { toParams } from '@gauzy/common-angular';
-import { API_PREFIX } from '../../constants';
 
 @Injectable({
     providedIn: 'root'
@@ -13,27 +9,4 @@ export class IntegrationMapService {
     constructor(
         private readonly _http: HttpClient
     ) { }
-
-    /**
-     * Synchronize a GitHub repository.
-     * @param input The synchronization input data.
-     * @returns An Observable of the synchronized IntegrationMap.
-     */
-    syncGithubRepository(input: IIntegrationMapSyncRepository): Observable<IIntegrationMap> {
-        const url = `${API_PREFIX}/integration-map/github/repository-sync`;
-        return this._http.post<IIntegrationMap>(url, input);
-    }
-
-    /**
-     * Get synced GitHub repositories based on input parameters
-     *
-     * @param input - Input parameters for the query
-     * @returns An Observable of type IIntegrationMap
-     */
-    getSyncedGithubRepository(input: IIntegrationSyncedRepositoryFindInput): Observable<IIntegrationMap> {
-        const url = `${API_PREFIX}/integration-map/github/repository-sync`;
-        return this._http.get<IIntegrationMap>(url, {
-            params: toParams(input)
-        });
-    }
 }
