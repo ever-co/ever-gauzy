e7a6641a0f63e1d9b9357a8f09b317854f5c9259
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-10-19 04:33:22 +0200

    feat: add injectionToken for gauzy environment and create an injector to use outside class

--- packages/desktop-ui-lib/src/lib/constants/app.constants.ts
+++ packages/desktop-ui-lib/src/lib/constants/app.constants.ts
@@ -1,3 +1,14 @@
+import {InjectionToken, Injector} from '@angular/core';
+import { environment } from '@env/environment';
+
 export const API_PREFIX = '/api';
 export const BACKGROUND_SYNC_INTERVAL = 25000; // milliseconds
 export const BACKGROUND_SYNC_OFFLINE_INTERVAL = 5000; // milliseconds
+export const GAUZY_ENV = new InjectionToken<any>(
+	'gauzyEnvironment'
+);
+export const injector = Injector.create({
+	providers: [
+		{ provide: GAUZY_ENV, useValue: environment }
+	]
+});
