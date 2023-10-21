a9ec79bb6878fedd2c4c78d6460d499b704d6354
Ruslan Konviser <evereq@gmail.com>
2023-10-21 16:44:05 +0200

    chore: bump script update

--- .scripts/bump-version-electron.js
+++ .scripts/bump-version-electron.js
@@ -124,82 +124,91 @@ module.exports.desktopTimer = (isProd) => {
 		let package = require('../apps/desktop-timer/src/package.json');
 		let currentVersion = package.version;
 
-		exec(
+		const repoURL = process.env.PROJECT_REPO;
+		console.log('repoURL', repoURL);
+
+		const timerAppName = process.env.DESKTOP_TIMER_APP_NAME;
+		console.log('timerAppName', timerAppName);
+
+		const command =
 			'git ls-remote --tags ' +
-				process.env.PROJECT_REPO +
-				" | grep -v '^{}' | awk -F'/' '/refs/tags// {print $3}' | grep -E '^v?[0-9]+.[0-9]+.[0-9]+' | sort -V | tail -n1",
-			(error, stdout) => {
-				if (error) {
-					console.error(`exec error: ${error}`);
-					return;
-				}
+			repoURL +
+			" | grep -v '\\^{}' | awk -F'/' '/refs/tags\\// {print $3}' | grep -E '^v?[0-9]+\\.[0-9]+\\.[0-9]+' | sort -V | tail -n1";
 
-				let newVersion = stdout.trim();
-				console.log('latest tag', newVersion);
-				if (newVersion) {
-					// let's remove "v" from version, i.e. first character
-					newVersion = newVersion.substring(1);
-					package.version = newVersion;
+		console.log('command', command);
 
-					package.name = process.env.DESKTOP_TIMER_APP_NAME;
-					package.productName =
-						process.env.DESKTOP_TIMER_APP_DESCRIPTION;
-					package.description =
-						process.env.DESKTOP_TIMER_APP_DESCRIPTION;
-					package.homepage = process.env.COMPANY_SITE_LINK;
-
-					package.build.appId = process.env.DESKTOP_TIMER_APP_ID;
-					package.build.productName =
-						process.env.DESKTOP_TIMER_APP_DESCRIPTION;
-					package.build.linux.executableName =
-						process.env.DESKTOP_TIMER_APP_NAME;
-
-					if (!isProd) {
-						package.build.publish = [
-							{
-								provider: 'github',
-								repo: process.env.DESKTOP_TIMER_APP_NAME,
-								releaseType: 'prerelease',
-							},
-							{
-								provider: 'spaces',
-								name: 'ever',
-								region: 'sfo3',
-								path: `/${process.env.DESKTOP_TIMER_APP_NAME}-pre`,
-								acl: 'public-read',
-							},
-						];
-					} else {
-						package.build.publish = [
-							{
-								provider: 'github',
-								repo: process.env.DESKTOP_TIMER_APP_NAME,
-								releaseType: 'release',
-							},
-							{
-								provider: 'spaces',
-								name: 'ever',
-								region: 'sfo3',
-								path: `/${process.env.DESKTOP_TIMER_APP_NAME}`,
-								acl: 'public-read',
-							},
-						];
-					}
-
-					fs.writeFileSync(
-						'./apps/desktop-timer/src/package.json',
-						JSON.stringify(package, null, 2)
-					);
-
-					let updated = require('../apps/desktop-timer/src/package.json');
-					console.log('Version updated to version', updated.version);
-				} else {
-					console.log(
-						'Latest tag is not found. build desktop-timer app with default version',
-						currentVersion
-					);
-				}
+		exec(command, (error, stdout) => {
+			if (error) {
+				console.error(`exec error: ${error}`);
+				return;
 			}
-		);
+
+			let newVersion = stdout.trim();
+			console.log('latest tag', newVersion);
+
+			if (newVersion) {
+				// let's remove "v" from version, i.e. first character
+				newVersion = newVersion.substring(1);
+				package.version = newVersion;
+
+				console.log('Version updated to version', newVersion);
+			} else {
+				console.log(
+					'Latest tag is not found. build desktop-timer app with default version',
+					currentVersion
+				);
+			}
+
+			package.name = timerAppName;
+			package.productName = process.env.DESKTOP_TIMER_APP_DESCRIPTION;
+			package.description = process.env.DESKTOP_TIMER_APP_DESCRIPTION;
+			package.homepage = process.env.COMPANY_SITE_LINK;
+
+			package.build.appId = process.env.DESKTOP_TIMER_APP_ID;
+			package.build.productName =
+				process.env.DESKTOP_TIMER_APP_DESCRIPTION;
+			package.build.linux.executableName = timerAppName;
+
+			if (!isProd) {
+				package.build.publish = [
+					{
+						provider: 'github',
+						repo: timerAppName,
+						releaseType: 'prerelease',
+					},
+					{
+						provider: 'spaces',
+						name: 'ever',
+						region: 'sfo3',
+						path: `/${timerAppName}-pre`,
+						acl: 'public-read',
+					},
+				];
+			} else {
+				package.build.publish = [
+					{
+						provider: 'github',
+						repo: timerAppName,
+						releaseType: 'release',
+					},
+					{
+						provider: 'spaces',
+						name: 'ever',
+						region: 'sfo3',
+						path: `/${timerAppName}`,
+						acl: 'public-read',
+					},
+				];
+			}
+
+			fs.writeFileSync(
+				'./apps/desktop-timer/src/package.json',
+				JSON.stringify(package, null, 2)
+			);
+
+			let updated = require('../apps/desktop-timer/src/package.json');
+
+			console.log('Version releasing', updated.version);
+		});
 	}
 };
