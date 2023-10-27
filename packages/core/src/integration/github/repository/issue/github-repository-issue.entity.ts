a107b9ef8ee3446f5304f6534f862cc0f5a7ae8c
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2023-10-27 17:32:42 +0530

    fix: #7000 removed integration map for github issue map table

--- packages/core/src/integration/github/repository/issue/github-repository-issue.entity.ts
+++ packages/core/src/integration/github/repository/issue/github-repository-issue.entity.ts
@@ -1,8 +1,8 @@
 import { Column, Entity, Index, JoinColumn, ManyToOne, RelationId, } from 'typeorm';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
-import { IIntegrationMap, IOrganizationGithubRepository, IOrganizationGithubRepositoryIssue } from '@gauzy/contracts';
-import { IntegrationMap, TenantOrganizationBaseEntity } from 'core/entities/internal';
+import { IOrganizationGithubRepository, IOrganizationGithubRepositoryIssue } from '@gauzy/contracts';
+import { TenantOrganizationBaseEntity } from 'core/entities/internal';
 import { OrganizationGithubRepository } from './../github-repository.entity';
 
 @Entity('organization_github_repository_issue')
@@ -48,25 +48,4 @@ export class OrganizationGithubRepositoryIssue extends TenantOrganizationBaseEnt
     @Index()
     @Column({ nullable: true })
     repositoryId?: IOrganizationGithubRepository['id'];
-
-    /**
-     * Integration Map
-     */
-    @ManyToOne(() => IntegrationMap, {
-        /** Indicates if relation column value can be nullable or not. */
-        nullable: true,
-
-        /** Database cascade action on delete. */
-        onDelete: 'SET NULL'
-    })
-    @JoinColumn()
-    integrationMap?: IOrganizationGithubRepository;
-
-    @ApiPropertyOptional({ type: () => String })
-    @IsOptional()
-    @IsUUID()
-    @RelationId((it: OrganizationGithubRepositoryIssue) => it.integrationMap)
-    @Index()
-    @Column({ nullable: true })
-    integrationMapId?: IIntegrationMap['id'];
 }
