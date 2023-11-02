917b7c0c2055fef82fccf2c7a811d9160de0532b
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2023-11-02 12:08:14 +0530

    fix: #7029 if email find in route hide form field

--- apps/gauzy/src/app/auth/register/register.component.ts
+++ apps/gauzy/src/app/auth/register/register.component.ts
@@ -1,39 +1,45 @@
 import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { filter, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
 import { NB_AUTH_OPTIONS, NbAuthOptions, NbAuthService, NbRegisterComponent } from '@nebular/auth';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { patterns } from '../../@shared/regex/regex-patterns.const';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
 	selector: 'ngx-register',
 	templateUrl: './register.component.html',
 	styleUrls: ['./register.component.scss']
 })
 export class NgxRegisterComponent extends NbRegisterComponent implements OnInit {
-	showPassword: boolean = false;
-	showConfirmPassword: boolean = false;
-	passwordNoSpaceEdges = patterns.passwordNoSpaceEdges;
-	emailPopulated = false; // Initialize as false
+
+	public showPassword: boolean = false;
+	public showConfirmPassword: boolean = false;
+	public passwordNoSpaceEdges = patterns.passwordNoSpaceEdges;
+	public queryParams$: Observable<Params>; // Observable for the query params
 
 	constructor(
-		protected nbAuthService: NbAuthService,
-		protected cdr: ChangeDetectorRef,
-		protected router: Router,
-		protected route: ActivatedRoute,
-		@Inject(NB_AUTH_OPTIONS)
-		options: NbAuthOptions
+		protected readonly nbAuthService: NbAuthService,
+		protected readonly cdr: ChangeDetectorRef,
+		protected readonly router: Router,
+		protected readonly activatedRoute: ActivatedRoute,
+		@Inject(NB_AUTH_OPTIONS) options: NbAuthOptions
 	) {
 		super(nbAuthService, options, cdr, router);
 	}
 
 	ngOnInit() {
-		this.route.queryParams.subscribe(({ email }) => {
-			if (email) {
-				// Populate the email field based on query parameters
-				this.user.email = email;
-				this.emailPopulated = true;
-				// Detect changes
-				this.cdr.detectChanges();
-			}
-		});
+		// Create an observable to listen to query parameter changes in the current route.
+		this.queryParams$ = this.activatedRoute.queryParams.pipe(
+			// Filter and ensure that query parameters are present.
+			filter((params: Params) => !!params),
+
+			// Tap into the observable to update the 'user.email' property with the 'email' query parameter.
+			tap(({ email }: Params) => this.user.email = email),
+
+			// Use 'untilDestroyed' to handle component lifecycle and avoid memory leaks.
+			untilDestroyed(this)
+		);
 	}
 }
