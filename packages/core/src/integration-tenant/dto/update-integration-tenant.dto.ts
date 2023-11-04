bf62f5146dcac738d82755864d59d7c140b8abc0
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2023-11-04 17:43:59 +0530

    fix: remove unnecessary comment

--- packages/core/src/integration-tenant/dto/update-integration-tenant.dto.ts
+++ packages/core/src/integration-tenant/dto/update-integration-tenant.dto.ts
@@ -4,9 +4,7 @@ import { TenantOrganizationBaseDTO } from "core/dto";
 import { IntegrationTenant } from '../integration-tenant.entity';
 
 /**
- * A TypeScript class that appears to represent a DTO (Data Transfer Object) for updating an integration tenant.
- * It combines properties from different types using IntersectionType.
- * Implements the Partial<IIntegrationTenantUpdateInput> interface, indicating that not all properties are required.
+ * Represent a DTO (Data Transfer Object) for updating an integration tenant.
  */
 export class UpdateIntegrationTenantDTO extends IntersectionType(
     TenantOrganizationBaseDTO, // Extends properties from the 'TenantOrganizationBaseDTO' type.
