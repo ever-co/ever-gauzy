a5beb54d993a25d751d8a07477bb08e4a325c105
coderNadir <78637183+coderNadir@users.noreply.github.com>
2023-11-15 21:34:18 +0100

    Add Passwordless (login-magic) page (#7116)
    * add login-magic page, refactor code (WhatsNew, SocialLinks), translate properties used in login-magic
    
    * fix SocialLinks alignments, add an extra email input validator (LoginMagic component), add a link on (Login component) to redirect to (LoginMagic component) and translated the text, removed unused import
    
    * remove demo credentials
    
    ---------
    
    Co-authored-by: Rahul R <rahulrathore576@gmail.com>

---
+++ apps/gauzy/src/app/auth/@shared/social-links/social-links.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SocialLinksComponent } from './social-links.component';
+
+describe('SocialLinksComponent', () => {
+  let component: SocialLinksComponent;
+  let fixture: ComponentFixture<SocialLinksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SocialLinksComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SocialLinksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- apps/gauzy/src/app/auth/auth-routing.module.ts
+++ apps/gauzy/src/app/auth/auth-routing.module.ts
@@ -12,6 +12,7 @@ import { EstimateEmailComponent, EstimateEmailResolver } from './estimate-email'
 import { NgxResetPasswordComponent } from "./reset-password/reset-password.component";
 import { ConfirmEmailComponent } from './confirm-email';
 import { ConfirmEmailResolver } from './confirm-email/confirm-email.resolver';
+import { NgxLoginMagicComponent } from './login-magic/login-magic.component';
 
 export const routes: Routes = [
 	{
@@ -42,6 +43,11 @@ export const routes: Routes = [
 			{
 				path: 'logout',
 				component: NbLogoutComponent
+			},
+			{
+				path: 'login-magic',
+				component: NgxLoginMagicComponent,
+				canActivate: [NoAuthGuard]
 			},
 			{
 				path: 'request-password',
@@ -77,10 +83,10 @@ export const routes: Routes = [
 				canActivate: [NoAuthGuard],
 				resolve: {
 					estimate: EstimateEmailResolver
-				},
+				}
 			}
-		],
-	},
+		]
+	}
 ];
 
 @NgModule({
