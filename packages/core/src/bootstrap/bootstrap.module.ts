78d58e3b3ea4a07b7e12931dfdc0ebf57b27ce29
Ruslan Konviser <evereq@gmail.com>
2023-11-28 23:05:57 +0100

    fix: typo in SigNoz

--- packages/core/src/bootstrap/bootstrap.module.ts
+++ packages/core/src/bootstrap/bootstrap.module.ts
@@ -31,7 +31,7 @@ export class BootstrapModule implements NestModule, OnApplicationShutdown {
 			if (signal === 'SIGTERM') {
 				Logger.log('SIGTERM shutting down. Please wait...');
 
-				tracer.sdk
+				tracer
 					.shutdown()
 					.then(() => console.log('Tracing terminated'))
 					.catch((error) => console.log('Error terminating tracing', error));
