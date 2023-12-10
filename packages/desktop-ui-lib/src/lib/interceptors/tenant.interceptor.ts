f6856fe1bb3cc2885e5b73b9fb11d44ead3e509c
Kifungo A <45813955+adkif@users.noreply.github.com>
2023-12-10 16:20:55 +0200

    Revert "feat: #7246 add organizationId to header request"
    This reverts commit 3456bc22c957bab7fbb2e60c7858c6cbe8551a4b.

--- packages/desktop-ui-lib/src/lib/interceptors/tenant.interceptor.ts
+++ packages/desktop-ui-lib/src/lib/interceptors/tenant.interceptor.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import {
+	HttpRequest,
+	HttpHandler,
+	HttpEvent,
+	HttpInterceptor,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Store } from '../services';
 
 @Injectable()
 export class TenantInterceptor implements HttpInterceptor {
-	constructor(private _store: Store) {}
+	constructor(private _store: Store) { }
 
-	intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+	intercept(
+		request: HttpRequest<unknown>,
+		next: HttpHandler
+	): Observable<HttpEvent<unknown>> {
 		const tenantId = this._store.tenantId;
-		const organizationId = this._store.organizationId;
-
-		if (tenantId && organizationId) {
+		if (tenantId) {
 			request = request.clone({
 				setHeaders: {
 					'Tenant-Id': `${tenantId}`,
-					'Organization-ID': `${organizationId}`
-				}
+				},
 			});
 		}
 		return next.handle(request);
