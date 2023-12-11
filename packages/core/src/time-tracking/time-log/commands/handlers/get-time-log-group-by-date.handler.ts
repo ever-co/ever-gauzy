2ef8d22eb1c7f9381a91d4f84acb7e1b655a797a
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2023-12-11 14:38:33 +0530

    fix: #7245 group by project for "Time and Activity" report

--- packages/core/src/time-tracking/time-log/commands/handlers/get-time-log-group-by-date.handler.ts
+++ packages/core/src/time-tracking/time-log/commands/handlers/get-time-log-group-by-date.handler.ts
@@ -1,16 +1,18 @@
 import { ICommandHandler, CommandHandler } from '@nestjs/cqrs';
-import { chain, pluck, reduce } from 'underscore';
+import { chain, pluck } from 'underscore';
 import * as moment from 'moment';
-import { ArraySum } from '@gauzy/common';
-import { IReportDayGroupByDate, ITimeLog, ITimeSlot } from '@gauzy/contracts';
+import { IReportDayGroupByDate, ITimeLog } from '@gauzy/contracts';
 import { GetTimeLogGroupByDateCommand } from '../get-time-log-group-by-date.command';
+import { calculateAverage, calculateAverageActivity } from './../../time-log.utils';
 
 @CommandHandler(GetTimeLogGroupByDateCommand)
-export class GetTimeLogGroupByDateHandler
-	implements ICommandHandler<GetTimeLogGroupByDateCommand>
-{
-	constructor() { }
+export class GetTimeLogGroupByDateHandler implements ICommandHandler<GetTimeLogGroupByDateCommand> {
 
+	/**
+	 * Executes the command to generate a time log report grouped by date.
+	 * @param command The command containing time logs and other parameters.
+	 * @returns A Promise that resolves to the generated report grouped by date.
+	 */
 	public async execute(
 		command: GetTimeLogGroupByDateCommand
 	): Promise<IReportDayGroupByDate> {
@@ -19,121 +21,53 @@ export class GetTimeLogGroupByDateHandler
 		const dailyLogs: any = chain(timeLogs)
 			.groupBy((log) => moment(log.startedAt).format('YYYY-MM-DD'))
 			.map((byDateLogs: ITimeLog[], date: string) => {
-				/**
-				 * calculate average duration for specific date range.
-				 */
-				const avgDuration = reduce(
-					pluck(byDateLogs, 'duration'),
-					ArraySum,
-					0
-				);
-				/**
-				 * calculate average activity for specific date range.
-				 */
-				const slots: ITimeSlot[] = chain(byDateLogs)
-					.pluck('timeSlots')
-					.flatten(true)
-					.value();
-				const avgActivity =
-					(reduce(pluck(slots, 'overall'), ArraySum, 0) * 100) /
-					reduce(pluck(slots, 'duration'), ArraySum, 0) || 0;
+				// Calculate average duration for specific date range.
+				const avgDuration = calculateAverage(pluck(byDateLogs, 'duration'));
 
-				const byProject = chain(byDateLogs)
-					.groupBy('projectId')
-					.map((byProjectLogs: ITimeLog[]) => {
-						const project =
-							byProjectLogs.length > 0
-								? byProjectLogs[0].project
-								: null;
+				// Calculate average activity for specific date range.
+				const avgActivity = calculateAverageActivity(chain(byDateLogs).pluck('timeSlots').flatten(true).value());
 
-						const client =
-							byProjectLogs.length > 0
-								? byProjectLogs[0].organizationContact
-								: project
-									? project.organizationContact
-									: null;
+				const byProject = chain(byDateLogs).groupBy('projectId').map((byProjectLogs: ITimeLog[]) => {
+					//
+					const project = byProjectLogs.length > 0 ? byProjectLogs[0].project : null;
+					const client = byProjectLogs.length > 0 ? byProjectLogs[0].organizationContact : project ? project.organizationContact : null;
 
-						const byEmployee = chain(byProjectLogs)
-							.groupBy('employeeId')
-							.map((byEmployeeLogs: ITimeLog[]) => {
-								/**
-								 * calculate average duration of the employee for specific date range.
-								 */
-								const sum = reduce(
-									pluck(byEmployeeLogs, 'duration'),
-									ArraySum,
-									0
-								);
-
-								/**
-								 * calculate average activity of the employee for specific date range.
-								 */
-								const slots: ITimeSlot[] = chain(byEmployeeLogs)
-									.pluck('timeSlots')
-									.flatten(true)
-									.value();
-
-								/**
-								 * Calculate Average activity of the employee
-								 */
-								const avgActivity =
-									(reduce(
-										pluck(slots, 'overall'),
-										ArraySum,
-										0
-									) *
-										100) /
-									reduce(
-										pluck(slots, 'duration'),
-										ArraySum,
-										0
-									) || 0;
-
-								const employee =
-									byEmployeeLogs.length > 0
-										? byEmployeeLogs[0].employee
-										: null;
-
-								const task =
-									byEmployeeLogs.length > 0
-										? byEmployeeLogs[0].task
-										: null;
-
-								const description = byEmployeeLogs.length > 0
-									? byEmployeeLogs[0].description
-									: null;
-
-								return {
-									description,
-									employee,
-									sum: sum,
-									task,
-									activity: parseFloat(
-										parseFloat(avgActivity + '').toFixed(2)
-									)
-								};
-							})
-							.value();
+					const byEmployee = chain(byProjectLogs).groupBy('employeeId').map((byEmployeeLogs: ITimeLog[]) => {
+						// Calculate average duration of the employee for specific date range.
+						const sum = calculateAverage(pluck(byEmployeeLogs, 'duration'));
+						// Calculate Average activity of the employee
+						const avgActivity = calculateAverageActivity(chain(byEmployeeLogs).pluck('timeSlots').flatten(true).value());
+						//
+						const employee = byEmployeeLogs.length > 0 ? byEmployeeLogs[0].employee : null;
+						//
+						const task = byEmployeeLogs.length > 0 ? byEmployeeLogs[0].task : null;
+						//
+						const description = byEmployeeLogs.length > 0 ? byEmployeeLogs[0].description : null;
 
 						return {
-							project,
-							client,
-							employeeLogs: byEmployee
+							description,
+							employee,
+							sum,
+							task,
+							activity: parseFloat(parseFloat(avgActivity + '').toFixed(2))
 						};
-					})
-					.value();
+					}).value();
+
+					return {
+						project,
+						client,
+						employeeLogs: byEmployee
+					};
+				}).value();
 
 				return {
 					date,
 					logs: byProject,
 					sum: avgDuration || null,
-					activity: parseFloat(
-						parseFloat(avgActivity + '').toFixed(2)
-					)
+					activity: parseFloat(parseFloat(avgActivity + '').toFixed(2))
 				};
-			})
-			.value();
+			});
 
-		return dailyLogs;
+		return dailyLogs.value();
 	}
 }
