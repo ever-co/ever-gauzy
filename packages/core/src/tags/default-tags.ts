c18f6fd66d6995b0361747eabc6b32813bd714cd
maceteligolden <maceteligolden@gmail.com>
2023-12-13 07:45:49 +0100

    update:add placeholder textcolor for default-tags

--- packages/core/src/tags/default-tags.ts
+++ packages/core/src/tags/default-tags.ts
@@ -39,6 +39,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.MOBILE,
 		icon: 'task-labels/mobile.svg',
 		color: '#4E4AE8',
+		textColor: '',
 		description: null,
 		isSystem: true
 	},
@@ -46,6 +47,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.FRONTEND,
 		icon: 'task-labels/frontend.svg',
 		color: '#41AB6B',
+		textColor: '',
 		description: null,
 		isSystem: true
 	},
@@ -53,6 +55,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.BACKEND,
 		icon: 'task-labels/backend.svg',
 		color: '#E84A5D',
+		textColor: '',
 		description: null,
 		isSystem: true
 	},
@@ -60,6 +63,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.WEB,
 		icon: 'task-labels/web.svg',
 		color: '#4192AB',
+		textColor: '',
 		description: null,
 		isSystem: true
 	},
@@ -67,6 +71,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.UI_UX,
 		icon: 'task-labels/ui-ux.svg',
 		color: '#9641AB',
+		textColor: '',
 		description: null,
 		isSystem: true
 	},
@@ -74,6 +79,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.FULL_STACK,
 		icon: 'task-labels/fullstack.svg',
 		color: '#AB9A41',
+		textColor: '',
 		description: null,
 		isSystem: true
 	},
@@ -81,6 +87,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.TABLET,
 		icon: 'task-labels/tablet.svg',
 		color: '#5CAB41',
+		textColor: '',
 		description: null,
 		isSystem: true
 	},
@@ -88,6 +95,7 @@ export const DEFAULT_TAGS: ITag[] = [
 		name: TagEnum.BUG,
 		icon: 'task-labels/bug.svg',
 		color: '#E78F5E',
+		textColor: '',
 		description: null,
 		isSystem: true
 	}
