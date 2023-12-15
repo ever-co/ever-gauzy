ad9d4f22a8019f801ae7ed7bfe5830e1e9eb9394
Ruslan Konviser <evereq@gmail.com>
2023-12-15 14:31:17 +0100

    chore: log request IP address on development envs

--- packages/core/src/throttler/throttler-behind-proxy.guard.ts
+++ packages/core/src/throttler/throttler-behind-proxy.guard.ts
@@ -1,10 +1,15 @@
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { Injectable } from '@nestjs/common';
+import { environment } from '@gauzy/config';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
 	protected getTracker(req: Record<string, any>): string {
 		const tracker = req.ips.length > 0 ? req.ips[0] : req.ip;
+		if (!environment.production) {
+			console.log(`Request IP: ${tracker}`);
+		}
+
 		return tracker;
 	}
 }
