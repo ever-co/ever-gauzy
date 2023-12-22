455652bb6a48d54fe1f295342f2e32dd1bd63feb
Kifungo A <45813955+adkif@users.noreply.github.com>
2023-12-22 22:20:11 +0000

    refactor: improve code robusteness

--- packages/desktop-libs/src/lib/server/reverse-ui-proxy.ts
+++ packages/desktop-libs/src/lib/server/reverse-ui-proxy.ts
@@ -1,7 +1,7 @@
 import { IServerConfig } from '../interfaces';
 import * as fs from 'fs';
 import { BaseReverseProxy } from '../decorators';
-import * as https from 'https';
+import { createServer } from 'https';
 import { ISsl } from '@gauzy/contracts';
 
 export class ReverseUiProxy extends BaseReverseProxy {
@@ -28,21 +28,41 @@ export class ReverseUiProxy extends BaseReverseProxy {
 	}
 
 	protected preprocess(): void {
-		const ssl: ISsl = {
-			key: fs.readFileSync(this._config.setting.secureProxy.ssl.key, 'utf8'),
-			cert: fs.readFileSync(this._config.setting.secureProxy.ssl.cert, 'utf8')
-		};
+		const sslConfig: ISsl = this.readSslConfig();
 		this.kill();
+		this.configureProxy(sslConfig);
+		this.createServerWithSsl(sslConfig);
+	}
+
+	private readSslConfig(): ISsl {
+		try {
+			return {
+				key: fs.readFileSync(this._config.setting.secureProxy.ssl.key, 'utf8'),
+				cert: fs.readFileSync(this._config.setting.secureProxy.ssl.cert, 'utf8')
+			};
+		} catch (error) {
+			console.error('ERROR: Reading SSL configuration:', error);
+		}
+	}
+
+	private configureProxy(sslConfig: ISsl): void {
 		this.port = this._config.uiPort;
 		this.app.all('*', (req, res) => {
 			this._proxy.web(req, res, {
 				target: { host: 'localhost', port: this._config.uiPort },
 				...(this._config.setting.secureProxy.enable && {
-					ssl,
+					ssl: sslConfig,
 					secure: this._config.setting.secureProxy.secure
 				})
 			});
 		});
-		this.server = https.createServer(ssl, this.app);
+	}
+
+	private createServerWithSsl(sslConfig: ISsl): void {
+		try {
+			this.server = createServer(sslConfig, this.app);
+		} catch (error) {
+			console.error('ERROR: Creating HTTPS server:', error);
+		}
 	}
 }
