56e6ab69c0f1211b2542793330682dd989362123
adkif <45813955+adkif@users.noreply.github.com>
2024-01-07 19:26:09 +0200

    fix: pipeline deals component

--- apps/gauzy/src/app/pages/pipelines/pipeline-deals/pipeline-deals.component.ts
+++ apps/gauzy/src/app/pages/pipelines/pipeline-deals/pipeline-deals.component.ts
@@ -1,33 +1,36 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IDeal, IPipeline, ComponentLayoutStyleEnum } from '@gauzy/contracts';
 import { PipelinesService } from '../../../@core/services/pipelines.service';
-import {
-	ActivatedRoute,
-	Router
-} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LocalDataSource, Cell } from 'angular2-smart-table';
-import { TranslationBaseComponent } from '../../../@shared/language-base/translation-base.component';
 import { TranslateService } from '@ngx-translate/core';
 import { NbDialogService } from '@nebular/theme';
 import { DeleteConfirmationComponent } from '../../../@shared/user/forms/delete-confirmation/delete-confirmation.component';
 import { DealsService } from '../../../@core/services/deals.service';
 import { ComponentEnum } from '../../../@core/constants/layout.constants';
 import { Store } from '../../../@core/services/store.service';
-import { filter } from 'rxjs/operators';
-import { firstValueFrom } from 'rxjs';
+import { debounceTime, filter, tap } from 'rxjs/operators';
+import { firstValueFrom, Subject } from 'rxjs';
 import { PipelineDealCreatedByComponent } from '../table-components/pipeline-deal-created-by/pipeline-deal-created-by';
 import { PipelineDealExcerptComponent } from '../table-components/pipeline-deal-excerpt/pipeline-deal-excerpt.component';
 import { PipelineDealProbabilityComponent } from '../table-components/pipeline-deal-probability/pipeline-deal-probability.component';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { ToastrService } from '../../../@core/services/toastr.service';
+import {
+	IPaginationBase,
+	PaginationFilterBaseComponent
+} from '../../../@shared/pagination/pagination-filter-base.component';
+import { distinctUntilChange } from '@gauzy/common-angular';
+
 @UntilDestroy({ checkProperties: true })
 @Component({
 	selector: 'ga-pipeline-deals',
 	templateUrl: './pipeline-deals.component.html',
 	styleUrls: ['./pipeline-deals.component.scss']
 })
-export class PipelineDealsComponent extends TranslationBaseComponent implements OnInit, OnDestroy {
-
+export class PipelineDealsComponent extends PaginationFilterBaseComponent implements OnInit, OnDestroy {
+	private _selectedOrganizationId: string;
+	private _refresh$: Subject<boolean> = new Subject();
 	deals = new LocalDataSource([] as IDeal[]);
 	dealsData: IDeal[];
 	filteredDeals: IDeal[];
@@ -37,9 +40,8 @@ export class PipelineDealsComponent extends TranslationBaseComponent implements 
 	viewComponentName: ComponentEnum;
 	dataLayoutStyle = ComponentLayoutStyleEnum.TABLE;
 	loading: boolean;
-
-	private _selectedOrganizationId: string;
 	public disableButton = true;
+	public componentLayoutStyleEnum = ComponentLayoutStyleEnum;
 	public readonly smartTableSettings = {
 		actions: false,
 		noDataMessage: '-',
@@ -59,7 +61,6 @@ export class PipelineDealsComponent extends TranslationBaseComponent implements 
 					instance.rowData = cell.getRow().getData();
 					instance.value = cell.getValue();
 				}
-
 			},
 			createdBy: {
 				title: 'Created by',
@@ -83,6 +84,7 @@ export class PipelineDealsComponent extends TranslationBaseComponent implements 
 			}
 		}
 	};
+	public gridDeals: IDeal[] = [];
 
 	constructor(
 		public translateService: TranslateService,
@@ -98,98 +100,41 @@ export class PipelineDealsComponent extends TranslationBaseComponent implements 
 		this.setView();
 	}
 
-	ngOnInit(): void {
-		this.store.user$
-			.pipe(
-				filter((user) => !!user),
-				untilDestroyed(this)
-			)
-			.subscribe((user) => {
-				this.updateViewData();
-			});
-		this.smartTableSettings.noDataMessage = this.getTranslation('SM_TABLE.NO_RESULT');
-		this.smartTableSettings.columns.title.title = this.getTranslation('SM_TABLE.TITLE');
-		this.smartTableSettings.columns.stage.title = this.getTranslation('SM_TABLE.STAGE');
-		this.smartTableSettings.columns.createdBy.title = this.getTranslation('SM_TABLE.CREATED_BY');
-		this.smartTableSettings.columns.probability.title = this.getTranslation('PIPELINE_DEAL_CREATE_PAGE.PROBABILITY');
-	}
-
-	setView() {
-		this.viewComponentName = ComponentEnum.PIPELINE_DEALS;
-		this.store
-			.componentLayout$(this.viewComponentName)
-			.pipe(untilDestroyed(this))
-			.subscribe((componentLayout) => {
-				this.dataLayoutStyle = componentLayout;
-			});
-	}
-
-	filterDealsByStage(): void {
-		const { stageId: search = '' } = this;
-
-		this.deals.setFilter([{ field: 'stageId', search }]);
-
-		if (this.stageId) {
-			this.filteredDeals = this.dealsData.filter(
-				(deal) => deal.stageId === this.stageId
-			);
-		} else {
-			this.filteredDeals = this.dealsData;
-		}
-	}
-
-	async deleteDeal(): Promise<void> {
-		const canProceed: 'ok' = await firstValueFrom(
-			this.dialogService
-				.open(DeleteConfirmationComponent, {
-					context: {
-						recordType: this.getTranslation(
-							'PIPELINE_DEALS_PAGE.RECORD_TYPE',
-							this.deal
-						)
-					}
-				})
-				.onClose
-		);
-
-		if ('ok' === canProceed) {
-			await this.dealsService.delete(this.deal.id);
-			this.updateViewData();
-			this.toastrService.success('PIPELINE_DEALS_PAGE.DEAL_DELETED', {
-				name: this.deal.title
-			});
-			delete this.deal;
-		}
+	private get _isGridLayout(): boolean {
+		return this.dataLayoutStyle === this.componentLayoutStyleEnum.CARDS_GRID;
 	}
 
 	private updateViewData(): void {
 		this.loading = true;
-		this.activatedRoute.params
-			.pipe(untilDestroyed(this))
-			.subscribe(async ({ pipelineId }) => {
-				const { tenantId } = this.store.user;
-				await this.pipelinesService
-					.getAll(['stages'], {
-						id: pipelineId,
-						tenantId
-					})
-					.then(({ items: [value] }) => (this.pipeline = value));
+		this.activatedRoute.params.pipe(untilDestroyed(this)).subscribe(async ({ pipelineId }) => {
+			const { tenantId } = this.store.user;
+			await this.pipelinesService
+				.getAll(['stages'], {
+					id: pipelineId,
+					tenantId
+				})
+				.then(({ items: [value] }) => (this.pipeline = value));
 
-				this._checkOrganization();
+			this._checkOrganization();
 
-				await this.pipelinesService
-					.findDeals(pipelineId, { tenantId })
-					.then(({ items }) => {
-						items.forEach((deal) => {
-							deal.stage = this.pipeline.stages.find(
-								({ id }) => id === deal.stageId
-							);
-						});
-						this.deals.load(items);
-						this.dealsData = items;
-						this.filterDealsByStage();
-					});
+			await this.pipelinesService.findDeals(pipelineId, { tenantId }).then(async ({ items }) => {
+				items.forEach((deal) => {
+					deal.stage = this.pipeline.stages.find(({ id }) => id === deal.stageId);
+				});
+				// Get pagination settings
+				const { activePage, itemsPerPage } = this.getPagination();
+				// Set paging for the Smart Table source
+				this.deals.setPaging(activePage, itemsPerPage, false);
+				await this.deals.load(items);
+				this.dealsData = items;
+				this.filterDealsByStage();
+				await this._loadDataGridLayout();
+				this.setPagination({
+					...this.getPagination(),
+					totalItems: this.deals.count()
+				});
 			});
+		});
 		this.loading = false;
 	}
 
@@ -201,13 +146,128 @@ export class PipelineDealsComponent extends TranslationBaseComponent implements 
 			)
 			.subscribe((org) => {
 				this._selectedOrganizationId = org.id;
-				if (
-					this.pipeline?.organizationId !==
-					this._selectedOrganizationId
-				) {
+				if (this.pipeline?.organizationId !== this._selectedOrganizationId) {
 					this.router.navigate(['pages/sales/pipelines']);
 				}
 			});
+	}
+
+	private loadSmartTableSource(): void {
+		const pagination: IPaginationBase = this.getPagination();
+		//this.smartTableSettings.
+		this.smartTableSettings.noDataMessage = this.getTranslation('SM_TABLE.NO_RESULT');
+		this.smartTableSettings.columns.title.title = this.getTranslation('SM_TABLE.TITLE');
+		this.smartTableSettings.columns.stage.title = this.getTranslation('SM_TABLE.STAGE');
+		this.smartTableSettings.columns.createdBy.title = this.getTranslation('SM_TABLE.CREATED_BY');
+		this.smartTableSettings.columns.probability.title = this.getTranslation(
+			'PIPELINE_DEAL_CREATE_PAGE.PROBABILITY'
+		);
+		// Configure Smart Table settings
+		Object.assign(this.smartTableSettings, {
+			pager: {
+				display: false,
+				perPage: pagination ? pagination.itemsPerPage : this.minItemPerPage
+			}
+		});
+	}
+
+	private async _loadDataGridLayout() {
+		if (this._isGridLayout) {
+			const deals: IDeal[] = await this.deals.getElements();
+			this.gridDeals.push(...deals);
+		}
+	}
+
+	ngOnInit(): void {
+		this.loadSmartTableSource();
+		this.subject$
+			.pipe(
+				debounceTime(300),
+				tap(() => this.updateViewData()),
+				untilDestroyed(this)
+			)
+			.subscribe();
+		this.store.user$
+			.pipe(
+				filter((user) => !!user),
+				tap(() => this.subject$.next(true)),
+				untilDestroyed(this)
+			)
+			.subscribe();
+		this.pagination$
+			.pipe(
+				// Debounce the observable to wait for 100 milliseconds of inactivity
+				debounceTime(100),
+				// Ensure distinct values are emitted
+				distinctUntilChange(),
+				// Update view
+				tap(() => this.subject$.next(true)),
+				// Unsubscribe from the observable when the component is destroyed
+				untilDestroyed(this)
+			)
+			.subscribe();
+		this._refresh$
+			.pipe(
+				filter(() => this._isGridLayout),
+				tap(() => this.refreshPagination()),
+				tap(() => (this.gridDeals = [])),
+				untilDestroyed(this)
+			)
+			.subscribe();
+	}
+
+	setView() {
+		this.viewComponentName = ComponentEnum.PIPELINE_DEALS;
+		this.store
+			.componentLayout$(this.viewComponentName)
+			.pipe(
+				// Wait for 300 milliseconds of inactivity
+				debounceTime(300),
+				// Only emit a new value if it's distinct from the previous one
+				distinctUntilChange(),
+				// Update the dataLayoutStyle based on the component layout
+				tap((componentLayout) => (this.dataLayoutStyle = componentLayout)),
+				// Trigger pagination refresh
+				filter(() => this._isGridLayout),
+				tap(() => this.refreshPagination()),
+				tap(() => (this.gridDeals = [])),
+				// emit value immediately
+				tap(() => this.subject$.next(true)),
+				// Automatically unsubscribe when the component is destroyed
+				untilDestroyed(this)
+			)
+			.subscribe();
+	}
+
+	filterDealsByStage(): void {
+		const { stageId: search = '' } = this;
+
+		this.deals.setFilter([{ field: 'stageId', search }]);
+
+		if (this.stageId) {
+			this.filteredDeals = this.dealsData.filter((deal) => deal.stageId === this.stageId);
+		} else {
+			this.filteredDeals = this.dealsData;
+		}
+	}
+
+	async deleteDeal(): Promise<void> {
+		const canProceed: 'ok' = await firstValueFrom(
+			this.dialogService.open(DeleteConfirmationComponent, {
+				context: {
+					recordType: this.getTranslation('PIPELINE_DEALS_PAGE.RECORD_TYPE', this.deal)
+				}
+			}).onClose
+		);
+
+		if ('ok' === canProceed) {
+			await this.dealsService.delete(this.deal.id);
+			this.subject$.next(true);
+			this.toastrService.success('PIPELINE_DEALS_PAGE.DEAL_DELETED', {
+				name: this.deal.title
+			});
+			delete this.deal;
+		}
 	}
 
 	selectPipelineDeals({ isSelected, data }) {
@@ -225,5 +285,5 @@ export class PipelineDealsComponent extends TranslationBaseComponent implements 
 		});
 	}
 
-	ngOnDestroy() { }
+	ngOnDestroy() {}
 }
