f2dc44463051ed89947dd801f7d284a7b0a9f380
CoderNadir <coder.nadir@gmail.com>
2024-01-22 18:17:38 +0100

    fix typo in error message, fix start/stop timer queries failing

--- packages/core/src/employee/commands/handlers/update-employee-total-worked-hours.handler.ts
+++ packages/core/src/employee/commands/handlers/update-employee-total-worked-hours.handler.ts
@@ -1,7 +1,7 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { getConfig, prepareSQLQuery as p } from '@gauzy/config';
+import { databaseTypes, getConfig, prepareSQLQuery as p } from '@gauzy/config';
 import { UpdateEmployeeTotalWorkedHoursCommand } from '../update-employee-total-worked-hours.command';
 import { EmployeeService } from '../../employee.service';
 import { TimeLog } from './../../../core/entities/internal';
@@ -26,14 +26,26 @@ export class UpdateEmployeeTotalWorkedHoursHandler
 		if (hours) {
 			totalWorkHours = hours;
 		} else {
+			let query:string = '';
+			switch(config.dbConnectionOptions.type) {
+				case databaseTypes.sqlite:
+				case databaseTypes.betterSqlite3:
+					query = 'SUM((julianday("stoppedAt") - julianday("startedAt")) * 86400)';
+					break;
+				case databaseTypes.postgres:
+					query = 'SUM(extract(epoch from ("stoppedAt" - "startedAt")))';
+					break;
+				case databaseTypes.mysql:
+					query = p('SUM(TIMESTAMPDIFF(SECOND, "startedAt", "stoppedAt"))');
+					break;
+				default:
+					throw Error(`cannot update employee total worked hours due to unsupported database type: ${config.dbConnectionOptions.type}`);
+
+			}
 			const logs = await this.timeLogRepository
 				.createQueryBuilder()
 				.select(
-					`${
-						['sqlite', 'better-sqlite3'].includes(config.dbConnectionOptions.type)
-							? 'SUM((julianday("stoppedAt") - julianday("startedAt")) * 86400)'
-							: p('SUM(extract(epoch from ("stoppedAt" - "startedAt")))')
-					}`,
+					query,
 					`duration`
 				)
 				.where({
