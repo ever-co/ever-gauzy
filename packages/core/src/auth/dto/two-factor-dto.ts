1fd58aeaddee8749c27357a4a900314a89a45577
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-02-03 10:52:01 +0530

    fix: added missing include teams query parameter

--- packages/core/src/auth/dto/two-factor-dto.ts
+++ packages/core/src/auth/dto/two-factor-dto.ts
@@ -1,13 +1,25 @@
-import { IntersectionType } from "@nestjs/swagger";
+import { ApiPropertyOptional, IntersectionType } from "@nestjs/swagger";
+import { IsBoolean, IsOptional } from "class-validator";
 import { IUserCodeInput, IUserEmailInput, IUserTokenInput } from "@gauzy/contracts";
 import { UserCodeDTO, UserEmailDTO, UserTokenDTO } from "../../user/dto";
 
+/**
+ *
+ */
 export class WorkspaceSigninEmailVerifyDTO extends IntersectionType(
     UserEmailDTO,
     UserCodeDTO,
-) implements IUserEmailInput, IUserCodeInput { }
+) implements IUserEmailInput, IUserCodeInput {
 
+    @ApiPropertyOptional({ type: () => Boolean })
+    @IsOptional()
+    @IsBoolean()
+    readonly includeTeams: boolean;
+}
 
+/**
+ *
+ */
 export class WorkspaceSigninDTO extends IntersectionType(
     UserEmailDTO,
     UserTokenDTO,
