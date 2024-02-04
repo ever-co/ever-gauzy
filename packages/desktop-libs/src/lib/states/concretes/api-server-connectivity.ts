90cb09466ed855c718555159dbb67a062faedf0c
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-02-04 11:49:13 +0200

    refactor: server connectivity check to use FetchCommand.
    Refactor server connectivity check to utilize FetchCommand for improved code readability and maintainability. Replace node-fetch with FetchCommand to handle server connectivity checks. Log any errors occurring during the request for improved error handling.
    
    node-fetch doesn't works properly since node v18.x.x inside electronJS

--- packages/desktop-libs/src/lib/states/concretes/api-server-connectivity.ts
+++ packages/desktop-libs/src/lib/states/concretes/api-server-connectivity.ts
@@ -1,17 +1,38 @@
-import { INetworkState } from '../../interfaces';
 import { LocalStore } from '../../desktop-store';
-import fetch from 'node-fetch';
+import { INetworkState } from '../../interfaces';
+import { FetchCommand } from '../../utils';
 
+interface IGauzyAPIResponse {
+	status: number;
+	message: string;
+}
+
+/**
+ * Represents a class responsible for checking the connectivity with the API server.
+ * Implements the INetworkState interface.
+ */
 export class ApiServerConnectivity implements INetworkState {
+	/**
+	 * Checks if the connection with the API server is established.
+	 * @returns A promise that resolves to a boolean indicating the connectivity status.
+	 */
 	public async established(): Promise<boolean> {
 		try {
-			const res = await fetch(LocalStore.getServerUrl() + '/api');
-			if (res.ok) {
-				return true;
-			}
-			return false;
+			// Retrieve the server URL from the LocalStore
+			const serverUrl = `${LocalStore.getServerUrl()}/api`;
+			// Create a curl command
+			const command = new FetchCommand(serverUrl);
+			// Make a GET request to the server
+			const response = await command.execute<IGauzyAPIResponse>();
+			// Check responses
+			console.info('stderr', response?.stderr);
+			// Check responses
+			console.info('stdout', response?.stdout);
+			// Check if response data is not empty or falsy
+			return response?.stdout?.status === 200;
 		} catch (error) {
-			console.error('[ApiServerConnectivity]: ', error);
+			// Log any errors that occur during the request
+			console.error('[ApiServerConnectivity]:', error);
 			return false;
 		}
 	}
