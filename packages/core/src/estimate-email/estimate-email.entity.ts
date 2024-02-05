3e0a544c85c9a766b2906b4bd447685178a7fc03
CoderNadir <coder.nadir@gmail.com>
2024-02-05 10:21:20 +0100

    fix: #7481 | fix email history throw duplicate name when API called, generate a migration file for estimateEmail token column for mysql, improve email history UI list item component, set spinner for the whole card to show in init and fetching new emails list

--- packages/core/src/estimate-email/estimate-email.entity.ts
+++ packages/core/src/estimate-email/estimate-email.entity.ts
@@ -4,13 +4,16 @@ import { Column } from 'typeorm';
 import { TenantOrganizationBaseEntity } from '../core/entities/internal';
 import { MultiORMEntity } from './../core/decorators/entity';
 import { MikroOrmEstimateEmailRepository } from './repository/mikro-orm-estimate-email.repository';
+import { isMySQL } from '@gauzy/config';
 
 @MultiORMEntity('estimate_email', { mikroOrmRepository: () => MikroOrmEstimateEmailRepository })
 export class EstimateEmail extends TenantOrganizationBaseEntity
 	implements IEstimateEmail {
 
 	@ApiProperty({ type: () => String })
-	@Column()
+	@Column({
+		...(isMySQL() ? { type: 'text' } : {})
+	})
 	token?: string;
 
 	@ApiProperty({ type: () => String })
