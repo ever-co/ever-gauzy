c9599e0245208b080f2b90be27fef93e1aa49917
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-02-08 18:32:49 +0530

    fix: add / edit manual timesheet entry

--- packages/core/src/shared/guards/permission.guard.ts
+++ packages/core/src/shared/guards/permission.guard.ts
@@ -1,55 +1,53 @@
 import { environment as env } from '@gauzy/config';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, Type } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { verify } from 'jsonwebtoken';
 import { isEmpty, PERMISSIONS_METADATA, removeDuplicates } from '@gauzy/common';
+import { PermissionsEnum } from '@gauzy/contracts';
 import { RequestContext } from './../../core/context';
 import { UserService } from './../../user/user.service';
-import { PermissionsEnum } from '@gauzy/contracts';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
 	constructor(
 		private readonly _reflector: Reflector,
 		private readonly userService: UserService
-	) {}
+	) { }
 
+	/**
+	 * Checks if the user is authorized based on specified permissions.
+	 * @param context The execution context.
+	 * @returns A promise that resolves to a boolean indicating authorization status.
+	 */
 	async canActivate(context: ExecutionContext): Promise<boolean> {
-		let isAuthorized = false;
-		/*
-		* Retrieve metadata for a specified key for a specified set of permissions
-		*/
-		const permissions = removeDuplicates(this._reflector.getAllAndOverride<PermissionsEnum[]>(PERMISSIONS_METADATA, [
-			context.getHandler(), // Method Roles
-			context.getClass(), // Controller Roles
-		])) || [];
+		// Retrieve permissions from metadata
+		const targets: Array<Function | Type<any>> = [context.getHandler(), context.getClass()];
+		const permissions = removeDuplicates(this._reflector.getAllAndOverride<PermissionsEnum[]>(PERMISSIONS_METADATA, targets)) || [];
+
+		// If no specific permissions are required, consider it authorized
 		if (isEmpty(permissions)) {
-			isAuthorized = true;
-		} else {
-			const token = RequestContext.currentToken();
-			const { id } = verify(token, env.JWT_SECRET) as {
-				id: string;
-				role: string;
-			};
-			const user = await this.userService.findOneByIdString(id, {
-				relations: {
-					role: {
-						rolePermissions: true
-					}
-				}
-			});
-			isAuthorized = !!user.role.rolePermissions.find(
-				(p) => permissions.indexOf(p.permission) > -1 && p.enabled
-			);
-			if (!isAuthorized) {
-				console.log(
-					'Unauthorized access blocked. UserId:',
-					id,
-					' Permissions Checked:',
-					permissions
-				);
-			}
+			return true;
 		}
+
+		// Check user authorization
+		const token = RequestContext.currentToken();
+		const { id, role } = verify(token, env.JWT_SECRET) as {
+			id: string;
+			role: string;
+		};
+
+		// Retrieve user with role and rolePermissions relations
+		const user = await this.userService.findOneByIdString(id, { relations: { role: { rolePermissions: true } } });
+
+		// Check if user has the required permissions
+		const isAuthorized = user?.role?.rolePermissions.some((p) => permissions.includes(p.permission) && p.enabled) || false;
+
+		// Log unauthorized access attempts
+		if (!isAuthorized) {
+			// Log unauthorized access attempts
+			console.log(`Unauthorized access blocked: User ID: ${id}, Role: ${role}, Permissions Checked: ${permissions.join(', ')}`);
+		}
+
 		return isAuthorized;
 	}
 }
