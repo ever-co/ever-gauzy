d1e6099b70331db3050ebf9feb0fdd534bea9e24
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-02-14 22:22:55 +0530

    feat: knex db connection module

--- packages/core/src/database/database.module.ts
+++ packages/core/src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { KnexModule } from 'nest-knexjs';
 import { ConfigModule, ConfigService } from '@gauzy/config';
 
 /**
@@ -38,6 +39,26 @@ import { ConfigModule, ConfigService } from '@gauzy/config';
 				const { dbConnectionOptions } = configService.config;
 				return dbConnectionOptions;
 			}
+		}),
+		/**
+		 * Configure the Knex.js module for the application using asynchronous options.
+		 */
+		KnexModule.forRootAsync({
+			useFactory: () => ({
+				config: {
+					client: 'pg', // Specify the database client (PostgreSQL in this case)
+					useNullAsDefault: true,
+					connection: {
+						host: process.env.DB_HOST || 'localhost',
+						port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+						user: process.env.DB_USER || 'root',
+						password: process.env.DB_PASS || 'root',
+						database: process.env.DB_NAME || 'postgres',
+					}
+				},
+				retryAttempts: 100, // Number of retry attempts in case of connection failures
+				retryDelay: 3000, // Delay between retry attempts in milliseconds
+			}),
 		})
 	],
 	exports: [
