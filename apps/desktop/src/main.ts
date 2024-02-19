bff79d0d60ac8c89ae9103efbfca8104dff95d91
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-02-19 16:10:32 +0530

    fix: desktop sentry initialize only when defined SENTRY_DSN

--- apps/desktop/src/main.ts
+++ apps/desktop/src/main.ts
@@ -1,7 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { akitaConfig, enableAkitaProdMode, persistState } from '@datorama/akita';
-import * as Sentry from '@sentry/angular-ivy';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -16,41 +15,6 @@ persistState({
 
 akitaConfig({
 	resettable: true
-});
-
-Sentry.init({
-	dsn: environment.SENTRY_DSN,
-	environment: environment.production ? 'production' : 'development',
-	debug: !environment.production,
-	integrations: [
-		// Registers and configures the Tracing integration,
-		// which automatically instruments your application to monitor its
-		// performance, including custom Angular routing instrumentation
-		Sentry.browserTracingIntegration(),
-		// Registers the Replay integration,
-		// which automatically captures Session Replays
-		Sentry.replayIntegration()
-	],
-
-	// Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-	tracePropagationTargets: [
-		'localhost',
-		/^https:\/\/api\.gauzy\.co\/api/,
-		/^https:\/\/apistage\.gauzy\.co\/api/,
-		/^https:\/\/apidemo\.gauzy\.co\/api/
-	],
-
-	// Capture Replay for 10% of all sessions,
-	// plus for 100% of sessions with an error
-	replaysSessionSampleRate: environment.SENTRY_TRACES_SAMPLE_RATE
-		? parseInt(environment.SENTRY_TRACES_SAMPLE_RATE)
-		: 0.01,
-	replaysOnErrorSampleRate: 1.0,
-
-	// Set tracesSampleRate to 1.0 to capture 100%
-	// of transactions for performance monitoring.
-	// We recommend adjusting this value in production
-	tracesSampleRate: environment.SENTRY_TRACES_SAMPLE_RATE ? parseInt(environment.SENTRY_TRACES_SAMPLE_RATE) : 0.01
 });
 
 platformBrowserDynamic()
