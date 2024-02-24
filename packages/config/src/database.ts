759778c7ea0577a86433d8f8f44a9ddf9854f092
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-02-24 16:02:53 +0530

    fix: enabled DB ssl connection for knex

--- packages/config/src/database.ts
+++ packages/config/src/database.ts
@@ -59,6 +59,9 @@ const idleTimeoutMillis = process.env.DB_IDLE_TIMEOUT ? parseInt(process.env.DB_
 const dbSlowQueryLoggingTimeout = process.env.DB_SLOW_QUERY_LOGGING_TIMEOUT ? parseInt(process.env.DB_SLOW_QUERY_LOGGING_TIMEOUT) : 10000; // 10 seconds default
 const dbSslMode = process.env.DB_SSL_MODE === 'true';
 
+// Get TLS (Transport Layer Security) options based on the specified SSL mode.
+const tlsOptions = getTlsOptions(dbSslMode);
+
 console.log('DB Pool Size: ' + dbPoolSize);
 console.log('DB Connection Timeout: ' + dbConnectionTimeout);
 console.log('DB Idle Timeout: ' + idleTimeoutMillis);
@@ -84,7 +87,7 @@ switch (dbType) {
 			entities: ['src/modules/not-exists/*.entity{.ts,.js}'],
 			driverOptions: {
 				connection: {
-					ssl: getTlsOptions(dbSslMode)
+					ssl: tlsOptions
 				}
 			},
 			pool: {
@@ -98,7 +101,7 @@ switch (dbType) {
 		// TypeORM DB Config (MySQL)
 		const typeOrmMySqlOptions: MysqlConnectionOptions = {
 			type: dbType,
-			ssl: getTlsOptions(dbSslMode),
+			ssl: tlsOptions,
 			host: process.env.DB_HOST || 'localhost',
 			port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
 			database: process.env.DB_NAME || 'mysql',
@@ -126,6 +129,7 @@ switch (dbType) {
 			config: {
 				client: 'mysql2', // Database client (MySQL in this case)
 				connection: {
+					ssl: tlsOptions ? { ca: tlsOptions.ca, rejectUnauthorized: tlsOptions.rejectUnauthorized } : false,
 					host: process.env.DB_HOST || 'localhost', // Database host (default: localhost)
 					port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306, // Database port (default: 3306)
 					database: process.env.DB_NAME || 'mysql', // Database name (default: mysql)
@@ -167,7 +171,7 @@ switch (dbType) {
 			entities: ['src/modules/not-exists/*.entity{.ts,.js}'],
 			driverOptions: {
 				connection: {
-					ssl: getTlsOptions(dbSslMode)
+					ssl: tlsOptions
 				}
 			},
 			pool: {
@@ -186,7 +190,7 @@ switch (dbType) {
 		// TypeORM DB Config (PostgreSQL)
 		const typeOrmPostgresOptions: PostgresConnectionOptions = {
 			type: dbType,
-			ssl: getTlsOptions(dbSslMode),
+			ssl: tlsOptions,
 			host: process.env.DB_HOST || 'localhost',
 			port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
 			database: process.env.DB_NAME || 'postgres',
@@ -222,6 +226,7 @@ switch (dbType) {
 			config: {
 				client: 'pg', // Database client (PostgreSQL in this case)
 				connection: {
+					ssl: tlsOptions ? { ca: tlsOptions.ca, rejectUnauthorized: tlsOptions.rejectUnauthorized } : false,
 					host: process.env.DB_HOST || 'localhost', // Database host (default: localhost)
 					port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432, // Database port (default: 5432)
 					database: process.env.DB_NAME || 'postgres', // Database name (default: postgres)
