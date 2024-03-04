269767e6e914204af2330402b7eabeff9215294d
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-03-04 22:37:14 +0530

    fix: removed tenant module (unnecessary module)

--- packages/core/src/request-approval/request-approval.module.ts
+++ packages/core/src/request-approval/request-approval.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
 import { RouterModule } from '@nestjs/core';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { RequestApproval } from './request-approval.entity';
 import { RequestApprovalController } from './request-approval.controller';
 import { RequestApprovalService } from './request-approval.service';
-import { Employee } from '../employee/employee.entity';
-import { OrganizationTeam } from '../organization-team/organization-team.entity';
-import { OrganizationTeamService } from '../organization-team/organization-team.service';
-import { EmployeeService } from '../employee/employee.service';
+import { OrganizationTeamModule } from '../organization-team/organization-team.module';
+import { EmployeeModule } from '../employee/employee.module';
 import { OrganizationTeamEmployeeModule } from '../organization-team-employee/organization-team-employee.module';
-import { TenantModule } from '../tenant/tenant.module';
 import { RolePermissionModule } from '../role-permission/role-permission.module';
 import { UserModule } from './../user/user.module';
 import { RoleModule } from './../role/role.module';
@@ -19,18 +17,18 @@ import { EquipmentSharingModule } from './../equipment-sharing/equipment-sharing
 import { TimeOffRequestModule } from './../time-off-request/time-off-request.module';
 import { CommandHandlers } from './commands/handlers';
 import { TaskModule } from './../tasks/task.module';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
 	imports: [
 		RouterModule.register([{ path: '/request-approval', module: RequestApprovalModule }]),
-		TypeOrmModule.forFeature([RequestApproval, Employee, OrganizationTeam]),
-		MikroOrmModule.forFeature([RequestApproval, Employee, OrganizationTeam]),
+		TypeOrmModule.forFeature([RequestApproval]),
+		MikroOrmModule.forFeature([RequestApproval]),
 		CqrsModule,
 		OrganizationTeamEmployeeModule,
-		TenantModule,
 		RolePermissionModule,
 		UserModule,
+		EmployeeModule,
+		OrganizationTeamModule,
 		RoleModule,
 		OrganizationModule,
 		EquipmentSharingModule,
@@ -38,7 +36,7 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 		TaskModule
 	],
 	controllers: [RequestApprovalController],
-	providers: [RequestApprovalService, OrganizationTeamService, EmployeeService, ...CommandHandlers],
+	providers: [RequestApprovalService, ...CommandHandlers],
 	exports: [RequestApprovalService]
 })
 export class RequestApprovalModule { }
