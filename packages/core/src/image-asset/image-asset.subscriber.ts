e902f8fa44a3e49d5a6048cb2170f3c3299e79a5
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-03-05 17:59:41 +0530

    wip: working user loading images

--- packages/core/src/image-asset/image-asset.subscriber.ts
+++ packages/core/src/image-asset/image-asset.subscriber.ts
@@ -18,16 +18,14 @@ export class ImageAssetSubscriber implements EntitySubscriberInterface<ImageAsse
     * @param entity
     * @param event
     */
-    async afterLoad(
-        entity: ImageAsset | Partial<ImageAsset>,
-        event?: LoadEvent<ImageAsset>
-    ): Promise<any | void> {
+    async afterLoad(entity: ImageAsset, event?: LoadEvent<ImageAsset>): Promise<any | void> {
         try {
             if (entity instanceof ImageAsset) {
                 const { storageProvider, url, thumb } = entity;
                 const store = new FileStorage().setProvider(storageProvider).getProviderInstance();
                 entity.fullUrl = await store.url(url);
                 entity.thumbUrl = await store.url(thumb);
+                console.log(entity, 'Image Asset Subscriber');
             }
         } catch (error) {
             console.error('Error in afterLoad:', error);
