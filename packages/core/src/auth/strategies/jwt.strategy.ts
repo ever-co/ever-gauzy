f376a1f09e531656e583a055ae4ef0c5009cd403
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-01 16:08:42 +0530

    fix: retrieve employee id associated with the user

--- packages/core/src/auth/strategies/jwt.strategy.ts
+++ packages/core/src/auth/strategies/jwt.strategy.ts
@@ -4,13 +4,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from 'jsonwebtoken';
 import { IUser } from '@gauzy/contracts';
-import { AuthService } from './../auth.service';
+import { AuthService } from '../auth.service';
+import { EmployeeService } from '../../employee/employee.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
 	constructor(
-		private readonly authService: AuthService
+		private readonly _authService: AuthService,
+		private readonly _employeeService: EmployeeService,
 	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -18,34 +20,37 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 		});
 	}
 
-	async validate(payload: JwtPayload, done: Function) {
+	/**
+	 * Validates the JWT payload.
+	 * @param {JwtPayload} payload - The JWT payload to validate.
+	 * @param {Function} done - The callback function to call when validation is complete.
+	 * @returns {void}
+	 */
+	async validate(payload: JwtPayload, done: Function): Promise<void> {
 		try {
 			const { id, thirdPartyId } = payload;
+
 			// We use this to also attach the user object to the request context.
-			const user: IUser = await this.authService.getAuthenticatedUser(
+			const user: IUser = await this._authService.getAuthenticatedUser(
 				id,
 				thirdPartyId
 			);
+
 			if (!user) {
 				return done(new UnauthorizedException('unauthorized'), false);
 			} else {
-				user.employeeId = payload.employeeId;
-
-				// You could add a function to the authService to verify the claims of the token:
-				// i.e. does the user still have the roles that are claimed by the token
-				// const validClaims = await this.authService.verifyTokenClaims(payload);
-
-				// if (!validClaims)
-				//    return done(new UnauthorizedException('invalid token claims'), false);
-
+				// Check if payload has employeeId
+				if (payload.employeeId) {
+					// If employeeId exists, retrieve employee details associated with the user
+					const employee = await this._employeeService.findOneByUserId(user.id);
+					// If employee is found, assign its id to user.employeeId, otherwise assign null
+					user.employeeId = employee ? employee.id : null;
+				}
 				done(null, user);
 			}
-		} catch (err) {
-			console.error(err);
-			return done(
-				new UnauthorizedException('unauthorized', err.message),
-				false
-			);
+		} catch (error) {
+			console.error('Error occurred during JWT validation:', error);
+			return done(new UnauthorizedException('unauthorized', error.message), false);
 		}
 	}
 }
