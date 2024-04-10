0ee978ccc9b686314da9e3e82443bdea225f7c44
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-10 13:07:32 +0530

    fix: if AI integration delete hide job browse & matching menu

--- apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.component.ts
+++ apps/gauzy/src/app/@shared/sidebar-menu/sidebar-menu.component.ts
@@ -16,26 +16,33 @@ export class SidebarMenuComponent implements AfterContentChecked {
 
 	@Input() items: IMenuItem[] = [];
 
-	constructor(
-		private readonly _cdr: ChangeDetectorRef,
-		private readonly _sidebarMenuService: SidebarMenuService
-	) { }
-
 	public get selectedItem() {
 		return this._sidebarMenuService.selectedItem;
 	}
-
 	public set selectedItem(value: IMenuItem) {
 		this._sidebarMenuService.selectedItem = value;
 		this._cdr.detectChanges();
 	}
 
-	public focusOn(event: IMenuItem) {
-		this._sidebarMenuService.selectedItem = event;
+	constructor(
+		private readonly _cdr: ChangeDetectorRef,
+		private readonly _sidebarMenuService: SidebarMenuService
+	) { }
+
+	ngAfterContentChecked(): void {
 		this._cdr.detectChanges();
 	}
 
-	ngAfterContentChecked(): void {
+	/**
+	 * Sets the selected item in the sidebar menu and triggers change detection.
+	 *
+	 * @param event The menu item to focus on.
+	 */
+	public focusOn(event: IMenuItem): void {
+		// Set the selected item in the sidebar menu
+		this._sidebarMenuService.selectedItem = event;
+
+		// Trigger change detection
 		this._cdr.detectChanges();
 	}
 }
