b783a8245abde8b68831ec65477bcdc0aa34208e
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-12 12:26:18 +0530

    feat: get target repository from active transactional entity manager

---
+++ packages/core/src/database/transactional-entity-manager.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager, EntitySchema, ObjectLiteral, ObjectType, Repository, } from 'typeorm';
+
+@Injectable()
+export class TransactionalEntityManager {
+
+    constructor(
+        @InjectEntityManager() private entityManager: EntityManager
+    ) { }
+
+    /**
+     * Retrieves the raw EntityManager instance.
+     *
+     * @returns The raw EntityManager instance.
+     */
+    get rawEntityManager(): EntityManager {
+        return this.entityManager;
+    }
+
+    /**
+     * Returns a TypeORM repository for the specified target entity.
+     *
+     * @param target The target entity type or entity schema for which to retrieve the repository.
+     * @returns The TypeORM repository for the specified target entity.
+     */
+    getRepository<Entity extends ObjectLiteral>(
+        target: ObjectType<Entity> | EntitySchema<Entity> | string,
+    ): Repository<Entity>;
+
+    /**
+     * Returns a TypeORM repository based on the provided target.
+     *
+     * @param target The target entity type or entity schema for which to retrieve the repository.
+     * @returns The TypeORM repository for the specified target entity.
+     */
+    getRepository<Entity extends ObjectLiteral>(
+        target?: ObjectType<Entity> | EntitySchema<Entity> | string,
+    ): Repository<Entity> {
+        return this.rawEntityManager.getRepository(target!);
+    }
+}
