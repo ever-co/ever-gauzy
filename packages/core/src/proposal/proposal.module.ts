fed54cc92cc24cba18d7e08fe79b97982c333e96
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-13 16:56:49 +0530

    fix: missing proposal update command handler

--- packages/core/src/proposal/proposal.module.ts
+++ packages/core/src/proposal/proposal.module.ts
@@ -1,20 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RouterModule } from '@nestjs/core';
+import { CqrsModule } from '@nestjs/cqrs';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Proposal } from './proposal.entity';
 import { ProposalController } from './proposal.controller';
 import { ProposalService } from './proposal.service';
-import { Employee } from '../employee/employee.entity';
 import { CommandHandlers } from './commands/handlers';
 import { RolePermissionModule } from '../role-permission/role-permission.module';
 
 @Module({
 	imports: [
-		RouterModule.register([{ path: '/proposal', module: ProposalModule }]),
-		TypeOrmModule.forFeature([Proposal, Employee]),
-		MikroOrmModule.forFeature([Proposal, Employee]),
-		RolePermissionModule
+		TypeOrmModule.forFeature([Proposal]),
+		MikroOrmModule.forFeature([Proposal]),
+		RolePermissionModule,
+		CqrsModule
 	],
 	controllers: [ProposalController],
 	providers: [ProposalService, ...CommandHandlers],
