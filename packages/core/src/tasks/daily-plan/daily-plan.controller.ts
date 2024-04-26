c53e30f2b7a97d9fe5f71e63a226bcf234e68ab8
GloireMutaliko21 <mufunyig@gmail.com>
2024-04-26 13:11:52 +0200

    [feat]: add get employee daily plans service and controller

--- packages/core/src/tasks/daily-plan/daily-plan.controller.ts
+++ packages/core/src/tasks/daily-plan/daily-plan.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CrudController, PaginationParams } from '../../core/crud';
+import { DailyPlan } from './daily-plan.entity';
+import { DailyPlanService } from './daily-plan.service';
+import { IDailyPlan, IPagination } from '@gauzy/contracts';
 
 // @UseGuards(TenantPermissionGuard)
 @ApiTags('Daily Plan')
 @Controller()
-export class DailyPlanController {
+export class DailyPlanController extends CrudController<DailyPlan> {
+	constructor(private readonly dailyPlanService: DailyPlanService) {
+		super(dailyPlanService);
+	}
+
+	/**
+	 * GET daily plans for a given employee
+	 *
+	 * @param options
+	 * @returns
+	 */
+	@ApiOperation({
+		summary: 'Find employee daily plans.'
+	})
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'Found plans',
+		type: DailyPlan
+	})
+	@ApiResponse({
+		status: HttpStatus.NOT_FOUND,
+		description: 'No Record found'
+	})
 	@Get()
-	tryWorks() {
-		return { message: 'Daily plan module online' };
+	async getEmployeeDailyPlans(@Query() params: PaginationParams<DailyPlan>): Promise<IPagination<IDailyPlan>> {
+		return await this.dailyPlanService.getEmployeeDailyPlans(params);
 	}
 }
--- packages/core/src/tasks/daily-plan/daily-plan.service.ts
+++ packages/core/src/tasks/daily-plan/daily-plan.service.ts
@@ -1,19 +1,53 @@
 import { InjectConnection } from 'nest-knexjs';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IsNull } from 'typeorm';
 import { Knex as KnexConnection } from 'knex';
 import { MikroOrmDailyPlanRepository, TypeOrmDailyPlanRepository } from './repository';
 import { DailyPlan } from './daily-plan.entity';
+import { IPagination } from '@gauzy/contracts';
+import { PaginationParams, TenantAwareCrudService } from '../../core/crud';
+// import { MultiORMEnum } from '../../core/utils';
+// import { isPostgres } from '@gauzy/config';
 
 @Injectable()
-export class DailyPlanService {
+export class DailyPlanService extends TenantAwareCrudService<DailyPlan> {
 	constructor(
 		@InjectRepository(DailyPlan)
-		readonly typeOrmTaskStatusRepository: TypeOrmDailyPlanRepository,
+		readonly typeOrmDailyPlanRepository: TypeOrmDailyPlanRepository,
 
-		readonly mikroOrmTaskStatusRepository: MikroOrmDailyPlanRepository,
+		readonly mikroOrmDailyPlanRepository: MikroOrmDailyPlanRepository,
 
 		@InjectConnection()
 		readonly knexConnection: KnexConnection
-	) {}
+	) {
+		super(typeOrmDailyPlanRepository, mikroOrmDailyPlanRepository);
+	}
+
+	/**
+	 * GET daily plans for a given employee
+	 *
+	 * @param options
+	 * @returns
+	 */
+	async getEmployeeDailyPlans(options: PaginationParams<DailyPlan>): Promise<IPagination<DailyPlan>> {
+		try {
+			if ('where' in options) {
+				const { where } = options;
+				if (where.employeeId === 'null') {
+					options.where.employeeId = IsNull();
+				}
+				return await super.findAll(options);
+			}
+		} catch (error) {
+			console.log(
+				'Failed to retrieve employee daily plans. Ensure that the provided parameters are valid and complete.',
+				error
+			);
+			throw new BadRequestException(
+				'Failed to retrieve employee daily plans. Ensure that the provided parameters are valid and complete.',
+				error
+			);
+		}
+	}
 }
