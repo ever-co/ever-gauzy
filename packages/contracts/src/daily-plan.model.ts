a79682a7ee969e2810e0c4d2a3e10ab70a1e589d
RAHUL RATHORE <41804588+rahul-rocket@users.noreply.github.com>
2024-04-30 12:25:35 +0530

    fix(@gauzy/contracts): updated daily plan model

--- packages/contracts/src/daily-plan.model.ts
+++ packages/contracts/src/daily-plan.model.ts
@@ -2,28 +2,24 @@ import { IBasePerTenantAndOrganizationEntityModel } from './base-entity.model';
 import { IRelationalEmployee } from './employee.model';
 import { ITask } from './task.model';
 
-export interface IDailyPlan extends IBasePerTenantAndOrganizationEntityModel, IRelationalEmployee {
-	date: Date;
-	workTimePlanned: number;
-	status: DailyPlanStatusEnum;
-	tasks?: ITask[];
-}
-
 export enum DailyPlanStatusEnum {
 	OPEN = 'open',
 	IN_PROGRESS = 'in-progress',
 	COMPLETED = 'completed'
 }
 
-export interface IDailyPlanCreateInput extends IBasePerTenantAndOrganizationEntityModel, IRelationalEmployee {
+export interface IDailyPlanBase extends IBasePerTenantAndOrganizationEntityModel {
 	date: Date;
 	workTimePlanned: number;
 	status: DailyPlanStatusEnum;
+}
+
+export interface IDailyPlan extends IDailyPlanBase, IRelationalEmployee {
+	tasks?: ITask[];
+}
+
+export interface IDailyPlanCreateInput extends IDailyPlanBase, IRelationalEmployee {
 	taskId?: ITask['id'];
 }
 
-export interface IDailyPlanUpdateInput extends IBasePerTenantAndOrganizationEntityModel {
-	date?: Date;
-	workTimePlanned?: number;
-	status?: DailyPlanStatusEnum;
-}
+export interface IDailyPlanUpdateInput extends Partial<IDailyPlanBase> { }
