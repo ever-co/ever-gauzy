60bd3c9b760ab200d5cb680b70175cbe7a18c2d5
Rahul R. <rahulrathore576@gmail.com>
2024-05-07 22:44:08 +0530

    fix: job search plugin tsconfig path

--- apps/gauzy/src/app/pages/jobs/jobs.module.ts
+++ apps/gauzy/src/app/pages/jobs/jobs.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { JobSearchUiModule } from '@gauzy/job-search-ui-plugin';
+import { JobSearchUiModule } from '@gauzy/plugins/job-search-ui';
 import { JobsRoutingModule } from './jobs-routing.module';
 import { JobLayoutComponent } from './job-layout/job-layout.component';
 import { SharedModule } from '../../@shared/shared.module';
