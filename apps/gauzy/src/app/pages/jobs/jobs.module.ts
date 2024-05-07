1b58ac0896800e6b081afcfed49f9652f9e71c8e
Rahul R. <rahulrathore576@gmail.com>
2024-05-07 12:37:49 +0530

    fix: angular build command for job search ui

--- apps/gauzy/src/app/pages/jobs/jobs.module.ts
+++ apps/gauzy/src/app/pages/jobs/jobs.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { JobSearchUiModule } from '@gauzy/job-search-ui-plugin';
 import { JobsRoutingModule } from './jobs-routing.module';
-
 import { JobLayoutComponent } from './job-layout/job-layout.component';
 import { SharedModule } from '../../@shared/shared.module';
 import { JobTableComponentsModule } from './table-components/job-table-components.module';
 
 @NgModule({
 	declarations: [JobLayoutComponent],
-	imports: [CommonModule, JobsRoutingModule, SharedModule, JobTableComponentsModule, JobSearchUiModule]
+	imports: [CommonModule, JobsRoutingModule, SharedModule, JobTableComponentsModule]
 })
 export class JobsModule { }
