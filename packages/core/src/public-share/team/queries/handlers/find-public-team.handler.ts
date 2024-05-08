a93dda714638103de2292e501b800b1bf7253956
Rahul R. <rahulrathore576@gmail.com>
2024-05-08 14:16:49 +0530

    fix: improved and optimize get public team by profile link

--- packages/core/src/public-share/team/queries/handlers/find-public-team.handler.ts
+++ packages/core/src/public-share/team/queries/handlers/find-public-team.handler.ts
@@ -10,8 +10,13 @@ export class FindPublicTeamHandler implements IQueryHandler<FindPublicTeamQuery,
         private readonly _publicTeamService: PublicTeamService
     ) { }
 
+	/**
+     * Executes a query to find a public team by a given profile link.
+     * @param query - An object containing the parameters and optional query options.
+     * @returns A promise that resolves to an `IOrganizationTeam`.
+     */
     async execute(query: FindPublicTeamQuery): Promise<IOrganizationTeam> {
-        const { params, options } = query;
-        return await this._publicTeamService.findOneByProfileLink(params, options);
+        const { params, options } = query; // Extract parameters and options from the query
+        return await this._publicTeamService.findOneByProfileLink(params, options); // Find the team by the profile link
     }
 }
