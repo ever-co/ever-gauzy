79cc593157b063b555d7096f33e44928b5daa210
Rahul R. <rahulrathore576@gmail.com>
2024-05-11 15:37:33 +0530

    fix: removed @gauzy/ui-sdk/button

--- packages/ui-sdk/src/lib/button/public-api.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/button.component';
-export * from './src/button.module';
--- packages/ui-sdk/src/lib/ui-sdk.module.ts
+++ packages/ui-sdk/src/lib/ui-sdk.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonModule } from '@gauzy/ui-sdk/button';
 
 @NgModule({
 	declarations: [],
-	imports: [CommonModule, ButtonModule],
+	imports: [CommonModule],
 	exports: [],
 	providers: []
 })
