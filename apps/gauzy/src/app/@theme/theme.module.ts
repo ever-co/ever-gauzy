57b206992ff7829f84aef29f69e9a3e7ffb1fe4d
Rahul R. <rahulrathore576@gmail.com>
2024-05-18 16:11:17 +0530

    fix: apply scss path from ui-sdk/static folder

--- apps/gauzy/src/app/@theme/theme.module.ts
+++ apps/gauzy/src/app/@theme/theme.module.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatRippleModule } from '@angular/material/core';
 import {
 	NbActionsModule,
 	NbLayoutModule,
@@ -16,10 +17,26 @@ import {
 	NbPopoverModule,
 	NbTooltipModule,
 	NbLayoutDirectionService,
-	NbSpinnerModule
+	NbSpinnerModule,
+	CORPORATE_THEME,
+	DARK_THEME,
+	NbAccordionModule,
+	NbToggleModule,
+	NbCardModule
 } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NbSecurityModule } from '@nebular/security';
+import { NgxPermissionsModule } from 'ngx-permissions';
+
+import {
+	COSMIC_THEME,
+	DEFAULT_THEME,
+	GAUZY_DARK,
+	GAUZY_LIGHT,
+	MATERIAL_DARK_THEME,
+	MATERIAL_LIGHT_THEME
+} from '@gauzy/ui-sdk/static/styles';
+import { TranslateModule } from '@gauzy/ui-sdk/i18n';
 
 import { FooterComponent, HeaderComponent, LayoutSelectorComponent, SearchInputComponent } from './components';
 import { Pipes } from './pipes';
@@ -30,28 +47,17 @@ import {
 	PublicLayoutComponent
 } from './layouts';
 
-import { DEFAULT_THEME } from './styles/theme.default';
-import { COSMIC_THEME } from './styles/theme.cosmic';
-import { CORPORATE_THEME } from './styles/theme.corporate';
-import { DARK_THEME } from './styles/theme.dark';
-import { MATERIAL_LIGHT_THEME } from './styles/material/theme.material-light';
-import { MATERIAL_DARK_THEME } from './styles/material/theme.material-dark';
-import { GAUZY_LIGHT } from './styles/gauzy/theme.gauzy-light';
-import { GAUZY_DARK } from './styles/gauzy/theme.gauzy-dark';
-
-import { NgxPermissionsModule } from 'ngx-permissions';
 import { WindowModeBlockScrollService } from './services';
 
 import { LanguagesService, OrganizationsService, UsersOrganizationsService, UsersService } from '../@core/services';
 import { HeaderSelectorsModule } from './components/header/selectors/selectors.module';
 import { EmployeeSelectorsModule } from './components/header/selectors/employee/employee.module';
+import { PopupComponent } from './components/popup/popup.component';
+import { ThemeLanguageSelectorService } from './components/theme-sidebar/theme-settings/components/theme-language-selector.service';
 import { SelectorService } from '../@core/services/selector.service';
 import { TimeTrackerModule } from '../@shared/time-tracker/time-tracker.module';
 import { ProjectSelectModule } from '../@shared/project-select/project-select.module';
-import { TranslateModule } from '@gauzy/ui-sdk/i18n';
 import { ThemeSidebarModule } from './components/theme-sidebar/theme-sidebar.module';
-import { MatRippleModule } from '@angular/material/core';
-import { NbAccordionModule, NbToggleModule, NbCardModule } from '@nebular/theme';
 import { GauzyLogoComponent } from './components/gauzy-logo/gauzy-logo.component';
 import { UserMenuComponent } from './components/user-menu/user-menu.component';
 import { UserComponent } from './components/user/user.component';
@@ -59,11 +65,9 @@ import { ThemeLanguageSelectorModule } from './components/theme-sidebar/theme-se
 import { ThemeSelectorModule } from './components/theme-sidebar/theme-settings/components/theme-selector/theme-selector.module';
 import { DateRangePickerModule } from './components/header/selectors/date-range-picker';
 import { WorkspacesModule } from '../@shared/workspaces/workspaces.module';
-import { PopupComponent } from './components/popup/popup.component';
 import { SidebarMenuModule } from '../@shared/sidebar-menu/sidebar-menu.module';
 import { DirectivesModule } from '../@shared/directives/directives.module';
 import { TimeTrackerStatusModule } from '../@shared/time-tracker/components/time-tracker-status/time-tracker-status.module';
-import { ThemeLanguageSelectorService } from './components/theme-sidebar/theme-settings/components/theme-language-selector.service';
 import { TeamSelectModule } from '../@shared/team-select/team-select.module';
 
 const NB_MODULES = [
