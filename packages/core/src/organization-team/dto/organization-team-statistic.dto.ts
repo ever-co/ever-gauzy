b151097e71b33d7dfd5e97d264076f30b34fbf6b
Rahul R. <rahulrathore576@gmail.com>
2024-06-17 11:26:31 +0530

    fix: organization team members timer statistics

--- packages/core/src/organization-team/dto/organization-team-statistic.dto.ts
+++ packages/core/src/organization-team/dto/organization-team-statistic.dto.ts
@@ -1,10 +1,7 @@
 import { ApiPropertyOptional, IntersectionType } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 import { Transform, TransformFnParams } from 'class-transformer';
-import {
-	IDateRangePicker,
-	IOrganizationTeamStatisticInput
-} from '@gauzy/contracts';
+import { IDateRangePicker, IOrganizationTeamStatisticInput } from '@gauzy/contracts';
 import { parseToBoolean } from '@gauzy/common';
 import { DateRangeQueryDTO, RelationsQueryDTO } from './../../shared/dto';
 
@@ -12,16 +9,16 @@ import { DateRangeQueryDTO, RelationsQueryDTO } from './../../shared/dto';
  * DTO for handling requests related to organization team statistics.
  * Combines date range and relations query features.
  */
-export class OrganizationTeamStatisticDTO extends IntersectionType(
-	DateRangeQueryDTO,
-	RelationsQueryDTO
-) implements IOrganizationTeamStatisticInput, IDateRangePicker {
+export class OrganizationTeamStatisticDTO
+	extends IntersectionType(DateRangeQueryDTO, RelationsQueryDTO)
+	implements IOrganizationTeamStatisticInput, IDateRangePicker
+{
 	/**
 	 * Indicates whether the last worked task row should be included in the entity result.
 	 * Default value is set to false.
 	 */
 	@ApiPropertyOptional({ type: Boolean, default: false })
 	@IsOptional()
-	@Transform(({ value }: TransformFnParams) => value ? parseToBoolean(value) : false)
+	@Transform(({ value }: TransformFnParams) => (value ? parseToBoolean(value) : false))
 	readonly withLastWorkedTask: boolean;
 }
