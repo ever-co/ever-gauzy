bd8770bb7f9d37fb498c0a680ef86628dfe58eb6
Ruslan Konviser <evereq@gmail.com>
2024-06-29 18:54:40 +0200

    chore: fix deploy

--- .deploy/webapp/entrypoint.prod.sh
+++ .deploy/webapp/entrypoint.prod.sh
@@ -3,8 +3,11 @@ set -ex
 
 # This Entrypoint used when we run Docker container outside of Docker Compose (e.g. in k8s)
 
+# Use envsubst to create the actual replacements_values.sed file with values from env vars
+envsubst < replacements.sed > replacements_values.sed
+
 # In production we should replace some values in generated JS code
-sed -i -f replacements.sed *.js
+sed -i -f replacements_values.sed *.js
 
 # We may not need to use that env vars now in nginx.config, but we may want later.
 # Also we just need to copy nginx.conf to correct place anyway...
