8ed65b1d8730f0cedaf9bc126ef204328f95fbb8
Rahul R. <rahulrathore576@gmail.com>
2024-07-01 19:43:58 +0530

    fix: moved probot module on specific folder

--- packages/plugins/integration-github/src/index.ts
+++ packages/plugins/integration-github/src/index.ts
@@ -1,6 +1,1 @@
-export * from './lib/hook.decorator';
-export * from './lib/octokit.service';
-export * from './lib/probot.discovery';
-export * from './lib/probot.helpers';
-export * from './lib/probot.module';
-export * from './lib/probot.types';
+export * from './lib/probot';
