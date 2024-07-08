6efae998902a59e990ef742261b723eca77c34dd
Rahul R. <rahulrathore576@gmail.com>
2024-07-08 23:18:59 +0530

    fix: employee job statistics & job status update API

--- packages/core/src/employee/commands/handlers/index.ts
+++ packages/core/src/employee/commands/handlers/index.ts
@@ -2,8 +2,6 @@ import { EmployeeBulkCreateHandler } from './employee.bulk.create.handler';
 import { EmployeeCreateHandler } from './employee.create.handler';
 import { EmployeeGetHandler } from './employee.get.handler';
 import { EmployeeUpdateHandler } from './employee.update.handler';
-import { GetEmployeeJobStatisticsHandler } from './get-employee-job-statistics.handler';
-import { UpdateEmployeeJobSearchStatusHandler } from './update-employee-job-search-status.handler';
 import { UpdateEmployeeTotalWorkedHoursHandler } from './update-employee-total-worked-hours.handler';
 import { WorkingEmployeeGetHandler } from './workig-employee.get.handler';
 
@@ -12,8 +10,6 @@ export const CommandHandlers = [
 	EmployeeBulkCreateHandler,
 	EmployeeGetHandler,
 	UpdateEmployeeTotalWorkedHoursHandler,
-	UpdateEmployeeJobSearchStatusHandler,
-	GetEmployeeJobStatisticsHandler,
 	EmployeeUpdateHandler,
 	WorkingEmployeeGetHandler
 ];
