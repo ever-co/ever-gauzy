bf39640950f6a7c3a673845c2218208b99697a24
Rahul R. <rahulrathore576@gmail.com>
2024-07-15 13:57:48 +0530

    refactor: bring latest role permission to the database for all roles

--- packages/core/src/database/migrations/1679765443208-MigrateRolePermisisons.ts
+++ packages/core/src/database/migrations/1679765443208-MigrateRolePermisisons.ts
@@ -1,59 +1,39 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import * as chalk from "chalk";
+import * as chalk from 'chalk';
 import { RolePermissionUtils } from '../../role-permission/utils';
-import { DatabaseTypeEnum } from "@gauzy/config";
+import { DatabaseTypeEnum } from '@gauzy/config';
 
 export class MigrateRolePermisisons1679765443208 implements MigrationInterface {
-    name = 'MigrateRolePermisisons1679765443208';
+	name = 'MigrateRolePermisisons1679765443208';
 
-    /**
-     * Up Migration
-     *
-     * @param queryRunner
-     */
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        console.log(chalk.yellow(this.name + ' start running!'));
+	/**
+	 * Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		console.log(chalk.yellow(`${this.constructor.name} start running!`));
 
-        switch (queryRunner.connection.options.type) {
-            case DatabaseTypeEnum.sqlite:
-            case DatabaseTypeEnum.betterSqlite3:
-            case DatabaseTypeEnum.postgres:
-                await this.sqlitePostgresMigrateRolePermissions(queryRunner);
-                break;
-            case DatabaseTypeEnum.mysql:
-                await this.mysqlMigrateRolePermissions(queryRunner);
-                break;
-            default:
-                throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
-        }
-    }
-    /**
-     * Down Migration
-     *
-     * @param queryRunner
-     */
-    public async down(queryRunner: QueryRunner): Promise<void> { }
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+			case DatabaseTypeEnum.postgres:
+			case DatabaseTypeEnum.mysql:
+				try {
+					await RolePermissionUtils.migrateRolePermissions(queryRunner);
+				} catch (error) {
+					console.log(chalk.red(`Error while migrating missing role permisions: ${error}`));
+				}
+				break;
+			default:
+				throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
+		}
+	}
 
-    /**
-     * Sqlite | better-sqlite3 | postgres Up Migration
-     *
-     * @param queryRunner
-     */
-    public async sqlitePostgresMigrateRolePermissions(queryRunner: QueryRunner): Promise<any> {
-        console.log(chalk.yellow(this.name + ' start running!'));
-
-        try {
-            await RolePermissionUtils.migrateRolePermissions(queryRunner);
-        } catch (error) {
-            console.log(`Error while migrating missing role permisions`, error);
-        }
-    }
-
-    /**
-     * MySQL Up Migration
-     *
-     * @param queryRunner
-     */
-    public async mysqlMigrateRolePermissions(queryRunner: QueryRunner): Promise<any> { }
-
+	/**
+	 * Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async down(queryRunner: QueryRunner): Promise<void> {}
 }
--- packages/core/src/role-permission/utils.ts
+++ packages/core/src/role-permission/utils.ts
@@ -2,108 +2,221 @@ import { QueryRunner } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 import * as chalk from 'chalk';
 import * as moment from 'moment';
-import { environment } from '@gauzy/config';
-import { PermissionsEnum } from '@gauzy/contracts';
-import { isEmpty } from '@gauzy/common';
+import { DatabaseTypeEnum, environment } from '@gauzy/config';
+import { ID, IRole, ITenant, PermissionsEnum, RolesEnum } from '@gauzy/contracts';
+import { isNotEmpty } from '@gauzy/common';
 import { DEFAULT_ROLE_PERMISSIONS } from './default-role-permissions';
 
 /**
  * Role permissions utils functions.
  */
 export class RolePermissionUtils {
+	/**
+	 * Migrate role permissions
+	 * @param queryRunner - The QueryRunner instance
+	 */
 	public static async migrateRolePermissions(queryRunner: QueryRunner): Promise<void> {
-		/** Removed below permissions for all users in DEMO mode  */
-		const exceptPermissions = [PermissionsEnum.ACCESS_DELETE_ACCOUNT, PermissionsEnum.ACCESS_DELETE_ALL_DATA];
-		/**
-		 * GET all tenants in the system
-		 */
-		const tenants = await queryRunner.connection.manager.query(`SELECT * FROM tenant`);
-		for await (const tenant of tenants) {
-			/**
-			 * GET all roles for specific tenant
-			 */
-			const tenantId = tenant.id;
-			const query = `SELECT * FROM "role" WHERE "role"."tenantId" = $1`;
-			const roles = await queryRunner.connection.manager.query(query, [tenantId]);
+		// Get all tenants from the database
+		const tenants = await this.getAllTenants(queryRunner);
 
+		// Get all permissions, excluding certain ones if in demo mode.
+		const permissions = this.getPermissions();
+
+		// Loop through all tenants
+		for await (const { id: tenantId, name: tenantName } of tenants) {
+			// Get all roles for specific tenant
+			const roles = await this.getRolesByTenantId(queryRunner, tenantId);
+
+			// Loop through all roles
 			for await (const role of roles) {
-				const permissions = Object.values(PermissionsEnum).filter((permission: PermissionsEnum) =>
-					environment.demo ? !exceptPermissions.includes(permission) : true
-				);
-				const { role: roleEnum, defaultEnabledPermissions = [] } = DEFAULT_ROLE_PERMISSIONS.find(
-					(defaultRole) => role.name === defaultRole.role
-				);
+				// Get default role permissions
+				const { role: roleEnum, defaultEnabledPermissions = [] } = this.getDefaultRolePermissions(role);
+
+				// Get role ID
+				const roleId = role.id;
+
+				// Loop through all permissions
 				for await (const permission of permissions) {
-					/**
-					 * Find role permissions already exist or not
-					 */
-					const query = `
-                        SELECT DISTINCT
-                            "distinctAlias"."role_permission_id"
-                        FROM (
-                            SELECT
-                                "role_permission"."id" AS "role_permission_id",
-                                "role_permission"."tenantId" AS "role_permission_tenantId",
-                                "role_permission"."permission" AS "role_permission_permission",
-                                "role_permission"."roleId" AS "role_permission_roleId"
-                            FROM
-                                "role_permission" "role_permission"
-                            INNER JOIN "role" "role"
-                                ON "role"."id"="role_permission"."roleId"
-                            WHERE (
-                                "role_permission"."tenantId" = $1 AND
-                                "role_permission"."permission" = $2 AND
-                                "role"."tenantId" = $3 AND
-                                "role"."name" = $4
-                            )
-                        )
-                        "distinctAlias" ORDER BY "role_permission_id" ASC LIMIT 1
-                    `;
-					const existed = await queryRunner.connection.manager.query(query, [
+					// Check permission existence
+					const isPermissionExisted = await this.checkPermissionExistence(
+						queryRunner,
 						tenantId,
-						permission,
-						tenantId,
-						roleEnum
-					]);
-					if (isEmpty(existed) && role) {
-						console.log(
-							chalk.magenta(
-								`${moment().format(
-									'DD.MM.YYYY HH:mm:ss'
-								)} unlocked missing permission for the tenant: ${tenant.name}`,
-								roleEnum,
-								permission
-							)
+						roleId,
+						permission
+					);
+
+					// If permission is not existed
+					if (!isPermissionExisted) {
+						// Log missing permission
+						const timestamp = moment().format('DD.MM.YYYY HH:mm:ss');
+						const message = `${timestamp} unlocked missing permission for the tenant: ${tenantName}`;
+						console.log(chalk.magenta(message, roleEnum, permission));
+
+						// Missing role permission payload for insert into database
+						const payload = this.getInsertPayload(
+							queryRunner,
+							tenantId,
+							roleId,
+							permission,
+							defaultEnabledPermissions.includes(permission)
 						);
-						/**
-						 * Missing role permission payload for insert into database
-						 */
-						const { id: roleId } = role;
-						const payload = [tenantId, roleId, permission, defaultEnabledPermissions.includes(permission)];
-						console.log(
-							chalk.magenta([
-								tenantId,
-								roleId,
-								permission,
-								defaultEnabledPermissions.includes(permission)
-							])
-						);
-						/**
-						 * SqliteDB Query Runner
-						 *
-						 * @param queryRunner
-						 */
-						if (queryRunner.connection.options.type === 'sqlite') {
-							payload.push(uuidV4());
-							const query = `INSERT INTO "role_permission" ("tenantId", "roleId", "permission", "enabled", "id") VALUES ($1, $2, $3, $4, $5)`;
-							await queryRunner.connection.manager.query(query, payload);
-						} else {
-							const query = `INSERT INTO "role_permission" ("tenantId", "roleId", "permission", "enabled") VALUES($1, $2, $3, $4)`;
-							await queryRunner.connection.manager.query(query, payload);
-						}
+
+						// Insert role permission into database
+						await this.insertRolePermissions(queryRunner, payload);
 					}
 				}
 			}
+		}
+	}
+
+	/**
+	 * Get all tenants from the database
+	 * @param queryRunner - The QueryRunner instance
+	 * @returns A promise that resolves to an array of tenants
+	 */
+	private static async getAllTenants(queryRunner: QueryRunner): Promise<ITenant[]> {
+		const query = `SELECT * FROM tenant`;
+		return await queryRunner.connection.manager.query(query);
+	}
+
+	/**
+	 * Get roles by tenant ID
+	 * @param queryRunner - The QueryRunner instance
+	 * @param tenantId - The tenant ID
+	 * @returns A promise that resolves to an array of roles
+	 */
+	private static async getRolesByTenantId(queryRunner: QueryRunner, tenantId: ID): Promise<IRole[]> {
+		const query = `SELECT * FROM "role" WHERE "tenantId" = $1`;
+		return await queryRunner.connection.manager.query(query, [tenantId]);
+	}
+
+	/**
+	 * Get all permissions, excluding certain ones if in demo mode.
+	 * @returns An array of permissions.
+	 */
+	private static getPermissions(): PermissionsEnum[] {
+		/** Permissions to exclude for all users in DEMO mode */
+		const excludePermissions: PermissionsEnum[] = [
+			PermissionsEnum.ACCESS_DELETE_ACCOUNT,
+			PermissionsEnum.ACCESS_DELETE_ALL_DATA
+		];
+
+		// If in demo mode, exclude certain permissions
+		return environment.demo
+			? Object.values(PermissionsEnum).filter((permission) => !excludePermissions.includes(permission))
+			: Object.values(PermissionsEnum);
+	}
+
+	/**
+	 * Get default role permissions
+	 * @param role - The role
+	 * @returns The default role permissions
+	 */
+	private static getDefaultRolePermissions(role: IRole):
+		| {
+				role: RolesEnum;
+				defaultEnabledPermissions: PermissionsEnum[];
+		  }
+		| undefined {
+		return DEFAULT_ROLE_PERMISSIONS.find((defaultRole) => role.name === defaultRole.role);
+	}
+
+	/**
+	 * Check if the permission already exists
+	 * @param queryRunner - The QueryRunner instance
+	 * @param tenantId - The tenant ID
+	 * @param roleId - The role ID
+	 * @param permission - The permission enum value
+	 * @returns A promise that resolves to a boolean indicating if the permission exists
+	 */
+	private static async checkPermissionExistence(
+		queryRunner: QueryRunner,
+		tenantId: ID,
+		roleId: ID,
+		permission: PermissionsEnum
+	): Promise<boolean> {
+		const query = `
+			SELECT DISTINCT
+				"distinctAlias"."role_permission_id"
+			FROM (
+				SELECT
+					"role_permission"."id" AS "role_permission_id",
+					"role_permission"."tenantId" AS "role_permission_tenantId",
+					"role_permission"."permission" AS "role_permission_permission",
+					"role_permission"."roleId" AS "role_permission_roleId"
+				FROM
+					"role_permission" "role_permission"
+				INNER JOIN "role" "role"
+					ON "role"."id" = "role_permission"."roleId"
+				WHERE (
+					"role_permission"."tenantId" = $1 AND
+					"role_permission"."permission" = $2 AND
+					"role_permission"."roleId" = $3 AND
+					"role"."tenantId" = $4 AND
+					"role"."id" = $5
+				)
+			)
+			"distinctAlias" ORDER BY "role_permission_id" ASC LIMIT 1
+		`;
+
+		const result = await queryRunner.connection.manager.query(query, [
+			tenantId,
+			permission,
+			roleId,
+			tenantId,
+			roleId
+		]);
+		return isNotEmpty(result);
+	}
+
+	/**
+	 * Get insert payload for role permission
+	 * @param queryRunner - The QueryRunner instance
+	 * @param tenantId - The tenant ID
+	 * @param roleId - The role ID
+	 * @param permission - The permission enum value
+	 * @param isEnabled - Boolean indicating if the permission is enabled
+	 * @returns The payload array for the insert query
+	 */
+	private static getInsertPayload(
+		queryRunner: QueryRunner,
+		tenantId: ID,
+		roleId: ID,
+		permission: PermissionsEnum,
+		isEnabled: boolean
+	): any[] {
+		const payload = [tenantId, roleId, permission, isEnabled];
+
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+				payload.push(uuidV4());
+				break;
+			default:
+				break;
+		}
+
+		console.log(chalk.magenta(payload));
+		return payload;
+	}
+
+	/**
+	 * Insert role permissions into the database
+	 * @param queryRunner - The QueryRunner instance
+	 * @param payload - The payload for the insert query
+	 */
+	private static async insertRolePermissions(queryRunner: QueryRunner, payload: any[]): Promise<void> {
+		let query: string;
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+				query = `INSERT INTO "role_permission" ("tenantId", "roleId", "permission", "enabled", "id") VALUES ($1, $2, $3, $4, $5)`;
+				await queryRunner.connection.manager.query(query, payload);
+				break;
+			case DatabaseTypeEnum.postgres:
+				query = `INSERT INTO "role_permission" ("tenantId", "roleId", "permission", "enabled") VALUES($1, $2, $3, $4)`;
+				await queryRunner.connection.manager.query(query, payload);
+				break;
 		}
 	}
 }
