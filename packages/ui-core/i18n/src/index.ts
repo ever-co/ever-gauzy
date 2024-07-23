34e681ba1dce4c89b805d3b34d3d08b8abb2b311
Rahul R. <rahulrathore576@gmail.com>
2024-07-23 16:28:48 +0530

    fix: added custom translate loader

--- packages/ui-core/i18n/src/index.ts
+++ packages/ui-core/i18n/src/index.ts
@@ -4,5 +4,6 @@
 export * from './lib/i18n-translate.module';
 export * from './lib/i18n-translate.service';
 export * from './lib/set-language-base.component';
+export * from './lib/custom-translate-loader';
 export * from './lib/translate-http-loader';
 export * from './lib/translation-base.component';
