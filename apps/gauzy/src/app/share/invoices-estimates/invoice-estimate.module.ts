3d398ea9ee59b836446ced51021d6a6ef159356f
Rahul R. <rahulrathore576@gmail.com>
2024-07-25 18:27:10 +0530

    fix: use ngx-translate/core

--- apps/gauzy/src/app/share/invoices-estimates/invoice-estimate.module.ts
+++ apps/gauzy/src/app/share/invoices-estimates/invoice-estimate.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NbCardModule } from '@nebular/theme';
-import { I18nTranslateModule } from '@gauzy/ui-core/i18n';
+import { TranslateModule } from '@ngx-translate/core';
 import { InvoicesService } from '@gauzy/ui-core/core';
 import { InvoiceEstimateRoutingModule } from './invoice-estimate.routing.module';
 import { InvoiceEstimateComponent } from './invoice-estimate.component';
 import { InvoicesModule } from '../../pages/invoices/invoices.module';
 
 @NgModule({
-	imports: [InvoiceEstimateRoutingModule, InvoicesModule, CommonModule, NbCardModule, I18nTranslateModule.forChild()],
+	imports: [InvoiceEstimateRoutingModule, InvoicesModule, CommonModule, NbCardModule, TranslateModule.forChild()],
 	declarations: [InvoiceEstimateComponent],
 	exports: [InvoiceEstimateComponent],
 	providers: [InvoicesService]
