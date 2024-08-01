a5539caa7e537ff5f58447cdb78ab1f7fdff207b
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-08-01 09:10:03 +0200

    feat: create auto refresh service and store

---
+++ packages/desktop-ui-lib/src/lib/recap/+state/auto-refresh/auto-refresh.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Observable, Subject, switchMap, tap, timer } from 'rxjs';
+import { AUTO_REFRESH_DELAY } from '../../../constants';
+import { AutoRefreshQuery } from './auto-refresh.query';
+import { AutoRefreshStore } from './auto-refresh.store';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AutoRefreshService implements OnDestroy {
+	private readonly _subject$ = new Subject<void>();
+	private readonly _refresh$ = new Subject<void>();
+
+	constructor(private readonly query: AutoRefreshQuery, private readonly store: AutoRefreshStore) {
+		this._subject$.pipe(switchMap(() => this.run$())).subscribe();
+
+		this.enabled$.subscribe((enabled) => {
+			enabled ? this.startRefresh() : this.stopRefresh();
+		});
+	}
+
+	public get enabled$(): Observable<boolean> {
+		return this.query.enabled$;
+	}
+
+	public get enabled(): boolean {
+		return this.query.enabled;
+	}
+
+	public set enabled(enabled: boolean) {
+		this.store.update({ enabled });
+	}
+
+	private startRefresh(): void {
+		this._subject$.next();
+	}
+
+	private stopRefresh(): void {
+		this._subject$.complete();
+	}
+
+	private run$(): Observable<number> {
+		return timer(AUTO_REFRESH_DELAY, AUTO_REFRESH_DELAY).pipe(tap(() => this.refresh()));
+	}
+
+	public refresh(): void {
+		this._refresh$.next();
+	}
+
+	public get refresh$(): Observable<void> {
+		return this._refresh$.asObservable();
+	}
+
+	ngOnDestroy(): void {
+		this._subject$.complete();
+	}
+}
