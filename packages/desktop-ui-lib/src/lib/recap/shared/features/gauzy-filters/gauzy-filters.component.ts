bc9e3e3589c86d7401d335ed4f80a849fea58ad2
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-08-01 09:13:13 +0200

    perf: improve gauzy filter performance

--- packages/desktop-ui-lib/src/lib/recap/shared/features/gauzy-filters/gauzy-filters.component.ts
+++ packages/desktop-ui-lib/src/lib/recap/shared/features/gauzy-filters/gauzy-filters.component.ts
@@ -1,21 +1,9 @@
 import { ChangeContext, Options } from '@angular-slider/ngx-slider';
-import {
-	AfterViewInit,
-	ChangeDetectionStrategy,
-	Component,
-	EventEmitter,
-	Input,
-	OnDestroy,
-	OnInit,
-	Output
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { ITimeLogFilters, PermissionsEnum, TimeFormatEnum, TimeLogSourceEnum, TimeLogType } from '@gauzy/contracts';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateService } from '@ngx-translate/core';
-import { Subject } from 'rxjs';
-import { debounceTime, take, tap } from 'rxjs/operators';
-import { pick } from 'underscore';
-import { TimesheetFilterService } from '../../../services/timesheet';
+import { tap } from 'rxjs';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
@@ -24,7 +12,7 @@ import { TimesheetFilterService } from '../../../services/timesheet';
 	styleUrls: ['./gauzy-filters.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GauzyFiltersComponent implements AfterViewInit, OnInit, OnDestroy {
+export class GauzyFiltersComponent implements OnInit, OnDestroy {
 	// declaration of variables
 	public PermissionsEnum = PermissionsEnum;
 	public TimeLogType = TimeLogType;
@@ -48,31 +36,31 @@ export class GauzyFiltersComponent implements AfterViewInit, OnInit, OnDestroy {
 	};
 	public readonly timeLogSourceSelectors = this.getTimeLogSourceSelectors();
 
-	/*
-	 * Getter & Setter for dynamic enabled/disabled element
-	 */
-	private filters$: Subject<any> = new Subject();
-	private _filters: ITimeLogFilters = {
-		timeFormat: TimeFormatEnum.FORMAT_12_HOURS,
-		source: [],
+	private readonly defaultFilter: ITimeLogFilters = {
+		source: [TimeLogSourceEnum.DESKTOP],
 		logType: [],
 		activityLevel: {
 			start: 0,
 			end: 100
 		}
 	};
-	get filters(): ITimeLogFilters {
+
+	/*
+	 * Getter & Setter for dynamic enabled/disabled element
+	 */
+	private _filters: ITimeLogFilters = this.defaultFilter;
+
+	public get filters(): ITimeLogFilters {
 		return this._filters;
 	}
+
 	@Input() set filters(filters: ITimeLogFilters) {
 		if (filters) {
 			this._filters = filters;
-			this.activityLevel = {
-				start: filters.activityLevel ? filters.activityLevel.start : 0,
-				end: filters.activityLevel ? filters.activityLevel.end : 100
-			};
+			this.activityLevel = filters.activityLevel || this.activityLevel;
 		}
 	}
+
 	@Input() isTimeformat: boolean = false;
 
 	@Output() filtersChange: EventEmitter<ITimeLogFilters> = new EventEmitter();
@@ -80,35 +68,17 @@ export class GauzyFiltersComponent implements AfterViewInit, OnInit, OnDestroy {
 	/**
 	 * define constructor
 	 */
-	constructor(
-		private readonly timesheetFilterService: TimesheetFilterService,
-		public readonly translateService: TranslateService
-	) {}
+	constructor(public readonly translateService: TranslateService) {}
 
 	ngOnInit() {
-		if (this.saveFilters) {
-			this.timesheetFilterService.filter$
-				.pipe(
-					take(1),
-					tap((filters: ITimeLogFilters) => {
-						this.filters = Object.assign({}, pick(filters, 'source', 'activityLevel', 'logType'));
-					}),
-					untilDestroyed(this)
-				)
-				.subscribe();
-		}
-		this.filters$
+		this.filtersChange
+			.asObservable()
 			.pipe(
-				debounceTime(400),
 				tap(() => (this.hasFilterApplies = this.hasFilter())),
-				tap(() => this.filtersChange.emit(this.arrangedFilters())),
 				untilDestroyed(this)
 			)
 			.subscribe();
-	}
-
-	ngAfterViewInit() {
-		this.triggerFilterChange();
+		this.hasFilterApplies = this.hasFilter();
 	}
 
 	/**
@@ -116,28 +86,38 @@ export class GauzyFiltersComponent implements AfterViewInit, OnInit, OnDestroy {
 	 *
 	 * @param activity - The change context containing the new activity level values.
 	 */
-	setActivityLevel(activity: ChangeContext): void {
+	public setActivityLevel(activity: ChangeContext): void {
 		this.filters.activityLevel = {
 			start: activity.value,
 			end: activity.highValue
 		};
 		this.activityLevel = this.filters.activityLevel;
-		this.triggerFilterChange();
+		this.notifyFilterChange();
+	}
+
+	public setSource(source: TimeLogSourceEnum[]) {
+		this.filters.source = source;
+		this.notifyFilterChange();
+	}
+
+	public setLogType(type: TimeLogType[]) {
+		this.filters.logType = type;
+		this.notifyFilterChange();
 	}
 
 	/**
-	 * Triggers the filter change event.
+	 * Notify the filter change event.
 	 */
-	triggerFilterChange(): void {
-		this.filters$.next(true);
+	public notifyFilterChange(): void {
+		this.filtersChange.emit(this.filters);
 	}
 
 	/**
 	 * Clears all filters and triggers a filter change.
 	 */
-	clearFilters(): void {
-		this.filters = this.timesheetFilterService.clear();
-		this.triggerFilterChange();
+	public clearFilters(): void {
+		this.filters = this.defaultFilter;
+		this.notifyFilterChange();
 	}
 
 	/**
@@ -145,22 +125,13 @@ export class GauzyFiltersComponent implements AfterViewInit, OnInit, OnDestroy {
 	 *
 	 * @returns True if any filters are applied, otherwise false.
 	 */
-	hasFilter(): boolean {
+	public hasFilter(): boolean {
 		return (
-			(this._filters.source && this._filters.source.length >= 1) ||
-			(this._filters.logType && this._filters.logType.length >= 1) ||
+			(this.filters.source && this.filters.source.length >= 1) ||
+			(this.filters.logType && this.filters.logType.length >= 1) ||
 			(this.activityLevel && this.activityLevel.end < 100) ||
 			(this.activityLevel && this.activityLevel.start > 0)
 		);
-	}
-
-	/**
-	 *
-	 * @returns
-	 */
-	arrangedFilters(): ITimeLogFilters {
-		Object.keys(this.filters).forEach((key) => (this.filters[key] === undefined ? delete this.filters[key] : {}));
-		return this.filters;
 	}
 
 	/**
@@ -168,9 +139,9 @@ export class GauzyFiltersComponent implements AfterViewInit, OnInit, OnDestroy {
 	 *
 	 * @param timeformat The new time format.
 	 */
-	timeFormatChanged(timeFormat: TimeFormatEnum): void {
+	public timeFormatChanged(timeFormat: TimeFormatEnum): void {
 		this.filters.timeFormat = timeFormat;
-		this.triggerFilterChange();
+		this.notifyFilterChange();
 	}
 
 	/**
@@ -178,45 +149,19 @@ export class GauzyFiltersComponent implements AfterViewInit, OnInit, OnDestroy {
 	 *
 	 * @param timezone The new time zone.
 	 */
-	timeZoneChanged(timeZone: string): void {
+	public timeZoneChanged(timeZone: string): void {
 		this.filters.timeZone = timeZone;
-		this.triggerFilterChange();
+		this.notifyFilterChange();
 	}
 
 	/**
 	 * Generate Dynamic Timelog Source Selector
 	 */
-	getTimeLogSourceSelectors(): Array<{ label: string; value: TimeLogSourceEnum }> {
-		return [
-			{
-				label: this.translateService.instant('TIMESHEET.SOURCES.WEB_TIMER'),
-				value: TimeLogSourceEnum.WEB_TIMER
-			},
-			{
-				label: this.translateService.instant('TIMESHEET.SOURCES.DESKTOP'),
-				value: TimeLogSourceEnum.DESKTOP
-			},
-			{
-				label: this.translateService.instant('TIMESHEET.SOURCES.MOBILE'),
-				value: TimeLogSourceEnum.MOBILE
-			},
-			{
-				label: this.translateService.instant('TIMESHEET.SOURCES.UPWORK'),
-				value: TimeLogSourceEnum.UPWORK
-			},
-			{
-				label: this.translateService.instant('TIMESHEET.SOURCES.HUBSTAFF'),
-				value: TimeLogSourceEnum.HUBSTAFF
-			},
-			{
-				label: this.translateService.instant('TIMESHEET.SOURCES.BROWSER_EXTENSION'),
-				value: TimeLogSourceEnum.BROWSER_EXTENSION
-			},
-			{
-				label: this.translateService.instant('TIMESHEET.SOURCES.TEAMS'),
-				value: TimeLogSourceEnum.TEAMS
-			}
-		];
+	public getTimeLogSourceSelectors(): Array<{ label: string; value: TimeLogSourceEnum }> {
+		return Object.keys(TimeLogSourceEnum).map((key) => ({
+			label: this.translateService.instant(`TIMESHEET.SOURCES.${key}`),
+			value: TimeLogSourceEnum[key as keyof typeof TimeLogSourceEnum]
+		}));
 	}
 
 	ngOnDestroy(): void {}
