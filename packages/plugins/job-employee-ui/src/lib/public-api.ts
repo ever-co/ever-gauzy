8d61dba4fa58ee63b7edda4e4fa193bcfa70fae7
Rahul R. <rahulrathore576@gmail.com>
2024-08-05 15:54:54 +0530

    feat: job employees list & statistics

--- packages/plugins/job-employee-ui/src/lib/public-api.ts
+++ packages/plugins/job-employee-ui/src/lib/public-api.ts
@@ -1,4 +1,6 @@
 /**
  * Public API Surface of job-employee-ui
  */
-export const placeholders = '@gauzy/plugin-job-employee-ui';
+export * from './job-employee.module';
+export * from './job-employee.routes';
+export * from './components/job-employee/job-employee.component';
