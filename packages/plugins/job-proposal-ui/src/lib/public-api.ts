994d09eacf2f809d6b864ba8040f8ff81ee06462
Rahul R. <rahulrathore576@gmail.com>
2024-08-11 16:32:36 +0530

    feat: job proposal template UI plugin/library

--- packages/plugins/job-proposal-ui/src/lib/public-api.ts
+++ packages/plugins/job-proposal-ui/src/lib/public-api.ts
@@ -1,4 +1,7 @@
 /**
  * Public API Surface of job-proposal-ui
  */
-export * from './job-proposal.module';
+export * from './job-proposal-template.module';
+export * from './job-proposal-template.routes';
+export * from './components/proposal-template/proposal-template.component';
+export * from './components/add-edit-proposal-template/add-edit-proposal-template.component';
