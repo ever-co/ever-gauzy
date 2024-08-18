26d83e100adc18bc5d30f8bbabe8ff57adadac32
samuelmbabhazi <samuelmbabhazi@gmail.com>
2024-08-18 22:39:46 +0200

    [Feature] Add Search Functionality and "Show Rejected" Checkbox for Candidate in Schedule Interview.

--- packages/ui-core/shared/src/lib/candidate/selectors/base-candidate-selector.component.ts
+++ packages/ui-core/shared/src/lib/candidate/selectors/base-candidate-selector.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { EMPTY, Observable, catchError, filter, map, of, switchMap } from 'rxjs';
+import { BehaviorSubject, EMPTY, Observable, catchError, combineLatest, filter, map, of, switchMap } from 'rxjs';
 import { CandidateStatusEnum, ICandidate, IOrganization, IPagination } from '@gauzy/contracts';
 import { distinctUntilChange } from '@gauzy/ui-core/common';
 import { CandidatesService, ErrorHandlingService, Store } from '@gauzy/ui-core/core';
@@ -10,7 +10,8 @@ import { CandidatesService, ErrorHandlingService, Store } from '@gauzy/ui-core/c
 	template: ''
 })
 export class BaseCandidateSelectorComponent implements OnInit {
-	public candidates$: Observable<ICandidate[]>; // Observable for an array of Organization candidates
+	public candidates$: Observable<ICandidate[]>;
+	public showRejected$ = new BehaviorSubject<boolean>(false);
 
 	constructor(
 		protected readonly _store: Store,
@@ -19,16 +20,16 @@ export class BaseCandidateSelectorComponent implements OnInit {
 	) {}
 
 	ngOnInit(): void {
-		this.candidates$ = this._store.selectedOrganization$.pipe(
-			filter((organization: IOrganization) => !!organization),
+		this.candidates$ = combineLatest([this._store.selectedOrganization$, this.showRejected$]).pipe(
+			filter(([organization]) => !!organization),
 			distinctUntilChange(),
-			switchMap((organization: IOrganization) => {
-				// Extract project properties
+			switchMap(([organization, showRejected]) => {
 				const { id: organizationId, tenantId } = organization;
-				// Ensure there is a valid organization
 				if (!organizationId) {
-					return of([]); // No valid organization, return empty array of candidates
+					return of([]);
 				}
+
+				const status = showRejected ? CandidateStatusEnum.REJECTED : CandidateStatusEnum.APPLIED;
 
 				return this._candidatesService
 					.getAll(['user'], {
@@ -36,22 +37,23 @@ export class BaseCandidateSelectorComponent implements OnInit {
 						tenantId,
 						isActive: true,
 						isArchived: false,
-						status: CandidateStatusEnum.APPLIED
+						status: status
 					})
 					.pipe(
 						map(({ items }: IPagination<ICandidate>) => items),
 						catchError((error) => {
-							// Handle and log errors
 							this._errorHandlingService.handleError(error);
 							return EMPTY;
 						}),
-						// Handle component lifecycle to avoid memory leaks
 						untilDestroyed(this)
 					);
 			}),
-			// Handle component lifecycle to avoid memory leaks
 			untilDestroyed(this)
 		);
+	}
+
+	toggleShowRejected() {
+		this.showRejected$.next(!this.showRejected$.value);
 	}
 
 	ngOnDestroy(): void {}
--- packages/ui-core/shared/src/lib/candidate/selectors/candidate-select/candidate-select.component.ts
+++ packages/ui-core/shared/src/lib/candidate/selectors/candidate-select/candidate-select.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import { Observable } from 'rxjs';
+import { map, startWith, switchMap } from 'rxjs/operators';
 import { ICandidate } from '@gauzy/contracts';
 import { Store } from '@gauzy/ui-core/core';
 import { CandidatesService, ErrorHandlingService } from '@gauzy/ui-core/core';
@@ -14,28 +16,52 @@ import { BaseCandidateSelectorComponent } from '../base-candidate-selector.compo
 })
 export class CandidateSelectComponent extends BaseCandidateSelectorComponent implements OnInit {
 	public select: FormControl = new FormControl();
+	public showRejected: boolean = false;
+	public searchControl: FormControl = new FormControl();
+	public filteredCandidates$: Observable<ICandidate[]>;
+	private candidatesMap: Map<ICandidate['id'], ICandidate> = new Map();
+
 	@Input() placeholder: string;
 	@Input() disabled = false;
 	@Input() public set reset(value: boolean | null) {
 		if (value) {
 			this.select.reset();
+			this.searchControl.setValue(''); // Clear the search input as well
 		}
 	}
 	@Output() selectedChange = new EventEmitter();
+	@ViewChild('autoInput') input;
 
 	constructor(store: Store, candidatesService: CandidatesService, errorHandlingService: ErrorHandlingService) {
 		super(store, candidatesService, errorHandlingService);
 	}
 
 	override ngOnInit(): void {
-		super.ngOnInit(); // Call the parent class's ngOnInit function
+		super.ngOnInit();
+		this.filteredCandidates$ = this.searchControl.valueChanges.pipe(
+			startWith(''),
+			switchMap((term) =>
+				this.candidates$.pipe(
+					map((candidates) => {
+						this.candidatesMap.clear(); // Clear previous candidates map
+						candidates.forEach((candidate) => this.candidatesMap.set(candidate.id, candidate));
+						return this.filterCandidatesList(candidates, term);
+					})
+				)
+			)
+		);
 	}
 
-	/**
-	 *
-	 * @param candidate
-	 */
-	onCandidateSelected(candidate: ICandidate['id']): void {
-		this.selectedChange.emit(candidate);
+	private filterCandidatesList(candidates: ICandidate[], term: string): ICandidate[] {
+		const lowerTerm = term.toLowerCase();
+		return candidates.filter((candidate) => candidate.user?.name.toLowerCase().includes(lowerTerm));
+	}
+
+	onCandidateSelected(candidateId: ICandidate['id']): void {
+		const selectedCandidate = this.candidatesMap.get(candidateId);
+		if (selectedCandidate) {
+			this.searchControl.setValue(selectedCandidate.user?.name || '');
+			this.selectedChange.emit(candidateId);
+		}
 	}
 }
