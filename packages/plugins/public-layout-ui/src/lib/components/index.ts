1072ad7554f73f81c8fd5055661d4081cfbd09e5
Rahul R. <rahulrathore576@gmail.com>
2024-08-24 12:35:17 +0530

    fix: rename invoice-estimate component

--- packages/plugins/public-layout-ui/src/lib/components/index.ts
+++ packages/plugins/public-layout-ui/src/lib/components/index.ts
@@ -1,5 +1,5 @@
 import { EmployeeComponent } from './employee/employee.component';
-import { InvoiceEstimateViewComponent } from './invoices-estimates/invoice-estimate-view.component';
+import { InvoiceEstimateViewComponent } from './invoice-estimate/invoice-estimate-view.component';
 import { PublicPageEmployeeMutationComponent } from './mutation/public-page-employee-mutation/public-page-employee-mutation.component';
 import { PublicPageOrganizationMutationComponent } from './mutation/public-page-organization-mutation/public-page-organization-mutation.component';
 import { OrganizationComponent } from './organization/organization.component';
