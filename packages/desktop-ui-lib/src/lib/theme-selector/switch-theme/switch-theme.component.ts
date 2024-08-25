7aa55353e0e8481c19f0b5c94d4c81e5307c0174
syns2191 <sutralian@gmail.com>
2024-08-25 23:30:55 +0700

    style: remove unused imported variable

--- packages/desktop-ui-lib/src/lib/theme-selector/switch-theme/switch-theme.component.ts
+++ packages/desktop-ui-lib/src/lib/theme-selector/switch-theme/switch-theme.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { ElectronService } from '../../electron/services';
 
