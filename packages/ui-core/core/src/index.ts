187e1db70919fdbac130ba4b30f65b286a39da60
Rahul R. <rahulrathore576@gmail.com>
2024-08-27 14:36:26 +0530

    feat: dynamic tabs registry component

--- packages/ui-core/core/src/index.ts
+++ packages/ui-core/core/src/index.ts
@@ -2,6 +2,7 @@
  * Public API Surface of @gauzy/ui-core/core
  */
 export * from './lib/auth';
+export * from './lib/common/component-registry.types';
 export * from './lib/components';
 export * from './lib/core.module';
 export * from './lib/guards';
