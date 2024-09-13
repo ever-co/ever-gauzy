d7d983a4f8537dfb6442009fb2aa60d91b453ad2
GloireMutaliko21 <mufunyig@gmail.com>
2024-09-13 13:48:26 +0200

    feat: added reaction commands and handlers

--- packages/core/src/reaction/reaction.service.ts
+++ packages/core/src/reaction/reaction.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { TenantAwareCrudService } from './../core/crud';
+import { UserService } from '../user/user.service';
+import { Reaction } from './reaction.entity';
+import { TypeOrmReactionRepository } from './repository/type-orm-reaction.repository';
+import { MikroOrmReactionRepository } from './repository/mikro-orm-reaction.repository';
 
 @Injectable()
-export class ReactionService {}
+export class ReactionService extends TenantAwareCrudService<Reaction> {
+	constructor(
+		readonly typeOrmReactionRepository: TypeOrmReactionRepository,
+		readonly mikroOrmReactionRepository: MikroOrmReactionRepository,
+		private readonly userService: UserService
+	) {
+		super(typeOrmReactionRepository, mikroOrmReactionRepository);
+	}
+}
--- packages/core/src/reaction/repository/index.ts
+++ packages/core/src/reaction/repository/index.ts
@@ -1,1 +1,2 @@
 export * from './mikro-orm-reaction.repository';
+export * from './type-orm-reaction.repository';
