264a756da8dd38648e64f5e334b0379e6c22a5ca
Rahul R. <rahulrathore576@gmail.com>
2024-09-16 19:12:34 +0530

    revert: fix: email templates rendering migrations

--- packages/core/src/database/migrations/1680539459969-UpdateEmailTemplates.ts
+++ packages/core/src/database/migrations/1680539459969-UpdateEmailTemplates.ts
@@ -1,6 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import * as chalk from 'chalk';
 import { EmailTemplateEnum } from '@gauzy/contracts';
+import { DatabaseTypeEnum } from '@gauzy/config';
 import { EmailTemplateUtils } from '../../email-template/utils';
 
 export class UpdateEmailTemplates1680539459969 implements MigrationInterface {
@@ -14,16 +15,17 @@ export class UpdateEmailTemplates1680539459969 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		console.log(chalk.yellow(this.name + ' start running!'));
 
-		// Migrate email templates for all templates
-		const templates = Object.values(EmailTemplateEnum);
-
-		// Iterate over each template and migrate it
-		for await (const template of templates) {
-			try {
-				await EmailTemplateUtils.migrateEmailTemplates(queryRunner, template);
-			} catch (error) {
-				console.log(`Error while migrating missing email templates for ${template}`, error);
-			}
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+			case DatabaseTypeEnum.postgres:
+				await this.sqlitePostgresUpdateEmailTemplates(queryRunner);
+				break;
+			case DatabaseTypeEnum.mysql:
+				await this.mysqlUpdateEmailTemplates(queryRunner);
+				break;
+			default:
+				throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
 		}
 	}
 	/**
@@ -32,4 +34,32 @@ export class UpdateEmailTemplates1680539459969 implements MigrationInterface {
 	 * @param queryRunner
 	 */
 	public async down(queryRunner: QueryRunner): Promise<void> {}
+
+	/**
+	 * Sqlite | better-sqlite3 | postgres Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async sqlitePostgresUpdateEmailTemplates(queryRunner: QueryRunner): Promise<any> {
+		console.log(chalk.yellow(this.name + ' start running!'));
+
+		// Migrate email templates for all templates
+		const templates = Object.values(EmailTemplateEnum);
+
+		// Iterate over each template and update it
+		for await (const template of templates) {
+			try {
+				await EmailTemplateUtils.migrateEmailTemplates(queryRunner, template);
+			} catch (error) {
+				console.log(`Error while migrating missing email templates for ${template}`, error);
+			}
+		}
+	}
+
+	/**
+	 * MySQL Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async mysqlUpdateEmailTemplates(queryRunner: QueryRunner): Promise<any> {}
 }
--- packages/core/src/database/migrations/1701353754397-MigrateEmailTemplates.ts
+++ packages/core/src/database/migrations/1701353754397-MigrateEmailTemplates.ts
@@ -1,6 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 import * as chalk from 'chalk';
 import { EmailTemplateEnum } from '@gauzy/contracts';
+import { DatabaseTypeEnum } from '@gauzy/config';
 import { EmailTemplateUtils } from '../../email-template/utils';
 
 export class MigrateEmailTemplates1701353754397 implements MigrationInterface {
@@ -14,10 +15,38 @@ export class MigrateEmailTemplates1701353754397 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		console.log(chalk.yellow(this.name + ' start running!'));
 
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+			case DatabaseTypeEnum.postgres:
+				await this.sqlitePostgresMigrateEmailTemplates(queryRunner);
+				break;
+			case DatabaseTypeEnum.mysql:
+				await this.mysqlMigrateEmailTemplates(queryRunner);
+				break;
+			default:
+				throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
+		}
+	}
+	/**
+	 * Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async down(queryRunner: QueryRunner): Promise<void> {}
+
+	/**
+	 * Sqlite | better-sqlite3 | postgres Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async sqlitePostgresMigrateEmailTemplates(queryRunner: QueryRunner): Promise<any> {
+		console.log(chalk.yellow(this.name + ' start running!'));
+
 		// Migrate email templates for all templates
 		const templates = Object.values(EmailTemplateEnum);
 
-		// Iterate over each template and migrate it
+		// Iterate over each template and migrate the data
 		for await (const template of templates) {
 			try {
 				await EmailTemplateUtils.migrateEmailTemplates(queryRunner, template);
@@ -28,9 +57,9 @@ export class MigrateEmailTemplates1701353754397 implements MigrationInterface {
 	}
 
 	/**
-	 * Down Migration
+	 * MySQL Up Migration
 	 *
 	 * @param queryRunner
 	 */
-	public async down(queryRunner: QueryRunner): Promise<void> {}
+	public async mysqlMigrateEmailTemplates(queryRunner: QueryRunner): Promise<any> {}
 }
