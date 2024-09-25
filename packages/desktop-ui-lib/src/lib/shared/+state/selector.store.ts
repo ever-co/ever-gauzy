73f7362789d285e77369c100231f5f13cc6dccce
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-09-25 08:07:40 +0200

    feat: improve selector with pagination params

--- packages/desktop-ui-lib/src/lib/shared/+state/selector.store.ts
+++ packages/desktop-ui-lib/src/lib/shared/+state/selector.store.ts
@@ -10,6 +10,15 @@ export abstract class SelectorStore<T> extends Store<ISelector<T>> {
 		this.update({ data });
 	}
 
+	public updateInfiniteList(list: { data: T[]; total: number }): void {
+		const { data, total } = list;
+		const items = this.getValue().data;
+		this.update({
+			data: [...new Map([...items, ...data].map((item) => [item['id'], item])).values()],
+			total
+		});
+	}
+
 	public updateSelected(selected: T | string): void {
 		if (!selected) {
 			this.update({ selected: null });
@@ -34,5 +43,10 @@ export abstract class SelectorStore<T> extends Store<ISelector<T>> {
 
 	public resetToInitialState(): void {
 		this.update(this.initialState);
+	}
+
+	public next(): void {
+		const current = this.getValue().page;
+		this.update({ page: current + 1 });
 	}
 }
