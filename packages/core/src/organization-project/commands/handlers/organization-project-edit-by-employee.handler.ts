7193286461f47946fa6c70feec683b2bc38e1c4c
GloireMutaliko21 <mufunyig@gmail.com>
2024-09-25 11:05:33 +0200

    fix: remove unecessary code extensions

--- packages/core/src/organization-project/commands/handlers/organization-project-edit-by-employee.handler.ts
+++ packages/core/src/organization-project/commands/handlers/organization-project-edit-by-employee.handler.ts
@@ -1,16 +1,12 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { UpdateEntityByMembersHandler } from '../../../shared/handlers';
 import { OrganizationProjectService } from '../../organization-project.service';
 import { OrganizationProjectEditByEmployeeCommand } from '../organization-project-edit-by-employee.command';
 
 @CommandHandler(OrganizationProjectEditByEmployeeCommand)
 export class OrganizationProjectEditByEmployeeHandler
-	extends UpdateEntityByMembersHandler
 	implements ICommandHandler<OrganizationProjectEditByEmployeeCommand>
 {
-	constructor(readonly organizationProjectService: OrganizationProjectService) {
-		super(organizationProjectService);
-	}
+	constructor(readonly organizationProjectService: OrganizationProjectService) {}
 
 	/**
 	 * Executes the organization project edit command by an employee.
@@ -20,6 +16,7 @@ export class OrganizationProjectEditByEmployeeHandler
 	 */
 	public async execute(command: OrganizationProjectEditByEmployeeCommand): Promise<any> {
 		// Extracts the input from the command and executes the command logic
-		return await this.organizationProjectService.updateByEmployee(command.input);
+		const { input } = command;
+		return await this.organizationProjectService.updateByEmployee(input);
 	}
 }
