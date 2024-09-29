5d0809a11c26f076b75ab295ffa2d49c963e5e50
Rahul R. <rahulrathore576@gmail.com>
2024-09-29 17:23:00 +0530

    fix: don't be use API to call employee

--- packages/ui-core/shared/src/lib/components/avatar/avatar.component.ts
+++ packages/ui-core/shared/src/lib/components/avatar/avatar.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ID } from '@gauzy/contracts';
-import { EmployeesService } from '@gauzy/ui-core/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ID, IEmployee } from '@gauzy/contracts';
 
 @Component({
 	selector: 'ngx-avatar',
@@ -11,17 +10,28 @@ import { map } from 'rxjs/operators';
 	styleUrls: ['./avatar.component.scss']
 })
 export class AvatarComponent implements OnInit {
+	public online$: Observable<boolean>;
+
 	@Input() size: 'lg' | 'sm' | 'md' = 'md';
 	@Input() src: string;
 	@Input() appendCaption: string;
 	@Input() caption: string;
-	@Input() id: string;
+	@Input() id: ID;
 	@Input() isOption: boolean;
 
-	online$: Observable<boolean>;
+	/**
+	 * A class member and getter/setter for managing an employee object.
+	 */
+	private _employee = new BehaviorSubject<any>(null);
+	@Input() set employee(value: IEmployee) {
+		this._employee.next(value);
+	}
+	get employee(): IEmployee {
+		return this._employee.getValue();
+	}
 
 	// Added for set component value when used for angular2-smart-table renderer.
-	@Input() set value(object) {
+	@Input() set value(object: any) {
 		for (const key in object) {
 			if (Object.prototype.hasOwnProperty.call(object, key)) {
 				this[key] = object[key];
@@ -30,32 +40,22 @@ export class AvatarComponent implements OnInit {
 	}
 
 	/**
-	 * A class member and getter/setter for managing a string name.
+	 * A class member and getter/setter for managing an employee name.
 	 */
-	_name: string;
-
-	/**
-	 * Getter method for the 'name' property, providing access to the private _name variable.
-	 * @returns The current value of the 'name' property.
-	 */
+	private _name: string;
+	@Input() set name(value: string) {
+		this._name = value;
+	}
 	get name(): string {
 		return this._name;
 	}
 
-	/**
-	 * Setter method for the 'name' property, annotated with @Input().
-	 * @param value - The new value to set for the 'name' property.
-	 */
-	@Input() set name(value: string) {
-		this._name = value;
-	}
-
-	constructor(private readonly router: Router, private readonly _employeeService: EmployeesService) {}
+	constructor(private readonly router: Router) {}
 
 	ngOnInit() {
 		if (this.id) {
-			this.online$ = this._employeeService
-				.getEmployeeById(this.id)
+			this.online$ = this._employee
+				.asObservable()
 				.pipe(map((employee) => employee?.isOnline && !employee?.isAway));
 		}
 	}
