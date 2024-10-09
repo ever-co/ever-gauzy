330f2b3bc6110e104fa097c898e7af715cd48b40
Rahul R. <rahulrathore576@gmail.com>
2024-10-09 13:57:38 +0530

    fix: #8339 date range picker parameters to URLs

--- packages/ui-core/core/src/lib/services/selector-builder/date-range-picker-builder.service.ts
+++ packages/ui-core/core/src/lib/services/selector-builder/date-range-picker-builder.service.ts
@@ -5,6 +5,7 @@ import { isNotEmpty } from '@gauzy/ui-core/common';
 import { IDateRangePicker } from '@gauzy/contracts';
 import { IDatePickerConfig } from './selector-builder-types';
 
+// Define the default date picker configuration
 export const DEFAULT_DATE_PICKER_CONFIG: IDatePickerConfig = {
 	unitOfTime: 'week',
 	isLockDatePicker: false,
@@ -14,30 +15,34 @@ export const DEFAULT_DATE_PICKER_CONFIG: IDatePickerConfig = {
 	isDisablePastDate: false
 };
 
+// Define the default date range picker
 export const DEFAULT_DATE_RANGE: IDateRangePicker = {
 	startDate: moment().startOf(DEFAULT_DATE_PICKER_CONFIG.unitOfTime).toDate(),
 	endDate: moment().endOf(DEFAULT_DATE_PICKER_CONFIG.unitOfTime).toDate(),
 	isCustomDate: false
 };
 
-@Injectable({
-	providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class DateRangePickerBuilderService {
 	private _datePickerConfig$: BehaviorSubject<IDatePickerConfig | null> = new BehaviorSubject(null);
 	public datePickerConfig$: Observable<IDatePickerConfig | null> = this._datePickerConfig$.asObservable();
 
-	public dates$: BehaviorSubject<IDateRangePicker> = new BehaviorSubject(DEFAULT_DATE_RANGE);
-
 	private _selectedDateRange$: BehaviorSubject<IDateRangePicker | null> = new BehaviorSubject(null);
 	public selectedDateRange$: Observable<IDateRangePicker | null> = this._selectedDateRange$.asObservable();
 
+	public dates$: BehaviorSubject<IDateRangePicker> = new BehaviorSubject(DEFAULT_DATE_RANGE);
+
 	/**
-	 * Getter & Setter for selected date range
+	 * Gets the currently selected date range.
 	 */
 	get selectedDateRange(): IDateRangePicker {
 		return this.dates$.getValue();
 	}
+	/**
+	 * Sets a new selected date range.
+	 *
+	 * @param range - The new date range to set.
+	 */
 	set selectedDateRange(range: IDateRangePicker) {
 		if (isNotEmpty(range)) {
 			this._selectedDateRange$.next(range);
@@ -45,22 +50,31 @@ export class DateRangePickerBuilderService {
 	}
 
 	/**
-	 * Override date range picker default configuration
-	 *
-	 * @param options
+	 * Gets the current date picker configuration.
 	 */
-	setDatePickerConfig(options: IDatePickerConfig) {
-		if (isNotEmpty(options)) {
-			this._datePickerConfig$.next(options);
+	get datePickerConfig(): IDatePickerConfig {
+		return this._datePickerConfig$.getValue();
+	}
+
+	/**
+	 * Sets a new date picker configuration.
+	 *
+	 * @param config - The new configuration to set.
+	 */
+	setDatePickerConfig(config: IDatePickerConfig) {
+		// If the provided config is not empty, merge it with the current config
+		if (isNotEmpty(config)) {
+			const updatedConfig = { ...this.datePickerConfig, ...config };
+			this._datePickerConfig$.next(updatedConfig);
 		}
 	}
 
 	/**
-	 * Override date range picker default values
+	 * Updates the date range picker with new start and end dates.
 	 *
-	 * @param dates An object containing the start date, end date, and possibly other properties related to the date range picker.
+	 * @param dates - An object containing the start date and end date.
 	 */
-	async setDateRangePicker(dates: IDateRangePicker) {
+	setDateRangePicker(dates: IDateRangePicker) {
 		// Check if dates object is not empty
 		if (isNotEmpty(dates)) {
 			// Update the BehaviorSubject `dates$` with the new dates
@@ -75,19 +89,12 @@ export class DateRangePickerBuilderService {
 	 */
 	refreshDateRangePicker(date: moment.Moment) {
 		// Extract the unit of time from the current date picker configuration
-		const { unitOfTime } = this._datePickerConfig$.getValue();
-
+		const unitOfTime = this.datePickerConfig.unitOfTime;
 		// Calculate the start and end dates based on the provided date and unit of time
-		const startDate = moment(date).startOf(unitOfTime);
-		const endDate = moment(date).endOf(unitOfTime);
+		const startDate = moment(date).startOf(unitOfTime).toDate();
+		const endDate = moment(date).endOf(unitOfTime).toDate();
 
-		// Update the date range picker with the new start and end dates
-		this.setDateRangePicker({
-			startDate: startDate.toDate(),
-			endDate: endDate.toDate()
-		});
-
-		// Maintain the current date picker configuration
-		this.setDatePickerConfig(this._datePickerConfig$.getValue());
+		this.setDateRangePicker({ startDate, endDate }); // Update the date range picker with the new start and end dates
+		this.setDatePickerConfig(this._datePickerConfig$.getValue()); // Maintain the current date picker configuration
 	}
 }
--- packages/ui-core/shared/src/lib/selectors/date-range-picker/date-picker.interface.ts
+++ packages/ui-core/shared/src/lib/selectors/date-range-picker/date-picker.interface.ts
@@ -1,27 +1,31 @@
 // Represents a date range with a start date and end date.
 export interface IDateRangePicker {
-    startDate: Date;      // The start date of the date range.
-    endDate: Date;        // The end date of the date range.
-    isCustomDate?: boolean; // Optional flag to indicate if it's a custom date range.
+	startDate: Date; // The start date of the date range.
+	endDate: Date; // The end date of the date range.
+	isCustomDate?: boolean; // Optional flag to indicate if it's a custom date range.
 }
 
 // Represents a time period with a start date and end date using moment.js.
 export interface TimePeriod {
-    startDate: moment.Moment; // The start date of the time period.
-    endDate: moment.Moment;   // The end date of the time period.
+	startDate: moment.Moment; // The start date of the time period.
+	endDate: moment.Moment; // The end date of the time period.
 }
 
 // Represents a collection of date ranges where each range is indexed by a string key.
 export interface DateRanges {
-    [index: string]: [moment.Moment, moment.Moment]; // Key-value pairs of date ranges.
+	[index: string]: [moment.Moment, moment.Moment]; // Key-value pairs of date ranges.
 }
 
 // Enum defining keys for common date range options.
 export enum DateRangeKeyEnum {
-    TODAY = 'Today',
-    YESTERDAY = 'Yesterday',
-    CURRENT_WEEK = 'Current week',
-    LAST_WEEK = 'Last week',
-    CURRENT_MONTH = 'Current month',
-    LAST_MONTH = 'Last month'
+	TODAY = 'Today',
+	YESTERDAY = 'Yesterday',
+	CURRENT_WEEK = 'Current Week',
+	LAST_WEEK = 'Last Week',
+	CURRENT_MONTH = 'Current Month',
+	LAST_MONTH = 'Last Month'
+}
+
+export interface DateRangeClicked {
+	label: DateRangeKeyEnum;
 }
