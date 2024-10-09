240529eadad680582059e99432d3d1c86e6277cc
Rahul R. <rahulrathore576@gmail.com>
2024-10-09 18:32:46 +0530

    feat: add standardWorkHoursPerDay column to organization entity

--- packages/core/src/organization/dto/organization-bonuses.dto.ts
+++ packages/core/src/organization/dto/organization-bonuses.dto.ts
@@ -1,27 +1,7 @@
-import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsOptional, IsEnum, IsNumber, Min, Max, ValidateIf } from "class-validator";
-import { BonusTypeEnum, DEFAULT_PROFIT_BASED_BONUS } from "@gauzy/contracts";
+import { PickType } from '@nestjs/swagger';
+import { Organization } from '../organization.entity';
 
 /**
  * Organization Bonuses DTO validation
  */
-export class OrganizationBonusesDTO {
-
-    @ApiPropertyOptional({
-		type: () => Number,
-		example: DEFAULT_PROFIT_BASED_BONUS
-	})
-    @ValidateIf((it) => it.bonusType)
-	@IsNumber()
-	@Min(0)
-	@Max(100)
-	readonly bonusPercentage: number;
-
-    @ApiPropertyOptional({
-		enum: BonusTypeEnum,
-		example: BonusTypeEnum.PROFIT_BASED_BONUS
-	})
-	@IsOptional()
-	@IsEnum(BonusTypeEnum)
-	readonly bonusType: BonusTypeEnum;
-}
+export class OrganizationBonusesDTO extends PickType(Organization, ['bonusPercentage', 'bonusType'] as const) {}
--- packages/core/src/organization/dto/organization-public-setting.dto.ts
+++ packages/core/src/organization/dto/organization-public-setting.dto.ts
@@ -1,53 +1,52 @@
-import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsOptional, IsBoolean } from "class-validator";
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsBoolean } from 'class-validator';
 
 /**
  * Organization Public Setting DTO
  */
 export class OrganizationPublicSettingDTO {
-
-    @ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@ApiPropertyOptional({ type: () => Boolean })
+	@IsOptional()
 	@IsBoolean()
 	readonly show_income: boolean;
 
-    @ApiPropertyOptional({ type: () => Boolean })
+	@ApiPropertyOptional({ type: () => Boolean })
 	@IsOptional()
-    @IsBoolean()
+	@IsBoolean()
 	readonly show_profits: boolean;
 
-    @ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@ApiPropertyOptional({ type: () => Boolean })
+	@IsOptional()
 	@IsBoolean()
 	readonly show_bonuses_paid: boolean;
 
 	@ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@IsOptional()
 	@IsBoolean()
 	readonly show_total_hours: boolean;
 
 	@ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@IsOptional()
 	@IsBoolean()
 	readonly show_minimum_project_size: boolean;
 
 	@ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@IsOptional()
 	@IsBoolean()
 	readonly show_projects_count: boolean;
 
 	@ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@IsOptional()
 	@IsBoolean()
 	readonly show_clients_count: boolean;
 
 	@ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@IsOptional()
 	@IsBoolean()
 	readonly show_clients: boolean;
 
 	@ApiPropertyOptional({ type: () => Boolean })
-    @IsOptional()
+	@IsOptional()
 	@IsBoolean()
 	readonly show_employees_count: boolean;
 }
