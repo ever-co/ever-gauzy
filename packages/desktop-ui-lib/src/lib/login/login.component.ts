087fa417ef23da6b43a92070fb10ed53a8e12283
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-11 01:38:54 +0500

    [Feat] Create Reusable Component and Add Workspace Links (#8383)
    * feat: export social links
    
    * feat: create a shared logo component
    
    * feat: create a shared social links component
    
    * feat: add reusable patterns to constants
    
    * feat: add link to magic workspace signin and use reusable logo, links and switch
    
    * feat: SwitchThemeModule, LogoComponent, and SocialLinksComponent to NgxLoginModule

--- packages/desktop-ui-lib/src/lib/login/login.component.ts
+++ packages/desktop-ui-lib/src/lib/login/login.component.ts
@@ -1,9 +1,7 @@
 import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { NB_AUTH_OPTIONS, NbAuthService, NbLoginComponent } from '@nebular/auth';
 import { UntilDestroy } from '@ngneat/until-destroy';
-import { GAUZY_ENV } from '../constants/app.constants';
 import { ElectronService } from '../electron/services';
 import { LanguageElectronService } from '../language/language-electron.service';
 
@@ -23,10 +21,7 @@ export class NgxLoginComponent extends NbLoginComponent implements OnInit {
 		public readonly cdr: ChangeDetectorRef,
 		public readonly router: Router,
 		@Inject(NB_AUTH_OPTIONS)
-		options: any,
-		private readonly _domSanitizer: DomSanitizer,
-		@Inject(GAUZY_ENV)
-		private readonly _environment: any
+		options: any
 	) {
 		super(nbAuthService, options, cdr, router);
 	}
@@ -41,9 +36,5 @@ export class NgxLoginComponent extends NbLoginComponent implements OnInit {
 
 	public register(): void {
 		this.electronService.shell.openExternal('https://app.gauzy.co/#/auth/register');
-	}
-
-	public get logoUrl(): SafeResourceUrl {
-		return this._domSanitizer.bypassSecurityTrustResourceUrl(this._environment.PLATFORM_LOGO);
 	}
 }
