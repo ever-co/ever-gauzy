b8292bc01307fe809dcf2711d81af91e49ad48e2
GloireMutaliko21 <mufunyig@gmail.com>
2024-10-10 09:05:25 +0200

    fix: remove unused DTO

--- packages/core/src/tasks/dto/index.ts
+++ packages/core/src/tasks/dto/index.ts
@@ -2,4 +2,3 @@ export * from './create-task.dto';
 export * from './task-max-number-query.dto';
 export * from './update-task.dto';
 export * from './get-task-by-id.dto';
-export * from './get-tasks-by-view-filters.dto';
