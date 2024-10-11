b7fa479a72e18a7e4bbe711d1dd713b5dc814732
Rahul R. <rahulrathore576@gmail.com>
2024-10-11 19:12:04 +0530

    fix: missing role-permission module

--- packages/core/src/api-call-log/api-call-log.module.ts
+++ packages/core/src/api-call-log/api-call-log.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolePermissionModule } from '../role-permission/role-permission.module';
 import { ApiCallLog } from './api-call-log.entity';
 import { ApiCallLogService } from './api-call-log.service';
 import { ApiCallLogController } from './api-call-log.controller';
 import { TypeOrmApiCallLogRepository } from './repository/type-orm-api-call-log.repository';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([ApiCallLog]), MikroOrmModule.forFeature([ApiCallLog])],
+	imports: [TypeOrmModule.forFeature([ApiCallLog]), MikroOrmModule.forFeature([ApiCallLog]), RolePermissionModule],
 	controllers: [ApiCallLogController],
 	providers: [ApiCallLogService, TypeOrmApiCallLogRepository],
 	exports: [TypeOrmModule, MikroOrmModule, ApiCallLogService, TypeOrmApiCallLogRepository]
