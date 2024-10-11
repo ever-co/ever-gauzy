b42e937c904b5ebd1a6ba49e72caf25bebfddb4b
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-10-11 16:18:02 +0200

    fix: task deselect on refresh

--- packages/desktop-ui-lib/src/lib/time-tracker/time-tracker.component.ts
+++ packages/desktop-ui-lib/src/lib/time-tracker/time-tracker.component.ts
@@ -40,6 +40,7 @@ import {
 	Observable,
 	of,
 	Subject,
+	switchMap,
 	tap
 } from 'rxjs';
 import * as _ from 'underscore';
@@ -609,17 +610,20 @@ export class TimeTrackerComponent implements OnInit, AfterViewInit {
 			.pipe(
 				filter(
 					(remoteTimer: IRemoteTimer) =>
+						!!remoteTimer.lastLog &&
 						this.xor(this.start, remoteTimer.running) &&
 						!this._isLockSyncProcess &&
 						this._isReady &&
 						this.inQueue.size === 0
 				),
-				tap(async (remoteTimer: IRemoteTimer) => {
-					this.projectSelectorService.selected = remoteTimer.lastLog.projectId;
-					this.taskSelectorService.selected = remoteTimer.lastLog.taskId;
-					this.noteService.note = remoteTimer.lastLog.description;
-					this.teamSelectorService.selected = remoteTimer.lastLog.organizationTeamId;
-					this.clientSelectorService.selected = remoteTimer.lastLog.organizationContactId;
+				switchMap(async (remoteTimer: IRemoteTimer) => {
+					this.timeTrackerFormService.setState({
+						clientId: remoteTimer.lastLog.organizationContactId,
+						teamId: remoteTimer.lastLog.organizationTeamId,
+						projectId: remoteTimer.lastLog.projectId,
+						note: remoteTimer.lastLog.description,
+						taskId: remoteTimer.lastLog.taskId
+					});
 					if (!this.isProcessingEnabled) {
 						await this.toggleStart(remoteTimer.running, false);
 					}
@@ -775,6 +779,13 @@ export class TimeTrackerComponent implements OnInit, AfterViewInit {
 				if (!this._isReady && this.appSetting?.alwaysOn) {
 					this.electronService.ipcRenderer.send('show_ao');
 				}
+				this.timeTrackerFormService.setState({
+					clientId: arg.organizationContactId,
+					teamId: arg.organizationTeamId,
+					projectId: arg.projectId,
+					taskId: arg.taskId,
+					note: arg.note
+				});
 				const parallelizedTasks: Promise<void>[] = [
 					this.loadStatuses(),
 					this.clientSelectorService.load(),
