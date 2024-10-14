35ffe1982af89d9df923fc4772dea0fda489bc47
GloireMutaliko21 <mufunyig@gmail.com>
2024-10-14 07:00:23 +0200

    fix: favorite entity type use global entity enum

--- packages/core/src/core/decorators/is-favoritable.ts
+++ packages/core/src/core/decorators/is-favoritable.ts
@@ -1,7 +1,7 @@
-import { FavoriteEntityEnum } from '@gauzy/contracts';
+import { EntityEnum } from '@gauzy/contracts';
 import { SetMetadata } from '@nestjs/common';
 
 export const FAVORITE_SERVICE = 'FAVORITE_SERVICE';
 export const FAVORITABLE_TYPE = 'favoriteEntity';
 
-export const FavoriteService = (type: FavoriteEntityEnum) => SetMetadata(FAVORITABLE_TYPE, type);
+export const FavoriteService = (type: EntityEnum) => SetMetadata(FAVORITABLE_TYPE, type);
