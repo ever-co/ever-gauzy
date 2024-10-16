b523d07d0ef15477d872f7c1264650f9431da154
Rahul R. <rahulrathore576@gmail.com>
2024-10-16 12:53:09 +0530

    fix(integration-hubstaff): prevent memory leaks in lazy-loaded modules

--- packages/plugins/integration-hubstaff-ui/src/lib/integration-hubstaff-ui.module.ts
+++ packages/plugins/integration-hubstaff-ui/src/lib/integration-hubstaff-ui.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { filter, merge, tap } from 'rxjs';
 import {
 	NbActionsModule,
 	NbButtonModule,
@@ -16,22 +15,24 @@ import {
 	NbToggleModule,
 	NbTooltipModule
 } from '@nebular/theme';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { NgxPermissionsModule, NgxPermissionsService } from 'ngx-permissions';
-import { LanguagesEnum } from '@gauzy/contracts';
-import { distinctUntilChange } from '@gauzy/ui-core/common';
-import { Store } from '@gauzy/ui-core/core';
-import { HttpLoaderFactory, I18nService } from '@gauzy/ui-core/i18n';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { HttpLoaderFactory } from '@gauzy/ui-core/i18n';
 import { getBrowserLanguage, SmartDataViewLayoutModule, SharedModule } from '@gauzy/ui-core/shared';
 import { IntegrationHubstaffRoutes } from './integration-hubstaff.routes';
+import { IntegrationHubstaffLayoutComponent } from './integration-hubstaff.layout.component';
 import { HubstaffComponent } from './components/hubstaff/hubstaff.component';
 import { HubstaffAuthorizeComponent } from './components/hubstaff-authorize/hubstaff-authorize.component';
 import { SettingsDialogComponent } from './components/settings-dialog/settings-dialog.component';
 
 @NgModule({
-	declarations: [HubstaffAuthorizeComponent, HubstaffComponent, SettingsDialogComponent],
+	declarations: [
+		IntegrationHubstaffLayoutComponent,
+		HubstaffAuthorizeComponent,
+		HubstaffComponent,
+		SettingsDialogComponent
+	],
 	imports: [
 		NbActionsModule,
 		NbButtonModule,
@@ -61,44 +62,4 @@ import { SettingsDialogComponent } from './components/settings-dialog/settings-d
 		SmartDataViewLayoutModule
 	]
 })
-@UntilDestroy()
-export class IntegrationHubstaffModule {
-	constructor(
-		readonly _translateService: TranslateService,
-		readonly _ngxPermissionsService: NgxPermissionsService,
-		readonly _store: Store,
-		readonly _i18nService: I18nService
-	) {
-		this.initializeUiPermissions(); // Initialize UI permissions
-		this.initializeUiLanguagesAndLocale(); // Initialize UI languages and Update Locale
-		console.log(`integration hubstaff ui module plugin initialized`);
-	}
-
-	/**
-	 * Initialize UI permissions
-	 */
-	private initializeUiPermissions() {
-		// Load permissions
-		const permissions = this._store.userRolePermissions.map(({ permission }) => permission);
-		this._ngxPermissionsService.flushPermissions(); // Flush permissions
-		this._ngxPermissionsService.loadPermissions(permissions); // Load permissions
-	}
-
-	/**
-	 * Initialize UI languages and Update Locale
-	 */
-	private initializeUiLanguagesAndLocale() {
-		// Observable that emits when preferred language changes.
-		const preferredLanguage$ = merge(this._store.preferredLanguage$, this._i18nService.preferredLanguage$).pipe(
-			distinctUntilChange(),
-			filter((lang: string | LanguagesEnum) => !!lang),
-			tap((lang: string | LanguagesEnum) => {
-				this._translateService.use(lang);
-			}),
-			untilDestroyed(this)
-		);
-
-		// Subscribe to initiate the stream
-		preferredLanguage$.subscribe();
-	}
-}
+export class IntegrationHubstaffModule {}
--- packages/plugins/integration-hubstaff-ui/src/lib/integration-hubstaff.routes.ts
+++ packages/plugins/integration-hubstaff-ui/src/lib/integration-hubstaff.routes.ts
@@ -2,27 +2,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HubstaffAuthorizeComponent } from './components/hubstaff-authorize/hubstaff-authorize.component';
 import { HubstaffComponent } from './components/hubstaff/hubstaff.component';
+import { IntegrationHubstaffLayoutComponent } from './integration-hubstaff.layout.component';
 
 @NgModule({
 	imports: [
 		RouterModule.forChild([
 			{
 				path: '',
-				component: HubstaffAuthorizeComponent,
-				data: {
-					state: true
-				}
-			},
-			{
-				path: 'regenerate',
-				component: HubstaffAuthorizeComponent,
-				data: {
-					state: false
-				}
-			},
-			{
-				path: ':id',
-				component: HubstaffComponent
+				component: IntegrationHubstaffLayoutComponent,
+				children: [
+					{
+						path: '',
+						component: HubstaffAuthorizeComponent,
+						data: {
+							state: true
+						}
+					},
+					{
+						path: 'regenerate',
+						component: HubstaffAuthorizeComponent,
+						data: {
+							state: false
+						}
+					},
+					{
+						path: ':id',
+						component: HubstaffComponent
+					}
+				]
 			}
 		])
 	],
