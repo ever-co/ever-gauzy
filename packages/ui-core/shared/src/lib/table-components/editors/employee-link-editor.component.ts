b4c5c91cacb5ed66dae4be51ed37ebe9b4e96943
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2024-10-17 17:37:50 +0200

    [Fix] Employee Field Shows [Object Object] on Edit and Should Be Non-editable on Job Page

---
+++ packages/ui-core/shared/src/lib/table-components/editors/employee-link-editor.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IEmployee, IUser } from '@gauzy/contracts';
+import { Cell, DefaultEditor } from 'angular2-smart-table';
+
+@Component({
+	template: `
+		<ng-container *ngIf="value">
+			<a *ngIf="value?.name">
+				<img *ngIf="value.imageUrl" width="18px" height="18px" [src]="value.imageUrl" />
+				<div class="names-wrapper">
+					{{ value.name }}
+				</div>
+			</a>
+		</ng-container>
+	`,
+	styleUrls: ['../employee-links/employee-links.component.scss']
+})
+export class EmployeeLinkEditorComponent extends DefaultEditor implements OnInit {
+	@Input() cell!: Cell; // Input to access the cell data
+	value!: IUser;
+
+	constructor() {
+		super();
+	}
+
+	ngOnInit() {
+		const employee: IEmployee | undefined = this.cell.getRow().getData();
+		if (employee.user) {
+			this.value = employee.user;
+		}
+	}
+}
