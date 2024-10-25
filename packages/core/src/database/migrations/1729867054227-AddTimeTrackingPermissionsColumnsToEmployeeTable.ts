21b3b2a823420aa561494e16d2d7c859e9150832
Rahul R. <rahulrathore576@gmail.com>
2024-10-25 20:15:20 +0530

    fix(migration): add time tracking settings for employee entity (PostgreSQL)

---
+++ packages/core/src/database/migrations/1729867054227-AddTimeTrackingPermissionsColumnsToEmployeeTable.ts
@@ -0,0 +1,104 @@
+import { Logger } from '@nestjs/common';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { yellow } from 'chalk';
+import { DatabaseTypeEnum } from '@gauzy/config';
+
+export class AddTimeTrackingPermissionsColumnsToEmployeeTable1729867054227 implements MigrationInterface {
+	name = 'AddTimeTrackingPermissionsColumnsToEmployeeTable1729867054227';
+
+	/**
+	 * Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		Logger.debug(yellow(this.name + ' start running!'), 'Migration');
+
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+				await this.sqliteUpQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.postgres:
+				await this.postgresUpQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.mysql:
+				await this.mysqlUpQueryRunner(queryRunner);
+				break;
+			default:
+				throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
+		}
+	}
+
+	/**
+	 * Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+				await this.sqliteDownQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.postgres:
+				await this.postgresDownQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.mysql:
+				await this.mysqlDownQueryRunner(queryRunner);
+				break;
+			default:
+				throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
+		}
+	}
+
+	/**
+	 * PostgresDB Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async postgresUpQueryRunner(queryRunner: QueryRunner): Promise<any> {
+		await queryRunner.query(`ALTER TABLE "employee" ADD "allowManualTime" boolean NOT NULL DEFAULT false`);
+		await queryRunner.query(`ALTER TABLE "employee" ADD "allowModifyTime" boolean NOT NULL DEFAULT false`);
+		await queryRunner.query(`ALTER TABLE "employee" ADD "allowDeleteTime" boolean NOT NULL DEFAULT false`);
+	}
+
+	/**
+	 * PostgresDB Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async postgresDownQueryRunner(queryRunner: QueryRunner): Promise<any> {
+		await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "allowDeleteTime"`);
+		await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "allowModifyTime"`);
+		await queryRunner.query(`ALTER TABLE "employee" DROP COLUMN "allowManualTime"`);
+	}
+
+	/**
+	 * SqliteDB and BetterSQlite3DB Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async sqliteUpQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+
+	/**
+	 * SqliteDB and BetterSQlite3DB Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async sqliteDownQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+
+	/**
+	 * MySQL Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async mysqlUpQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+
+	/**
+	 * MySQL Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async mysqlDownQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+}
