b3c82ca2d3c3ad59fafe2c10964dad3e93d8261d
Ruslan Konviser <evereq@gmail.com>
2024-10-27 11:09:49 +0100

    feat: remove SQlite3 native package, switch all to Better SQLite!

--- packages/desktop-api/src/app/database.module.ts
+++ packages/desktop-api/src/app/database.module.ts
@@ -2,36 +2,36 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { EntityCaseNamingStrategy } from '@mikro-orm/core';
-import { SqliteDriver, Options as MikroOrmSqliteOptions } from '@mikro-orm/sqlite';
+import { BetterSqliteDriver, Options as MikroOrmSqliteOptions } from '@mikro-orm/better-sqlite';
 import { Wakatime } from '@gauzy/integration-wakatime';
 
 const coreEntities = [Wakatime];
 const dbPath = process.env.GAUZY_USER_PATH ? `${process.env.GAUZY_USER_PATH}/gauzy.sqlite3` : '';
 
 @Module({
-    imports: [
-        // TypeORM DB Config (SQLite3)
-        TypeOrmModule.forRootAsync({
-            useFactory: () => ({
-                type: 'sqlite',
-                database: dbPath,
-                keepConnectionAlive: true,
-                logging: 'all',
-                logger: 'file', //Removes console logging, instead logs all queries in a file ormlogs.log
-                synchronize: true,
-                entities: coreEntities
-            })
-        }),
-        // MikroORM DB Config (SQLite3)
-        MikroOrmModule.forRootAsync({
-            useFactory: (): MikroOrmSqliteOptions => ({
-                driver: SqliteDriver,
-                dbName: dbPath,
-                entities: coreEntities,
-                namingStrategy: EntityCaseNamingStrategy,
-                debug: ['query'] // by default set to false only
-            })
-        }),
-    ]
+	imports: [
+		// TypeORM DB Config (SQLite3)
+		TypeOrmModule.forRootAsync({
+			useFactory: () => ({
+				type: 'better-sqlite3',
+				database: dbPath,
+				keepConnectionAlive: true,
+				logging: 'all',
+				logger: 'file', //Removes console logging, instead logs all queries in a file ormlogs.log
+				synchronize: true,
+				entities: coreEntities
+			})
+		}),
+		// MikroORM DB Config (SQLite3)
+		MikroOrmModule.forRootAsync({
+			useFactory: (): MikroOrmSqliteOptions => ({
+				driver: BetterSqliteDriver,
+				dbName: dbPath,
+				entities: coreEntities,
+				namingStrategy: EntityCaseNamingStrategy,
+				debug: ['query'] // by default set to false only
+			})
+		})
+	]
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
