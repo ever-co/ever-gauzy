f3f42b714e953cbd1a1ba07123793dc32a557127
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2024-10-30 09:21:32 +0200

    Extend organization project module service

--- packages/ui-core/core/src/lib/services/organization/organization-projects-module.service.ts
+++ packages/ui-core/core/src/lib/services/organization/organization-projects-module.service.ts
@@ -1,40 +1,18 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {
-	IOrganizationProjectModule,
-	IPagination,
-	ID,
-	IOrganizationProjectModuleCreateInput,
-	IOrganizationProjectModuleUpdateInput
-} from '@gauzy/contracts';
+import { IOrganizationProjectModule, IPagination, ID } from '@gauzy/contracts';
 import { API_PREFIX } from '@gauzy/ui-core/common';
+import { CrudService } from '../crud';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class OrganizationProjectModuleService {
-	private readonly API_URL = `${API_PREFIX}/organization-project-module`;
+export class OrganizationProjectModuleService extends CrudService<IOrganizationProjectModule> {
+	private static readonly API_URL = `${API_PREFIX}/organization-project-module`;
 
-	constructor(private readonly http: HttpClient) {}
-
-	/**
-	 * Create a new project module with the provided data.
-	 * @param {IOrganizationProjectModuleCreateInput} data - The data for creating the new project module.
-	 * @returns An Observable that emits the newly created project module.
-	 */
-	create(data: IOrganizationProjectModuleCreateInput): Observable<IOrganizationProjectModule> {
-		return this.http.post<IOrganizationProjectModule>(this.API_URL, data);
-	}
-
-	/**
-	 * Update an existing project module identified by its ID with the new data.
-	 * @param {ID} id - The unique identifier of the project module to update.
-	 * @param {IOrganizationProjectModuleUpdateInput} data - The new data for updating the project module.
-	 * @returns An Observable that emits the updated project module or result.
-	 */
-	update(id: ID, data: IOrganizationProjectModuleUpdateInput): Observable<IOrganizationProjectModule> {
-		return this.http.put<IOrganizationProjectModule>(`${this.API_URL}/${id}`, data);
+	constructor(http: HttpClient) {
+		super(http, OrganizationProjectModuleService.API_URL);
 	}
 
 	/**
@@ -68,15 +46,6 @@ export class OrganizationProjectModuleService {
 	}
 
 	/**
-	 * Retrieve all project modules with pagination parameters.
-	 * @param params - The pagination parameters for filtering all project modules.
-	 * @returns An Observable that emits the paginated list of project modules.
-	 */
-	findAll(params: HttpParams): Observable<IPagination<IOrganizationProjectModule>> {
-		return this.http.get<IPagination<IOrganizationProjectModule>>(this.API_URL, { params });
-	}
-
-	/**
 	 * Find a specific project module by its unique identifier.
 	 * @param id - The unique identifier of the project module.
 	 * @param params - Additional query parameters if required.
@@ -84,14 +53,5 @@ export class OrganizationProjectModuleService {
 	 */
 	findById(id: ID, params: HttpParams): Observable<IOrganizationProjectModule> {
 		return this.http.get<IOrganizationProjectModule>(`${this.API_URL}/${id}`, { params });
-	}
-
-	/**
-	 * Delete an existing project module by its unique identifier.
-	 * @param id - The unique identifier of the project module to delete.
-	 * @returns An Observable that emits the result of the delete operation.
-	 */
-	delete(id: ID): Observable<void> {
-		return this.http.delete<void>(`${this.API_URL}/${id}`);
 	}
 }
