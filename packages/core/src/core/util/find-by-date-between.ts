d0be38af3b2fc4ea3db1da365122112e586b3b60
GloireMutaliko21 <mufunyig@gmail.com>
2024-11-21 09:05:01 +0200

    fix: improve by coderabbit

--- packages/core/src/core/util/find-by-date-between.ts
+++ packages/core/src/core/util/find-by-date-between.ts
@@ -1,7 +1,7 @@
 import { BadRequestException } from '@nestjs/common';
 import { SelectQueryBuilder } from 'typeorm';
 
-type AllowedFields = 'startDate' | 'dueDate' | 'createdAt';
+type AllowedFields = 'startDate' | 'dueDate' | 'endDate' | 'createdAt';
 
 function isValidField(field: string): field is AllowedFields {
 	return ['startDate', 'dueDate', 'endDate', 'createdAt'].includes(field);
@@ -28,16 +28,20 @@ export function addBetween<T>(
 		throw new BadRequestException(`Invalid field name: ${field}`);
 	}
 
-	if (from && to) {
-		if (from > to) {
+	if (from || to) {
+		if (from && to && from > to) {
 			throw new BadRequestException('"From" date must not be after "to" date');
 		}
-
 		// Convert dates to UTC for consistent comparison
-		const utcFrom = from.toISOString();
-		const utcTo = to.toISOString();
-
-		query.andWhere(p(`"${query.alias}"."${field}" BETWEEN :from AND :to`), { from: utcFrom, to: utcTo });
+		const utcFrom = from?.toISOString();
+		const utcTo = to?.toISOString();
+		if (from && to) {
+			query.andWhere(p(`"${query.alias}"."${field}" BETWEEN :from AND :to`), { from: utcFrom, to: utcTo });
+		} else if (from) {
+			query.andWhere(p(`"${query.alias}"."${field}" >= :from`), { from: utcFrom });
+		} else if (to) {
+			query.andWhere(p(`"${query.alias}"."${field}" <= :to`), { to: utcTo });
+		}
 	}
 	return query;
 }
