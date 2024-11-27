354bdf4087cea33221586df22ece0fcdc08fa095
GloireMutaliko21 <mufunyig@gmail.com>
2024-11-27 06:37:31 +0200

    feat: add subscription model

--- packages/contracts/src/subscription.model.ts
+++ packages/contracts/src/subscription.model.ts
@@ -1,3 +1,17 @@
-import { IBasePerEntityType, IBasePerTenantAndOrganizationEntityModel } from './base-entity.model';
+import { IBasePerEntityType, IBasePerTenantAndOrganizationEntityModel, ID } from './base-entity.model';
+import { IUser } from './user.model';
 
-export interface ISubscription extends IBasePerTenantAndOrganizationEntityModel, IBasePerEntityType {}
+export interface ISubscription extends IBasePerTenantAndOrganizationEntityModel, IBasePerEntityType {
+	subscriptionType: SubscriptionTypeEnum;
+	userId: ID;
+	user?: IUser;
+}
+
+export enum SubscriptionTypeEnum {
+	MANUAL = 'manual',
+	MENTION = 'mention',
+	ASSIGNMENT = 'assignment',
+	COMMENT = 'comment'
+}
+
+export interface ISubscriptionCreateInput extends Omit<ISubscription, 'subscriptionType'> {}
