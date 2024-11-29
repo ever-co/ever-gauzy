79abcddf08df372a252bff5a5c4bf1973e2177ad
GloireMutaliko21 <mufunyig@gmail.com>
2024-11-29 09:06:48 +0200

    feat: task mentions and subscription

--- packages/core/src/tasks/task.service.ts
+++ packages/core/src/tasks/task.service.ts
@@ -31,6 +31,7 @@ import { PaginationParams, TenantAwareCrudService } from './../core/crud';
 import { addBetween } from './../core/util';
 import { RequestContext } from '../core/context';
 import { TaskViewService } from './views/view.service';
+import { MentionService } from '../mention/mention.service';
 import { ActivityLogService } from '../activity-log/activity-log.service';
 import { Task } from './task.entity';
 import { TypeOrmOrganizationSprintTaskHistoryRepository } from './../organization-sprint/repository/type-orm-organization-sprint-task-history.repository';
@@ -46,6 +47,7 @@ export class TaskService extends TenantAwareCrudService<Task> {
 		readonly mikroOrmTaskRepository: MikroOrmTaskRepository,
 		readonly typeOrmOrganizationSprintTaskHistoryRepository: TypeOrmOrganizationSprintTaskHistoryRepository,
 		private readonly taskViewService: TaskViewService,
+		private readonly mentionService: MentionService,
 		private readonly activityLogService: ActivityLogService
 	) {
 		super(typeOrmTaskRepository, mikroOrmTaskRepository);
@@ -62,14 +64,14 @@ export class TaskService extends TenantAwareCrudService<Task> {
 		try {
 			const tenantId = RequestContext.currentTenantId() || input.tenantId;
 			const userId = RequestContext.currentUserId();
-			const { organizationSprintId } = input;
+			const { mentionIds, ...data } = input;
 
 			// Find task relations
 			const relations = this.typeOrmTaskRepository.metadata.relations.map((relation) => relation.propertyName);
 
 			const task = await this.findOneByIdString(id, { relations });
 
-			if (input.projectId && input.projectId !== task.projectId) {
+			if (data.projectId && data.projectId !== task.projectId) {
 				const { organizationId, projectId } = task;
 
 				// Get the maximum task number for the project
@@ -88,21 +90,27 @@ export class TaskService extends TenantAwareCrudService<Task> {
 
 			// Update the task with the provided data
 			const updatedTask = await super.create({
-				...input,
+				...data,
 				id
 			});
 
 			// Register Task Sprint moving history
+			const { organizationSprintId } = data;
 			if (organizationSprintId && organizationSprintId !== task.organizationSprintId) {
 				await this.typeOrmOrganizationSprintTaskHistoryRepository.save({
 					fromSprintId: task.organizationSprintId || organizationSprintId, // Use incoming sprint ID if the task's organizationSprintId was previously null or undefined
 					toSprintId: organizationSprintId,
 					taskId: updatedTask.id,
 					movedById: userId,
-					reason: input.taskSprintMoveReason,
-					organizationId: input.organizationId,
+					reason: data.taskSprintMoveReason,
+					organizationId: data.organizationId,
 					tenantId
 				});
+			}
+
+			// Synchronize mentions
+			if (data.description) {
+				await this.mentionService.updateEntityMentions(BaseEntityEnum.Task, id, mentionIds);
 			}
 
 			// Generate the activity log
@@ -117,7 +125,7 @@ export class TaskService extends TenantAwareCrudService<Task> {
 				organizationId,
 				tenantId,
 				task,
-				input
+				data
 			);
 
 			// Return the updated Task
