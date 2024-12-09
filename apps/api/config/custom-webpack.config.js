db68621368762d4d3776271eefa792b81e23cb78
Rahul R. <rahulrathore576@gmail.com>
2024-12-09 20:28:04 +0530

    chore: add resolve alias generation for Nx libraries

--- apps/api/config/custom-webpack.config.js
+++ apps/api/config/custom-webpack.config.js
@@ -1,5 +1,9 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+console.log('Using custom Webpack Config -> __dirname: ' + __dirname);
+console.log('Using custom Webpack Config -> process.cwd: ' + process.cwd());
 
 // Nx plugins for webpack.
 module.exports = composePlugins(
@@ -15,12 +19,20 @@ module.exports = composePlugins(
 			}),
 		];
 
+		// Add resolve aliases for Nx libraries
+		config.resolve.alias = {
+			...(config.resolve.alias || {}),
+			'@gauzy/core': path.join(__dirname, '../../dist/packages/core')
+		}
+
 		// Disable watch for the 'public' folder
 		config.watchOptions = {
 			ignored: ['**/public/**/*'], // Ignore changes in the 'public' folder
 			aggregateTimeout: 300, // Delay rebuild after the first change (optional)
 			poll: false, // Use polling or not
 		};
+
+		console.log('Using custom Webpack Config -> config: ' + JSON.stringify(config, null, 2));
 
 		// Update the webpack config as needed here.
 		// e.g. `config.plugins.push(new MyPlugin())`
