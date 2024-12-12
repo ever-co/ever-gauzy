4e90bde14b9a3d6161182349c34712166f66f780
GloireMutaliko21 <mufunyig@gmail.com>
2024-12-12 14:49:34 +0200

    feat: employee setting commands

--- packages/core/src/lib/employee-setting/employee-setting.module.ts
+++ packages/core/src/lib/employee-setting/employee-setting.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RouterModule } from '@nestjs/core';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { CommandHandlers } from './commands/handlers';
 import { EmployeeSetting } from './employee-setting.entity';
 import { EmployeeSettingService } from './employee-setting.service';
 import { EmployeeSettingController } from './employee-setting.controller';
@@ -15,7 +16,7 @@ import { RolePermissionModule } from '../role-permission/role-permission.module'
 		RolePermissionModule
 	],
 	controllers: [EmployeeSettingController],
-	providers: [EmployeeSettingService],
+	providers: [EmployeeSettingService, ...CommandHandlers],
 	exports: [EmployeeSettingService]
 })
-export class EmployeeSettingModule { }
+export class EmployeeSettingModule {}
