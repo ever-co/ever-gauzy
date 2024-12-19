712d64c88737dc9cf95c428333ebb272341554b1
GloireMutaliko21 <mufunyig@gmail.com>
2024-12-19 12:21:22 +0200

    fix: dashboard entity fields

--- packages/contracts/src/lib/dashboard.model.ts
+++ packages/contracts/src/lib/dashboard.model.ts
@@ -1,9 +1,13 @@
-import { IBasePerTenantAndOrganizationEntityModel } from './base-entity.model';
+import { IBasePerTenantAndOrganizationEntityModel, ID } from './base-entity.model';
+import { IUser } from './user.model';
 
 export interface IDashboard extends IBasePerTenantAndOrganizationEntityModel {
 	name?: string;
 	description?: string;
+	code?: string;
 	isDefault?: boolean;
+	creator?: IUser;
+	creatorId?: ID;
 }
 
 export interface IDashboardCreateInput extends IDashboard {}
