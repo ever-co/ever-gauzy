1348bfe7f9d9be810f93d135d322fc9e40b6a111
syns <sutralian@gmail.com>
2024-12-22 22:17:59 +0700

    Fix/server app api not running (#8609)
    * fix: server app api not running
    
    * fix: remove compression maximum
    
    * Update apps/server/src/index.ts
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ruslan Konviser <evereq@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -350,6 +350,7 @@ const getEnvApi = () => {
 			DB_USER: config[provider]?.dbUsername,
 			DB_PASS: config[provider]?.dbPassword
 		}),
+		DEBUG: process.env.NODE_ENV !== 'production' ? 'true' : 'false',
 		API_PORT: String(config.port),
 		...addsConfig
 	};
