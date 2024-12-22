9d110c60cf69aaaeaacc8787c219bcc178e36f6a
Rahul R. <rahulrathore576@gmail.com>
2024-12-22 15:12:46 +0530

    fix(ui-core): resolve circular dependency in project mutation components

--- packages/ui-core/shared/src/lib/project/index.ts
+++ packages/ui-core/shared/src/lib/project/index.ts
@@ -1,3 +1,1 @@
-export * from './project-mutation/project-mutation.component';
-export * from './project-mutation/project-mutation.module';
-export * from './project-module/index';
+export * from './project-module';
