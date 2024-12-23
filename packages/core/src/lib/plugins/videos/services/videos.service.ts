2c8c93d3dcd573762848f32d6291910b4d6da4e1
Kifungo A <45813955+adkif@users.noreply.github.com>
2024-12-23 15:49:42 +0200

    feat: adds video service
    Introduces a new service to manage video entities. It utilizes both TypeORM and MikroORM repositories for data access.

---
+++ packages/core/src/lib/plugins/videos/services/videos.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { TenantAwareCrudService } from '../../../core/crud/tenant-aware-crud.service';
+import { Video } from '../entities/video.entity';
+import { MikroOrmVideoRepository } from '../repositories/mikro-orm-video.repository';
+import { TypeOrmVideoRepository } from '../repositories/type-orm-video.repository';
+
+@Injectable()
+export class VideosService extends TenantAwareCrudService<Video> {
+	constructor(
+		public readonly typeOrmVideoRepository: TypeOrmVideoRepository,
+		public readonly mikroOrmVideoRepository: MikroOrmVideoRepository
+	) {
+		super(typeOrmVideoRepository, mikroOrmVideoRepository);
+	}
+}
