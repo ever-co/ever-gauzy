f2247e4bcf5e3db0c11d100824e4ea2c124dee09
GloireMutaliko21 <mufunyig@gmail.com>
2024-12-25 07:59:51 +0200

    feat: handle dashboard subscriber

--- packages/core/src/lib/dashboard/dashboard.module.ts
+++ packages/core/src/lib/dashboard/dashboard.module.ts
@@ -2,14 +2,16 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { Dashboard } from './dashboard.entity';
+import { DashboardService } from './dashboard.service';
+import { DashboardController } from './dashboard.controller';
 
 @Module({
 	imports: [
 		TypeOrmModule.forFeature([Dashboard]),
 		MikroOrmModule.forFeature([Dashboard]),
 	],
-	controllers: [],
-	providers: [],
+	controllers: [DashboardController],
+	providers: [DashboardService],
 	exports: []
 })
 export class DashboardModule {}
