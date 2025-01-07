1666dec807d5de577f51753d49516fbea654070a
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2025-01-07 11:10:20 +0200

    Add Tag field && Fix DeepScan

--- packages/core/src/lib/tag-type/dto/update-tag-type.dto.ts
+++ packages/core/src/lib/tag-type/dto/update-tag-type.dto.ts
@@ -1,10 +1,13 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
-import { ID, ITagTypeUpdateInput } from '@gauzy/contracts';
-import { TenantOrganizationBaseDTO } from '../../core';
+import { ITagTypeUpdateInput } from '@gauzy/contracts';
+import { Tag, TenantOrganizationBaseDTO } from '../../core';
 
 export class UpdateTagTypeDTO extends TenantOrganizationBaseDTO implements ITagTypeUpdateInput {
 	@ApiProperty({ type: () => String })
 	@IsString()
 	readonly type: string;
+
+	@ApiPropertyOptional({ type: () => Array, isArray: true })
+	readonly tags: Tag[];
 }
