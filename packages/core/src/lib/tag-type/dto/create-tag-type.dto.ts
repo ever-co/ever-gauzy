47ccd2d911559850aa3fc4c18564723ca703e471
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2025-01-07 10:21:56 +0200

    Integrating feedback

--- packages/core/src/lib/tag-type/dto/create-tag-type.dto.ts
+++ packages/core/src/lib/tag-type/dto/create-tag-type.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-import { Tag } from '../../core';
 import { ITagTypeCreateInput } from '@gauzy/contracts';
+import { IsString } from 'class-validator';
+import { Tag, TenantOrganizationBaseDTO } from '../../core';
 
-export class CreateTagTypeDTO implements ITagTypeCreateInput {
+export class CreateTagTypeDTO extends TenantOrganizationBaseDTO implements ITagTypeCreateInput {
 	@ApiProperty({ type: () => String })
 	@IsString()
 	readonly type: string;
