c4a89eb8033c012c77166e8c52535bf31ec0547f
GloireMutaliko21 <mufunyig@gmail.com>
2025-01-08 20:35:43 +0200

    fix: tenant api key entity and migration

--- packages/contracts/src/lib/tenant-api-key.model.ts
+++ packages/contracts/src/lib/tenant-api-key.model.ts
@@ -3,8 +3,6 @@ import { IBasePerTenantEntityModel } from './base-entity.model';
 export interface ITenantApiKey extends IBasePerTenantEntityModel {
 	apiKey: string;
 	apiSecret: string;
-	openAiSecretKey?: string;
-	openAiOrganizationId?: string;
 }
 
 export interface ITenantApiKeyCreateInput extends ITenantApiKey {}
--- packages/core/src/lib/database/migrations/1736357496620-CreateTenantApiKeyTable.ts
+++ packages/core/src/lib/database/migrations/1736357496620-CreateTenantApiKeyTable.ts
@@ -2,8 +2,8 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 import * as chalk from 'chalk';
 import { DatabaseTypeEnum } from '@gauzy/config';
 
-export class CreateTenantApiKeyTable1736330073887 implements MigrationInterface {
-	name = 'CreateTenantApiKeyTable1736330073887';
+export class CreateTenantApiKeyTable1736357496620 implements MigrationInterface {
+	name = 'CreateTenantApiKeyTable1736357496620';
 
 	/**
 	 * Up Migration
@@ -58,7 +58,7 @@ export class CreateTenantApiKeyTable1736330073887 implements MigrationInterface 
 	 */
 	public async postgresUpQueryRunner(queryRunner: QueryRunner): Promise<any> {
 		await queryRunner.query(
-			`CREATE TABLE "tenant_api_key" ("deletedAt" TIMESTAMP, "id" uuid NOT NULL DEFAULT gen_random_uuid(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "isActive" boolean DEFAULT true, "isArchived" boolean DEFAULT false, "archivedAt" TIMESTAMP, "tenantId" uuid, "apiKey" character varying NOT NULL, "apiSecret" character varying NOT NULL, "openAiSecretKey" character varying, "openAiOrganizationId" character varying, CONSTRAINT "PK_83032288ad7173ddc31dc5a0c34" PRIMARY KEY ("id"))`
+			`CREATE TABLE "tenant_api_key" ("deletedAt" TIMESTAMP, "id" uuid NOT NULL DEFAULT gen_random_uuid(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "isActive" boolean DEFAULT true, "isArchived" boolean DEFAULT false, "archivedAt" TIMESTAMP, "tenantId" uuid, "apiKey" character varying NOT NULL, "apiSecret" character varying NOT NULL, CONSTRAINT "PK_83032288ad7173ddc31dc5a0c34" PRIMARY KEY ("id"))`
 		);
 		await queryRunner.query(`CREATE INDEX "IDX_686a4464635f3dbb77411dd5e8" ON "tenant_api_key" ("isActive") `);
 		await queryRunner.query(`CREATE INDEX "IDX_21056fa2d8b060d3049f50ec0b" ON "tenant_api_key" ("isArchived") `);
@@ -88,7 +88,7 @@ export class CreateTenantApiKeyTable1736330073887 implements MigrationInterface 
 	 */
 	public async sqliteUpQueryRunner(queryRunner: QueryRunner): Promise<any> {
 		await queryRunner.query(
-			`CREATE TABLE "tenant_api_key" ("deletedAt" datetime, "id" varchar PRIMARY KEY NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), "isActive" boolean DEFAULT (1), "isArchived" boolean DEFAULT (0), "archivedAt" datetime, "tenantId" varchar, "apiKey" varchar NOT NULL, "apiSecret" varchar NOT NULL, "openAiSecretKey" varchar, "openAiOrganizationId" varchar)`
+			`CREATE TABLE "tenant_api_key" ("deletedAt" datetime, "id" varchar PRIMARY KEY NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), "isActive" boolean DEFAULT (1), "isArchived" boolean DEFAULT (0), "archivedAt" datetime, "tenantId" varchar, "apiKey" varchar NOT NULL, "apiSecret" varchar NOT NULL)`
 		);
 		await queryRunner.query(`CREATE INDEX "IDX_686a4464635f3dbb77411dd5e8" ON "tenant_api_key" ("isActive") `);
 		await queryRunner.query(`CREATE INDEX "IDX_21056fa2d8b060d3049f50ec0b" ON "tenant_api_key" ("isArchived") `);
@@ -97,10 +97,10 @@ export class CreateTenantApiKeyTable1736330073887 implements MigrationInterface 
 		await queryRunner.query(`DROP INDEX "IDX_21056fa2d8b060d3049f50ec0b"`);
 		await queryRunner.query(`DROP INDEX "IDX_df44cc2b5ba1a4cc95850eea7c"`);
 		await queryRunner.query(
-			`CREATE TABLE "temporary_tenant_api_key" ("deletedAt" datetime, "id" varchar PRIMARY KEY NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), "isActive" boolean DEFAULT (1), "isArchived" boolean DEFAULT (0), "archivedAt" datetime, "tenantId" varchar, "apiKey" varchar NOT NULL, "apiSecret" varchar NOT NULL, "openAiSecretKey" varchar, "openAiOrganizationId" varchar, CONSTRAINT "FK_df44cc2b5ba1a4cc95850eea7c2" FOREIGN KEY ("tenantId") REFERENCES "tenant" ("id") ON DELETE CASCADE ON UPDATE NO ACTION)`
+			`CREATE TABLE "temporary_tenant_api_key" ("deletedAt" datetime, "id" varchar PRIMARY KEY NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), "isActive" boolean DEFAULT (1), "isArchived" boolean DEFAULT (0), "archivedAt" datetime, "tenantId" varchar, "apiKey" varchar NOT NULL, "apiSecret" varchar NOT NULL, CONSTRAINT "FK_df44cc2b5ba1a4cc95850eea7c2" FOREIGN KEY ("tenantId") REFERENCES "tenant" ("id") ON DELETE CASCADE ON UPDATE NO ACTION)`
 		);
 		await queryRunner.query(
-			`INSERT INTO "temporary_tenant_api_key"("deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret", "openAiSecretKey", "openAiOrganizationId") SELECT "deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret", "openAiSecretKey", "openAiOrganizationId" FROM "tenant_api_key"`
+			`INSERT INTO "temporary_tenant_api_key"("deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret") SELECT "deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret" FROM "tenant_api_key"`
 		);
 		await queryRunner.query(`DROP TABLE "tenant_api_key"`);
 		await queryRunner.query(`ALTER TABLE "temporary_tenant_api_key" RENAME TO "tenant_api_key"`);
@@ -120,10 +120,10 @@ export class CreateTenantApiKeyTable1736330073887 implements MigrationInterface 
 		await queryRunner.query(`DROP INDEX "IDX_686a4464635f3dbb77411dd5e8"`);
 		await queryRunner.query(`ALTER TABLE "tenant_api_key" RENAME TO "temporary_tenant_api_key"`);
 		await queryRunner.query(
-			`CREATE TABLE "tenant_api_key" ("deletedAt" datetime, "id" varchar PRIMARY KEY NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), "isActive" boolean DEFAULT (1), "isArchived" boolean DEFAULT (0), "archivedAt" datetime, "tenantId" varchar, "apiKey" varchar NOT NULL, "apiSecret" varchar NOT NULL, "openAiSecretKey" varchar, "openAiOrganizationId" varchar)`
+			`CREATE TABLE "tenant_api_key" ("deletedAt" datetime, "id" varchar PRIMARY KEY NOT NULL, "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), "isActive" boolean DEFAULT (1), "isArchived" boolean DEFAULT (0), "archivedAt" datetime, "tenantId" varchar, "apiKey" varchar NOT NULL, "apiSecret" varchar NOT NULL)`
 		);
 		await queryRunner.query(
-			`INSERT INTO "tenant_api_key"("deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret", "openAiSecretKey", "openAiOrganizationId") SELECT "deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret", "openAiSecretKey", "openAiOrganizationId" FROM "temporary_tenant_api_key"`
+			`INSERT INTO "tenant_api_key"("deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret") SELECT "deletedAt", "id", "createdAt", "updatedAt", "isActive", "isArchived", "archivedAt", "tenantId", "apiKey", "apiSecret" FROM "temporary_tenant_api_key"`
 		);
 		await queryRunner.query(`DROP TABLE "temporary_tenant_api_key"`);
 		await queryRunner.query(`CREATE INDEX "IDX_df44cc2b5ba1a4cc95850eea7c" ON "tenant_api_key" ("tenantId") `);
@@ -142,7 +142,7 @@ export class CreateTenantApiKeyTable1736330073887 implements MigrationInterface 
 	 */
 	public async mysqlUpQueryRunner(queryRunner: QueryRunner): Promise<any> {
 		await queryRunner.query(
-			`CREATE TABLE \`tenant_api_key\` (\`deletedAt\` datetime(6) NULL, \`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`isActive\` tinyint NULL DEFAULT 1, \`isArchived\` tinyint NULL DEFAULT 0, \`archivedAt\` datetime NULL, \`tenantId\` varchar(255) NULL, \`apiKey\` varchar(255) NOT NULL, \`apiSecret\` varchar(255) NOT NULL, \`openAiSecretKey\` varchar(255) NULL, \`openAiOrganizationId\` varchar(255) NULL, INDEX \`IDX_686a4464635f3dbb77411dd5e8\` (\`isActive\`), INDEX \`IDX_21056fa2d8b060d3049f50ec0b\` (\`isArchived\`), INDEX \`IDX_df44cc2b5ba1a4cc95850eea7c\` (\`tenantId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+			`CREATE TABLE \`tenant_api_key\` (\`deletedAt\` datetime(6) NULL, \`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`isActive\` tinyint NULL DEFAULT 1, \`isArchived\` tinyint NULL DEFAULT 0, \`archivedAt\` datetime NULL, \`tenantId\` varchar(255) NULL, \`apiKey\` varchar(255) NOT NULL, \`apiSecret\` varchar(255) NOT NULL, INDEX \`IDX_686a4464635f3dbb77411dd5e8\` (\`isActive\`), INDEX \`IDX_21056fa2d8b060d3049f50ec0b\` (\`isArchived\`), INDEX \`IDX_df44cc2b5ba1a4cc95850eea7c\` (\`tenantId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
 		);
 		await queryRunner.query(
 			`ALTER TABLE \`tenant_api_key\` ADD CONSTRAINT \`FK_df44cc2b5ba1a4cc95850eea7c2\` FOREIGN KEY (\`tenantId\`) REFERENCES \`tenant\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
