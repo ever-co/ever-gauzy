e4ade2f7f9ec9d4489dd5366faea2faca6c2b9bb
GloireMutaliko21 <mufunyig@gmail.com>
2025-01-08 14:01:49 +0200

    fix(cspell): typos errors

--- packages/core/src/lib/tenant-api-key/tenant-api-key.module.ts
+++ packages/core/src/lib/tenant-api-key/tenant-api-key.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { TenantApiKey } from './tenant-api-key.entity';
 import { TenantApiKeyService } from './tenant-api-key.service';
-import { TypeOrmTenantApiKeyRepository } from './repository/type-orm-tanant-api-key.repository';
+import { TypeOrmTenantApiKeyRepository } from './repository/type-orm-tenant-api-key.repository';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([TenantApiKey]), MikroOrmModule.forFeature([TenantApiKey])],
