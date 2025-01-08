0f28978af2f021ab7c5f69d17a66ac181464d59e
Rahul R. <rahulrathore576@gmail.com>
2025-01-08 16:30:42 +0530

    chore(deps): bump @fortawesome/angular-fontawesome from 0.10.1 to 0.15.0

--- packages/desktop-ui-lib/src/lib/time-tracker/time-tracker-status/time-tracker-status.module.ts
+++ packages/desktop-ui-lib/src/lib/time-tracker/time-tracker-status/time-tracker-status.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NbTooltipModule } from '@nebular/theme';
 import { TimeTrackerStatusService } from './time-tracker-status.service';
 import { TimeTrackerStatusComponent } from './time-tracker-status.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TranslateModule } from '@ngx-translate/core';
-import { NbTooltipModule } from '@nebular/theme';
 
 @NgModule({
 	declarations: [TimeTrackerStatusComponent],
