e8ed611324542191657bcaa0e0e8fd1afc83bdb5
Rahul R. <rahulrathore576@gmail.com>
2025-01-08 11:32:32 +0530

    chore: bump @angular-eslint/* packages to version 18.4.3

--- .deploy/api/entrypoint.compose.sh
+++ .deploy/api/entrypoint.compose.sh
@@ -8,14 +8,5 @@ export WAIT_HOSTS=$DB_HOST:$DB_PORT
 # in Docker Compose we should wait other services start
 ./wait
 
-# echo 'Version of PM2 …'
-# pm2 --version
-
-# echo 'Resetting PM2 Metadata …'
-# pm2 reset all
-
-# echo 'Updating PM2 …'
-# pm2 update
-
 echo 'Starting …'
 exec "$@"
