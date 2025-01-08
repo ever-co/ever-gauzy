3f36819de4c13fdc60311e150cd94fd61a7e2f15
Rahul R. <rahulrathore576@gmail.com>
2025-01-08 11:32:32 +0530

    chore: bump @angular-eslint/* packages to version 18.4.3

--- .deploy/api/entrypoint.prod.sh
+++ .deploy/api/entrypoint.prod.sh
@@ -3,14 +3,5 @@ set -ex
 
 # This Entrypoint used when we run Docker container outside of Docker Compose (e.g. in k8s)
 
-# echo 'Version of PM2 …'
-# pm2 --version
-
-# echo 'Resetting PM2 Metadata …'
-# pm2 reset all
-
-# echo 'Updating PM2 …'
-# pm2 update
-
 echo 'Starting …'
 exec "$@"
