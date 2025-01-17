0d2aca5bf5882ae8147016dd7aa45ff8fcce3b89
adkif <akifungo@gmail.com>
2025-01-18 00:03:38 +0200

    enhance: uses DatabaseTypeEnum for database config
    Replaces string literals with DatabaseTypeEnum values for database configuration.
    
    This change improves type safety and consistency across different database providers.

--- packages/desktop-lib/src/lib/offline/databases/migrations/20231122133813_alter-table-interval.ts
+++ packages/desktop-lib/src/lib/offline/databases/migrations/20231122133813_alter-table-interval.ts
@@ -1,8 +1,9 @@
 import { Knex } from 'knex';
 import { ProviderFactory, TABLE_NAME_INTERVALS, TABLE_NAME_TIMERS } from '../../../offline';
+import { DatabaseTypeEnum } from '@gauzy/config';
 
 export async function up(knex: Knex): Promise<void> {
-	if (ProviderFactory.instance.dialect === 'postgres') return;
+	if (ProviderFactory.instance.dialect === DatabaseTypeEnum.postgres) return;
 	await knex.schema.alterTable(TABLE_NAME_INTERVALS, (table: Knex.TableBuilder) => {
 		table.integer('timerId').unsigned().references('id').inTable(TABLE_NAME_TIMERS).onDelete('SET NULL').alter();
 	});
