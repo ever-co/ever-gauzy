3b0baa5a45c6c9f08449a4c0efa51670efe71ed9
adkif <akifungo@gmail.com>
2025-01-21 19:28:38 +0200

    fix: removes unused video-preview component and related files.
    Moves download-related services and components into the video-ui module for better organization.  Updates imports accordingly.

--- packages/plugins/videos-ui/src/lib/features/video-preview/video-preview.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-	selector: 'plug-video-preview',
-	templateUrl: './video-preview.component.html',
-	styleUrl: './video-preview.component.scss',
-	changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class VideoPreviewComponent {}
--- packages/plugins/videos-ui/src/lib/video-ui.module.ts
+++ packages/plugins/videos-ui/src/lib/video-ui.module.ts
@@ -18,33 +18,31 @@ import { provideEffects, provideEffectsManager } from '@ngneat/effects-ng';
 import { VideoEffects } from './+state/video.effect';
 import { VideoQuery } from './+state/video.query';
 import { VideoStore } from './+state/video.store';
+import { VideoDownloadManagerComponent } from './features/video-download-manager/video-download-manager.component';
 import { VideoListComponent } from './features/video-list/video-list.component';
-import { VideoPreviewComponent } from './features/video-preview/video-preview.component';
 import { VideoComponent } from './features/video/video.component';
 import { VideoDetailPageComponent } from './pages/video-detail-page/video-detail-page.component';
 import { VideoPageComponent } from './pages/video-page/video-page.component';
+import { DownloadQueueService } from './shared/services/download/download-queue.service';
+import { FileDownloadService } from './shared/services/download/file-download.service';
+import { FileSaveStrategy } from './shared/services/download/strategies/file-save.strategy';
 import { VideoService } from './shared/services/video.service';
 import { WebShareService } from './shared/services/web-share.service';
 import { ActionButtonGroupComponent } from './shared/ui/video-actions/buttons/action-button-group/action-button-group.component';
 import { ActionButtonComponent } from './shared/ui/video-actions/buttons/action-button/action-button.component';
 import { VideoEditComponent } from './shared/ui/video-edit/video-edit.component';
+import { VideoItemSkeletonComponent } from './shared/ui/video-item-skeleton/video-item-skeleton.component';
 import { VideoItemComponent } from './shared/ui/video-item/video-item.component';
 import { VideoMetadataComponent } from './shared/ui/video-metadata/video-metadata.component';
 import { VideoPlayerComponent } from './shared/ui/video-player/video-player.component';
-import { VideoUiRoutingModule } from './video-ui-routing.module';
-import { DownloadQueueService } from './shared/services/download/download-queue.service';
-import { FileDownloadService } from './shared/services/download/file-download.service';
-import { VideoDownloadManagerComponent } from './features/video-download-manager/video-download-manager.component';
-import { FileSaveStrategy } from './shared/services/download/strategies/file-save.strategy';
-import { VideoItemSkeletonComponent } from './shared/ui/video-item-skeleton/video-item-skeleton.component';
 import { VideoSkeletonComponent } from './shared/ui/video-skeleton/video-skeleton.component';
+import { VideoUiRoutingModule } from './video-ui-routing.module';
 
 @NgModule({
 	declarations: [
 		VideoListComponent,
 		VideoItemComponent,
 		VideoPlayerComponent,
-		VideoPreviewComponent,
 		VideoComponent,
 		VideoPageComponent,
 		VideoDetailPageComponent,
