12a254c70ae39bf1fda5a90a269718e653469b7e
Rahul R. <41804588+rahul-rocket@users.noreply.github.com>
2025-01-22 12:01:03 +0530

    [Fix] Tag Type DTO  (#8740)
    * fix: optional tag type field DTO
    
    * fix: applied coderabbitai suggestion

--- packages/core/src/lib/warehouse/warehouse.module.ts
+++ packages/core/src/lib/warehouse/warehouse.module.ts
@@ -1,30 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { Product } from './../core/entities/internal';
 import { RolePermissionModule } from '../role-permission/role-permission.module';
+import { ProductModule } from '../product/product.module';
 import { WarehouseService } from './warehouse.service';
 import { WarehouseController } from './warehouse.controller';
 import { Warehouse } from './warehouse.entity';
 import { WarehouseProductVariant } from './warehouse-product-variant.entity';
 import { WarehouseProduct } from './warehouse-product.entity';
 import { WarehouseProductService } from './warehouse-product-service';
-import { TypeOrmProductRepository } from '../product/repository/type-orm-product.repository';
 import { TypeOrmWarehouseRepository } from './repository/type-orm-warehouse.repository';
 import { TypeOrmWarehouseProductRepository } from './repository/type-orm-warehouse-product.repository ';
 import { TypeOrmWarehouseProductVariantRepository } from './repository/type-orm-warehouse-product-variant.repository';
 
 @Module({
 	imports: [
-		TypeOrmModule.forFeature([Warehouse, Product, WarehouseProduct, WarehouseProductVariant]),
-		MikroOrmModule.forFeature([Warehouse, Product, WarehouseProduct, WarehouseProductVariant]),
-		RolePermissionModule
+		TypeOrmModule.forFeature([Warehouse, WarehouseProduct, WarehouseProductVariant]),
+		MikroOrmModule.forFeature([Warehouse, WarehouseProduct, WarehouseProductVariant]),
+		RolePermissionModule,
+		ProductModule
 	],
 	controllers: [WarehouseController],
 	providers: [
 		WarehouseService,
 		WarehouseProductService,
-		TypeOrmProductRepository,
 		TypeOrmWarehouseRepository,
 		TypeOrmWarehouseProductRepository,
 		TypeOrmWarehouseProductVariantRepository
