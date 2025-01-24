966919c14fea7a22f1756ca67cd4189cd8c62942
Rahul R. <rahulrathore576@gmail.com>
2025-01-24 16:52:04 +0530

    refactor: plain & interface of object uses

--- packages/utils/src/lib/is-object.ts
+++ packages/utils/src/lib/is-object.ts
@@ -2,17 +2,18 @@
  * Checks if the given value is a valid object.
  *
  * Unlike `instanceof Object`, this method avoids cross-context issues.
- * Unlike `typeof`, it correctly excludes `null`, which is mistakenly classified as an object.
+ * Unlike `typeof`, it correctly excludes `null` and `undefined`, which are incorrectly classified as objects.
  *
  * @param {unknown} val - The value to check.
  * @returns {boolean} - Returns `true` if the value is an object, otherwise `false`.
  *
  * @example
- * console.log(ObjectUtils.isObject({})); // true
- * console.log(ObjectUtils.isObject(null)); // false
- * console.log(ObjectUtils.isObject([])); // true (arrays are also objects)
- * console.log(ObjectUtils.isObject('text')); // false
+ * console.log(isObject({})); // true
+ * console.log(isObject(null)); // false
+ * console.log(isObject(undefined)); // false
+ * console.log(isObject([])); // true (arrays are also objects)
+ * console.log(isObject(new Date())); // true
  */
 export function isObject(val: unknown): val is object {
-	return val !== null && typeof val === 'object';
+	return val !== null && val !== undefined && typeof val === 'object';
 }
