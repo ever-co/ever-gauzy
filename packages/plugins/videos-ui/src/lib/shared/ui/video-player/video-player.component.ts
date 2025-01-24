9aaf99f3c508bbd524529de46c15d9817031c051
adkif <akifungo@gmail.com>
2025-01-24 17:40:29 +0200

    fix: sanitizes video source URL
    Uses `DomSanitizer` to bypass security and prevent XSS vulnerabilities when binding to the `src` attribute of the video player.  This ensures that untrusted URLs are handled safely.

--- packages/plugins/videos-ui/src/lib/shared/ui/video-player/video-player.component.ts
+++ packages/plugins/videos-ui/src/lib/shared/ui/video-player/video-player.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
 	selector: 'plug-video-player',
@@ -7,14 +8,24 @@ import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VideoPlayerComponent {
+	private _src!: string;
 	@ViewChild('video')
 	private video!: ElementRef<HTMLVideoElement>;
 	@Input()
-	public src!: string;
-	@Input()
 	public controls = false;
+
+	constructor(private readonly sanitizer: DomSanitizer) {}
 
 	public get player(): HTMLVideoElement {
 		return this.video.nativeElement;
+	}
+
+	public get src(): SafeUrl {
+		return this.sanitizer.bypassSecurityTrustUrl(this._src);
+	}
+
+	@Input()
+	public set src(value: string) {
+		this._src = value;
 	}
 }
