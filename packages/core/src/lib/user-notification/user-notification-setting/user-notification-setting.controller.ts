a37d4eff89b37bb2e41482f73ad4ff6da8e4ca0b
GloireMutaliko21 <mufunyig@gmail.com>
2025-01-31 15:45:24 +0200

    feat: add notification setting service

--- packages/core/src/lib/user-notification/user-notification-setting/user-notification-setting.controller.ts
+++ packages/core/src/lib/user-notification/user-notification-setting/user-notification-setting.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { CrudController } from '../../core/crud';
+import { UserNotificationSetting } from './user-notification-setting.entity';
+import { UserNotificationSettingService } from './user-notification-setting.service';
 
-@Controller('user-notification-setting')
-export class UserNotificationSettingController {}
+@Controller('/user-notification-setting')
+export class UserNotificationSettingController extends CrudController<UserNotificationSetting> {
+	constructor(private readonly userNotificationSettingService: UserNotificationSettingService) {
+		super(userNotificationSettingService);
+	}
+}
