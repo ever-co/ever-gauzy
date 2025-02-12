aa715598c9d65bce8d262741c13c43404bb9f690
Reinier Millo Sánchez <reinier.millo@dspot.com.pl>
2025-02-11 23:06:20 -0500

    fix: Fix base eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,33 @@
-const { FlatESLint } = require('@nx/eslint-plugin-nx');
+const nx = require('@nx/eslint-plugin');
 
-module.exports = new FlatESLint({
-  	overrides: [],
-});
+module.exports = [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+];
