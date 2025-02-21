232517f5e0db79e41efc5a5556923a6a5f9b71cc
Rahul R. <rahulrathore576@gmail.com>
2025-02-21 16:29:30 +0530

    feat(migration): alter "organization_project_module" entity for MySQL

--- packages/contracts/src/lib/common.model.ts
+++ packages/contracts/src/lib/common.model.ts
@@ -2,6 +2,6 @@
  * Interface for entities that can be assigned to a manager.
  */
 export interface IManagerAssignable {
-	isManager?: boolean; // Manager of the organization team
-	assignedAt?: Date; // Assigned At Manager of the organization team
+	isManager?: boolean; // Is Manager for the entity
+	assignedAt?: Date; // Assigned At Manager for the entity
 }
