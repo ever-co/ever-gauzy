b0a32d4ad0db4c067d7c9294e6f76ddd910d5f85
rolandm99 <manfulmweze99@gmail.com>
2025-02-25 19:40:07 +0200

    feat: create command handler methods to process and publish actions events

---
+++ packages/core/src/lib/time-tracking/timer/handlers/stop-timer.handler.ts
@@ -0,0 +1,26 @@
+import { CommandHandler, ICommandHandler, EventBus } from '@nestjs/cqrs';
+import { Injectable } from '@nestjs/common';
+import { StopTimerCommand } from '../commands';
+import { TimerStoppedEvent } from '../events';
+import { ITimeLog } from '@gauzy/contracts';
+import { TimerService } from '../timer.service';
+
+@Injectable()
+@CommandHandler(StopTimerCommand)
+export class StopTimerHandler implements ICommandHandler<StopTimerCommand> {
+  constructor(
+    private readonly timerService: TimerService,
+    private readonly eventBus: EventBus,
+  ) {}
+
+  async execute(command: StopTimerCommand): Promise<ITimeLog> {
+    const { input } = command;
+
+    // Use the existing implementation
+    const timeLog = await this.timerService._stopTimerImplementation(input);
+
+    this.eventBus.publish(new TimerStoppedEvent(timeLog));
+
+    return timeLog;
+  }
+}
