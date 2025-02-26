dcf3f1e345e11d8cf438affe8bcbd36cc903c482
Rahul R. <rahulrathore576@gmail.com>
2025-02-26 14:27:37 +0530

    refactor: updated email history model and commands/handlers

--- packages/contracts/src/lib/email.model.ts
+++ packages/contracts/src/lib/email.model.ts
@@ -1,44 +1,23 @@
 import { IBasePerTenantAndOrganizationEntityModel } from './base-entity.model';
-import {
-	IEmailTemplateFindInput,
-	IRelationalEmailTemplate
-} from './email-template.model';
+import { IRelationalEmailTemplate } from './email-template.model';
 import { IRelationalUser } from './user.model';
 
-export interface IEmailHistory extends IBasePerTenantAndOrganizationEntityModel, IRelationalUser, IRelationalEmailTemplate {
+export enum EmailStatusEnum {
+	SENT = 'SENT',
+	FAILED = 'FAILED'
+}
+
+export interface IEmailHistoryBase extends IBasePerTenantAndOrganizationEntityModel {
 	name: string;
 	content: string;
 	email: string;
 	status?: EmailStatusEnum;
 }
 
-export interface IEmailUpdateInput extends IBasePerTenantAndOrganizationEntityModel, IRelationalUser, Partial<IRelationalEmailTemplate> {
-	name?: string;
-	content?: string;
-	email?: string;
-	isArchived?: boolean;
-}
+export interface IEmailHistory extends IEmailHistoryBase, IRelationalUser, IRelationalEmailTemplate {}
 
-export interface IResendEmailInput extends IBasePerTenantAndOrganizationEntityModel, IRelationalUser, Partial<IRelationalEmailTemplate> {
-	id: IEmailHistory['id'];
-	[x: string]: any;
-}
+export interface IResendEmailInput extends IBasePerTenantAndOrganizationEntityModel {}
 
-export interface IEmailFindInput extends IBasePerTenantAndOrganizationEntityModel, IRelationalUser {
-	emailTemplate?: IEmailTemplateFindInput;
-	emailTemplateId?: string;
-	email?: string;
-	isArchived?: boolean;
-	status?: EmailStatusEnum;
-}
+export interface IEmailFindInput extends Partial<IEmailHistory> {}
 
-export interface DisplayEmail {
-	from: string;
-	to: string;
-	date: string;
-}
-
-export enum EmailStatusEnum {
-	SENT = 'SENT',
-	FAILED = 'FAILED'
-}
+export type IEmailUpdateInput = Partial<IEmailHistoryBase> & IRelationalUser & Partial<IRelationalEmailTemplate>;
