fac2380d67967ae7498a80aa92f78f33cea26e98
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2025-02-27 17:30:27 +0200

    feedback integration

--- packages/core/src/lib/organization-project/decorators/manager-or-permission.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata, UseGuards, applyDecorators } from '@nestjs/common';
-import { PERMISSIONS_METADATA } from '@gauzy/constants';
-import { PermissionsEnum } from '@gauzy/contracts';
-import { ProjectManagerOrPermissionGuard } from '../guards/project-manager-or-permission.guard';
-
-export const ManagerOrPermissions = (...permissions: PermissionsEnum[]) =>
-	applyDecorators(SetMetadata(PERMISSIONS_METADATA, permissions), UseGuards(ProjectManagerOrPermissionGuard));
