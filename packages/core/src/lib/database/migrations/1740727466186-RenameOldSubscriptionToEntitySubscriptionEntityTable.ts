a0fd379585c41fcad3c35e30fbf7727285ce19fd
Rahul R. <rahulrathore576@gmail.com>
2025-02-28 13:13:18 +0530

    chore(migrations): Rename "subscription" table to "entity_subscription"
    - Added migrations to rename the "subscription" table to "entity_subscription" across Postgres, SQLite, and MySQL.

---
+++ packages/core/src/lib/database/migrations/1740727466186-RenameOldSubscriptionToEntitySubscriptionEntityTable.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as chalk from 'chalk';
+
+export class RenameOldSubscriptionToEntitySubscriptionEntityTable1740727466186 implements MigrationInterface {
+	name = 'RenameOldSubscriptionToEntitySubscriptionEntityTable1740727466186';
+
+	/**
+	 * Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		console.log(chalk.yellow(this.name + ' start running!'));
+
+		await queryRunner.renameTable('subscription', 'entity_subscription');
+	}
+
+	/**
+	 * Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.renameTable('entity_subscription', 'subscription');
+	}
+}
