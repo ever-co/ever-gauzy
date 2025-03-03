af914aef77f7a87f42785fbc87fcb5b2ccbd229e
Rahul R. <rahulrathore576@gmail.com>
2025-03-03 20:22:54 +0530

    fix: alter "organization_contact" entity table for PostgreSQL

--- packages/core/src/lib/organization-contact/organization-contact.service.ts
+++ packages/core/src/lib/organization-contact/organization-contact.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { Brackets, In, Raw, WhereExpressionBuilder } from 'typeorm';
-import { IEmployee, IOrganizationContact, IOrganizationContactFindInput, IPagination } from '@gauzy/contracts';
+import { Brackets, In, Raw } from 'typeorm';
+import { ID, IOrganizationContact, IOrganizationContactFindInput, IPagination } from '@gauzy/contracts';
 import { isPostgres } from '@gauzy/config';
 import { isNotEmpty } from '@gauzy/utils';
 import { RequestContext } from '../core/context';
@@ -26,11 +26,11 @@ export class OrganizationContactService extends TenantAwareCrudService<Organizat
 	 * @param options
 	 * @returns
 	 */
-	async findByEmployee(
-		employeeId: IEmployee['id'],
-		options: IOrganizationContactFindInput
-	): Promise<IOrganizationContact[]> {
+	async findByEmployee(employeeId: ID, options: IOrganizationContactFindInput): Promise<IOrganizationContact[]> {
 		try {
+			const tenantId = RequestContext.currentTenantId() ?? options.tenantId;
+			const { organizationId, contactType } = options;
+
 			const query = this.typeOrmRepository.createQueryBuilder(this.tableName);
 			query.setFindOptions({
 				select: {
@@ -40,20 +40,14 @@ export class OrganizationContactService extends TenantAwareCrudService<Organizat
 				}
 			});
 			query.innerJoin(`${query.alias}.members`, 'member');
-			query.andWhere(
-				new Brackets((qb: WhereExpressionBuilder) => {
-					const tenantId = RequestContext.currentTenantId();
-					const { organizationId, contactType } = options;
+			query.andWhere(p('member.id = :employeeId'), { employeeId });
+			query.andWhere(p(`"${query.alias}"."tenantId" = :tenantId`), { tenantId });
+			query.andWhere(p(`"${query.alias}"."organizationId" = :organizationId`), { organizationId });
 
-					qb.andWhere(p('member.id = :employeeId'), { employeeId });
-					qb.andWhere(p(`"${query.alias}"."tenantId" = :tenantId`), { tenantId });
-					qb.andWhere(p(`"${query.alias}"."organizationId" = :organizationId`), { organizationId });
+			if (isNotEmpty(contactType)) {
+				query.andWhere(p(`${query.alias}.contactType = :contactType`), { contactType });
+			}
 
-					if (isNotEmpty(contactType)) {
-						qb.andWhere(p(`${query.alias}.contactType = :contactType`), { contactType });
-					}
-				})
-			);
 			return await query.getMany();
 		} catch (error) {
 			throw new BadRequestException(error);
@@ -81,7 +75,9 @@ export class OrganizationContactService extends TenantAwareCrudService<Organizat
 	async getOrganizationContactByEmployee(data: any) {
 		const { relations, findInput } = data;
 		const { employeeId, organizationId, contactType } = findInput;
-		const { tenantId, id: createdBy } = RequestContext.currentUser();
+
+		const createdByUserId = RequestContext.currentUserId();
+		const tenantId = RequestContext.currentTenantId() ?? findInput.tenantId;
 
 		const query = this.typeOrmRepository.createQueryBuilder('organization_contact');
 		if (relations.length > 0) {
@@ -99,28 +95,22 @@ export class OrganizationContactService extends TenantAwareCrudService<Organizat
 			new Brackets((subQuery) => {
 				subQuery
 					.where('members.id =:employeeId', { employeeId })
-					.orWhere(`${query.alias}.createdBy = :createdBy`, {
-						createdBy
-					});
+					.orWhere(`${query.alias}.createdByUserId = :createdByUserId`, { createdByUserId });
 			})
 		);
-		query.andWhere(`${query.alias}.contactType = :contactType`, {
-			contactType
-		});
+
+		query.andWhere(`${query.alias}.contactType = :contactType`, { contactType });
+		query.andWhere(`${query.alias}.tenantId = :tenantId`, { tenantId });
+
 		if (organizationId) {
-			query.andWhere(`${query.alias}.organizationId = :organizationId`, {
-				organizationId
-			});
+			query.andWhere(`${query.alias}.organizationId = :organizationId`, { organizationId });
 		}
-		query.andWhere(`${query.alias}.tenantId = :tenantId`, {
-			tenantId
-		});
 
 		const [items, total] = await query.getManyAndCount();
 		return { items, total };
 	}
 
-	async findById(id: string, relations: string[]): Promise<IOrganizationContact> {
+	async findById(id: ID, relations: string[]): Promise<IOrganizationContact> {
 		return await this.findOneByIdString(id, { relations });
 	}
 
