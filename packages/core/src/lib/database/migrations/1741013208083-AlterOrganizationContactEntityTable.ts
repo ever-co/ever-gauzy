5951607ea62b7746fde0b960a434189faad027a4
Rahul R. <rahulrathore576@gmail.com>
2025-03-03 20:22:54 +0530

    fix: alter "organization_contact" entity table for PostgreSQL

---
+++ packages/core/src/lib/database/migrations/1741013208083-AlterOrganizationContactEntityTable.ts
@@ -0,0 +1,111 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as chalk from 'chalk';
+import { DatabaseTypeEnum } from '@gauzy/config';
+
+export class AlterOrganizationContactEntityTable1741013208083 implements MigrationInterface {
+	name = 'AlterOrganizationContactEntityTable1741013208083';
+
+	/**
+	 * Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		console.log(chalk.yellow(this.name + ' start running!'));
+
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+				await this.sqliteUpQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.postgres:
+				await this.postgresUpQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.mysql:
+				await this.mysqlUpQueryRunner(queryRunner);
+				break;
+			default:
+				throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
+		}
+	}
+
+	/**
+	 * Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		switch (queryRunner.connection.options.type) {
+			case DatabaseTypeEnum.sqlite:
+			case DatabaseTypeEnum.betterSqlite3:
+				await this.sqliteDownQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.postgres:
+				await this.postgresDownQueryRunner(queryRunner);
+				break;
+			case DatabaseTypeEnum.mysql:
+				await this.mysqlDownQueryRunner(queryRunner);
+				break;
+			default:
+				throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
+		}
+	}
+
+	/**
+	 * PostgresDB Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async postgresUpQueryRunner(queryRunner: QueryRunner): Promise<any> {
+		// Step 1: Copy data from "createdBy" to "createdByUserId"
+		console.log('Step 1: Copying data from createdBy to createdByUserId');
+		await queryRunner.query(`UPDATE "organization_contact" SET "createdByUserId" = "createdBy"::uuid`);
+
+		// Step 2: Drop the "createdBy" column
+		console.log('Step 2: Dropping column createdBy');
+		await queryRunner.query(`ALTER TABLE "organization_contact" DROP COLUMN "createdBy"`);
+	}
+
+	/**
+	 * PostgresDB Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async postgresDownQueryRunner(queryRunner: QueryRunner): Promise<any> {
+		// Step 1: Re-add the "createdBy" column
+		console.log('Step 1: Adding column createdBy');
+		await queryRunner.query(`ALTER TABLE "organization_contact" ADD "createdBy" character varying`);
+
+		// Step 2: Copy data from "createdByUserId" to "createdBy"
+		console.log('Step 2: Copying data from createdByUserId to createdBy');
+		await queryRunner.query(`UPDATE "organization_contact" SET "createdBy" = "createdByUserId"::text`);
+	}
+
+	/**
+	 * SqliteDB and BetterSQlite3DB Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async sqliteUpQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+
+	/**
+	 * SqliteDB and BetterSQlite3DB Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async sqliteDownQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+
+	/**
+	 * MySQL Up Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async mysqlUpQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+
+	/**
+	 * MySQL Down Migration
+	 *
+	 * @param queryRunner
+	 */
+	public async mysqlDownQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+}
