15a55a4f59f77bf835f3823e467c5cc8fdec2da6
adkif <akifungo@gmail.com>
2025-03-06 09:53:24 +0200

    feat: localizes plugin settings tabs
    Translates "Discover" and "Installed" plugin tabs using the translate service.  This allows the tab titles to be displayed in the user's selected language.
    
    Adds `PLUGIN.LAYOUT.DISCOVER` and `PLUGIN.LAYOUT.INSTALLED` keys to the translation files.

--- packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-layout/plugin-layout.component.ts
+++ packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-layout/plugin-layout.component.ts
@@ -1,26 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NbRouteTab } from '@nebular/theme';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
 	selector: 'ngx-plugin-layout',
 	templateUrl: './plugin-layout.component.html',
 	styleUrls: ['./plugin-layout.component.scss']
 })
-export class PluginLayoutComponent {
-	public readonly tabs: NbRouteTab[] = [
-		{
-			title: 'Discover',
-			route: '/settings/marketplace-plugins',
-			icon: 'search-outline',
-			responsive: true,
-			activeLinkOptions: {
-				exact: false
+export class PluginLayoutComponent implements OnInit, OnDestroy {
+	public tabs: NbRouteTab[] = [];
+	private destroy$ = new Subject<void>();
+
+	constructor(private readonly translateService: TranslateService) {}
+
+	ngOnInit() {
+		// Initialize tabs immediately
+		this.updateTabs();
+
+		// Use takeUntil for cleaner subscription management
+		this.translateService.onLangChange.pipe(takeUntil(this.destroy$)).subscribe(() => this.updateTabs());
+	}
+
+	ngOnDestroy() {
+		// Emit completion and clean up all subscriptions
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
+
+	private updateTabs() {
+		this.tabs = [
+			{
+				title: this.translateService.instant('PLUGIN.LAYOUT.DISCOVER'),
+				route: '/settings/marketplace-plugins',
+				icon: 'search-outline',
+				responsive: true,
+				activeLinkOptions: {
+					exact: false
+				}
+			},
+			{
+				title: this.translateService.instant('PLUGIN.LAYOUT.INSTALLED'),
+				route: '/settings/plugins',
+				icon: 'checkmark-circle-2-outline',
+				activeLinkOptions: {
+					exact: false
+				}
 			}
-		},
-		{
-			title: 'Installed',
-			route: '/settings/plugins',
-			icon: 'checkmark-circle-2-outline'
-		}
-	];
+		];
+	}
 }
