e2a923445987e1b4f74c53f16a2d3636e1fffea2
adkif <akifungo@gmail.com>
2025-03-14 09:27:03 +0200

    feat: adds `UpdatePluginCommand`
    This change introduces the `UpdatePluginCommand` along with its required DTO and dependencies.  It allows updating plugin information.

--- packages/plugins/plugin-registry/src/lib/application/commands/update-plugin.command.ts
+++ packages/plugins/plugin-registry/src/lib/application/commands/update-plugin.command.ts
@@ -1,3 +1,7 @@
+import { ID } from '@gauzy/contracts';
 import { ICommand } from '@nestjs/cqrs';
+import { UpdatePluginDTO } from '../../shared/dto/update-plugin.dto';
 
-export class UpdatePluginCommand implements ICommand {}
+export class UpdatePluginCommand implements ICommand {
+	constructor(public readonly id: ID, public readonly input: UpdatePluginDTO) {}
+}
