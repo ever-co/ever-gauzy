08aea76ebce9cabb86c731248f1cb70e9621ba47
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2025-03-14 14:26:27 +0200

    Fix TypeOrmDailyPlanRepository initialization issue

--- packages/core/src/lib/tasks/daily-plan/repository/type-orm-daily-plan.repository.ts
+++ packages/core/src/lib/tasks/daily-plan/repository/type-orm-daily-plan.repository.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { DailyPlan } from '../daily-plan.entity';
 
 @Injectable()
-export class TypeOrmDailyPlanRepository extends Repository<DailyPlan> { }
+export class TypeOrmDailyPlanRepository extends Repository<DailyPlan> {
+	constructor(@InjectRepository(DailyPlan) readonly repository: Repository<DailyPlan>) {
+		super(repository.target, repository.manager, repository.queryRunner);
+	}
+}
