2d6abaa6d26eefeca64572606204ec4861e02944
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2025-03-15 21:40:18 -0500

    refactor: Update Cypress configuration to work with version 14.2

--- apps/gauzy-e2e/cypress.config.js
+++ apps/gauzy-e2e/cypress.config.js
@@ -1,41 +1,38 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    "projectId": "2n3iur",
-    "fileServerFolder": ".",
-    "fixturesFolder": "src/fixtures",
-    "integrationFolder": "src/integration",
-    "videosFolder": "../../dist/cypress/apps/gauzy-e2e/videos",
-    "screenshotsFolder": "../../dist/cypress/apps/gauzy-e2e/screenshots",
-    "video": false,
-    "videoUploadOnPasses": false,
-    "chromeWebSecurity": false,
-    "viewportWidth": 1920,
-    "viewportHeight": 1080,
-    "baseUrl": "http://localhost:4200",
-    "defaultCommandTimeout": 60000,
-    "pageLoadTimeout": 60000,
-    "taskTimeout": 60000,
-    "requestTimeout": 60000,
-    "execTimeout": 60000,
-    "responseTimeout": 60000,
-    "retries": {
-        "runMode": 1,
-        "openMode": 0
+    projectId: "2n3iur",
+    fileServerFolder: ".",
+    fixturesFolder: "./src/fixtures",
+    downloadsFolder: "../../dist/cypress/apps/gauzy-e2e/downloads",
+    videosFolder: "../../dist/cypress/apps/gauzy-e2e/videos",
+    screenshotsFolder: "../../dist/cypress/apps/gauzy-e2e/screenshots",
+    video: false,
+    chromeWebSecurity: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 60000,
+    pageLoadTimeout: 60000,
+    taskTimeout: 60000,
+    requestTimeout: 60000,
+    execTimeout: 60000,
+    responseTimeout: 60000,
+    retries: {
+        runMode: 1,
+        openMode: 0
     },
-    "env": {
-        "coverage": false,
-        "codeCoverage": {
-            "url": "http://localhost:3001/__coverage__"
+    env: {
+        coverage: false,
+        codeCoverage: {
+            url: "http://localhost:3001/__coverage__"
         }
     },
-    "e2e": {
-        "specPattern": "src/integration/**/*.{js,jsx,ts,tsx}",
-        "supportFile": "src/support/index.ts",
-        "setupNodeEvents": (on, config) => {
-            return require('./src/plugins/index.js')(on, config);
-        },
-        "experimentalSessionAndOrigin": true,
-        "experimentalModuleSupport": true
+    e2e: {
+        baseUrl: "http://localhost:4200",
+        specPattern: "./src/e2e/**/*.{ts,tsx}",
+        supportFile: "./src/support/index.ts",
+        setupNodeEvents(on, config) {
+            return require('./src/plugins/index')(on, config);
+        }
     }
 });
