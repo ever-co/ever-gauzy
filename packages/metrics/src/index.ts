ff9c03b702307d88562b1ca424f6c2b7bca98aa3
Reinier Millo Sánchez <reinier.millo@dspot.com.pl>
2025-03-19 19:35:16 -0500

    refactor: Update metrics exports

--- packages/metrics/src/index.ts
+++ packages/metrics/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * Public API Surface of @gauzy/metrics
  */
-export * from './lib/measurements/measurements.module';
-export * from './lib/measurements/time';
-export * from './lib/measurements/memory';
+export { MeasurementsModule } from './lib/measurements/measurements.module';
+export { TimeMetric } from './lib/measurements/time';
+export { MemoryMetric } from './lib/measurements/memory';
