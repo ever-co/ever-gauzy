cbf8e4910da3cf8d3fc76e48de752d5bbf598909
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2025-03-21 00:24:48 -0500

    feat: Prevent forgot password screen to be loaded
    - If the email/password login is disabled then return to login

--- packages/ui-auth/src/lib/components/forgot-password/forgot-password.component.ts
+++ packages/ui-auth/src/lib/components/forgot-password/forgot-password.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 import { NbRequestPasswordComponent } from '@nebular/auth';
+import { Router } from '@angular/router';
+import { NbAuthService, NB_AUTH_OPTIONS } from '@nebular/auth';
+import { Observable, map } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { AppService } from '@gauzy/ui-core/core';
+import { IAppConfig } from '@gauzy/contracts';
 
+@UntilDestroy({ checkProperties: true })
 @Component({
 	selector: 'ngx-forgot-password',
 	templateUrl: './forgot-password.component.html',
 	styleUrls: ['./forgot-password.component.scss']
 })
-export class NgxForgotPasswordComponent extends NbRequestPasswordComponent {}
+export class NgxForgotPasswordComponent extends NbRequestPasswordComponent implements OnInit {
+
+	public allowEmailPasswordLogin$: Observable<boolean>;
+
+	constructor(
+		public readonly nbAuthService: NbAuthService,
+		public readonly cdr: ChangeDetectorRef,
+		public readonly router: Router,
+		private readonly appService: AppService,
+		@Inject(NB_AUTH_OPTIONS) options
+	) {
+		super(nbAuthService, options, cdr, router);
+	}
+
+	ngOnInit() {
+		// Load the configuration to check if the email/password login is enabled.
+		this.allowEmailPasswordLogin$ = this.appService.getAppConfigs().pipe(
+			map((configs: IAppConfig) => configs.email_password_login),
+			untilDestroyed(this)
+		);
+
+		// If the email/password login is not enabled, navigate to the login page.
+		this.allowEmailPasswordLogin$.subscribe((allowEmailPasswordLogin: boolean) => {
+			if (!allowEmailPasswordLogin) {
+				this.router.navigate(['/auth/login']);
+			}
+		});
+	}
+}
