70644e8bbf836ebdab36d08a475a9d56d6b3f9c6
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2025-03-21 11:23:25 -0500

    refactor: Guard moved to auth module

--- packages/ui-core/core/src/lib/guards/index.ts
+++ packages/ui-core/core/src/lib/guards/index.ts
@@ -2,4 +2,3 @@ export * from './invite.guard';
 export * from './permission.guard';
 export * from './role.guard';
 export * from './external-redirect.guard';
-export * from './no-email-password.guard';
