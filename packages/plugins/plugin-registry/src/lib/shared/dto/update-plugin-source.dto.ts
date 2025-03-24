23e6b38a5827f509ccda5e4111e9c50ad1670146
adkif <akifungo@gmail.com>
2025-03-24 23:51:12 +0200

    feat(plugins): Add DTOs for updating plugin entities
    Introduces new Data Transfer Objects (DTOs) for updating plugin sources, versions, and plugins themselves.  These DTOs allow for partial updates of plugin-related information, enhancing flexibility and control over plugin management.  They include validation and type enforcement for data integrity.

---
+++ packages/plugins/plugin-registry/src/lib/shared/dto/update-plugin-source.dto.ts
@@ -0,0 +1,14 @@
+import { ID } from '@gauzy/contracts';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
+import { IsNotEmpty, IsUUID } from 'class-validator';
+import { PluginSourceDTO } from './plugin-source.dto';
+
+export class UpdatePluginSourceDTO extends PartialType(OmitType(PluginSourceDTO, ['versions'] as const)) {
+	@ApiProperty({
+		description: 'Unique identifier for the plugin source',
+		example: '123e4567-e89b-12d3-a456-426614174000'
+	})
+	@IsUUID('4', { message: 'The plugin source ID must be a valid UUID v4' })
+	@IsNotEmpty({ message: 'The plugin source ID is required' })
+	readonly id: ID;
+}
--- packages/plugins/plugin-registry/src/lib/shared/dto/update-plugin.dto.ts
+++ packages/plugins/plugin-registry/src/lib/shared/dto/update-plugin.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/swagger';
+import { ID } from '@gauzy/contracts';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsUUID, ValidateNested } from 'class-validator';
 import { CreatePluginDTO } from './create-plugin.dto';
+import { UpdatePluginVersionDTO } from './update-plugin-version.dto';
 
-export class UpdatePluginDTO extends PartialType(CreatePluginDTO) {}
+export class UpdatePluginDTO extends PartialType(OmitType(CreatePluginDTO, ['version'] as const)) {
+	@ApiProperty({
+		description: 'Unique identifier for the plugin',
+		example: '123e4567-e89b-12d3-a456-426614174000'
+	})
+	@IsUUID('4', { message: 'The plugin ID must be a valid UUID v4' })
+	@IsNotEmpty({ message: 'The plugin ID is required' })
+	readonly id: ID;
+
+	@ApiProperty({
+		description: 'Updated version details for the plugin',
+		required: false,
+		type: UpdatePluginVersionDTO
+	})
+	@ValidateNested()
+	@Type(() => UpdatePluginVersionDTO)
+	readonly version?: UpdatePluginVersionDTO;
+}
