c6369cf49735a553c135a8240a741ce9c884a5a9
YanaYestyle <yanayestyle@gmail.com>
2025-03-28 16:40:36 +0100

    feat: fix pipe

--- packages/ui-core/shared/src/lib/pipes/duration-to-hours-format.pipe.ts
+++ packages/ui-core/shared/src/lib/pipes/duration-to-hours-format.pipe.ts
@@ -4,18 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HoursDurationFormatPipe implements PipeTransform {
 	/**
-	 * Transforms the given number of seconds into a formatted duration string in the format Hh.
+	 * Transforms the given number of seconds into hours.
 	 *
 	 * @param {number} seconds - The number of seconds to transform.
-	 * @return {string} The formatted duration string in the format Hh.
 	 */
 	transform(seconds: number): number {
 		const duration = seconds < 0 ? 0 : seconds;
-		const hours: number = Math.floor(duration / 3600);
-		const remainingSeconds: number = duration % 3600;
-		const minutes: number = Math.floor(remainingSeconds / 60);
-		const remainingSecs: number = remainingSeconds % 60;
-		const totalHours: number = hours + minutes / 60 + remainingSecs / 3600;
+		const totalHours: number = duration / 3600;
 		const roundedTotalHours = Math.round((totalHours + Number.EPSILON) * 1000) / 1000;
 		return roundedTotalHours;
 	}
