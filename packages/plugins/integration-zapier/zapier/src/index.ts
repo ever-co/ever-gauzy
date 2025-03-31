cd3da3f079e46d7fe0eaa55382b72b9f5bf280ac
rolandm99 <manfulmweze99@gmail.com>
2025-04-01 00:47:48 +0200

    fix: set zapier CLI generated configuration

--- packages/plugins/integration-zapier/zapier/src/index.ts
+++ packages/plugins/integration-zapier/zapier/src/index.ts
@@ -2,6 +2,7 @@ import { version as platformVersion } from 'zapier-platform-core';
 import { authentication } from './authentication';
 
 const { version } = require('../package.json');
+
 import startTimer, { startTimerKey } from './creates/startTimer';
 import stopTimer, { stopTimerKey } from './creates/stopTimer';
 import timerStatus, { timerStatusKey } from './triggers/timerStatus';
