012274822d058f999b654cc3bd30bcf69fa3a4fc
Rahul R. <rahulrathore576@gmail.com>
2025-04-07 14:16:58 +0530

    feat(migration): add 'zapier_webhook_subscription' table for MySQL

--- packages/core/src/lib/database/migrations/1744014217496-CreateZapierWebhookSubscriptionRepositoryTable.ts
+++ packages/core/src/lib/database/migrations/1744014217496-CreateZapierWebhookSubscriptionRepositoryTable.ts
@@ -166,12 +166,64 @@ export class CreateZapierWebhookSubscriptionRepositoryTable1744014217496 impleme
 	 *
 	 * @param queryRunner
 	 */
-	public async mysqlUpQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+	public async mysqlUpQueryRunner(queryRunner: QueryRunner): Promise<any> {
+		await queryRunner.query(
+			`CREATE TABLE \`zapier_webhook_subscription\` (\`deletedAt\` datetime(6) NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`createdByUserId\` varchar(255) NULL, \`updatedByUserId\` varchar(255) NULL, \`deletedByUserId\` varchar(255) NULL, \`id\` varchar(36) NOT NULL, \`isActive\` tinyint NULL DEFAULT 1, \`isArchived\` tinyint NULL DEFAULT 0, \`archivedAt\` datetime NULL, \`tenantId\` varchar(255) NULL, \`organizationId\` varchar(255) NULL, \`targetUrl\` varchar(255) NOT NULL, \`event\` varchar(255) NOT NULL, \`integrationId\` varchar(255) NULL, INDEX \`IDX_bb6329dba3611e13d777e77178\` (\`createdByUserId\`), INDEX \`IDX_faf0707617abf423320b5a8377\` (\`updatedByUserId\`), INDEX \`IDX_3bf7411675f6cb093aadd19689\` (\`deletedByUserId\`), INDEX \`IDX_08f81916eaa1967b6f6b1903bd\` (\`isActive\`), INDEX \`IDX_e6ce7627d2bb5bd4ba0ed53217\` (\`isArchived\`), INDEX \`IDX_4481c88f8c66731a436bb3d5d8\` (\`tenantId\`), INDEX \`IDX_14479d220903bd9110f0267072\` (\`organizationId\`), INDEX \`IDX_22d421bdeb03778ca787febacf\` (\`targetUrl\`), INDEX \`IDX_8ca54536e5f73094ccc51a74ea\` (\`event\`), INDEX \`IDX_b9bb1dffd8837c6909ec342a7d\` (\`integrationId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` ADD CONSTRAINT \`FK_bb6329dba3611e13d777e771787\` FOREIGN KEY (\`createdByUserId\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` ADD CONSTRAINT \`FK_faf0707617abf423320b5a8377f\` FOREIGN KEY (\`updatedByUserId\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` ADD CONSTRAINT \`FK_3bf7411675f6cb093aadd19689f\` FOREIGN KEY (\`deletedByUserId\`) REFERENCES \`user\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` ADD CONSTRAINT \`FK_4481c88f8c66731a436bb3d5d8a\` FOREIGN KEY (\`tenantId\`) REFERENCES \`tenant\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` ADD CONSTRAINT \`FK_14479d220903bd9110f02670723\` FOREIGN KEY (\`organizationId\`) REFERENCES \`organization\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` ADD CONSTRAINT \`FK_b9bb1dffd8837c6909ec342a7df\` FOREIGN KEY (\`integrationId\`) REFERENCES \`integration_tenant\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+		);
+	}
 
 	/**
 	 * MySQL Down Migration
 	 *
 	 * @param queryRunner
 	 */
-	public async mysqlDownQueryRunner(queryRunner: QueryRunner): Promise<any> {}
+	public async mysqlDownQueryRunner(queryRunner: QueryRunner): Promise<any> {
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` DROP FOREIGN KEY \`FK_b9bb1dffd8837c6909ec342a7df\``
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` DROP FOREIGN KEY \`FK_14479d220903bd9110f02670723\``
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` DROP FOREIGN KEY \`FK_4481c88f8c66731a436bb3d5d8a\``
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` DROP FOREIGN KEY \`FK_3bf7411675f6cb093aadd19689f\``
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` DROP FOREIGN KEY \`FK_faf0707617abf423320b5a8377f\``
+		);
+		await queryRunner.query(
+			`ALTER TABLE \`zapier_webhook_subscription\` DROP FOREIGN KEY \`FK_bb6329dba3611e13d777e771787\``
+		);
+		await queryRunner.query(`DROP INDEX \`IDX_b9bb1dffd8837c6909ec342a7d\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_8ca54536e5f73094ccc51a74ea\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_22d421bdeb03778ca787febacf\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_14479d220903bd9110f0267072\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_4481c88f8c66731a436bb3d5d8\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_e6ce7627d2bb5bd4ba0ed53217\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_08f81916eaa1967b6f6b1903bd\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_3bf7411675f6cb093aadd19689\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_faf0707617abf423320b5a8377\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP INDEX \`IDX_bb6329dba3611e13d777e77178\` ON \`zapier_webhook_subscription\``);
+		await queryRunner.query(`DROP TABLE \`zapier_webhook_subscription\``);
+	}
 }
