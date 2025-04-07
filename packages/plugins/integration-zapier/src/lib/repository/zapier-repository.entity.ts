ec23b16a95a5bf93f520c94914358de3a5da5c9f
Rahul R. <rahulrathore576@gmail.com>
2025-04-07 14:00:34 +0530

    feat(migration): add 'zapier_webhook_subscription' entity [table] for PostgreSQL

--- packages/plugins/integration-zapier/src/lib/repository/zapier-repository.entity.ts
+++ packages/plugins/integration-zapier/src/lib/repository/zapier-repository.entity.ts
@@ -8,41 +8,41 @@ import { ID, IIntegrationTenant } from '@gauzy/contracts';
 
 // Gauzy core imports
 import {
-    ColumnIndex,
-    IntegrationTenant,
-    MultiORMColumn,
-    MultiORMEntity,
-    MultiORMManyToOne,
-    TenantOrganizationBaseEntity
+	ColumnIndex,
+	IntegrationTenant,
+	MultiORMColumn,
+	MultiORMEntity,
+	MultiORMManyToOne,
+	TenantOrganizationBaseEntity
 } from '@gauzy/core';
 
 // Local imports
 import { MikroOrmZapierWebhookSubscriptionRepository } from './mikro-orm-zapier.repository';
 
-@MultiORMEntity('zapier_webhook_subscriptions', {
-    mikroOrmRepository: () => MikroOrmZapierWebhookSubscriptionRepository
+@MultiORMEntity('zapier_webhook_subscription', {
+	mikroOrmRepository: () => MikroOrmZapierWebhookSubscriptionRepository
 })
 export class ZapierWebhookSubscriptionRepository extends TenantOrganizationBaseEntity {
-    @ApiProperty({ type: () => String })
-    @IsString()
-    @ColumnIndex()
-    @MultiORMColumn()
-    targetUrl!: string;
+	@ApiProperty({ type: () => String })
+	@IsString()
+	@ColumnIndex()
+	@MultiORMColumn()
+	targetUrl!: string;
 
-    @ApiProperty({ type: () => String })
-    @IsString()
-    @ColumnIndex()
-    @MultiORMColumn()
-    event!: string;
+	@ApiProperty({ type: () => String })
+	@IsString()
+	@ColumnIndex()
+	@MultiORMColumn()
+	event!: string;
 
-    @ApiProperty({ type: () => String })
-    @IsUUID()
-    @RelationId((it: ZapierWebhookSubscriptionRepository) => it.integration)
-    @ColumnIndex()
-    @MultiORMColumn({ nullable: true, relationId: true })
-    integrationId!: ID;
+	@ApiProperty({ type: () => String })
+	@IsUUID()
+	@RelationId((it: ZapierWebhookSubscriptionRepository) => it.integration)
+	@ColumnIndex()
+	@MultiORMColumn({ nullable: true, relationId: true })
+	integrationId!: ID;
 
-    @MultiORMManyToOne(() => IntegrationTenant, {
+	@MultiORMManyToOne(() => IntegrationTenant, {
 		/** Indicates if relation column value can be nullable or not. */
 		nullable: true,
 
