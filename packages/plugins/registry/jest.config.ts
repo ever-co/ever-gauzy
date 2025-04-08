666c63a89e45202f3cd8fc15d562fb270474ecd5
Ruslan Konviser <evereq@gmail.com>
2025-04-08 17:30:10 +0200

    fix: build fix and rename plugin folder

--- packages/plugins/registry/jest.config.ts
+++ packages/plugins/registry/jest.config.ts
@@ -6,5 +6,5 @@ export default {
 		'^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
 	},
 	moduleFileExtensions: ['ts', 'js', 'html'],
-	coverageDirectory: '../../../coverage/packages/plugins/plugin-registry'
+	coverageDirectory: '../../../coverage/packages/plugins/registry'
 };
