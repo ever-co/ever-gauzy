f630bdffc5bf11ddcd334207f4e9e4bf66f9b6f5
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2025-04-08 23:24:23 -0500

    refactor: Prevent showing add button if the user dont have an associated employee

--- apps/gauzy/src/app/pages/invoices/invoices/by-role/invoices-by-role.component.ts
+++ apps/gauzy/src/app/pages/invoices/invoices/by-role/invoices-by-role.component.ts
@@ -27,7 +27,8 @@ import {
 	InvoiceTabsEnum,
 	DiscountTaxTypeEnum,
 	IDateRangePicker,
-	ICurrency
+	ICurrency,
+	IUser
 } from '@gauzy/contracts';
 import { distinctUntilChange, isNotEmpty, API_PREFIX, ComponentEnum, toInvoiceDateFilter } from '@gauzy/ui-core/common';
 import { Router } from '@angular/router';
@@ -91,6 +92,8 @@ export class InvoicesByRoleComponent extends PaginationFilterBaseComponent imple
 	invoices$: Subject<IInvoice[]> = this.subject$;
 	nbTab$: Subject<string> = new BehaviorSubject(InvoiceTabsEnum.ACTIONS);
 	private readonly _refresh$: Subject<void> = new Subject();
+
+	currentUser: IUser;
 
 	/*
 	 * getter setter for check estimate or invoice
@@ -211,6 +214,17 @@ export class InvoicesByRoleComponent extends PaginationFilterBaseComponent imple
 					});
 				}
 			});
+
+		// Get the current user
+		this.store.user$
+			.pipe(
+				filter((user) => !!user),
+				tap((user) => {
+					this.currentUser = user;
+				}),
+				untilDestroyed(this)
+			)
+			.subscribe();
 	}
 
 	setView() {
