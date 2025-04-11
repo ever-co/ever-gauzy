6a39e81cdc8091478491f8e1406076cfb4b1227f
samuelmbabhazi <samuelmbabhazi@gmail.com>
2025-04-11 21:27:45 +0200

    feat(posthog): create main module with forRoot and forRootAsync

--- packages/plugins/posthog/src/lib/posthog.module.ts
+++ packages/plugins/posthog/src/lib/posthog.module.ts
@@ -1,8 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
+import { PosthogCoreModule } from './posthog-core.module';
+import { PosthogModuleOptions, PosthogModuleAsyncOptions } from './posthog.interfaces';
 
-@Module({
-	controllers: [],
-	providers: [],
-	exports: []
-})
-export class PosthogModule {}
+/**
+ * Entry module for PostHog integration.
+ */
+@Module({})
+export class PosthogModule {
+	/**
+	 * Static method to create a dynamic module with synchronous configuration.
+	 */
+	public static forRoot(options: PosthogModuleOptions): DynamicModule {
+		return {
+			module: PosthogModule,
+			imports: [PosthogCoreModule.forRoot(options)]
+		};
+	}
+
+	/**
+	 * Static method to create a dynamic module with asynchronous configuration.
+	 */
+	public static forRootAsync(options: PosthogModuleAsyncOptions): DynamicModule {
+		return {
+			module: PosthogModule,
+			imports: [PosthogCoreModule.forRootAsync(options)]
+		};
+	}
+}
