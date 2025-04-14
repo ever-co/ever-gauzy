f81eb11eb9b18b980a93a2c625d2433c418b5cd0
Reinier Millo Sánchez <reinier.millo@dspot.com.pl>
2025-04-14 12:11:01 -0500

    fix: Add fallback image to desktop app

--- packages/desktop-ui-lib/src/lib/directives/desktop-directive.module.ts
+++ packages/desktop-ui-lib/src/lib/directives/desktop-directive.module.ts
@@ -5,10 +5,11 @@ import { DebounceClickDirective } from './debounce-click.directive';
 import { DynamicDirective } from './dynamic.directive';
 import { SpinnerButtonDirective } from './spinner-button.directive';
 import { TextMaskDirective } from './text-mask.directive';
+import { ImgDirective } from './img.directive';
 
 @NgModule({
-	declarations: [SpinnerButtonDirective, DynamicDirective, TextMaskDirective, DebounceClickDirective],
-	exports: [SpinnerButtonDirective, DynamicDirective, TextMaskDirective, DebounceClickDirective],
+	declarations: [SpinnerButtonDirective, DynamicDirective, TextMaskDirective, DebounceClickDirective, ImgDirective],
+	exports: [SpinnerButtonDirective, DynamicDirective, TextMaskDirective, DebounceClickDirective, ImgDirective],
 	imports: [CommonModule, NbSpinnerModule]
 })
 export class DesktopDirectiveModule {}
