bc3c8485eeec70d5b3f70f5e4627c36a861e7af2
syns2191 <sutralian@gmail.com>
2025-05-03 15:59:46 +0700

    feat: keyboard and mouse activity recorded data as json

---
+++ packages/desktop-activity/src/lib/kb-mouse-activity-stores.ts
@@ -0,0 +1,82 @@
+
+import { app } from 'electron';
+
+type TMouseEvents = {
+	moveTo: {
+		from: {
+			x: number;
+			y: number;
+		},
+		to: {
+			x: number;
+			y: number;
+		}
+	}
+}
+
+type TkbMouseActivity = {
+	kbPressCount: number;
+	kbSequence: number[];
+	mouseMovementsCount: number;
+	mouseLeftClickCount: number;
+	mouseRightClickCount: number;
+	mouseEvents: TMouseEvents[]
+
+}
+class KeyboardMouseActivityStores {
+	currentActivityData: TkbMouseActivity;
+	userPath: string;
+	constructor() {
+		this.userPath = app.getPath('userData');
+		this.resetCurrentAcitivity();
+	}
+
+	writeData() {
+		console.log('current last data activities', this.currentActivityData);
+	}
+
+	resetCurrentAcitivity() {
+		this.currentActivityData = {
+			kbSequence: [],
+			kbPressCount: 0,
+			mouseRightClickCount: 0,
+			mouseLeftClickCount: 0,
+			mouseMovementsCount: 0,
+			mouseEvents: []
+		};
+	}
+
+	readLastData() {
+
+	}
+
+	updateCurrentKeyPressCount() {
+		this.currentActivityData.kbPressCount = (this.currentActivityData?.kbPressCount || 0) + 1;
+	}
+
+	updateMouseMovementsCount() {
+		this.currentActivityData.mouseMovementsCount = (this.currentActivityData?.mouseMovementsCount || 0) + 1;
+	}
+
+	updateMouseLeftClick() {
+		this.currentActivityData.mouseLeftClickCount = (this.currentActivityData?.mouseLeftClickCount || 0) + 1;
+	}
+
+	updateMouseRightClick() {
+		this.currentActivityData.mouseRightClickCount = (this.currentActivityData?.mouseRightClickCount || 0) + 1;
+	}
+
+	updateKbSequence(keyCode: number) {
+		(this.currentActivityData?.kbSequence || []).push(keyCode);
+	}
+
+	updateMouseEvents() {
+
+	}
+
+	flushCurrentData() {
+
+	}
+}
+
+export default KeyboardMouseActivityStores;
--- packages/desktop-activity/src/lib/kb-mouse-event-counter.ts
+++ packages/desktop-activity/src/lib/kb-mouse-event-counter.ts
@@ -1,7 +1,16 @@
 import KeyboardMouse from './kb-mouse';
+import KeyboardMouseActivityStores from './kb-mouse-activity-stores';
+
 export class KeyboardMouseEventCounter {
 	private isStarted: boolean;
 	private keyboardMouse: KeyboardMouse;
+	private keyboardMouseActivityStores = new KeyboardMouseActivityStores();
+	private timeSecond: number = 0;
+	private timeInterval: ReturnType<typeof setInterval>;
+	private timeSlotBuffer: number;
+	private activityBuffer: number = 10;
+	private currentTimeActivity: number = 0;
+	private currentTimeSlot: number = 0;
 	constructor() {
 		this.isStarted = false;
 		this.keyboardMouse = new KeyboardMouse();
@@ -9,20 +18,60 @@ export class KeyboardMouseEventCounter {
 
 	registerEvent() {
 		this.keyboardMouse.on('keydown', (e) => {
-			console.log('keyboard pressed at key', e.keycode);
+			try {
+				this.keyboardMouseActivityStores.updateKbSequence(e.keycode);
+				this.keyboardMouseActivityStores.updateCurrentKeyPressCount();
+			} catch (error) {
+				console.log('error', error);
+			}
 		});
 
 		this.keyboardMouse.on('click', (e) => {
-			console.log('mouse click at position', `${e.x}, ${e.y}`);
+			if (e.button === 1) {
+				this.keyboardMouseActivityStores.updateMouseLeftClick();
+			}
+
+			if (e.button === 2) {
+				this.keyboardMouseActivityStores.updateMouseRightClick();
+			}
 		});
 
 		this.keyboardMouse.on('mousemove', (e) => {
-			console.log('mouse moved at position', `${e.x}, ${e.y}`)
+			// console.log('mouse moved at position', `${e.x}, ${e.y}`)
+			this.keyboardMouseActivityStores.updateMouseMovementsCount();
+			console.log('movement', e);
 		});
 
 		this.keyboardMouse.on('wheel', (e) => {
-			console.log('mouse wheeled at position', `${e.direction} to ${e.x}, ${e.y}`);
+			// console.log('mouse wheeled at position', `${e.direction} to ${e.x}, ${e.y}`);
 		});
+	}
+
+	timeActivity() {
+		// buffer activity write
+		if (this.activityBuffer === this.currentTimeActivity) {
+			this.endBufferActivity();
+		}
+	}
+
+	endBufferActivity() {
+		this.currentTimeActivity = 0;
+		this.keyboardMouseActivityStores.writeData();
+	}
+
+	startTimer() {
+		clearInterval(this.timeInterval);
+		this.timeInterval = setInterval(() => {
+			this.timeSecond += 1;
+			this.currentTimeSlot += 1;
+			this.currentTimeActivity += 1;
+			this.timeActivity();
+		}, 1000);
+	}
+
+	endTimer() {
+		clearInterval(this.timeInterval);
+		this.timeSecond = 0;
 	}
 
 	startListener() {
@@ -30,6 +79,7 @@ export class KeyboardMouseEventCounter {
 			this.registerEvent();
 			this.keyboardMouse.start();
 			this.isStarted = true;
+			this.startTimer();
 		}
 	}
 
@@ -38,6 +88,7 @@ export class KeyboardMouseEventCounter {
 			this.keyboardMouse.stop();
 			this.keyboardMouse.removeAllListeners();
 			this.isStarted = false;
+			this.endTimer();
 		}
 	}
 }
