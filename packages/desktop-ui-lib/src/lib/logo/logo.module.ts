e78533ab4c5993c29483834e5982c568aaad8780
YanaYestyle <yanayestyle@gmail.com>
2025-05-14 13:29:09 +0200

    fix: fix warnings after rebase

--- packages/desktop-ui-lib/src/lib/logo/logo.module.ts
+++ packages/desktop-ui-lib/src/lib/logo/logo.module.ts
@@ -6,7 +6,6 @@ import { NbThemeModule } from '@nebular/theme';
 @NgModule({
 	declarations: [LogoComponent],
 	imports: [CommonModule, NbThemeModule],
-	exports: [LogoComponent],
 	providers: []
 })
-export class LogoModule { }
+export class LogoModule {}
