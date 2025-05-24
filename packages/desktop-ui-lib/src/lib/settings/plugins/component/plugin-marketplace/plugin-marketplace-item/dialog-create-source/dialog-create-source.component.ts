a88d55230bc6d0237bb2d568adc5a9f7235043ba
adkif <akifungo@gmail.com>
2025-05-24 22:40:08 +0200

    feat(plugin-marketplace): add dialog for creating plugin sources
    - Add new dialog component for plugin source creation
    - Implement source creation form interface
    - Support plugin source management workflow

---
+++ packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-marketplace/plugin-marketplace-item/dialog-create-source/dialog-create-source.component.ts
@@ -0,0 +1,93 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { IPlugin, IPluginSource, IPluginVersion, PluginSourceType } from '@gauzy/contracts';
+import { NbDialogRef } from '@nebular/theme';
+import { SourceContext } from '../../plugin-marketplace-upload/plugin-source/creator/source.context';
+
+/**
+ * Component for creating a new plugin source through a dialog
+ */
+@Component({
+	selector: 'lib-dialog-create-source',
+	standalone: false,
+	templateUrl: './dialog-create-source.component.html',
+	styleUrl: './dialog-create-source.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DialogCreateSourceComponent implements OnInit {
+	/** The plugin for which the source is being created */
+	public readonly plugin!: IPlugin;
+	/** The version of the plugin for which the source is being created */
+	public readonly version!: IPluginVersion;
+	/** Available source types for the plugin */
+	public readonly sourceTypes = Object.values(PluginSourceType);
+	/** Form group for managing the source creation */
+	public form!: FormGroup;
+
+	constructor(
+		private readonly dialogRef: NbDialogRef<DialogCreateSourceComponent>,
+		private readonly sourceContext: SourceContext
+	) {}
+
+	/**
+	 * Initializes the component and sets up the form
+	 */
+	ngOnInit(): void {
+		this.initializeForm();
+	}
+
+	/**
+	 * Initializes the form with default values and validators
+	 */
+	private initializeForm(): void {
+		this.form = new FormGroup({
+			pluginId: new FormControl(this.plugin.id, Validators.required),
+			versionId: new FormControl(this.version.id, Validators.required),
+			sources: new FormArray([this.sourceContext.getCreator(PluginSourceType.CDN).createSource()])
+		});
+	}
+
+	public addSource(type: PluginSourceType = PluginSourceType.CDN, data?: IPluginSource): void {
+		const source = this.sourceContext.getCreator(type).createSource(data);
+		this.sources.push(source);
+	}
+
+	public removeSource(index: number): void {
+		this.sources.removeAt(index);
+	}
+
+	public get sources(): FormArray {
+		return this.form.get('sources') as FormArray;
+	}
+
+	/**
+	 * Closes the dialog without submitting
+	 */
+	public dismiss(): void {
+		this.dialogRef.close();
+	}
+
+	/**
+	 * Submits the form if valid
+	 */
+	public submit(): void {
+		if (this.form.invalid) {
+			this.markFormGroupTouched(this.form);
+			return;
+		}
+		this.dialogRef.close(this.form.value);
+	}
+
+	/**
+	 * Marks all controls in a form group as touched
+	 * @param formGroup The form group to mark as touched
+	 */
+	private markFormGroupTouched(formGroup: FormGroup): void {
+		Object.values(formGroup.controls).forEach((control) => {
+			control.markAsTouched();
+			if (control instanceof FormGroup) {
+				this.markFormGroupTouched(control);
+			}
+		});
+	}
+}
