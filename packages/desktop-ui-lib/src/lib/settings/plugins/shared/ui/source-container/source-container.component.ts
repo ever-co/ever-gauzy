47ec688d4b75d2704a51d2428e6b7207c84bc5f7
adkif <akifungo@gmail.com>
2025-05-26 20:49:31 +0200

    refactor(source-container): emit source index on restore event
    The restore event now emits the index of the source being restored.

--- packages/desktop-ui-lib/src/lib/settings/plugins/shared/ui/source-container/source-container.component.ts
+++ packages/desktop-ui-lib/src/lib/settings/plugins/shared/ui/source-container/source-container.component.ts
@@ -18,7 +18,7 @@ export class SourceContainerComponent {
 
 	@Output() add = new EventEmitter<PluginSourceType>();
 	@Output() remove = new EventEmitter<number>();
-	@Output() restore = new EventEmitter<void>();
+	@Output() restore = new EventEmitter<number>();
 
 	public drop(event: CdkDragDrop<string[]>) {
 		moveItemInArray(this.sources.controls, event.previousIndex, event.currentIndex);
