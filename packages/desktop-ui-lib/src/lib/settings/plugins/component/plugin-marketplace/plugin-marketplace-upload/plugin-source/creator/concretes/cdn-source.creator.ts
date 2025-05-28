f1f2c02410f8f9bf406b742447c42ede50195cad
adkif <akifungo@gmail.com>
2025-05-28 08:38:37 +0200

    refactor(plugin-marketplace): use website url pattern constant
    Replace inline regex for URL validation with imported constant pattern

--- packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-marketplace/plugin-marketplace-upload/plugin-source/creator/concretes/cdn-source.creator.ts
+++ packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-marketplace/plugin-marketplace-upload/plugin-source/creator/concretes/cdn-source.creator.ts
@@ -1,5 +1,6 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ICDNSource, PluginSourceType } from '@gauzy/contracts';
+import { patterns } from '../../../../../../../../constants';
 import { SourceCreator } from '../source.creator';
 
 export class CdnSourceCreator extends SourceCreator {
@@ -7,10 +8,7 @@ export class CdnSourceCreator extends SourceCreator {
 		const base = this.createBaseSource(PluginSourceType.CDN, source);
 		return new FormGroup({
 			...base,
-			url: new FormControl(source?.url || '', [
-				Validators.required,
-				Validators.pattern(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w.-]*)\/?$/)
-			]),
+			url: new FormControl(source?.url || '', [Validators.required, Validators.pattern(patterns.websiteUrl)]),
 			integrity: new FormControl(source?.integrity || ''),
 			crossOrigin: new FormControl(source?.crossOrigin || '', Validators.maxLength(50))
 		});
