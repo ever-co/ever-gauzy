d9da133578bc0b89983b2b994ca14e12a098fea8
samuelmbabhazi <samuelmbabhazi@gmail.com>
2025-06-03 12:47:38 +0200

    chore: update index and test setup for Zapier integration

--- packages/plugins/integration-zapier-ui/src/index.ts
+++ packages/plugins/integration-zapier-ui/src/index.ts
@@ -1,3 +1,8 @@
 /**
  * Public API Surface of @gauzy/plugin-integration-zapier-ui
  */
+
+export * from './lib/integration-zapier-ui.module';
+export * from './lib/integration-zapier.layout.component';
+export * from './lib/components/overview/overview.component';
+export * from './lib/components/overview/overview.module';
