8809d68a7060873e4b40ad0003617a7f2b0d8b68
adkif <akifungo@gmail.com>
2025-06-11 13:10:43 +0200

    chore(soundshot): adjust delete soundshot command type format
    Update the static type string to follow the [Plugin] Action pattern.

--- packages/plugins/soundshot/src/lib/commands/delete-soundshot.command.ts
+++ packages/plugins/soundshot/src/lib/commands/delete-soundshot.command.ts
@@ -3,6 +3,6 @@ import { DeleteSoundshotDTO } from "../dtos/delete-soundshot.dto";
 import { ID } from "@gauzy/contracts";
 
 export class DeleteSoundshotCommand implements ICommand {
-	public static readonly type = '[Delete] Soundshot';
+	public static readonly type = '[Soundshot] Delete';
 	constructor(public readonly id: ID, public readonly input: DeleteSoundshotDTO) { }
 }
