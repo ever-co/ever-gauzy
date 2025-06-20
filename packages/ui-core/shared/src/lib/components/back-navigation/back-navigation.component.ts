9ec2ea78bb65be32f906e670a717fa7e3d0ec865
YanaYestr <yana.yeustratsyeva@dspot.com.pl>
2025-06-20 10:29:48 +0200

    fix: fix back arrow button navigates via browser history instead of redirecting to employee list (#35)

--- packages/ui-core/shared/src/lib/components/back-navigation/back-navigation.component.ts
+++ packages/ui-core/shared/src/lib/components/back-navigation/back-navigation.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'ngx-back-navigation',
-    templateUrl: './back-navigation.component.html',
-    styleUrls: ['./back-navigation.component.scss'],
-    standalone: false
+	selector: 'ngx-back-navigation',
+	templateUrl: './back-navigation.component.html',
+	styleUrls: ['./back-navigation.component.scss'],
+	standalone: false
 })
-export class BackNavigationComponent implements OnInit {
+export class BackNavigationComponent {
 	@Input() haveLink = false;
+	@Input() link: string;
 
-	constructor(private readonly location: Location) {}
-
-	ngOnInit() {}
+	constructor(private readonly location: Location, private readonly router: Router) {}
 
 	goBack() {
-		if (!this.haveLink) this.location.back();
+		if (this.link) {
+			this.router.navigateByUrl(this.link);
+		} else if (!this.haveLink) {
+			this.location.back();
+		}
 	}
 }
