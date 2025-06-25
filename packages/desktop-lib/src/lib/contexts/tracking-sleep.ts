92b7120989b8e7f37054b3d4739c5743a8a3eee3
adkif <akifungo@gmail.com>
2025-06-25 19:55:31 +0200

    refactor(tracking-sleep): rename sleep tracking variables
    Improve consistency and clarity of private variable names within the TrackingSleep context.

--- packages/desktop-lib/src/lib/contexts/tracking-sleep.ts
+++ packages/desktop-lib/src/lib/contexts/tracking-sleep.ts
@@ -1,22 +1,22 @@
-import {ITrackingSleep, ITrackingSleepStrategy} from "../interfaces";
-import {LocalStore} from "../desktop-store";
-import {AlwaysTrackingSleep, NeverTrackingSleep} from "../strategies";
-import {BrowserWindow} from "electron";
+import { ITrackingSleep, ITrackingSleepStrategy } from '../interfaces';
+import { LocalStore } from '../desktop-store';
+import { AlwaysTrackingSleep, NeverTrackingSleep } from '../strategies';
+import { BrowserWindow } from 'electron';
 
 export class TrackingSleep implements ITrackingSleep {
-	private readonly _alwaysSleepTracking: ITrackingSleepStrategy;
-	private readonly _neverSleepTracking: ITrackingSleepStrategy;
+	private readonly _alwaysTrackingSleep: ITrackingSleepStrategy;
+	private readonly _neverTrackingSleep: ITrackingSleepStrategy;
 
 	constructor(window: BrowserWindow) {
-		this._alwaysSleepTracking = new AlwaysTrackingSleep(window);
-		this._neverSleepTracking = new NeverTrackingSleep(window);
+		this._alwaysTrackingSleep = new AlwaysTrackingSleep(window);
+		this._neverTrackingSleep = new NeverTrackingSleep(window);
 	}
 
 	get strategy(): ITrackingSleepStrategy {
-		return this._isTrackingOnSleep ? this._alwaysSleepTracking : this._neverSleepTracking;
+		return this._isTrackingSleep ? this._alwaysTrackingSleep : this._neverTrackingSleep;
 	}
 
-	private get _isTrackingOnSleep(): boolean {
+	private get _isTrackingSleep(): boolean {
 		const setting = LocalStore.getStore('appSetting');
 		return setting ? setting.trackOnPcSleep : false;
 	}
