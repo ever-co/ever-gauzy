6a61cff52481e14231e6e17c895ea99a4a403d3a
samuelmbabhazi <samuelmbabhazi@gmail.com>
2025-06-26 09:28:59 +0200

    feat(favorite): add FavoriteService with strict typing and index export for UI favorite functionality (#9026)

---
+++ packages/ui-core/core/src/lib/services/favorite/favorite.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { IFavorite, IFavoriteCreateInput } from '@gauzy/contracts';
+import { API_PREFIX } from '@gauzy/ui-core/common';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class FavoriteService {
+	FAVORITE_URL = `${API_PREFIX}/favorite`;
+
+	constructor(private http: HttpClient) {}
+
+	create(favorite: IFavoriteCreateInput): Promise<IFavorite> {
+		return firstValueFrom(this.http.post<IFavorite>(`${this.FAVORITE_URL}`, favorite));
+	}
+
+	findByEmployee(params?: any): Promise<{ items: IFavorite[]; total: number }> {
+		return firstValueFrom(
+			this.http.get<{ items: IFavorite[]; total: number }>(`${this.FAVORITE_URL}/employee`, { params })
+		);
+	}
+
+	getFavoriteDetails(params?: any): Promise<{ items: IFavorite[]; total: number }> {
+		return firstValueFrom(
+			this.http.get<{ items: IFavorite[]; total: number }>(`${this.FAVORITE_URL}/type`, { params })
+		);
+	}
+
+	delete(id: string): Promise<any> {
+		return firstValueFrom(this.http.delete(`${this.FAVORITE_URL}/${id}`));
+	}
+}
