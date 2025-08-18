b92931d8b701b671792573fc4656f6de4c52db1c
samuel mbabhazi <111171386+samuelmbabhazi@users.noreply.github.com>
2025-08-18 22:16:53 +0200

    Merge pull request #9090 from ever-co/feature/workspace-management-workflow
    [Feat] workspace management workflow

---
+++ packages/plugins/public-layout-ui/src/lib/components/workspace-actions/workspace-create/workspace-create.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import {
+	NbButtonModule,
+	NbIconModule,
+	NbCardModule,
+	NbInputModule,
+	NbFormFieldModule,
+	NbSpinnerModule,
+	NbTooltipModule
+} from '@nebular/theme';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule, OrganizationsStepFormModule, PasswordFormFieldModule } from '@gauzy/ui-core/shared';
+import { ThemeModule } from '@gauzy/ui-core/theme';
+import { TenantService, OrganizationsService, UsersService } from '@gauzy/ui-core/core';
+import { WorkspaceCreateComponent } from './workspace-create.component';
+import { WorkspaceSharedModule } from '../shared/workspace-shared.module';
+import { CountdownTimerService, WorkspaceAuthService } from '../shared';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: WorkspaceCreateComponent
+	}
+];
+
+@NgModule({
+	declarations: [WorkspaceCreateComponent],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		RouterModule.forChild(routes),
+		NbButtonModule,
+		NbIconModule,
+		NbCardModule,
+		NbInputModule,
+		NbFormFieldModule,
+		NbSpinnerModule,
+		NbTooltipModule,
+		TranslateModule.forChild(),
+		SharedModule,
+		OrganizationsStepFormModule,
+		PasswordFormFieldModule,
+		ThemeModule,
+		WorkspaceSharedModule
+	],
+	providers: [TenantService, OrganizationsService, UsersService, WorkspaceAuthService, CountdownTimerService]
+})
+export class WorkspaceCreateModule {}
