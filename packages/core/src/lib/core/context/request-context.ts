5fe4361e425696d3c2547ca49686f5d13874a2b1
adkif <akifungo@gmail.com>
2025-10-18 12:54:39 +0200

    feat(core): add currentOrganizationId to RequestContext
    Adds a static method to RequestContext to easily retrieve the organization ID of the currently authenticated user. This simplifies access to the organization ID throughout the application.

--- packages/core/src/lib/core/context/request-context.ts
+++ packages/core/src/lib/core/context/request-context.ts
@@ -183,6 +183,17 @@ export class RequestContext {
 	}
 
 	/**
+	 * Retrieves the current organization ID associated with the user in the RequestContext.
+	 * Returns the organization ID if available, otherwise returns null.
+	 *
+	 * @returns {string | null} - The current organization ID or null if not available.
+	 */
+	static currentOrganizationId(): ID | null {
+		const user: IUser | null = RequestContext.currentUser();
+		return user?.employee?.organizationId || null;
+	}
+
+	/**
 	 * Retrieves the current role ID associated with the user in the RequestContext.
 	 * Returns the role ID if available, otherwise returns null.
 	 *
