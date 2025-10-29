6c2817e244482ec2134f54d5927a512fc1015512
Roland MN <manfulmweze99@gmail.com>
2025-10-29 14:55:53 +0200

    Merge pull request #9137 from ever-co/feat/activepieces-ui
    [Feat] Implement the UI pages for Activepieces integration

---
+++ packages/plugins/integration-activepieces-ui/README.md
@@ -0,0 +1,23 @@
+# @gauzy/plugin-integration-activepieces-ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `yarn nx build plugin-integration-activepieces-ui` to build the library.
+
+## Publishing
+
+After building your library with `yarn nx build plugin-integration-activepieces-ui`,
+go to the dist folder `dist/packages/plugins/integration-activepieces-ui`
+and run `npm publish`.
+
+## Installation
+
+Install the Integration Activepieces UI Plugin using your preferred package manager:
+
+```bash
+npm install @gauzy/plugin-integration-activepieces-ui
+# or
+yarn add @gauzy/plugin-integration-activepieces-ui
+```
---
+++ packages/plugins/integration-activepieces-ui/src/lib/integration-activepieces.layout.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { filter, merge, tap, distinctUntilChanged } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { LanguagesEnum } from '@gauzy/contracts';
+import { Store } from '@gauzy/ui-core/core';
+import { I18nService } from '@gauzy/ui-core/i18n';
+
+@UntilDestroy({ checkProperties: true })
+@Component({
+	selector: 'ngx-integration-activepieces-layout',
+	template: `<router-outlet></router-outlet>`,
+	standalone: false
+})
+export class IntegrationActivepiecesLayoutComponent implements OnInit, OnDestroy {
+	constructor(
+		private readonly _translateService: TranslateService,
+		private readonly _store: Store,
+		private readonly _i18nService: I18nService
+	) {}
+
+	ngOnInit() {
+		this.initializeUiLanguagesAndLocale(); // Initialize UI languages and Update Locale
+	}
+
+	/**
+	 * Initialize UI languages and Update Locale
+	 */
+	private initializeUiLanguagesAndLocale() {
+		// Observable that emits when preferred language changes.
+		const preferredLanguage$ = merge(
+			this._store.preferredLanguage$,
+			this._i18nService.preferredLanguage$
+		).pipe(
+			distinctUntilChanged(),
+			filter((lang: string | LanguagesEnum) => !!lang),
+			tap((lang: string | LanguagesEnum) => {
+				this._translateService.use(lang);
+			}),
+			untilDestroyed(this)
+		);
+
+		// Subscribe to initiate the stream
+		preferredLanguage$.subscribe();
+	}
+
+	ngOnDestroy(): void {}
+}
