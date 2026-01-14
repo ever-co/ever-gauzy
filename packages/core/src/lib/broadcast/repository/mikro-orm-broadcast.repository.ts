12fa6f027ed08dfae91e46103b0b8e8f8334ee6e
GloireMutaliko21 <mufunyig@gmail.com>
2026-01-14 12:22:33 +0200

    feat: created and registered broadcast entity

---
+++ packages/core/src/lib/broadcast/repository/mikro-orm-broadcast.repository.ts
@@ -0,0 +1,4 @@
+import { MikroOrmBaseEntityRepository } from '../../core/repository/mikro-orm-base-entity.repository';
+import { Broadcast } from '../broadcast.entity';
+
+export class MikroOrmBroadcastRepository extends MikroOrmBaseEntityRepository<Broadcast> {}
