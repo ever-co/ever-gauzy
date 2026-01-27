a51e64789c9734dc9fce1ab575f3faf8677f9bf1
adkif <akifungo@gmail.com>
2026-01-27 15:14:02 +0200

    refactor(plugins): remove PendingInstallationResolver
    Remove the PendingInstallationResolver and its application from the
    plugin marketplace and installed routes. This resolver previously
    triggered a dialog for pending plugin installations during navigation
    in a desktop environment.

--- packages/desktop-ui-lib/src/lib/settings/plugins/plugin.route.ts
+++ packages/desktop-ui-lib/src/lib/settings/plugins/plugin.route.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { pluginInstalledRoutes } from './component/plugin-installed-routing.module';
 import { pluginMarketplaceRoutes } from './component/plugin-marketplace/plugin-marketplace-routing.module';
-import { PendingInstallationResolver, PluginUploadResolver } from './services/resolvers';
+import { PluginUploadResolver } from './services/resolvers';
 
 export const routes: Routes = [
 	{
@@ -12,12 +12,11 @@ export const routes: Routes = [
 	{
 		path: 'marketplace',
 		children: pluginMarketplaceRoutes,
-		resolve: { isUploadAvailable: PluginUploadResolver, pendingCheck: PendingInstallationResolver }
+		resolve: { isUploadAvailable: PluginUploadResolver }
 	},
 	{
 		path: 'installed',
-		children: pluginInstalledRoutes,
-		resolve: { pendingCheck: PendingInstallationResolver }
+		children: pluginInstalledRoutes
 	},
 	{
 		path: '**',
