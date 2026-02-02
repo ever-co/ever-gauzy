d28fe1693ff48cce0ab360a46f19b5b68433d632
Rahul R. <rahulrathore576@gmail.com>
2026-02-02 16:41:26 +0530

    fix(build): add jest config for missing packages

---
+++ packages/plugins/camshot/jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+	displayName: 'plugin-camshot',
+	preset: '../../../jest.preset.js',
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../../coverage/packages/plugins/camshot'
+};
