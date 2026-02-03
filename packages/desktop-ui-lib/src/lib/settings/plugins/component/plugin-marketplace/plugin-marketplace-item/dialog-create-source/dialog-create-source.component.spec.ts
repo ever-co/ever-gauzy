27e7b0ec0c5f58b2d0b70e7ed1a83c459929c363
Rahul R. <rahulrathore576@gmail.com>
2026-02-03 10:38:44 +0530

    fix(nx-migrations): migrate nx from 21.5.1 to 22.4.4

--- packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-marketplace/plugin-marketplace-item/dialog-create-source/dialog-create-source.component.spec.ts
+++ packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-marketplace/plugin-marketplace-item/dialog-create-source/dialog-create-source.component.spec.ts
@@ -1,21 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DialogCreateSourceComponent } from './dialog-create-source.component';
-
 describe('DialogCreateSourceComponent', () => {
 	let component: DialogCreateSourceComponent;
 	let fixture: ComponentFixture<DialogCreateSourceComponent>;
-
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [DialogCreateSourceComponent]
 		}).compileComponents();
-
 		fixture = TestBed.createComponent(DialogCreateSourceComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
-
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
--- packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-marketplace/plugin-marketplace-upload/form-row/form-row.component.spec.ts
+++ packages/desktop-ui-lib/src/lib/settings/plugins/component/plugin-marketplace/plugin-marketplace-upload/form-row/form-row.component.spec.ts
@@ -1,21 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FormRowComponent } from './form-row.component';
-
 describe('FormRowComponent', () => {
 	let component: FormRowComponent;
 	let fixture: ComponentFixture<FormRowComponent>;
-
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [FormRowComponent]
 		}).compileComponents();
-
 		fixture = TestBed.createComponent(FormRowComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 	});
-
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
