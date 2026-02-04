27f7aeb4d4d0e24783355c2bc6f6c1ad2ee5d885
adkif <akifungo@gmail.com>
2026-02-04 12:25:28 +0200

    refactor: migrate server applications to standalone bootstrapping
    Remove AppModule from both server and server-api applications and
    transition to the Angular standalone bootstrap API. Provider
    configurations are moved to main.ts using bootstrapApplication and
    importProvidersFrom.

--- apps/server-api/src/main.ts
+++ apps/server-api/src/main.ts
@@ -1,12 +1,100 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { enableProdMode, ErrorHandler, importProvidersFrom, inject, provideAppInitializer } from '@angular/core';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { Router, RouterModule } from '@angular/router';
+import {
+	AboutModule,
+	ElectronService,
+	GAUZY_ENV,
+	LanguageModule,
+	LoggerService,
+	NgxDesktopThemeModule,
+	PluginsModule,
+	ServerDashboardModule,
+	SettingsModule,
+	SetupModule
+} from '@gauzy/desktop-ui-lib';
+import { environment as gauzyEnvironment } from '@gauzy/ui-config';
+import {
+	NbButtonModule,
+	NbCardModule,
+	NbDialogModule,
+	NbDialogService,
+	NbLayoutModule,
+	NbMenuModule,
+	NbSidebarModule,
+	NbThemeModule,
+	NbToastrModule
+} from '@nebular/theme';
+import * as Sentry from '@sentry/angular';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { AppService } from './app/app.service';
+import { initializeSentry } from './app/sentry';
 import { environment } from './environments/environment';
 
 if (environment.production) {
 	enableProdMode();
 }
 
-platformBrowser()
-	.bootstrapModule(AppModule)
-	.catch((err) => console.error(err));
+if (environment.SENTRY_DSN) {
+	if (environment.SENTRY_DSN === 'DOCKER_SENTRY_DSN') {
+		console.warn('You are running inside Docker but does not have SENTRY_DSN env set');
+	} else {
+		console.log(`Enabling Sentry with DSN: ${environment.SENTRY_DSN}`);
+		initializeSentry();
+	}
+}
+
+bootstrapApplication(AppComponent, {
+	providers: [
+		importProvidersFrom(
+			NbLayoutModule,
+			NbDialogModule.forRoot(),
+			NbToastrModule.forRoot(),
+			NbCardModule,
+			NbButtonModule,
+			BrowserModule,
+			RouterModule,
+			AppRoutingModule,
+			NbThemeModule,
+			NgxDesktopThemeModule,
+			NbMenuModule.forRoot(),
+			NbSidebarModule.forRoot(),
+			SetupModule,
+			SettingsModule,
+			ServerDashboardModule,
+			AboutModule,
+			LanguageModule.forRoot(),
+			PluginsModule
+		),
+		AppService,
+		NbDialogService,
+		ElectronService,
+		LoggerService,
+		{
+			provide: ErrorHandler,
+			useValue: Sentry.createErrorHandler({
+				showDialog: true
+			})
+		},
+		{
+			provide: Sentry.TraceService,
+			deps: [Router]
+		},
+		provideAppInitializer(() => {
+			const initializerFn = ((trace: Sentry.TraceService) => () => {})(inject(Sentry.TraceService));
+			return initializerFn();
+		}),
+		{
+			provide: GAUZY_ENV,
+			useValue: {
+				...gauzyEnvironment,
+				...environment
+			}
+		},
+		provideHttpClient(withInterceptorsFromDi()),
+		provideAnimations()
+	]
+}).catch((err) => console.error(err));
