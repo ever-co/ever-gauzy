f4c08d95fc5c66aebc60e5745fc707d8d309940e
syns2191 <sutralian@gmail.com>
2026-02-26 17:21:38 +0700

    fix: image view error 400

--- packages/desktop-ui-lib/src/lib/interceptors/token.interceptor.ts
+++ packages/desktop-ui-lib/src/lib/interceptors/token.interceptor.ts
@@ -3,18 +3,15 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable } from 'rxjs';
 import { Store } from '../services';
 import { TokenRequestCloner } from '../auth';
-import { GAUZY_ENV } from './../constants/app.constants';
+import { API_PREFIX } from './../constants/app.constants';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 	private readonly requestCloner = inject(TokenRequestCloner);
 	private readonly store = inject(Store);
-	private readonly environment = inject(GAUZY_ENV);
 
 	isApiRequest(url: string) {
-		const baseUrl = this.environment?.API_BASE_URL;
-		const apiBaseUrl = baseUrl ? baseUrl : this.store.host;
-		return url.startsWith(apiBaseUrl);
+		return url.includes(API_PREFIX);
 	}
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
