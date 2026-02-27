32769f45870f08d82167f2fdf001ca3a82edb189
adkif <akifungo@gmail.com>
2026-02-27 18:03:31 +0200

    feat(scheduler): add dynamic imports support in SchedulerFeatureOptions

--- packages/scheduler/src/lib/scheduler.module.ts
+++ packages/scheduler/src/lib/scheduler.module.ts
@@ -61,7 +61,7 @@ export class SchedulerModule {
 
 		return {
 			module: SchedulerModule,
-			imports,
+			imports: [...imports, ...(options.imports ?? [])],
 			providers: [...jobProviders],
 			exports: [...jobProviders]
 		};
