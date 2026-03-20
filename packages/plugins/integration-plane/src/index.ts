10e50c7a0fe996cf4421b53cc167e523ae5e4c61
rolandm99 <manfulmweze99@gmail.com>
2026-03-20 14:29:08 +0200

    chore(plane-plugin): initialize plane plugin implementation

---
+++ packages/plugins/integration-plane/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Public API Surface of @gauzy/plugin-integration-plane
+ */
+export * from './lib/integration-plane.plugin';
+export * from './lib/plane.module';
+export * from './lib/plane-integration.service';
+export * from './lib/plane-proxy.service';
+export * from './lib/plane-setting.enum';
+export * from './lib/dto';
