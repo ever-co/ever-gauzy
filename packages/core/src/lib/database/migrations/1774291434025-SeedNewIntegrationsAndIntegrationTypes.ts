42259ccbc2fe1f2d4d3b48812d6ecbd1145cab7e
rolandm99 <manfulmweze99@gmail.com>
2026-03-23 21:10:14 +0200

    chore: create migrations file for the new integrations file

---
+++ packages/core/src/lib/database/migrations/1774291434025-SeedNewIntegrationsAndIntegrationTypes.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import * as chalk from 'chalk';
+import { IntegrationTypeEnum } from "@gauzy/contracts";
+import { DEFAULT_INTEGRATIONS, DEFAULT_SYSTEM_INTEGRATIONS, DEFAULT_AI_INTEGRATIONS } from "../../integration/default-integration";
+import { IntegrationsUtils } from "../../integration/utils";
+import { DatabaseTypeEnum } from "@gauzy/config";
+
+export class SeedNewIntegrationsAndIntegrationTypes1774291434025 implements MigrationInterface {
+
+    name = 'SeedNewIntegrationsAndIntegrationTypes1774291434025';
+
+    /**
+     * Up Migration
+     *
+     * @param queryRunner
+     */
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        console.log(chalk.yellow(this.name + ' start running!'));
+
+        switch (queryRunner.connection.options.type) {
+            case DatabaseTypeEnum.sqlite:
+            case DatabaseTypeEnum.betterSqlite3:
+            case DatabaseTypeEnum.postgres:
+                await this.sqlitePostgresUpsert(queryRunner);
+                break;
+            case DatabaseTypeEnum.mysql:
+                await this.mysqlUpsert(queryRunner);
+                break;
+            default:
+                throw Error(`Unsupported database: ${queryRunner.connection.options.type}`);
+        }
+    }
+    /**
+     * Down Migration
+     *
+     * @param queryRunner
+     */
+    public async down(queryRunner: QueryRunner): Promise<void> { }
+
+    /**
+    * Sqlite | better-sqlite3 | Postgres Up Migration
+    *
+    * @param queryRunner
+    */
+    public async sqlitePostgresUpsert(queryRunner: QueryRunner): Promise<any> {
+        console.log(chalk.yellow(this.name + ' start running!'));
+
+        await IntegrationsUtils.upsertIntegrationTypes(queryRunner, [IntegrationTypeEnum.AUTOMATION_TOOLS, IntegrationTypeEnum.AI_AGENTS]);
+        await IntegrationsUtils.upsertIntegrationsAndIntegrationTypes(queryRunner, DEFAULT_SYSTEM_INTEGRATIONS);
+        await IntegrationsUtils.upsertIntegrationsAndIntegrationTypes(queryRunner, DEFAULT_AI_INTEGRATIONS);
+        await IntegrationsUtils.upsertIntegrationsAndIntegrationTypes(queryRunner, DEFAULT_INTEGRATIONS);
+    }
+
+    /**
+    * MySQL Up Migration
+    *
+    * @param queryRunner
+    */
+    public async mysqlUpsert(queryRunner: QueryRunner): Promise<any> { }
+}
